3a7c5d1b44ac4c0d3881d77570d631c8f2ed6b1a
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-01-06 20:32:36 +0000

    autoconnect working for /overview/[address]

--- pages/owner/[address]/index.tsx
+++ pages/owner/[address]/index.tsx
@@ -1,5 +1,26 @@
+import { isAppContextAvailable, useAppContext } from 'components/AppContextProvider';
+import { BasicLayout } from 'components/Layouts';
+import { useObservable } from 'helpers/observableHook';
 import { Grid } from 'theme-ui'
 
-export default function Index() {
-    return <Grid>Overview</Grid>;
+function OverviewPage() {
+
+    const { web3Context$ } = useAppContext()
+    const web3Context = useObservable(web3Context$)
+
+
+    return <Grid>Connected Address :: {web3Context?.account}</Grid>;
+
 }
+
+export default function Overview() {
+    return isAppContextAvailable() ? <OverviewPage /> : null
+}
+
+OverviewPage.layout = BasicLayout
+OverviewPage.layoutProps = {
+    backLink: {
+        href: '/',
+    },
+}
+
