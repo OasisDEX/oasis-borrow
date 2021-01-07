8b7c2311afc60721b316421c88bdfc045c404eab
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-01-07 17:02:18 +0000

    formatting

--- pages/[vault]/index.tsx
+++ pages/[vault]/index.tsx
@@ -1,28 +1,27 @@
-import { isAppContextAvailable, useAppContext } from 'components/AppContextProvider';
-import { AppLayout, BasicLayout } from 'components/Layouts';
-import { useObservable } from 'helpers/observableHook';
+import { isAppContextAvailable, useAppContext } from 'components/AppContextProvider'
+import { AppLayout, BasicLayout } from 'components/Layouts'
+import { useObservable } from 'helpers/observableHook'
 import { Grid, Text } from 'theme-ui'
 import { useRouter } from 'next/router'
 
 export default function Vault() {
-    const { web3Context$ } = useAppContext()
-    const web3Context = useObservable(web3Context$)
-    const { query: { vault } } = useRouter()
+  const { web3Context$ } = useAppContext()
+  const web3Context = useObservable(web3Context$)
+  const {
+    query: { vault },
+  } = useRouter()
 
-    return <Grid>
-        <Text>
-            Connected Address :: {web3Context?.account}
-        </Text>
-        <Text>
-            Vault  :: {vault}
-        </Text>
-    </Grid>;
-
+  return (
+    <Grid>
+      <Text>Connected Address :: {web3Context?.account}</Text>
+      <Text>Vault :: {vault}</Text>
+    </Grid>
+  )
 }
 
 Vault.layout = AppLayout
 Vault.layoutProps = {
-    backLink: {
-        href: '/',
-    },
+  backLink: {
+    href: '/',
+  },
 }
