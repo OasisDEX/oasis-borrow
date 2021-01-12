0fb90455fadfee1b6f952c048e1c3dea5d403bfd
Maciek Kamiński <aundumla@gmail.com>
2021-01-12 16:57:30 +0100

    end, spot call defs

---
+++ features/vaults/end.ts
@@ -0,0 +1,14 @@
+import { CallDef } from '../../components/blockchain/calls/callsHelpers'
+import { McdEnd } from '../../types/web3-v1-contracts/mcd-end'
+
+export const endLive: CallDef<void, boolean> = {
+  call: (_, { contract, mcdEnd }) => contract<McdEnd>(mcdEnd).methods.live,
+  prepareArgs: () => [],
+  postprocess: (result: any) => result.eq(0)
+}
+
+export const endWhen: CallDef<void, Date> = {
+  call: (_, { contract, mcdEnd }) => contract<McdEnd>(mcdEnd).methods.when,
+  prepareArgs: () => [],
+  postprocess: (result: any) => new Date(result.toNumber() * 1000)
+}
