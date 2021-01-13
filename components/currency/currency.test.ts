33211a4f4651eee7c8551c5d540ffa0bbbea8aac
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-01-13 16:30:30 +0000

    pushing

--- components/currency/currency.test.ts
+++ components/currency/currency.test.ts
@@ -14,19 +14,32 @@ import BigNumber from 'bignumber.js'
     }
 
     rate = rmul(rpow(add(base, ilks[ilk].duty), now - ilks[ilk].rho, ONE), prev);
+
+    base - "0"
+    duty - "1000000000782997609082909351"
+    ONE - 10 ** 27
+    prev - "1026317734294511797161403874
+    rate - "1026317878139697269316455705"
+
  */
 
 describe.only('currency', () => {
-  const ONE_WAD_TKN = WAD('TKN')(1n) // 18
-  const ONE_RAY_TKN = RAY('TKN')(1n) // 27
-  const ONE_RAD_TKN = RAD('TKN')(1n) // 45
+  // const ONE_WAD_TKN = WAD('TKN')(1n) // 18
+  // const ONE_RAY_TKN = RAY('TKN')(1n) // 27
+  // const ONE_RAD_TKN = RAD('TKN')(1n) // 45
 
-  describe('$add', () => {
-    it('adds', () => {
-      const amount = $add({ a: ONE_WAD_TKN, b: ONE_RAD_TKN })
-      expect($print(amount as Currency)).to.equal(
-        '0.000000000000000001000000000000000000000000001 TKN',
-      )
-    })
-  })
+  // describe('$add', () => {
+
+  //   it('adds', () => {
+  //     const amount = $add({ a: ONE_WAD_TKN, b: ONE_RAD_TKN })
+  //     expect($print(amount as Currency)).to.equal(
+  //       '0.000000000000000001000000000000000000000000001 TKN',
+  //     )
+  //   })
+  // })
+
+  const rawDuty = '1000000000782997609082909351'
+  const ONE = '1000000000000000000000000000'
+  const base = '0'
+  const now = (Date.now() / 1000).toString()
 })
