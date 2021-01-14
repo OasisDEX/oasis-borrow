2f6da6b80e45855922166bb3214c9accef9150ec
Leonid Logvinov <logvinov.leon@gmail.com>
2021-01-14 13:43:53 +0100

    Fix a bug when hex chainId was not converted

--- components/blockchain/wsEnhancedProviderProxy.ts
+++ components/blockchain/wsEnhancedProviderProxy.ts
@@ -1,5 +1,6 @@
 import { JSONRPCRequestPayload } from 'ethereum-protocol'
 import { providers } from 'ethers'
+import _ from 'lodash'
 import { JsonRpcResponse } from 'web3-core-helpers'
 
 import { networksById } from './config'
@@ -7,9 +8,12 @@ import { networksById } from './config'
 const wsProviderByChainId: Record<number, providers.WebSocketProvider> = {}
 
 async function getWSProviderAsync(
-  chainIdPromise: Promise<number>,
+  chainIdPromise: Promise<number | string>,
 ): Promise<providers.WebSocketProvider> {
-  const chainId = await chainIdPromise
+  let chainId = await chainIdPromise
+  if (_.isString(chainId)) {
+    chainId = parseInt(chainId, 16)
+  }
   if (!wsProviderByChainId[chainId]) {
     wsProviderByChainId[chainId] = new providers.WebSocketProvider(
       networksById[chainId].infuraUrlWS,
@@ -22,17 +26,20 @@ async function getWSProviderAsync(
 const web3ProviderByChainId: Record<number, providers.Web3Provider> = {}
 
 async function getRPCProviderAsync(
-  chainIdPromise: Promise<number>,
+  chainIdPromise: Promise<number | string>,
   web3Provider: providers.ExternalProvider,
 ): Promise<providers.Web3Provider> {
-  const chainId = await chainIdPromise
+  let chainId = await chainIdPromise
+  if (_.isString(chainId)) {
+    chainId = parseInt(chainId, 16)
+  }
   if (!web3ProviderByChainId[chainId]) {
     web3ProviderByChainId[chainId] = new providers.Web3Provider(web3Provider, chainId)
   }
   return web3ProviderByChainId[chainId]
 }
 
-function getHandler(chainIdPromise: Promise<number>): ProxyHandler<any> {
+function getHandler(chainIdPromise: Promise<number | string>): ProxyHandler<any> {
   const handler = {
     get: (target: any, name: string) => {
       if (name === 'sendAsync') {
@@ -60,6 +67,6 @@ function getHandler(chainIdPromise: Promise<number>): ProxyHandler<any> {
   return handler
 }
 
-export function wsEnhanceProvider(provider: any, chainIdPromise: Promise<number>) {
+export function wsEnhanceProvider(provider: any, chainIdPromise: Promise<number | string>) {
   return new Proxy(provider, getHandler(chainIdPromise))
 }
