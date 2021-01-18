6c09dc2e3d3925671fb928552acb096a71b5187c
Damian Paszkowski <paszko.damian@gmail.com>
2021-01-18 14:32:08 +0100

    fix useObservable type when used with ramda curry

--- helpers/observableHook.ts
+++ helpers/observableHook.ts
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import { Observable } from 'rxjs'
 
-export function useObservable<O>(o$: Observable<O>): O | undefined {
-  const [value, setValue] = useState<O | undefined>(undefined)
+type Unpack<T extends Observable<any>> = T extends Observable<infer U> ? U : never; 
+
+export function useObservable<O extends Observable<any>>(o$: O): Unpack<O> | undefined {
+  const [value, setValue] = useState<Unpack<O> | undefined>(undefined)
 
   useEffect(() => {
-    const subscription = o$.subscribe((v: O) => {
+    const subscription = o$.subscribe((v: Unpack<O>) => {
       setValue(v)
     })
     return () => subscription.unsubscribe()
