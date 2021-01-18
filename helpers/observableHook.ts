ac703debb0cbb6bd19e72a393500315ac453122c
Damian Paszkowski <paszko.damian@gmail.com>
2021-01-18 15:06:25 +0100

    add comment

--- helpers/observableHook.ts
+++ helpers/observableHook.ts
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
 import { Observable } from 'rxjs'
 
+// In order to infer proper type of observable returned by curry from ramda which uses recursive typing 
+// we need to postpone inference. 
+// Type Unpack is used in order to extract inner type of Observable
 type Unpack<T extends Observable<any>> = T extends Observable<infer U> ? U : never; 
-
 export function useObservable<O extends Observable<any>>(o$: O): Unpack<O> | undefined {
   const [value, setValue] = useState<Unpack<O> | undefined>(undefined)
 
