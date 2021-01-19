d896be62bb94a47df94f0a09f57a141351210392
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-01-19 12:54:28 +0000

    latest

--- components/currency/currency.test.ts
+++ components/currency/currency.test.ts
@@ -1,75 +1,93 @@
+import BigNumber from 'bignumber.js'
 import { expect } from 'chai'
-import { Discrete } from 'money-ts/lib/Discrete'
-import { Dense } from 'money-ts/lib/Dense'
+import { Currency } from './currency'
 
-import { wrap as wrapAsInteger } from 'money-ts/lib/Integer'
+describe.only('currency', () => {
+  const iso = 'TKN'
+  const unit = 2
 
-import { wrap as wrapAsNatural, one as N_ONE } from 'money-ts/lib/Natural'
-import { wrap as wrapAsNonZeroInteger, one as NZ_ONE } from 'money-ts/lib/NonZeroInteger'
-import { NonZeroRational } from 'money-ts/lib/NonZeroRational'
+  const c = (v: any) => new Currency(iso, unit, v)
 
-import bigInt from 'big-integer'
-import { Rational, show } from 'money-ts/lib/Rational'
-
-describe.only('money-ts', () => {
-  describe('Discrete', () => {
-    it('add', () => {
-      const intX = wrapAsInteger(bigInt(101))
-      const intY = wrapAsInteger(bigInt(2))
-
-      const x = new Discrete({ dimension: 'USD', unit: 2 }, intX)
-      const y = new Discrete({ dimension: 'USD', unit: 2 }, intY)
-
-      const z = x.add(y)
-      expect(z.toString()).equals('USD 2 103') // 1.03 USD
-    })
-
-    it('precision behaviour', () => {
-      const intA = wrapAsInteger(bigInt(4))
-      const intB = wrapAsNonZeroInteger(bigInt(3)).getOrElse(NZ_ONE)
-
-      const disA = new Discrete({ dimension: 'USD', unit: 2 }, intA)
-
-      const disB = disA.div(intB)
-      expect(disB.toString()).equals('USD 2 1') // 0.01 USD
-      const disC = disB.mul(intB)
-      expect(disC.toString()).equals('USD 2 3') // 0.03 USD - lost 0.01 in division
-    })
+  it('can be constructed with integer strings', () => {
+    expect(c('1').toString()).equals('TKN 2 1')
+    expect(c('-1').toString()).equals('TKN 2 -1')
+    expect(c('0').toString()).equals('TKN 2 0')
+    expect(c('-0').toString()).equals('TKN 2 0')
+    expect(c('100').toString()).equals('TKN 2 100')
+    expect(c(Number.MAX_SAFE_INTEGER.toString()).toString()).equals('TKN 2 9007199254740991')
+    expect(c(Number.MIN_SAFE_INTEGER.toString()).toString()).equals('TKN 2 -9007199254740991')
   })
 
-  describe('Dense', () => {
-    it('add', () => {
-      const intX = wrapAsInteger(bigInt(101))
-      const natX = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
-      const ratX = [intX, natX] as Rational
-
-      const intY = wrapAsInteger(bigInt(2))
-      const natY = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
-      const ratY = [intY, natY] as Rational
-
-      const x = new Dense('USD', ratX)
-      const y = new Dense('USD', ratY)
-
-      const z = x.add(y)
-
-      expect(z.toString()).equals('USD 103 / 100')
-    })
-
-    it('precision behaviour', () => {
-      const intA = wrapAsInteger(bigInt(4))
-      const natA = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
-      const ratA = [intA, natA] as Rational
-
-      const intB = wrapAsNonZeroInteger(bigInt(3)).getOrElse(NZ_ONE)
-      const natB = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
-      const ratB = [intB, natB] as NonZeroRational
-
-      const denA = new Dense('USD', ratA) // 0.04 USD
-
-      const denB = denA.div(ratB)
-      expect(denB.toString()).equals('USD 4 / 3') // 0.013333333333 ... USD
-      const denC = denB.mul(ratB)
-      expect(denC.toString()).equals('USD 1 / 25') // 0.04 USD
-    })
+  it('can be constructed with integer bignumbers', () => {
+    expect(c(new BigNumber('1')).toString()).equals('TKN 2 1')
+    expect(c(new BigNumber('-1')).toString()).equals('TKN 2 -1')
+    expect(c(new BigNumber('0')).toString()).equals('TKN 2 0')
+    expect(c(new BigNumber('-0')).toString()).equals('TKN 2 0')
+    expect(c('100').toString()).equals('TKN 2 100')
+    expect(c(Number.MAX_SAFE_INTEGER.toString()).toString()).equals('TKN 2 9007199254740991')
+    expect(c(Number.MIN_SAFE_INTEGER.toString()).toString()).equals('TKN 2 -9007199254740991')
   })
 })
+
+// describe.only('money-ts', () => {
+//   describe('Discrete', () => {
+//     it('add', () => {
+//       const intX = wrapAsInteger(bigInt(101))
+//       const intY = wrapAsInteger(bigInt(2))
+
+//       const x = new Discrete({ dimension: 'USD', unit: 2 }, intX)
+//       const y = new Discrete({ dimension: 'USD', unit: 2 }, intY)
+
+//       const z = x.add(y)
+//       expect(z.toString()).equals('USD 2 103') // 1.03 USD
+//     })
+
+//     it('precision behaviour', () => {
+//       const intA = wrapAsInteger(bigInt(4))
+//       const intB = wrapAsNonZeroInteger(bigInt(3)).getOrElse(NZ_ONE)
+
+//       const disA = new Discrete({ dimension: 'USD', unit: 2 }, intA)
+
+//       const disB = disA.div(intB)
+//       expect(disB.toString()).equals('USD 2 1') // 0.01 USD
+//       const disC = disB.mul(intB)
+//       expect(disC.toString()).equals('USD 2 3') // 0.03 USD - lost 0.01 in division
+//     })
+//   })
+
+//   describe('Dense', () => {
+//     it('add', () => {
+//       const intX = wrapAsInteger(bigInt(101))
+//       const natX = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
+//       const ratX = [intX, natX] as Rational
+
+//       const intY = wrapAsInteger(bigInt(2))
+//       const natY = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
+//       const ratY = [intY, natY] as Rational
+
+//       const x = new Dense('USD', ratX)
+//       const y = new Dense('USD', ratY)
+
+//       const z = x.add(y)
+
+//       expect(z.toString()).equals('USD 103 / 100')
+//     })
+
+//     it('precision behaviour', () => {
+//       const intA = wrapAsInteger(bigInt(4))
+//       const natA = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
+//       const ratA = [intA, natA] as Rational
+
+//       const intB = wrapAsNonZeroInteger(bigInt(3)).getOrElse(NZ_ONE)
+//       const natB = wrapAsNatural(bigInt(100)).getOrElse(N_ONE)
+//       const ratB = [intB, natB] as NonZeroRational
+
+//       const denA = new Dense('USD', ratA) // 0.04 USD
+
+//       const denB = denA.div(ratB)
+//       expect(denB.toString()).equals('USD 4 / 3') // 0.013333333333 ... USD
+//       const denC = denB.mul(ratB)
+//       expect(denC.toString()).equals('USD 1 / 25') // 0.04 USD
+//     })
+//   })
+// })
--- components/currency/currency.ts
+++ components/currency/currency.ts
@@ -1,138 +1,44 @@
 import BigNumber from 'bignumber.js'
-import { curry } from 'ramda'
+import { Discrete } from 'money-ts/lib/Discrete'
+import { Integer, wrap as integerWrap, zero } from 'money-ts/lib/Integer'
+import bigInt from 'big-integer'
 
-type Numeric = Number | string | BigNumber
+type Numeric = BigInteger | string
 
-export function $parse(v: Numeric): bigint | never {
-  if (BigNumber.isBigNumber(v)) {
-    return 1n
-  } else if (typeof v === 'number') {
-    return 1n
-  } else if (typeof v === 'string') {
-    return 1n
+export class Currency<I extends string, U extends number> {
+  readonly _iso: I
+  readonly _unit: U
+  readonly _value: Discrete<I, U>
+
+  constructor(iso: I, unit: U, amount: Integer) {
+    this._iso = iso
+    this._unit = unit
+    this._value = new Discrete({ dimension: iso, unit }, amount)
   }
-  throw new Error(`Could not parse input: ${v}`)
-}
 
-export interface Currency {
-  amount: bigint
-  symbol: string
-  decimals: bigint
-}
+  // private _parseAsInteger(v: Numeric): Integer {
+  //   if (typeof v === 'string') {
+  //     // if v is string just try convert to bignumber
+  //     return integerWrap(bigInt(v))
+  //   }
+  //   if (BigNumber.isBigNumber(v) && (v as BigNumber).isInteger() && !(v as BigNumber).isNaN()) {
+  //     return integerWrap(bigInt(v.toString()))
+  //   }
+  //   if (Number.isSafeInteger(v)) {
+  //     return integerWrap(bigInt(v.toString()))
+  //   }
+  //   if (bigInt.isInstance(v)) {
+  //     return integerWrap(v)
+  //   }
 
-export function $createCurrencyFn(
-  decimals: bigint,
-  symbol: string,
-  amount: bigint,
-): Currency | never {
-  if (decimals < 0) {
-    throw new Error('Currency must have positive decimals')
+  //   throw new Error('could not parse as integer')
+  // }
+
+  public add(c: Currency<I, U>): Currency<I, U> {
+    return this
   }
-  return {
-    decimals,
-    symbol,
-    amount,
+
+  public toString() {
+    return this._value.toString()
   }
-}
-
-const $createCurrency = curry($createCurrencyFn)
-
-export const WAD = $createCurrency(18n)
-export const RAY = $createCurrency(27n)
-export const RAD = $createCurrency(45n)
-
-export const DAI = WAD('DAI')
-
-// upscales and
-export function $scale(c: Currency, scale: BigInt): Currency | undefined {
-  if (scale > 0n) {
-    if (scale === c.decimals) {
-      return c
-    }
-    if (scale > c.decimals) {
-      // upscale
-      return c
-    }
-    if (scale < c.decimals) {
-      // NOTE: This will lose precision
-      return c
-    }
-  }
-  return undefined
-}
-
-export function $add({ a, b }: { a: Currency; b: Currency }): Currency | undefined {
-  let decimalDiff, offset, upscaledAmount
-  if (a.symbol === b.symbol) {
-    if (a.decimals === b.decimals) {
-      return $createCurrency(a.decimals, a.symbol, a.amount + b.amount)
-    } else if (a.decimals > b.decimals) {
-      decimalDiff = a.decimals - b.decimals
-      offset = 10n ** decimalDiff
-      upscaledAmount = b.amount * offset
-      return $createCurrency(a.decimals, a.symbol, a.amount + upscaledAmount)
-    } else {
-      decimalDiff = b.decimals - a.decimals
-      offset = 10n ** decimalDiff
-      upscaledAmount = a.amount * offset
-      return $createCurrency(b.decimals, a.symbol, upscaledAmount + b.amount)
-    }
-  }
-  return undefined
-}
-
-// antecedent : consequent
-// base : quote
-// numerator : denominator
-interface BinaryRatio<T> {
-  antecedent: T
-  consequent: T
-}
-
-type Ratio = BinaryRatio<BigInt>
-type Price = BinaryRatio<Currency>
-
-//
-
-// UI specific print fn's to transform Currency
-export function $printRatio(r: Ratio): string {
-  return 'ratio'
-}
-export function $printPrice(p: Price): string {
-  return 'price'
-}
-
-interface PrintCurrencyOptions {
-  showSymbol?: boolean
-}
-
-export function $print(
-  c: Currency,
-  options: PrintCurrencyOptions = {
-    showSymbol: true,
-  },
-): string {
-  const isNeg = c.amount < 0n
-  if (isNeg) {
-    c.amount = c.amount * -1n
-  }
-  let displayString = ''
-  const amtStr = c.amount.toString()
-  const lessThanZeroStart = `0.`
-  const position = amtStr.length - Number(c.decimals)
-
-  if (position > 0) {
-    displayString = `${amtStr.substring(0, position)}.${amtStr.substring(position)}`
-  } else {
-    displayString = `${lessThanZeroStart}${amtStr.padStart(Number(c.decimals), '0')}`
-  }
-  let [left, right] = displayString.split('.')
-  right = right.replace(/0+$/, '')
-  left = isNeg ? `-${left}` : left
-
-  if (!right.length) displayString = `${left}`
-  else {
-    displayString = `${left}.${right}`
-  }
-  return `${displayString}${options.showSymbol ? ` ${c.symbol}` : ''}`
 }
