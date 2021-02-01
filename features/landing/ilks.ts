740534dbc7c1eb155aa3d8ebda10eb94fd2b3899
Damian Paszkowski <paszko.damian@gmail.com>
2021-02-01 14:06:09 +0100

    filter SAI

--- features/landing/ilks.ts
+++ features/landing/ilks.ts
@@ -4,5 +4,5 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 
 export function createIlks$(context$: Observable<Context>): Observable<string[]> {
-  return context$.pipe(map((context) => Object.keys(context.joins).filter(join => join !== 'DAI')))
+  return context$.pipe(map((context) => Object.keys(context.joins).filter(join => join !== 'DAI' && join !== 'SAI')))
 }
