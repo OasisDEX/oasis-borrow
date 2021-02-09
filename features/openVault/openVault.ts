9de70fdf0f75c4cdd8c2d2b9cfa6f4a97ef672f9
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-02-09 16:07:19 +0000

    validating if ilk exists

--- features/openVault/openVault.ts
+++ features/openVault/openVault.ts
@@ -2,18 +2,41 @@ import { Context } from 'blockchain/network'
 import { combineLatest, Observable, of } from 'rxjs'
 import { switchMap } from 'rxjs/operators'
 
+export type OpenVaultStage =
+  | 'editing'
+  | 'proxyWaitingForConfirmation'
+  | 'proxyWaitingForApproval'
+  | 'proxyInProgress'
+  | 'proxyFiasco'
+  | 'allowanceWaitingForConfirmation'
+  | 'allowanceWaitingForApproval'
+  | 'allowanceInProgress'
+  | 'allowanceFiasco'
+  | 'waitToContinue'
+  | 'transactionWaitingForConfirmation'
+  | 'transactionWaitingForApproval'
+  | 'transactionInProgress'
+  | 'transactionFiasco'
+  | 'transactionSuccess'
+
 export interface OpenVaultState {
-  stage: 'start'
+  stage: OpenVaultStage
+  isValidIlk: boolean
+  ilk: string
 }
 
 export function createOpenVault$(
   context$: Observable<Context>,
   ilks$: Observable<string[]>,
   ilk: string,
-): Observable<any> {
+): Observable<OpenVaultState> {
   return ilks$.pipe(
     switchMap((ilks) => {
-      return of(ilks)
+      if (!ilks.some((i) => i === ilk)) {
+        return of({ stage: 'editing', isValidIlk: false, ilk })
+      }
+
+      return of({ stage: 'editing', isValidIlk: true, ilk })
     }),
   )
 }
--- features/openVault/openVaultView.tsx
+++ features/openVault/openVaultView.tsx
@@ -1,45 +1,67 @@
 import { useAppContext } from 'components/AppContextProvider'
 import { useObservable } from 'helpers/observableHook'
 import React from 'react'
-import { Box, Card, Grid, Heading, Text } from 'theme-ui'
+import { Box, Card, Grid, Heading, Spinner, Text } from 'theme-ui'
+import { OpenVaultState } from './openVault'
 
-function OpenVaultCard() {
+function NewVaultDetails({}: any) {
+  return (
+    <Grid columns="1fr 1fr" gap={6} sx={{ justifyContent: 'space-between' }}>
+      <Grid>
+        <Text>Liquidation Price</Text>
+        <Heading>$1425.20</Heading>
+        <Text>After: $2500.20</Text>
+      </Grid>
+
+      <Grid sx={{ textAlign: 'right' }}>
+        <Text>Collateralization Ratio</Text>
+        <Heading>200.00%</Heading>
+        <Text>After: 300.00%</Text>
+      </Grid>
+
+      <Grid>
+        <Text>Current ETH/USD Price in 9 mins</Text>
+        <Heading>$1375.0000</Heading>
+        <Text>Next price: $1325.0000 (-2.30%)</Text>
+      </Grid>
+
+      <Grid sx={{ textAlign: 'right' }}>
+        <Text>Collateral Locked</Text>
+        <Heading>200.00 ETH</Heading>
+        <Text>$182,000.20 USD</Text>
+      </Grid>
+    </Grid>
+  )
+}
+
+function OpenVaultCard({}: OpenVaultState) {
   return <Card>Vault Creation modal</Card>
 }
 
 export function OpenVaultView({ ilk }: { ilk: string }) {
   const { openVault$ } = useAppContext()
-  const ilks = useObservable(openVault$(ilk))
-  console.log(ilks)
+  const openVault = useObservable(openVault$(ilk))
+
+  if (!openVault) {
+    return (
+      <Grid sx={{ width: '100%', height: '50vh', justifyItems: 'center', alignItems: 'center' }}>
+        <Spinner size={50} />
+      </Grid>
+    )
+  }
+
+  if (!openVault.isValidIlk) {
+    return (
+      <Grid sx={{ width: '100%', height: '50vh', justifyItems: 'center', alignItems: 'center' }}>
+        <Box>Ilk {ilk} does not exist, please update the ilk registry if passed by governance</Box>
+      </Grid>
+    )
+  }
 
   return (
     <Grid columns="2fr 1fr" gap={5} sx={{ width: '100%' }}>
-      <Grid columns="1fr 1fr" gap={6} sx={{ justifyContent: 'space-between' }}>
-        <Grid>
-          <Text>Liquidation Price</Text>
-          <Heading>$1425.20</Heading>
-          <Text>After: $2500.20</Text>
-        </Grid>
-
-        <Grid sx={{ textAlign: 'right' }}>
-          <Text>Collateralization Ratio</Text>
-          <Heading>200.00%</Heading>
-          <Text>After: 300.00%</Text>
-        </Grid>
-
-        <Grid>
-          <Text>Current ETH/USD Price in 9 mins</Text>
-          <Heading>$1375.0000</Heading>
-          <Text>Next price: $1325.0000 (-2.30%)</Text>
-        </Grid>
-
-        <Grid sx={{ textAlign: 'right' }}>
-          <Text>Collateral Locked</Text>
-          <Heading>200.00 ETH</Heading>
-          <Text>$182,000.20 USD</Text>
-        </Grid>
-      </Grid>
-      <OpenVaultCard />
+      <NewVaultDetails />
+      <OpenVaultCard {...openVault} />
     </Grid>
   )
 }
