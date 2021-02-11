e9572c1d016c10f309e78f501d59b5d7e598cac0
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-02-11 11:36:40 +0000

    fix web3Connection in openVault page

--- pages/vaults/open/[ilk]/index.tsx
+++ pages/vaults/open/[ilk]/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppLayout } from 'components/Layouts'
 import { useRouter } from 'next/router'
 import { OpenVaultView } from 'features/openVault/openVaultView'
+import { useAppContext } from 'components/AppContextProvider'
+import { getConnector } from 'components/connectWallet/ConnectWallet'
+import { getNetworkId } from '@oasisdex/web3-context'
 
 export default function OpenPage() {
   const {
     query: { ilk },
   } = useRouter()
+
+  const { web3Context$ } = useAppContext()
+
+  useEffect(() => {
+    const subscription = web3Context$.subscribe(async (web3Context) => {
+      if (web3Context.status === 'notConnected') {
+        web3Context.connect(await getConnector('network', getNetworkId()), 'network')
+      }
+    })
+    return () => subscription.unsubscribe()
+  }, [])
+
   return <OpenVaultView {...{ ilk: ilk as string }} />
 }
 
