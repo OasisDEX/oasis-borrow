069a4079327bcfb1545959b91049570f5bb2ea10
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-04-08 14:16:04 +0100

    linting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,11 +4,11 @@ import { ThemeProvider } from 'theme-ui'
 import { theme } from '../theme'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n'
-import { addons } from '@storybook/addons';
+import { addons } from '@storybook/addons'
 
 addons.setConfig({
   showPanel: false,
-});
+})
 addDecorator((storyFn) => <I18nextProvider i18n={i18n}>{storyFn()}</I18nextProvider>)
 addDecorator((storyFn) => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>)
 
