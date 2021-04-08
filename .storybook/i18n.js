956dc09d9f38b7962ce08cfe61dc6591c0bd2790
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-04-08 15:00:34 +0100

    import translations directly

--- .storybook/i18n.js
+++ .storybook/i18n.js
@@ -1,22 +1,19 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import Backend from 'i18next-http-backend'
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    lng: 'en',
-    fallbackLng: 'en',
-    debug: true,
-    defaultNS: 'common',
-    //loadPath: '/locales/{{lng}}/{{ns}}.json',
-    backend: {
-      loadPath:
-        process.env.NODE_ENV !== 'production'
-          ? `./locales/{{lng}}/{{ns}}.json`
-          : `/locales/{{lng}}/{{ns}}.json`,
-    },
-  })
+import enTranslation from '../public/locales/en/common.json'
+
+const resources = {
+  en: {
+    translation: enTranslation,
+  },
+}
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  debug: true,
+})
 
 export default i18n
