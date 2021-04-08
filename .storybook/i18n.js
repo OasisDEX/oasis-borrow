788bdaff1f6dd9b15b94cd4edd26dbb542dee358
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-04-08 14:43:12 +0100

    linting

--- .storybook/i18n.js
+++ .storybook/i18n.js
@@ -2,18 +2,21 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 
-i18n.use(Backend).use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  debug: true,
-  defaultNS: 'common',
-  //loadPath: '/locales/{{lng}}/{{ns}}.json',
-  backend: {
-    loadPath:
-      process.env.NODE_ENV !== "production"
-        ? `./locales/{{lng}}/{{ns}}.json`
-        : `/locales/{{lng}}/{{ns}}.json`,
-  },
-})
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: true,
+    defaultNS: 'common',
+    //loadPath: '/locales/{{lng}}/{{ns}}.json',
+    backend: {
+      loadPath:
+        process.env.NODE_ENV !== 'production'
+          ? `./locales/{{lng}}/{{ns}}.json`
+          : `/locales/{{lng}}/{{ns}}.json`,
+    },
+  })
 
 export default i18n
