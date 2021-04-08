48b5f1a232d0e91f4940d083dcc4e10af564b288
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-04-08 14:13:17 +0100

    added loadPath config

--- .storybook/i18n.js
+++ .storybook/i18n.js
@@ -3,9 +3,11 @@ import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 
 i18n.use(Backend).use(initReactI18next).init({
+  lng: 'en',
   fallbackLng: 'en',
   debug: true,
   defaultNS: 'common',
+  loadPath: '/locales/{{lng}}/{{ns}}.json',
 })
 
 export default i18n
