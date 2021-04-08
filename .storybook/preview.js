3addb2b3bcf419843c2e386018231c87be9cdd98
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-04-08 15:15:39 +0100

    configure panels

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,15 +4,15 @@ import { ThemeProvider } from 'theme-ui'
 import { theme } from '../theme'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n'
-import { addons } from '@storybook/addons'
 
-addons.setConfig({
-  showPanel: false,
-})
 addDecorator((storyFn) => <I18nextProvider i18n={i18n}>{storyFn()}</I18nextProvider>)
 addDecorator((storyFn) => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>)
-
 addDecorator((story, context) => <Suspense fallback="Loading...">{story(context)}</Suspense>)
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  options: {
+    showPanel: false,
+    panelPosition: "right"
+  }
 }
