84b6689252e328668c121c82f46926a8547f9d80
Damian Paszkowski <paszko.damian@gmail.com>
2021-04-29 13:50:16 +0200

    Revert "remove button reveling generate option"
    This reverts commit 15ea86412cbfaf5e7324a5dd76c6d5a613ff1ecc.

--- features/openVault/OpenVaultEditing.tsx
+++ features/openVault/OpenVaultEditing.tsx
@@ -1,11 +1,33 @@
+// @ts-ignore
+import { Icon } from '@makerdao/dai-ui-icons'
 import { VaultActionInput } from 'components/VaultActionInput'
 import { handleNumericInput } from 'helpers/input'
+import { useTranslation } from 'next-i18next'
 import React from 'react'
-import { Grid } from 'theme-ui'
+import { Box, Grid, Text } from 'theme-ui'
 
 import { OpenVaultState } from './openVault'
 
+const PlusIcon = () => (
+  <Icon
+    name="plus"
+    color="onSuccess"
+    size={20}
+    sx={{ display: 'inline', verticalAlign: 'bottom', marginRight: 1 }}
+  />
+)
+const MinusIcon = () => (
+  <Icon
+    name="minus"
+    color="onSuccess"
+    size={20}
+    sx={{ display: 'inline', verticalAlign: 'bottom', marginRight: 1 }}
+  />
+)
+
 export function OpenVaultEditing(props: OpenVaultState) {
+  const { t } = useTranslation()
+
   const {
     token,
     depositAmount,
@@ -19,11 +41,15 @@ export function OpenVaultEditing(props: OpenVaultState) {
     maxDepositAmountUSD,
     updateGenerate,
     updateGenerateMax,
+    showGenerateOption,
+    toggleGenerateOption,
   } = props
+
+  const showGenerateOptionButton = depositAmount && !depositAmount.isZero()
 
   return (
     <Grid>
-      <Grid gap={1}>
+      <Box>
         <VaultActionInput
           action="Deposit"
           token={token}
@@ -39,18 +65,41 @@ export function OpenVaultEditing(props: OpenVaultState) {
           maxAmountLabel={'Balance'}
           hasError={false}
         />
-        <VaultActionInput
-          action="Generate"
-          amount={generateAmount}
-          token={'DAI'}
-          showMax={true}
-          maxAmount={maxGenerateAmount}
-          maxAmountLabel={'Maximum'}
-          onSetMax={updateGenerateMax}
-          onChange={handleNumericInput(updateGenerate!)}
-          hasError={false}
-        />
-      </Grid>
+        {showGenerateOptionButton && (
+          <Text
+            mt={3}
+            sx={{
+              cursor: 'pointer',
+              fontSize: 3,
+              fontWeight: 'semiBold',
+              color: 'onSuccess',
+              userSelect: 'none',
+              lineHeight: 1.25,
+            }}
+            onClick={toggleGenerateOption!}
+          >
+            {showGenerateOption ? <MinusIcon /> : <PlusIcon />}
+            {t('manage-vault.action-option', {
+              action: t('vault-actions.generate'),
+              token: 'DAI',
+            })}
+          </Text>
+        )}
+
+        {showGenerateOption && (
+          <VaultActionInput
+            action="Generate"
+            amount={generateAmount}
+            token={'DAI'}
+            showMax={true}
+            maxAmount={maxGenerateAmount}
+            maxAmountLabel={'Maximum'}
+            onSetMax={updateGenerateMax}
+            onChange={handleNumericInput(updateGenerate!)}
+            hasError={false}
+          />
+        )}
+      </Box>
     </Grid>
   )
 }
