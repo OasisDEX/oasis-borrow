1377fcd5a70f9c649f0155534cbecbd67c5723a9
Damian Paszkowski <paszko.damian@gmail.com>
2021-04-29 09:47:04 +0200

    style proxy view

--- features/openVault/OpenVaultProxy.tsx
+++ features/openVault/OpenVaultProxy.tsx
@@ -1,5 +1,6 @@
 import { Icon } from '@makerdao/dai-ui-icons'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Card, Flex, Grid, Link, Spinner, Text } from 'theme-ui'
 
 import { OpenVaultState } from './openVault'
@@ -11,23 +12,27 @@ export function OpenVaultProxy({
   proxyTxHash,
   etherscan,
 }: OpenVaultState) {
+  const { t } = useTranslation()
   return (
     <Grid>
       {stage === 'proxyInProgress' && (
-        <Card sx={{ backgroundColor: 'warning', border: 'none' }}>
-          <Flex sx={{ alignItems: 'center' }}>
-            <Spinner size={25} color="onWarning" />
-            <Grid pl={2} gap={1}>
-              <Text color="onWarning" sx={{ fontSize: 1 }}>
-                {proxyConfirmations || 0} of {safeConfirmations}: Proxy deployment confirming
+        <Card sx={{ backgroundColor: 'warning', border: 'none', p: 2, borderRadius: 'round' }}>
+          <Flex sx={{ alignItems: 'center', p: 1 }}>
+            <Spinner size={20} color="onWarning" />
+            <Grid pl={2} gap={0}>
+              <Text variant="paragraph3" color="onWarning">
+                {t('proxy-deployment-confirming', {
+                  proxyConfirmations: proxyConfirmations || 0,
+                  safeConfirmations,
+                })}
               </Text>
               <Link
                 href={`${etherscan}/tx/${proxyTxHash}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Text color="onWarning" sx={{ fontSize: 1 }}>
-                  View on etherscan -{'>'}
+                <Text variant="paragraph4" sx={{ fontWeight: 'semiBold' }} color="onWarning">
+                  {t('view-on-etherscan')}
                 </Text>
               </Link>
             </Grid>
@@ -35,20 +40,31 @@ export function OpenVaultProxy({
         </Card>
       )}
       {stage === 'proxySuccess' && (
-        <Card sx={{ backgroundColor: 'success', border: 'none' }}>
-          <Flex sx={{ alignItems: 'center' }}>
-            <Icon name="checkmark" size={25} color="onSuccess" />
-            <Grid pl={2} gap={1}>
-              <Text color="onSuccess" sx={{ fontSize: 1 }}>
-                {safeConfirmations} of {safeConfirmations}: Proxy deployment confirmed
+        <Card sx={{ backgroundColor: 'success', border: 'none', p: 2, borderRadius: 'round' }}>
+          <Flex sx={{ alignItems: 'center', p: 1 }}>
+            <Icon name="checkmark" size={20} color="onSuccess" />
+            <Grid pl={2} gap={0}>
+              <Text variant="paragraph3" color="onSuccess">
+                {t('proxy-deployment-confirming', {
+                  proxyConfirmations: safeConfirmations,
+                  safeConfirmations,
+                })}
               </Text>
               <Link
                 href={`${etherscan}/tx/${proxyTxHash}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Text color="onSuccess" sx={{ fontSize: 1 }}>
-                  View on etherscan -{'>'}
+                <Text
+                  variant="paragraph4"
+                  sx={{
+                    fontWeight: 'semiBold',
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                  color="onSuccess"
+                >
+                  {t('view-on-etherscan')}
                 </Text>
               </Link>
             </Grid>
