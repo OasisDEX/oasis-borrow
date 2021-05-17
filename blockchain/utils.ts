d829cefb6b5aa82094e1be03822cb4566e16c6f5
Damian Paszkowski <paszko.damian@gmail.com>
2021-05-17 16:33:52 +0200

    Add amount to wad function

--- blockchain/utils.ts
+++ blockchain/utils.ts
@@ -3,7 +3,7 @@ import padEnd from 'lodash/padEnd'
 //@ts-ignore
 import ethAbi from 'web3-eth-abi'
 
-import { RAD, RAY } from '../components/constants'
+import { RAD, RAY, WAD } from '../components/constants'
 import { getToken } from './tokensMetadata'
 
 export function amountFromRay(amount: BigNumber): BigNumber {
@@ -22,4 +22,8 @@ export function funcSigTopic(v: string): string {
 export function amountToWei(amount: BigNumber, token: string): BigNumber {
   const precision = getToken(token).precision
   return amount.times(new BigNumber(10).pow(precision))
+}
+
+export function amountToWad(amount: BigNumber): BigNumber {
+  return amount.times(WAD)
 }
