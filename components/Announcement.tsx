80f46a2f670a56a1ba49e848f873978e8be6d0ed
pacxiu <nedvedek111@gmail.com>
2021-06-22 15:01:37 +0200

    Add infra migration announcement

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -1,7 +1,9 @@
 import { Icon } from '@makerdao/dai-ui-icons'
 import { WithChildren } from 'helpers/types'
 import React from 'react'
-import { Flex, SxProps } from 'theme-ui'
+import { Flex, Grid, SxProps, Text } from 'theme-ui'
+
+import { AppLink } from './Links'
 
 export function Announcement({ children, sx }: WithChildren & SxProps) {
   return (
@@ -29,9 +31,30 @@ export function Announcement({ children, sx }: WithChildren & SxProps) {
           justifyContent: 'center',
         }}
       >
-        <Icon name="announcement" height="25px" width="25px" sx={{ flexShrink: 0 }} />
+        <Icon name="announcement" height="25px" width="25px" />
       </Flex>
       {children}
     </Flex>
+  )
+}
+
+export function InfrastructureAnnouncement() {
+  return (
+    <Announcement sx={{ mb: 3, textAlign: 'left', zIndex: 1, mx: 'auto' }}>
+      <Grid gap={2} sx={{ flex: 1 }}>
+        <Text variant="paragraph3" sx={{ fontWeight: 'semiBold', fontSize: [1, 2], mr: 3 }}>
+          Our team will be performing maintenance on Oasis.app servers on Wednesday June 23rd.
+          <br />
+          To avoid downtime, we have set-up a back-up version at{' '}
+          <AppLink href="https://oazo.app/borrow">Oazo.app</AppLink> and{' '}
+          <AppLink href="https://oazo.io/borrow">Oazo.io</AppLink>. <br />
+          Check{' '}
+          <AppLink href="https://twitter.com/oasisdotapp">
+            https://twitter.com/oasisdotapp
+          </AppLink>{' '}
+          for updates.
+        </Text>
+      </Grid>
+    </Announcement>
   )
 }
