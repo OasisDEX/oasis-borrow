8aaaf3eeb5b7581daecc01f0c5279c67659a0eb9
Rafał <nedvedek111@gmail.com>
2021-06-23 15:31:49 +0200

    Rz/infra migration banner (#270)
    * Add infra migration announcement
    
    * Add infra migration announcement
    
    * Extract Announcement, update copy and linking
    
    * Update boxShadow and mobile pading
    
    * Update copy

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -1,7 +1,10 @@
 import { Icon } from '@makerdao/dai-ui-icons'
 import { WithChildren } from 'helpers/types'
+import { useTranslation } from 'next-i18next'
 import React from 'react'
-import { Flex, SxProps } from 'theme-ui'
+import { Box, Flex, Grid, SxProps, Text } from 'theme-ui'
+
+import { AppLink } from './Links'
 
 export function Announcement({ children, sx }: WithChildren & SxProps) {
   return (
@@ -29,9 +32,96 @@ export function Announcement({ children, sx }: WithChildren & SxProps) {
           justifyContent: 'center',
         }}
       >
-        <Icon name="announcement" height="25px" width="25px" sx={{ flexShrink: 0 }} />
+        <Icon name="announcement" height="25px" width="25px" />
       </Flex>
       {children}
     </Flex>
+  )
+}
+
+function WithArrow({ children }: React.PropsWithChildren<{}>) {
+  return (
+    <Text
+      variant="paragraph3"
+      sx={{
+        fontWeight: 'semiBold',
+        fontSize: [1, 2],
+        position: 'relative',
+        '& .arrow': {
+          transition: 'ease-in-out 0.2s',
+          transform: 'translateX(0px)',
+        },
+        '&:hover .arrow': {
+          transform: 'translateX(5px)',
+        },
+      }}
+    >
+      <Box sx={{ display: 'inline', mr: 2 }}>{children}</Box>
+      <Box className="arrow" sx={{ display: 'inline', position: 'absolute' }}>
+        →
+      </Box>
+    </Text>
+  )
+}
+
+// To be removed after we migrate infrastructure
+export function InfrastructureAnnouncement() {
+  const { t } = useTranslation()
+
+  return (
+    <Box sx={{ px: 3 }}>
+      <Announcement sx={{ mb: 3, textAlign: 'left', zIndex: 1, mx: 'auto', boxShadow: 'banner' }}>
+        <Grid gap={2} sx={{ flex: 1 }}>
+          <Text variant="paragraph3" sx={{ fontWeight: 'semiBold', fontSize: [1, 2], mr: 3 }}>
+            Our team will be performing maintenance on Oasis.app servers on Thursday June 24th.
+            <br />
+            To avoid downtime, we have set-up a back-up version at{' '}
+            <AppLink href="https://oazo.io/">Oazo.io</AppLink>. <br />
+            Check{' '}
+            <AppLink href="https://twitter.com/oasisdotapp">
+              https://twitter.com/oasisdotapp
+            </AppLink>{' '}
+            for updates.
+          </Text>
+          <AppLink href={`${window.location.origin}/borrow-old`}>
+            <WithArrow>{t('visit-old-oasis')}</WithArrow>
+          </AppLink>
+        </Grid>
+      </Announcement>
+    </Box>
+  )
+}
+
+export function WelcomeAnnouncement() {
+  const { t } = useTranslation()
+
+  return (
+    <Announcement sx={{ mb: 3, textAlign: 'left' }}>
+      <Flex sx={{ flexDirection: ['column', 'row'] }}>
+        <Text variant="paragraph3" sx={{ fontWeight: 'semiBold', fontSize: [1, 2], mr: 3 }}>
+          {t('welcome')}
+        </Text>
+        <Flex sx={{ flexDirection: ['column', 'row'] }}>
+          <AppLink href="https://blog.oasis.app/introducing-the-redesigned-oasis-borrow/">
+            <WithArrow>{t('read-blog-post')}</WithArrow>
+          </AppLink>
+          <Text
+            variant="paragraph3"
+            sx={{
+              fontWeight: 'semiBold',
+              color: 'muted',
+              mx: 3,
+              ml: 4,
+              display: ['none', 'block'],
+            }}
+          >
+            |
+          </Text>
+          <AppLink href={`${window.location.origin}/borrow-old`}>
+            <WithArrow>{t('visit-old-oasis')}</WithArrow>
+          </AppLink>
+        </Flex>
+      </Flex>
+    </Announcement>
   )
 }
