94cd2ddb17df738d835cfc749d812dc2875c0ae4
pacxiu <nedvedek111@gmail.com>
2021-07-05 14:39:58 +0200

    Change basePath

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
 const { i18n } = require('./next-i18next.config')
 
 const isProduction = process.env.NODE_ENV === 'production'
-const basePath = isProduction ? '/borrow' : ''
+const basePath = ''
 
 module.exports = withBundleAnalyzer(
   withPWA(
