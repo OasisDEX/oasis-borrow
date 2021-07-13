8dd91256cbeb301ab69b152b4b5efa606893aed7
Damian Paszkowski <paszko.damian@gmail.com>
2021-07-13 08:40:25 +0200

    fix linting error

--- features/openMultiplyVault/stories/OpenMultiplyView.stories.tsx
+++ features/openMultiplyVault/stories/OpenMultiplyView.stories.tsx
@@ -6,6 +6,7 @@ export const WaitingForIlksToBeFetched = openMultiplyVaultStory({
   title: 'Open multiply vault empty state',
 })()
 
+// eslint-disable-next-line import/no-default-export
 export default {
   title: 'OpenMultiplyVault/Non-Blocking',
   component: OpenMultiplyVaultView,
