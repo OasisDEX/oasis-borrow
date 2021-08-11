5cedd346dfc9764a5795a536cf7ab12fa371ae1d
pacxiu <nedvedek111@gmail.com>
2021-08-11 11:04:35 +0200

    Add 1inch loading + error state

--- features/manageMultiplyVault/manageMultiplyVaultConditions.ts
+++ features/manageMultiplyVault/manageMultiplyVaultConditions.ts
@@ -106,6 +106,7 @@ export interface ManageVaultConditions {
 
   debtWillBeLessThanDebtFloor: boolean
   isLoadingStage: boolean
+  isExchangeLoading: boolean
 
   insufficientCollateralAllowance: boolean
   customCollateralAllowanceAmountEmpty: boolean
@@ -152,6 +153,7 @@ export const defaultManageVaultConditions: ManageVaultConditions = {
 
   debtWillBeLessThanDebtFloor: false,
   isLoadingStage: false,
+  isExchangeLoading: false,
 
   insufficientCollateralAllowance: false,
   customCollateralAllowanceAmountEmpty: false,
@@ -199,6 +201,10 @@ export function applyManageVaultConditions(
     maxWithdrawAmountAtNextPrice,
     maxGenerateAmountAtCurrentPrice,
     maxGenerateAmountAtNextPrice,
+
+    quote,
+    swap,
+    exchangeError,
   } = state
   const depositAndWithdrawAmountsEmpty = isNullish(depositAmount) && isNullish(withdrawAmount)
   const generateAndPaybackAmountsEmpty = isNullish(generateAmount) && isNullish(paybackAmount)
@@ -372,11 +378,14 @@ export function applyManageVaultConditions(
       customDaiAllowanceAmountExceedsMaxUint256 ||
       customDaiAllowanceAmountLessThanPaybackAmount)
 
+  const isExchangeLoading = !quote && !swap && !exchangeError
+
   const canProgress = !(
     isLoadingStage ||
     editingProgressionDisabled ||
     collateralAllowanceProgressionDisabled ||
-    daiAllowanceProgressionDisabled
+    daiAllowanceProgressionDisabled ||
+    isExchangeLoading
   )
 
   const canRegress = ([
@@ -421,6 +430,7 @@ export function applyManageVaultConditions(
     shouldPaybackAll,
     debtWillBeLessThanDebtFloor,
     isLoadingStage,
+    isExchangeLoading,
 
     insufficientCollateralAllowance,
     customCollateralAllowanceAmountEmpty,
