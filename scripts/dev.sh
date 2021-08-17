94cd91c9167288d178b320aadf0cf3f2cfeafef9
cristidas <cristi.dascalu@gmail.com>
2021-08-17 11:25:17 +0200

    optimizing dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,11 +3,11 @@ set -e
 cd "$(dirname "$0")"
 
 echo "Starting dependencies..."
+docker-compose down
 docker rm -f postgres-oasis-borrow || true
 docker rm -f multiply-proxy-actions || true
 docker rm -f oasis-borrow || true
-docker-compose down
 (sleep 10 && cd .. && yarn migrate)&
 
 export DATABASE_URL="postgresql://user:pass@postgres-oasis-borrow:5432/db?schema=public"
-docker-compose --env-file ../.env up
+docker-compose --env-file ../.env up -d
