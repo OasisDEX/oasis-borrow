d44a9e2087d44eb1ff8c3fb0cdcdd57a0e5e2e87
Rafał <nedvedek111@gmail.com>
2021-09-01 21:06:10 +0200

    Update mainnet contracts (#382)

--- blockchain/config.ts
+++ blockchain/config.ts
@@ -76,9 +76,9 @@ const protoMain = {
   dssProxyActions: contractDesc(dssProxyActions, mainnetAddresses.PROXY_ACTIONS),
   dssMultiplyProxyActions: contractDesc(
     dssMultiplyProxyActions,
-    getConfig()?.publicRuntimeConfig?.multiplyProxyActions || '',
+    '0xEAE4061009f0b804aAfC76f3ae67567D0ABE9c27',
   ),
-  exchange: contractDesc(exchange, getConfig()?.publicRuntimeConfig?.exchangeAddress || ''),
+  exchange: contractDesc(exchange, '0xb5eB8cB6cED6b6f8E13bcD502fb489Db4a726C7B'),
   aaveLendingPool: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
   etherscan: {
     url: 'https://etherscan.io',
