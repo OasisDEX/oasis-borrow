a6f3562ca125bc2bfcb73538ad5e0ee40256dfc0
Kuba Włodarczyk <fenris85@gmail.com>
2021-09-13 09:06:32 +0200

    Update cache API endpoint for mainnet

--- blockchain/config.ts
+++ blockchain/config.ts
@@ -93,7 +93,7 @@ const protoMain = {
   magicLink: {
     apiKey: 'pk_live_3256343D62443CE6',
   },
-  cacheApi: 'https://oazo-bcache.new.oasis.app/api/v1',
+  cacheApi: 'https://oazo-bcache-mainnet-staging.new.oasis.app/api/v1',
 }
 
 export type NetworkConfig = typeof protoMain
