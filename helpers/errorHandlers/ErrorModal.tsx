a35990f10f05694453ca30b2667de03a807ff395
Johnnie Lucas <johnnieskywalker@users.noreply.github.com>
2021-09-15 12:04:50 +0200

    Lw/error new modal (#448)
    * fixed issue exposure
    
    * new Error/Lost connection modal according to design https://www.figma.com/file/EdkPKn73UBzSbkr1xKwo6c/App\?node-id\=8618%3A41561

--- helpers/errorHandlers/ErrorModal.tsx
+++ helpers/errorHandlers/ErrorModal.tsx
@@ -1,8 +1,8 @@
-import { Modal, ModalCloseIcon } from 'components/Modal'
+import { Modal } from 'components/Modal'
 import { ModalProps } from 'helpers/modalHook'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
-import { Box, Button, Grid, Heading } from 'theme-ui'
+import { Box, Button, Flex, Grid, Heading, Text } from 'theme-ui'
 
 export function ErrorModal({ close, error }: ModalProps & { error: string }) {
   const { t } = useTranslation()
@@ -17,26 +17,46 @@ export function ErrorModal({ close, error }: ModalProps & { error: string }) {
     setShowResults(!showResults)
   }
   return (
-    <Modal close={close}>
-      <ModalCloseIcon {...{ close }} />
-      <Grid gap={4} sx={{ justifyContent: 'center', textAlign: 'center', mt: 5, mx: 'auto' }}>
-        <Heading>{t('error-message')}</Heading>
-        <Grid columns="1fr 1fr">
-          <Button onClick={onClickDetails} sx={{ maxWidth: 300, mx: 'auto', zIndex: 1 }}>
-            {t('error-button')}
-          </Button>
-          <Button onClick={onClickReload} sx={{ maxWidth: 300, mx: 'auto', zIndex: 1 }}>
+    <Modal close={close} sx={{ maxWidth: '450px', margin: '0px auto' }}>
+      <Flex
+        p={3}
+        sx={{
+          minHeight: '300px',
+          alignItems: 'center',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          px: 3,
+        }}
+      >
+        <Grid gap={3}>
+          <Box px={2}>
+            <Heading sx={{ textAlign: 'center', pb: 1, pt: 3, fontSize: 7 }}>
+              {t('error-header')}
+            </Heading>
+            <Text mt={3} sx={{ fontWeight: '400', fontSize: '14px', justifySelf: 'center' }}>
+              {t('error-message')}
+            </Text>
+          </Box>
+          <Button sx={{ width: '80%', justifySelf: 'center' }} onClick={onClickReload}>
+            {' '}
             {t('error-button-reload')}
           </Button>
+          <Button
+            variant="textual"
+            sx={{ width: '80%', justifySelf: 'center', color: 'lavender' }}
+            onClick={onClickDetails}
+          >
+            {t('error-button')}
+          </Button>
+          <Box sx={{ justifyContent: 'center', textAlign: 'center', wordWrap: 'break-word' }}>
+            {showResults
+              ? Array.isArray(error)
+                ? error.map((el, i) => <Box key={i}>{el ? el.message || 'Error' : null}</Box>)
+                : error
+              : null}
+          </Box>
         </Grid>
-        <Box sx={{ justifyContent: 'center', textAlign: 'center', wordWrap: 'break-word' }}>
-          {showResults
-            ? Array.isArray(error)
-              ? error.map((el, i) => <Box key={i}>{el ? el.message || 'Error' : null}</Box>)
-              : error
-            : null}
-        </Box>
-      </Grid>
+      </Flex>
     </Modal>
   )
 }
