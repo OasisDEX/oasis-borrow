d2eb0b43130d17607c420f0549dbc69c8577bf06
johnnieskywalker <lwolski0@gmail.com>
2021-09-29 18:08:46 +0200

    added gas price handler

---
+++ pages/api/gasPrice.tsx
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ajax } from 'rxjs/ajax'
+
+export default async function (_req: NextApiRequest, res: NextApiResponse) {
+
+  ajax({
+    url: `https://api.blocknative.com/gasprices/blockprices`,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': '44352ff7-890f-4334-a9e0-a9cfe0edd5cb'
+    },
+  }).subscribe(json => console.log(json));
+
+}
