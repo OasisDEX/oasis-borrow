a02eacd708625e9d88e782a2800a6880d1eaa8d8
Adam Skrodzki <adamskrodzki@gmail.com>
2021-09-30 09:53:50 +0200

    logging

--- pages/api/gasPrice.tsx
+++ pages/api/gasPrice.tsx
@@ -2,14 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { ajax } from 'rxjs/ajax'
 
 export default async function (_req: NextApiRequest, res: NextApiResponse) {
-  console.log('Hello call from gasPrice');
+  console.log('Hello call from gasPrice')
   ajax({
     url: `https://api.blocknative.com/gasprices/blockprices`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': '44352ff7-890f-4334-a9e0-a9cfe0edd5cb'
+      Authorization: '44352ff7-890f-4334-a9e0-a9cfe0edd5cb',
     },
-  }).subscribe(json => console.log(json));
-
+  }).subscribe((json) => console.log(json))
 }
