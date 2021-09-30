8a4c4ee7cbd91d98d903d3648880c90d6029d03b
johnnieskywalker <lwolski0@gmail.com>
2021-09-30 15:27:25 +0200

    added saving estimated prices in cache and reading from it

--- pages/api/gasPrice.tsx
+++ pages/api/gasPrice.tsx
@@ -15,13 +15,16 @@ export default async function (_req: NextApiRequest, res: NextApiResponse) {
 
   function callback(error: any, response: any, body: any) {
     const responseFromBlocknative = JSON.parse(body)
-    const estimatedPricesForNextBlock = responseFromBlocknative.blockPrices[0].estimatedPrices
+    const estimatedPricesForNextBlock = responseFromBlocknative?.blockPrices[0].estimatedPrices
+    // TODO LW - what do to when responseFromBlocknative had no blockPrices ? It happens when doing multiple requests one after another
     const estimatedPriceFor95PercentConfidence = estimatedPricesForNextBlock[1]
     if (!error && response.statusCode === 200) {
       cache.set('time', new Date().getTime())
+      cache.set('estimatedPriceFor95PercentConfidence', estimatedPriceFor95PercentConfidence)
       res.status(200)
       res.json({
         time: cache.get('time'),
+        fromCache: false,
         estimatedPriceFor95PercentConfidence
       })
     }
@@ -35,6 +38,7 @@ export default async function (_req: NextApiRequest, res: NextApiResponse) {
     res.json({
       time: time,
       fromCache: true,
+      estimatedPriceFor95PercentConfidence: cache.get('estimatedPriceFor95PercentConfidence'),
     })
   }
 }
