8c99abdcebdf5d06225cff8170eb9549fb9f20ef
johnnieskywalker <lwolski0@gmail.com>
2021-09-30 13:49:28 +0200

    made endpoint return values only for 95% confidence

--- pages/api/gasPrice.tsx
+++ pages/api/gasPrice.tsx
@@ -10,12 +10,17 @@ export default async function (_req: NextApiRequest, res: NextApiResponse) {
       Authorization: '44352ff7-890f-4334-a9e0-a9cfe0edd5cb',
     },
   }
+  console.log('gasPrice')
 
   function callback(error: any, response: any, body: any) {
     console.log('inside request', error)
-    console.log('inside request', body)
+    const responseFromBlocknative = JSON.parse(body)
+    const estimatedPricesForNextBlock = responseFromBlocknative.blockPrices[0].estimatedPrices
+    const estimatedPriceFor95PercentConfidence = estimatedPricesForNextBlock[1]
+    console.log('inside request', estimatedPriceFor95PercentConfidence)
     if (!error && response.statusCode === 200) {
-      res.json(body)
+      res.status(200)
+      res.json(estimatedPriceFor95PercentConfidence)
     }
   }
 
