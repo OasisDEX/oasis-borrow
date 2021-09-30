68bcc0fa3bdd5032cd39c0140d09c7932c5d638e
johnnieskywalker <lwolski0@gmail.com>
2021-09-30 11:43:12 +0200

    debug changes in gasPrice endpoint

--- pages/api/gasPrice.tsx
+++ pages/api/gasPrice.tsx
@@ -1,3 +1,4 @@
+import { json } from 'body-parser';
 import { NextApiRequest, NextApiResponse } from 'next'
 import { ajax } from 'rxjs/ajax'
 
@@ -10,6 +11,17 @@ export default async function (_req: NextApiRequest, res: NextApiResponse) {
       'Content-Type': 'application/json',
       'Authorization': '44352ff7-890f-4334-a9e0-a9cfe0edd5cb'
     },
-  }).subscribe(json => console.log(json));
+  }).subscribe((json : any) => {
+    console.log(json)
+    console.log('aasdsad')
+    res.status(200).json(json)
+    return res
+  },
+  (error: any) => {
+    console.log(error)
+    console.log('4pud')
+    res.status(200).json(error)
+  }
+  );
 
 }
