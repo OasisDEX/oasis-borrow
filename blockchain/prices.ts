6f3c9885c53839e3fd8ffc50c2ee0a3d874d5342
Adam Skrodzki <adam@oazoapps.com>
2021-10-12 20:28:11 +0200

    remove console logs

--- blockchain/prices.ts
+++ blockchain/prices.ts
@@ -45,7 +45,6 @@ export function createGasPrice$(
       return response
     }),
     map(({ response }) => {
-      console.log('blockNativeResp', response)
       const maxFeePerGas = new BigNumber(response.maxFeePerGas)
       const maxPriorityFeePerGas = new BigNumber(response.maxPriorityFeePerGas)
       return {
@@ -69,18 +68,12 @@ export function createGasPrice$(
           maxFeePerGas: new BigNumber((block as any).baseFeePerGas).multipliedBy(2).plus(minersTip),
           maxPriorityFeePerGas: minersTip,
         } as GasPriceParams
-        console.log(
-          `Initial gas fees Total=${gasFees.maxFeePerGas} priority = ${gasFees.maxPriorityFeePerGas}`,
-        )
         if (blockNative.maxFeePerGas.gt(0)) {
           gasFees.maxFeePerGas = new BigNumber(1000000000).multipliedBy(blockNative.maxFeePerGas)
           gasFees.maxPriorityFeePerGas = new BigNumber(1000000000).multipliedBy(
             blockNative.maxPriorityFeePerGas,
           )
         }
-        console.log(
-          `Final gas fees Total=${gasFees.maxFeePerGas} priority = ${gasFees.maxPriorityFeePerGas}`,
-        )
         return gasFees
       },
     ),
