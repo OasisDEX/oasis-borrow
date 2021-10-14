8f4aa1b7f828878c6f5481241eb24b3951d67154
Adam Skrodzki <adamskrodzki@gmail.com>
2021-10-14 09:03:50 +0200

    lint:fix

--- blockchain/calls/proxyActions.ts
+++ blockchain/calls/proxyActions.ts
@@ -608,7 +608,11 @@ export const closeVaultCall: TransactionDef<CloseVaultData> = {
   },
   prepareArgs: (data, context) => {
     const { dssMultiplyProxyActions, dssProxyActions } = context
-    console.log(`ExchangeData inside closeVaultCall '${data.exchangeData}' -> ${data.exchangeData?true:false}`)
+    console.log(
+      `ExchangeData inside closeVaultCall '${data.exchangeData}' -> ${
+        data.exchangeData ? true : false
+      }`,
+    )
     if (data.exchangeData) {
       return [dssMultiplyProxyActions.address, getCloseVaultCallData(data, context).encodeABI()]
     } else {
