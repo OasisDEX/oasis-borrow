c7020ba05293c4d3f1807d3918989aa180162b65
sebastianPiekarczyk <sebastian@oazoapps.com>
2021-10-27 14:10:42 +0200

    added / updated unit tests

---
+++ features/ilks/ilksPerToken.test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai'
+import { mockIlkDataList } from 'helpers/mocks/ilks.mock'
+import { getStateUnpacker } from 'helpers/testHelpers'
+import { of } from 'rxjs'
+
+import { createIlksPerToken$ } from './ilksPerToken'
+
+describe('createIlksPerToken$', () => {
+  it('should order correctly', () => {
+    const state = getStateUnpacker(createIlksPerToken$(of(mockIlkDataList())))
+
+    expect(state()).to.have.property('WBTC')
+    expect(state()).to.have.property('USDC')
+    expect(state()).to.have.property('ETH')
+    expect(state()).to.have.property('LINK')
+  })
+})
--- features/landing/tests/landing.test.ts
+++ features/landing/tests/landing.test.ts
@@ -6,7 +6,6 @@ import { mockIlkData } from 'helpers/mocks/ilks.mock'
 import { getStateUnpacker } from 'helpers/testHelpers'
 import { of } from 'rxjs'
 
-import { createFeaturedIlks$ } from '../featuredIlksData'
 import { createLanding$ } from '../landing'
 
 describe('Landing', () => {
@@ -16,44 +15,97 @@ describe('Landing', () => {
         ilk: 'USDC-NO-DEBT-AVAILABLE',
         debtCeiling: new BigNumber(10),
         ilkDebt: new BigNumber(10),
+        token: 'ETH',
       })(),
       mockIlkData({
         ilk: 'ETH-MOST-POPULAR',
         debtCeiling: new BigNumber(200),
         ilkDebt: new BigNumber(100),
+        token: 'ETH',
       })(),
       ...new Array(20).fill(
         mockIlkData({
           ilk: 'ETH-A',
           debtCeiling: new BigNumber(20),
           ilkDebt: new BigNumber(10),
+          token: 'ETH',
         })(),
       ), // make additional 20 copies of ilks
       mockIlkData({
         ilk: 'UNIV2DAIUSDC-LP-TOKEN',
         debtCeiling: new BigNumber(200),
         ilkDebt: new BigNumber(100),
+        token: 'UNIV2DAIUSDC',
       })(),
     ])
   }
 
+  function prepareIlksPerToken$() {
+    return of({
+      ETH: [
+        mockIlkData({
+          ilk: 'ETH-A',
+          debtCeiling: new BigNumber(1000),
+          ilkDebt: new BigNumber(900),
+          token: 'ETH',
+        })(),
+        mockIlkData({
+          ilk: 'ETH-B',
+          debtCeiling: new BigNumber(1000),
+          ilkDebt: new BigNumber(900),
+          token: 'ETH',
+        })(),
+        mockIlkData({
+          ilk: 'ETH-C',
+          debtCeiling: new BigNumber(200),
+          ilkDebt: new BigNumber(100),
+          token: 'ETH',
+        })(),
+      ],
+      WBTC: [
+        mockIlkData({
+          ilk: 'WBTC-A',
+          debtCeiling: new BigNumber(200),
+          ilkDebt: new BigNumber(100),
+          token: 'WBTC',
+        })(),
+      ],
+      'UNI LP Tokens': [
+        mockIlkData({
+          ilk: 'UNIV2DAIETH-A',
+          debtCeiling: new BigNumber(200),
+          ilkDebt: new BigNumber(100),
+          token: 'UNIV2DAIETH-A',
+        })(),
+        mockIlkData({
+          ilk: '"UNIV2WBTCETH-A"',
+          debtCeiling: new BigNumber(200),
+          ilkDebt: new BigNumber(100),
+          token: 'UNIV2DAIETH-A',
+        })(),
+        mockIlkData({
+          ilk: '"UNIV2USDCETH-A"',
+          debtCeiling: new BigNumber(200),
+          ilkDebt: new BigNumber(100),
+          token: 'UNIV2DAIETH-A',
+        })(),
+      ],
+    })
+  }
+
   let ilkDataList$ = prepareIlkDataList$()
+  let ilksPerToken$ = prepareIlksPerToken$()
 
-  beforeEach(() => (ilkDataList$ = prepareIlkDataList$()))
-
-  it('should return popular assets by default', () => {
-    const landing$ = createLanding$(ilkDataList$, createFeaturedIlks$(ilkDataList$))
-    const landing = getStateUnpacker(landing$)
-
-    expect(landing().ilks.filters.tagFilter).to.equal('popular')
-    expect(landing().ilks.data.length).to.equal(12)
+  beforeEach(() => {
+    ilkDataList$ = prepareIlkDataList$()
+    ilksPerToken$ = prepareIlksPerToken$()
   })
 
-  it('should be able to filter by tag', () => {
-    const landing$ = createLanding$(ilkDataList$, createFeaturedIlks$(ilkDataList$))
+  it('should return newest and popular assets', () => {
+    const landing$ = createLanding$(ilkDataList$, ilksPerToken$)
     const landing = getStateUnpacker(landing$)
 
-    landing().ilks.filters.change({ kind: 'tagFilter', tagFilter: 'lp-token' })
-    expect(landing().ilks.data[0].ilk).to.equal('UNIV2DAIUSDC-LP-TOKEN')
+    expect(landing().newest).to.have.property('UNI LP Tokens')
+    expect(landing().popular).to.have.property('ETH')
   })
 })
