5cd6111842e02e7aa799b0cc888887d3d170b701
sennett <anthony.sennett@gmail.com>
2021-12-03 17:13:10 +0000

    guard against non-primitive types in has changed since first reneder hook

--- helpers/useHasChangedSinceFirstRender.ts
+++ helpers/useHasChangedSinceFirstRender.ts
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
-export function useHasChangedSinceFirstRender<T>(state: T): boolean {
+type Comparable = string | boolean | number
+
+export function useHasChangedSinceFirstRender<T extends Comparable>(state: T): boolean {
   const [hasChanged, setHasChanged] = useState(false)
   const [previousState, setPreviousState] = useState<T>(state)
   if (state !== previousState) {
