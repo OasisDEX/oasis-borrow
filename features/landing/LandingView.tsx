dbe8027bff11e9d5f0d59831594ee172b8119e08
FBrinkkemper <Frankbrinkkemper@gmail.com>
2021-12-03 13:09:33 +0100

    Commented out Typeform

--- features/landing/LandingView.tsx
+++ features/landing/LandingView.tsx
@@ -20,7 +20,7 @@ import { Box, Button, Card, Flex, Grid, Heading, Image, SxStyleProp, Text } from
 import { fadeInAnimation, slideInAnimation } from 'theme/animations'
 
 import { FeaturedIlks, FeaturedIlksPlaceholder } from './FeaturedIlks'
-import { TypeformWidget } from './TypeformWidget'
+//import { TypeformWidget } from './TypeformWidget'
 
 const {
   publicRuntimeConfig: { apiHost },
@@ -320,7 +320,7 @@ export function LandingView() {
         </WithLoadingIndicator>
       </WithErrorHandler>
       <LandingCards />
-      <TypeformWidget />
+//      <TypeformWidget />
     </Grid>
   )
 }
