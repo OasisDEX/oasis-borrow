86e486d554a84f63de47e8ec40e16f6c84c0e856
Damian Paszkowski <paszko.damian@gmail.com>
2021-12-26 12:55:32 +0100

    use lower fees exchange

--- blockchain/calls/proxyActions.ts
+++ blockchain/calls/proxyActions.ts
@@ -747,7 +747,7 @@ function getGuniCloseVaultData(data: CloseGuniMultiplyData, context: ContextConn
     guniRouter,
   } = context
 
-  const exchange = context['noFeesExchange']
+  const exchange = context['lowerFeesExchange']
 
   const { token0: token0Symbol, token1: token1Symbol } = getToken(data.token)
 
