7224434f0949b2f59a595e5281b4fb7dfd8261b2
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-01-12 10:32:07 +0100

    added product card to storybook (#694)
    * added product card to storybook
    
    * updated image and image size
    
    * updated prop name
    
    * added placeholder translations for other languages
    
    * removed variants from FloatingLabel
    
    * prettier

---
+++ components/FloatingLabel.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Flex, SxStyleProp, Text } from 'theme-ui'
+
+interface FloatingLabelProps {
+  text: string
+  variant?: 'primary'
+  flexSx?: SxStyleProp
+  textSx?: SxStyleProp
+}
+
+export function FloatingLabel({ text, flexSx = {}, textSx = {} }: FloatingLabelProps) {
+  return (
+    <Flex
+      sx={{
+        borderRadius: '14px',
+        height: '28px',
+        py: 1,
+        px: 3,
+        position: 'absolute',
+        alignItems: 'center',
+        backgroundColor: '#5C5FFE',
+        ...flexSx,
+      }}
+    >
+      <Text sx={{ color: 'text.contrast', fontSize: 1, fontWeight: 'heading', ...textSx }}>
+        {text}
+      </Text>
+    </Flex>
+  )
+}
