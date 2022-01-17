004ba27dfdf2702cd17a3892dae4b926d9515011
sennett <anthony@oazoapps.com>
2022-01-17 16:26:32 +0000

    remove console log

--- features/landing/landing.ts
+++ features/landing/landing.ts
@@ -1,7 +1,7 @@
 import { IlkDataList } from 'blockchain/ilks'
 import { popularIlksWithFilter$, PopularIlksWithFilters } from 'features/ilks/popularIlksFilters'
 import { combineLatest, Observable } from 'rxjs'
-import { map, tap } from 'rxjs/operators'
+import { map } from 'rxjs/operators'
 
 import { FeaturedIlk } from './featuredIlksData'
 
@@ -15,7 +15,6 @@ export function createLanding$(
   featuredIlks: Observable<FeaturedIlk[]>,
 ): Observable<Landing> {
   return combineLatest(popularIlksWithFilter$(ilkDataList$), featuredIlks).pipe(
-    tap(console.log),
     map(([ilks, featuredIlks]) => ({
       ilks,
       featuredIlks,
