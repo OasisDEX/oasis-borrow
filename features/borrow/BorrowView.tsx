f903d6c7a9069fca6ce3a2966617fe42354dedd7
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-01-18 15:45:59 +0100

    reused ProductCardMultiply and ProductCardBorrow

--- features/borrow/BorrowView.tsx
+++ features/borrow/BorrowView.tsx
@@ -1,18 +1,15 @@
-import { BigNumber } from 'bignumber.js'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
 import { Flex, Grid } from 'theme-ui'
 
 import { useAppContext } from '../../components/AppContextProvider'
-import { ProductCard } from '../../components/ProductCard'
+import { ProductCardBorrow } from '../../components/ProductCardBorrow'
 import { ProductCardsFilter } from '../../components/ProductCardsFilter'
 import { ProductHeader } from '../../components/ProductHeader'
 import { AppSpinner, WithLoadingIndicator } from '../../helpers/AppSpinner'
 import { WithErrorHandler } from '../../helpers/errorHandlers/WithErrorHandler'
-import { formatPercent } from '../../helpers/formatters/format'
 import { useObservableWithError } from '../../helpers/observableHook'
 import { borrowPageCardsData, productCardsConfig } from '../../helpers/productCards'
-import { one } from '../../helpers/zero'
 
 export function BorrowView() {
   const { t } = useTranslation()
@@ -48,45 +45,9 @@ export function BorrowView() {
             <ProductCardsFilter filters={productCardsConfig.borrow.cardsFilters}>
               {(cardsFilter) => (
                 <Grid columns={[1, 2, 3]} sx={{ justifyItems: 'center' }}>
-                  {borrowPageCardsData({ productCardsData, cardsFilter }).map((cardData) => {
-                    const maxBorrowAmount = new BigNumber(
-                      one
-                        .div(cardData.liquidationRatio)
-                        .multipliedBy(cardData.currentCollateralPrice.times(100)),
-                    ).toFixed(0)
-
-                    return (
-                      <ProductCard
-                        key={cardData.ilk}
-                        tokenImage={cardData.bannerIcon}
-                        title={cardData.ilk}
-                        description={t(`product-card.borrow.description`, {
-                          token: cardData.token,
-                        })}
-                        banner={{
-                          title: t('product-card-banner.with', {
-                            value: 100,
-                            token: cardData.token,
-                          }),
-                          description: t(`product-card-banner.borrow.description`, {
-                            value: maxBorrowAmount,
-                          }),
-                        }}
-                        leftSlot={{
-                          title: t('system.min-coll-ratio'),
-                          value: `${formatPercent(cardData.liquidationRatio.times(100), {
-                            precision: 2,
-                          })}`,
-                        }}
-                        rightSlot={{
-                          title: t(t('system.stability-fee')),
-                          value: formatPercent(cardData.stabilityFee.times(100), { precision: 2 }),
-                        }}
-                        button={{ link: `/vaults/open/${cardData.ilk}`, text: t('nav.borrow') }}
-                        background={cardData.background}
-                      />
-                    )
-                  })}
+                  {borrowPageCardsData({ productCardsData, cardsFilter }).map((cardData) => (
+                    <ProductCardBorrow cardData={cardData} />
+                  ))}
                 </Grid>
               )}
             </ProductCardsFilter>
--- features/multiply/MultiplyView.tsx
+++ features/multiply/MultiplyView.tsx
@@ -3,15 +3,13 @@ import React from 'react'
 import { Flex, Grid } from 'theme-ui'
 
 import { useAppContext } from '../../components/AppContextProvider'
-import { ProductCard } from '../../components/ProductCard'
+import { ProductCardMultiply } from '../../components/ProductCardMultiply'
 import { ProductCardsFilter } from '../../components/ProductCardsFilter'
 import { ProductHeader } from '../../components/ProductHeader'
 import { AppSpinner, WithLoadingIndicator } from '../../helpers/AppSpinner'
 import { WithErrorHandler } from '../../helpers/errorHandlers/WithErrorHandler'
-import { formatPercent } from '../../helpers/formatters/format'
 import { useObservableWithError } from '../../helpers/observableHook'
 import { multiplyPageCardsData, productCardsConfig } from '../../helpers/productCards'
-import { one } from '../../helpers/zero'
 
 export function MultiplyView() {
   const { t } = useTranslation()
@@ -47,43 +45,9 @@ export function MultiplyView() {
             <ProductCardsFilter filters={productCardsConfig.multiply.cardsFilters}>
               {(cardsFilter) => (
                 <Grid columns={[1, 2, 3]} sx={{ justifyItems: 'center' }}>
-                  {multiplyPageCardsData({ productCardsData, cardsFilter }).map((cardData) => {
-                    const maxMultiple = one.div(cardData.liquidationRatio.minus(one))
-
-                    return (
-                      <ProductCard
-                        key={cardData.ilk}
-                        tokenImage={cardData.bannerIcon}
-                        title={cardData.ilk}
-                        description={t(`product-card.multiply.description`, {
-                          token: cardData.token,
-                        })}
-                        banner={{
-                          title: t('product-card-banner.with', {
-                            value: '100',
-                            token: cardData.token,
-                          }),
-                          description: t(`product-card-banner.multiply.description`, {
-                            value: maxMultiple.times(100).toFixed(2),
-                            token: cardData.token,
-                          }),
-                        }}
-                        leftSlot={{
-                          title: t('system.max-multiple'),
-                          value: `${maxMultiple.toFixed(2)}x`,
-                        }}
-                        rightSlot={{
-                          title: t(t('system.stability-fee')),
-                          value: formatPercent(cardData.stabilityFee.times(100), { precision: 2 }),
-                        }}
-                        button={{
-                          link: `/vaults/open-multiply/${cardData.ilk}`,
-                          text: t('nav.multiply'),
-                        }}
-                        background={cardData.background}
-                      />
-                    )
-                  })}
+                  {multiplyPageCardsData({ productCardsData, cardsFilter }).map((cardData) => (
+                    <ProductCardMultiply cardData={cardData} />
+                  ))}
                 </Grid>
               )}
             </ProductCardsFilter>
