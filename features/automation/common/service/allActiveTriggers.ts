892622ed47dacc8afa1e5bdadff66cc03b1785e7
johnnieskywalker <lwolski0@gmail.com>
2022-01-19 17:18:42 +0100

    added filter by cdpId

--- features/automation/common/service/allActiveTriggers.ts
+++ features/automation/common/service/allActiveTriggers.ts
@@ -2,21 +2,26 @@ import { AutomationBaseTriggerData } from 'blockchain/calls/automationBot'
 import { gql, GraphQLClient } from 'graphql-request'
 
 const query = gql`
-query {
-    allActiveTriggers(orderBy: [BLOCK_ID_ASC]) {
-    nodes {
-      id
-      triggerId
-      triggerType
-      cdpId
-      blockId
-      triggerData
-    }
-    }
-  }`
-
-async function getAllActiveTriggers(client: GraphQLClient): Promise<AutomationBaseTriggerData[]> {
-    const data = await client.request(query)
+query activeTriggersForVault($vaultId: BigFloat){
+  allActiveTriggers(
+    filter: {cdpId: {equalTo: $vaultId}}
+    orderBy: [BLOCK_ID_ASC]) {
+  nodes {
+    id
+    triggerId
+    triggerType
+    cdpId
+    blockId
+    triggerData
+    txId
+    
+  }
+  }
+}
+`
+// vaultId is string here because gql expects type BigFloat but can only parse string values
+async function getAllActiveTriggers(client: GraphQLClient, vaultId: string): Promise<AutomationBaseTriggerData[]> {
+    const data = await client.request(query, { cdpId: vaultId })
   
     return data.allActiveTriggers.nodes as AutomationBaseTriggerData[]
   }
