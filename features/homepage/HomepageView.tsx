96190fa917a6251643dbb719de8d109e0428d607
sennett <anthony@oazoapps.com>
2022-01-19 11:32:13 +0000

    moving to a translation

--- features/homepage/HomepageView.tsx
+++ features/homepage/HomepageView.tsx
@@ -69,7 +69,6 @@ export function HomepageView() {
         flex: 1,
       }}
     >
-      here
       <Hero
         isConnected={context?.status === 'connected'}
         sx={{ ...slideInAnimation, position: 'relative' }}
@@ -130,44 +129,44 @@ export function HomepageView() {
         }}
       >
         <InfoCard
-          title="Learn"
-          subtitle="Deep dive into oasis.app functionalities and glossary"
+          title={t('landing.info-cards.learn.learn')}
+          subtitle={t('landing.info-cards.learn.deep-dive')}
           links={[
             {
               href: 'https://kb.oasis.app/help/getting-started',
-              text: 'Get started',
+              text: t('landing.info-cards.learn.get-started'),
             },
             {
               href: 'https://kb.oasis.app/help/tutorials',
-              text: 'Tutorials',
+              text: t('landing.info-cards.learn.tutorials'),
             },
             {
               href: 'https://kb.oasis.app/help/borrow',
-              text: 'Key concepts',
+              text: t('landing.info-cards.learn.key-concepts'),
             },
           ]}
           backgroundGradient="linear-gradient(127.5deg, #EEE1F9 0%, #FFECE8 56.77%, #DDFFF7 100%)"
           backgroundImage="/static/img/info_cards/cubes_nov27.png"
         />
         <InfoCard
-          title="Support"
-          subtitle="Contact Oasis.app team whenever you need"
+          title={t('landing.info-cards.support.support')}
+          subtitle={t('landing.info-cards.support.contact-whenever')}
           links={[
             {
               href: '/support',
-              text: 'FAQ',
+              text: t('landing.info-cards.support.faq'),
             },
             {
               href: 'https://discord.gg/Kc2bBB59GC',
-              text: 'Discord',
+              text: t('landing.info-cards.support.discord'),
             },
             {
               href: '/daiwallet/contact',
-              text: 'Contact us',
+              text: t('landing.info-cards.support.contact-us'),
             },
             {
               href: 'https://twitter.com/oasisdotapp',
-              text: 'Twitter',
+              text: t('landing.info-cards.support.twitter'),
             },
           ]}
           backgroundGradient="linear-gradient(135.35deg, #FEF7FF 0.6%, #FEE9EF 100%), radial-gradient(261.45% 254.85% at 3.41% 2.19%, #FFFADD 0%, #FFFBE3 0.01%, #F0FFF2 52.6%, #FBEDFD 100%)"
@@ -175,7 +174,7 @@ export function HomepageView() {
         />
       </Grid>
       <Text variant="header2" sx={{ textAlign: 'center', mt: 7, mb: 4 }}>
-        Get started with Oasis.app
+        {t('landing.info-cards.get-started')}
       </Text>
       <Grid
         gap={4}
@@ -185,20 +184,18 @@ export function HomepageView() {
           gridTemplateColumns: ['1fr', '379px 1fr'],
           gridTemplateAreas: [
             'none',
-            `
-            "left topRight"
-            "left bottomRight"
-          `,
+            `"left topRight"
+            "left bottomRight"`,
           ],
         }}
       >
         <InfoCard
-          title="Multiply your assets"
-          subtitle="In a single transaction you can open a Maker vault, generate DAI, and spend them for more collateral."
+          title={t('landing.info-cards.multiply.multiply')}
+          subtitle={t('landing.info-cards.multiply.subtitle')}
           links={[
             {
               href: '/multiply',
-              text: 'Open vault',
+              text: t('landing.info-cards.multiply.open-vault'),
             },
           ]}
           backgroundGradient="linear-gradient(141.11deg, #EBFAFF 0.79%, #EBF2FF 98.94%), linear-gradient(127.5deg, #EEE1F9 0%, #FFECE8 56.77%, #DDFFF7 100%)"
@@ -211,12 +208,12 @@ export function HomepageView() {
           sx={{
             gridArea: [null, 'topRight'],
           }}
-          title="Borrow Dai"
-          subtitle="Choose your preferred token as collateral and open a Maker vault to get DAI"
+          title={t('landing.info-cards.borrow.borrow-dai')}
+          subtitle={t('landing.info-cards.borrow.choose-your-preferred-token')}
           links={[
             {
               href: '/borrow',
-              text: 'Open vault',
+              text: t('landing.info-cards.borrow.open-vault'),
             },
           ]}
           backgroundGradient="linear-gradient(98.21deg, #FFFBE8 2.63%, #FFF0E8 99.63%), linear-gradient(127.5deg, #E4F9C9 0%, #E8FFF5 49.48%, #F9E1EB 100%)"
@@ -226,12 +223,12 @@ export function HomepageView() {
           sx={{
             gridArea: [null, 'bottomRight'],
           }}
-          title="Manage your vault"
-          subtitle="Make actions on your vault whenever you want"
+          title={t('landing.info-cards.manage.manage-your-vault')}
+          subtitle={t('landing.info-cards.manage.make-actions')}
           links={[
             {
-              href: '/support',
-              text: 'Connect your wallet',
+              href: '/connect',
+              text: t('landing.info-cards.manage.connect-your-wallet'),
             },
           ]}
           backgroundGradient="linear-gradient(127.5deg, #E8EAFF 0%, #EEF0FF 0%, #FFF3FA 100%), linear-gradient(127.5deg, #DDFFF7 0%, #E8EAFF 61.98%, #F9E1EF 100%)"
