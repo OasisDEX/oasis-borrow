35d433d189e68dd321dc3ef9b12e60b6c4a69b6f
Damian Paszkowski <paszko.damian@gmail.com>
2022-01-20 14:00:58 +0100

    remove seperate page

--- features/asset/AssetView.tsx
+++ features/asset/AssetView.tsx
@@ -9,7 +9,7 @@ import { AssetPageContent } from 'content/assets'
 import { AppSpinner, WithLoadingIndicator } from 'helpers/AppSpinner'
 import { WithErrorHandler } from 'helpers/errorHandlers/WithErrorHandler'
 import { useObservableWithError } from 'helpers/observableHook'
-import { ProductCardData } from 'helpers/productCards'
+import { ProductCardData, uniLpProductCards } from 'helpers/productCards'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
 import { Box, Flex, Grid, Heading, Text } from 'theme-ui'
@@ -49,6 +49,34 @@ function TabContent(props: {
     </WithErrorHandler>
   )
 }
+
+function LpCards() {
+  const { productCardsData$ } = useAppContext()
+  const { error: productCardsDataError, value: productCardsDataValue } = useObservableWithError(
+    productCardsData$,
+  )
+
+  return (
+    <WithErrorHandler error={[productCardsDataError]}>
+      <WithLoadingIndicator
+        value={[productCardsDataValue]}
+        customLoader={
+          <Flex sx={{ alignItems: 'flex-start', justifyContent: 'center', height: '500px' }}>
+            <AppSpinner sx={{ mt: 5 }} variant="styles.spinner.large" />
+          </Flex>
+        }
+      >
+        {([productCardsData]) => (
+          <Grid sx={{ margin: '0 auto' }} columns={[1, 2, 3]}>
+            {uniLpProductCards(productCardsData).map((cardData) => (
+              <ProductCardBorrow cardData={cardData} />
+            ))}
+          </Grid>
+        )}
+      </WithLoadingIndicator>
+    </WithErrorHandler>
+  )
+}
 export function AssetView({ content }: { content: AssetPageContent }) {
   const { t } = useTranslation()
 
@@ -76,37 +104,41 @@ export function AssetView({ content }: { content: AssetPageContent }) {
         </Box>
       </Flex>
       <Grid sx={{ flex: 1, position: 'relative', mt: 5 }}>
-        <TabSwitcher
-          narrowTabsSx={{
-            display: ['block', 'none'],
-            maxWidth: '343px',
-            width: '100%',
-            mb: 4,
-          }}
-          wideTabsSx={{ display: ['none', 'block'], mb: 5 }}
-          tabs={[
-            {
-              tabLabel: t('landing.tabs.multiply.tabLabel'),
-              tabContent: (
-                <TabContent
-                  ilks={content.ilks}
-                  type="multiply"
-                  renderProductCard={ProductCardMultiply}
-                />
-              ),
-            },
-            {
-              tabLabel: t('landing.tabs.borrow.tabLabel'),
-              tabContent: (
-                <TabContent
-                  ilks={content.ilks}
-                  type="borrow"
-                  renderProductCard={ProductCardBorrow}
-                />
-              ),
-            },
-          ]}
-        />
+        {content.slug === 'lp-token' ? (
+          <LpCards />
+        ) : (
+          <TabSwitcher
+            narrowTabsSx={{
+              display: ['block', 'none'],
+              maxWidth: '343px',
+              width: '100%',
+              mb: 4,
+            }}
+            wideTabsSx={{ display: ['none', 'block'], mb: 5 }}
+            tabs={[
+              {
+                tabLabel: t('landing.tabs.multiply.tabLabel'),
+                tabContent: (
+                  <TabContent
+                    ilks={content.ilks}
+                    type="multiply"
+                    renderProductCard={ProductCardMultiply}
+                  />
+                ),
+              },
+              {
+                tabLabel: t('landing.tabs.borrow.tabLabel'),
+                tabContent: (
+                  <TabContent
+                    ilks={content.ilks}
+                    type="borrow"
+                    renderProductCard={ProductCardBorrow}
+                  />
+                ),
+              },
+            ]}
+          />
+        )}
       </Grid>
     </Grid>
   )
