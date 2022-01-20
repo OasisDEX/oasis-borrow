058642b858038ae410cb5b83d087bc7f53ea2275
Roman Krasiuk <rokrassyuk@gmail.com>
2022-01-20 11:23:14 +0100

    fix ens lookup

--- blockchain/ens.ts
+++ blockchain/ens.ts
@@ -8,7 +8,7 @@ export function resolveENSName$(context$: Observable<Context>, address: string) 
   return context$.pipe(
     switchMap(async (context) => {
       const provider = new ethers.providers.JsonRpcProvider(context.infuraUrl)
-      return await provider.lookupAddress(address)
+      return await provider.lookupAddress(address).catch(() => null)
     }),
   )
 }
