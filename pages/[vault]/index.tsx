c79676cf62bca0ff7c8c3eac14719d7fb87cd86a
Adam Skrodzki <adam@oazoapps.com>
2022-01-25 16:29:34 +0100

    fix TabSwitcher

--- pages/[vault]/index.tsx
+++ pages/[vault]/index.tsx
@@ -1,6 +1,12 @@
 import BigNumber from 'bignumber.js'
 import { WithConnection } from 'components/connectWallet/ConnectWallet'
 import { AppLayout } from 'components/Layouts'
+import { TabSwitcher } from 'components/TabSwitcher'
+import { TabSwitchLayout, VaultViewMode } from 'components/TabSwitchLayout'
+import { DefaultVaultHeaderControl } from 'components/vault/DefaultVaultHeaderControl'
+import { DefaultVaultLayout } from 'components/vault/DefaultVaultLayout'
+import { AdjustSlFormControl } from 'features/automation/controls/AdjustSlFormControl'
+import { ProtectionDetailsControl } from 'features/automation/controls/ProtectionDetailsControl'
 import { GuniTempBanner } from 'features/banners/guniTempBanner'
 import { VaultBannersView } from 'features/banners/VaultsBannersView'
 import { GeneralManageVaultView } from 'features/generalManageVault/GeneralManageVaultView'
@@ -36,7 +42,30 @@ export default function Vault({ id }: { id: string }) {
             <>
               <VaultBannersView id={vaultId} />
               <GuniTempBanner id={vaultId} />
-              <GeneralManageVaultView id={vaultId} />
+
+              <TabSwitcher
+                tabs={[{
+                  tabLabel:'Overview',
+                  tabContent:(<GeneralManageVaultView id={vaultId} />)
+                },{
+                  tabLabel:'Protection',
+                  tabContent:(<h1>TODO History</h1>)
+                },{
+                  tabLabel:'History',
+                  tabContent:(<DefaultVaultLayout
+                    detailsViewControl={<ProtectionDetailsControl id={vaultId} />}
+                    editForm={<AdjustSlFormControl id={vaultId} />}
+                    headerControl={<DefaultVaultHeaderControl vaultId={vaultId} />}
+                  />)
+                }]}
+                narrowTabsSx={{
+                  display: ['block', 'none'],
+                  maxWidth: '343px',
+                  width: '100%',
+                  mb: 4,
+                }}
+                wideTabsSx={{ display: ['none', 'block'], mb: 5 }}
+              />
             </>
           ) : (
             <Box sx={{ position: 'relative', zIndex: 1 }}>
