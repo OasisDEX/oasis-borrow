b498d18c57f910b430f87ee3ed0a009194b12e4f
sennett <anthony@oazoapps.com>
2022-01-26 09:18:54 +0000

    linting

--- components/ProductCardsWrapper.tsx
+++ components/ProductCardsWrapper.tsx
@@ -12,10 +12,14 @@ export function ProductCardsWrapper({ children }: ProductCardWrapperProps) {
   return (
     <Grid
       columns={productCards.length < 2 ? 1 : [1, 2, 3]}
-      sx={{ justifyItems: 'center', ...slideInAnimation, position: 'relative',
+      sx={{
+        justifyItems: 'center',
+        ...slideInAnimation,
+        position: 'relative',
         width: ['100%', productCards.length < 2 ? '378px' : '100%'],
         gap: 4,
-        margin: '0 auto',}}
+        margin: '0 auto',
+      }}
     >
       {React.Children.toArray(
         productCards.map((productCard, index) => (
