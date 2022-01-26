e4ba6bc36f2150f61c9b413994e297d15d14c7d2
sennett <anthony@oazoapps.com>
2022-01-26 11:47:58 +0000

    remove code duplication

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,71 +1,39 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Box, Card, Heading, SxStyleProp, Text } from 'theme-ui'
 
 import { staticFilesRuntimeUrl } from '../helpers/staticPaths'
 import { AppLink } from './Links'
+import { WithChildren } from '../helpers/types'
 
-function CardWithManyLinks({
+function CardContent({
   title,
   subtitle,
-  links,
-  backgroundImage,
-  backgroundGradient,
-  sx,
+  children,
 }: {
   title: string
   subtitle: string
-  links: Array<{ href: string; text: string }>
-  backgroundImage: string
-  backgroundGradient: string
-  sx?: SxStyleProp
-}) {
+} & WithChildren) {
   return (
-    <Card
-      sx={{
-        ...sx,
-        p: 4,
-        borderRadius: 'large',
-        border: 'none',
-        backgroundImage: `url(${staticFilesRuntimeUrl(backgroundImage)}), ${backgroundGradient}`,
-        backgroundPosition: 'bottom 0px right 0px',
-        backgroundRepeat: 'no-repeat',
-        minHeight: ['414px', 'unset'],
-      }}
-    >
-      <Box sx={{ position: 'relative', zIndex: 2 }}>
-        <Heading sx={{ my: 2, fontWeight: 'bold', color: 'primary' }}>{title}</Heading>
-        <Text
-          sx={{ mb: 3, color: 'text.subtitle', minHeight: '3em', maxWidth: ['unset', '320px'] }}
-        >
-          {subtitle}
-        </Text>
-        {links.map(({ href, text }) => (
-          <Box sx={{ pb: 3 }} key={href}>
-            <AppLink href={href} sx={{ fontSize: 3, color: 'primary' }}>
-              {text} -&gt;
-            </AppLink>
-          </Box>
-        ))}
-      </Box>
-    </Card>
+    <Box sx={{ position: 'relative', zIndex: 2 }}>
+      <Heading sx={{ my: 2, fontWeight: 'bold', color: 'primary' }}>{title}</Heading>
+      <Text sx={{ mb: 3, color: 'text.subtitle', minHeight: '3em', maxWidth: ['unset', '320px'] }}>
+        {subtitle}
+      </Text>
+      {children}
+    </Box>
   )
 }
 
-function CardWithOneLink({
-  title,
-  subtitle,
-  links,
+function CardWrapper({
   backgroundImage,
   backgroundGradient,
   sx,
+  children,
 }: {
-  title: string
-  subtitle: string
-  links: Array<{ href: string; text: string }>
   backgroundImage: string
   backgroundGradient: string
   sx?: SxStyleProp
-}) {
+} & WithChildren) {
   return (
     <Card
       sx={{
@@ -76,27 +44,12 @@ function CardWithOneLink({
         backgroundPosition: 'bottom 0px right 0px',
         backgroundRepeat: 'no-repeat',
         minHeight: ['414px', 'unset'],
-        padding: 0,
       }}
     >
-      <AppLink href={links[0].href} variant="unStyled" sx={{ display: 'block', p: 4 }}>
-        <Box sx={{ position: 'relative', zIndex: 2 }}>
-          <Heading sx={{ my: 2, fontWeight: 'bold', color: 'primary' }}>{title}</Heading>
-          <Text
-            sx={{ mb: 3, color: 'text.subtitle', minHeight: '3em', maxWidth: ['unset', '320px'] }}
-          >
-            {subtitle}
-          </Text>
-          <Box sx={{ pb: 3, fontSize: 3, color: 'primary', fontWeight: 'semiBold' }}>
-            {links[0].text} -&gt;
-          </Box>
-        </Box>
-      </AppLink>
+      {children}
     </Card>
   )
 }
-
-// function CardInternal ({})
 
 type InfoCardProps = {
   title: string
@@ -109,8 +62,30 @@ type InfoCardProps = {
 
 export function InfoCard(props: InfoCardProps) {
   if (props.links.length === 1) {
-    return <CardWithOneLink {...props} />
+    return (
+      <CardWrapper {...props} sx={{ ...props.sx, p: 0 }}>
+        <AppLink href={props.links[0].href} variant="unStyled" sx={{ display: 'block', p: 4 }}>
+          <CardContent title={props.title} subtitle={props.subtitle}>
+            <Box sx={{ pb: 3, fontSize: 3, color: 'primary', fontWeight: 'semiBold' }}>
+              {props.links[0].text} -&gt;
+            </Box>
+          </CardContent>
+        </AppLink>
+      </CardWrapper>
+    )
   } else {
-    return <CardWithManyLinks {...props} />
+    return (
+      <CardWrapper {...props} sx={{ ...props.sx, p: 4 }}>
+        <CardContent title={props.title} subtitle={props.subtitle}>
+          {props.links.map(({ href, text }) => (
+            <Box sx={{ pb: 3 }} key={href}>
+              <AppLink href={href} sx={{ fontSize: 3, color: 'primary' }}>
+                {text} -&gt;
+              </AppLink>
+            </Box>
+          ))}
+        </CardContent>
+      </CardWrapper>
+    )
   }
 }
