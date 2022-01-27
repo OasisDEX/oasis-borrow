cf11a327904eae7f45d791355ef844e39cd838ad
Anthony Sennett <anthony.sennett@gmail.com>
2022-01-27 14:33:46 +0000

    slide in pills fix header (#764)

--- features/homepage/HomepageView.tsx
+++ features/homepage/HomepageView.tsx
@@ -139,7 +139,7 @@ export function HomepageView() {
         }}
       />
 
-      <Pills sx={{ mb: 6 }} />
+      <Pills sx={{ mb: 6, ...slideInAnimation, position: 'relative', animationDuration: '0.7s' }} />
       <Box
         sx={{
           ...slideInAnimation,
@@ -361,7 +361,7 @@ export function Hero({ sx, isConnected }: { sx?: SxStyleProp; isConnected: boole
       <Heading as="h1" variant="header1" sx={{ mb: 3 }}>
         {t(heading)}
       </Heading>
-      <Text variant="paragraph1" sx={{ mb: 4, color: 'lavender' }}>
+      <Text variant="paragraph1" sx={{ mb: 4, color: 'lavender', maxWidth: '740px' }}>
         <Trans i18nKey={subheading} components={[<br />]} />
       </Text>
       {greyCircles}
