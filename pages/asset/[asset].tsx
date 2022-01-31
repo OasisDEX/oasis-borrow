65e4a95254147b1ade8c93d17cc097aa60bc6217
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-01-31 09:25:47 +0100

    asset pages generated at built time

--- pages/asset/[asset].tsx
+++ pages/asset/[asset].tsx
@@ -2,20 +2,36 @@ import { WithConnection } from 'components/connectWallet/ConnectWallet'
 import { ProductPagesLayout } from 'components/Layouts'
 import { AssetPageContent, assetsPageContentBySlug } from 'content/assets'
 import { WithTermsOfService } from 'features/termsOfService/TermsOfService'
-import { GetServerSidePropsContext } from 'next'
+import { GetServerSidePropsContext, GetStaticPaths } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useRouter } from 'next/router'
 import React from 'react'
 
 import { AssetView } from '../../features/asset/AssetView'
 
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+export const getStaticPaths: GetStaticPaths<{ asset: string }> = async () => {
+  return {
+    paths: [
+      { params: { asset: 'eth' } },
+      { params: { asset: 'btc' } },
+      { params: { asset: 'lp-token' } },
+      { params: { asset: 'yfi' } },
+      { params: { asset: 'uni' } },
+    ], // these paths will be generated at built time
+    fallback: 'blocking',
+  }
+}
+
+export async function getStaticProps(
+  ctx: GetServerSidePropsContext & { params: { asset: string } },
+) {
   const content: AssetPageContent | undefined =
-    assetsPageContentBySlug[ctx.query.asset.toString().toLocaleLowerCase()]
+    assetsPageContentBySlug[ctx.params.asset.toString().toLocaleLowerCase()]
+
   return {
     props: {
       ...(await serverSideTranslations(ctx.locale!, ['common'])),
-      asset: ctx.query.asset || null,
+      asset: ctx.params.asset || null,
       content: content || null,
     },
   }
