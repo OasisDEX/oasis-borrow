d112eb4c82b5138e635132786e19976b95fec406
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-01-31 16:34:12 +0100

    static ilks page try (#771)
    * static ilks page try
    
    * adjusted static paths
    
    * asset pages generated at built time
    
    * owner page static
    
    * changed fallback type
    
    * mapping over collections
    
    * map improvement

--- pages/vaults/open/[ilk].tsx
+++ pages/vaults/open/[ilk].tsx
@@ -2,16 +2,26 @@ import { WithWalletConnection } from 'components/connectWallet/ConnectWallet'
 import { AppLayout } from 'components/Layouts'
 import { OpenVaultView } from 'features/borrow/open/containers/OpenVaultView'
 import { WithTermsOfService } from 'features/termsOfService/TermsOfService'
-import { GetServerSidePropsContext } from 'next'
+import { GetServerSidePropsContext, GetStaticPaths } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 import { BackgroundLight } from 'theme/BackgroundLight'
 
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+import { supportedBorrowIlks } from '../../../helpers/productCards'
+
+export const getStaticPaths: GetStaticPaths<{ ilk: string }> = async () => {
+  const paths = supportedBorrowIlks.map((ilk) => ({ params: { ilk } })) // these paths will be generated at built time
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(ctx: GetServerSidePropsContext & { params: { ilk: string } }) {
   return {
     props: {
       ...(await serverSideTranslations(ctx.locale!, ['common'])),
-      ilk: ctx.query.ilk || null,
+      ilk: ctx.params.ilk || null,
     },
   }
 }
