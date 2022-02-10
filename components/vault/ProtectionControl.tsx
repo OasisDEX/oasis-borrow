c2ce20a445c557399a02d3705ddf7d89c329bcbc
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-02-10 12:32:40 +0100

    added ZeroDebtProtectionBanner when vault debt is 0

--- components/vault/ProtectionControl.tsx
+++ components/vault/ProtectionControl.tsx
@@ -1,14 +1,40 @@
+import { Icon } from '@makerdao/dai-ui-icons'
+import { useTranslation } from 'next-i18next'
 import React from 'react'
 
 import { IlkDataList } from '../../blockchain/ilks'
 import { Vault } from '../../blockchain/vaults'
 import { ProtectionDetailsControl } from '../../features/automation/controls/ProtectionDetailsControl'
 import { ProtectionFormControl } from '../../features/automation/controls/ProtectionFormControl'
+import { VaultBanner } from '../../features/banners/VaultsBannersView'
 import { VaultContainerSpinner, WithLoadingIndicator } from '../../helpers/AppSpinner'
 import { WithErrorHandler } from '../../helpers/errorHandlers/WithErrorHandler'
 import { useObservableWithError } from '../../helpers/observableHook'
 import { useAppContext } from '../AppContextProvider'
+import { AppLink } from '../Links'
 import { DefaultVaultLayout } from './DefaultVaultLayout'
+
+function ZeroDebtProtectionBanner() {
+  const { t } = useTranslation()
+
+  return (
+    <VaultBanner
+      status={<Icon size="34px" name="warning" />}
+      withClose={false}
+      header={t('protection.zero-debt-heading')}
+      subheader={
+        <>
+          {t('protection.zero-debt-description')}
+          {', '}
+          <AppLink href="https://kb.oasis.app/help" sx={{ fontSize: 3 }}>
+            {t('here')}.
+          </AppLink>
+        </>
+      }
+      color="primary"
+    />
+  )
+}
 
 interface ProtectionControlProps {
   vault: Vault
@@ -21,7 +47,7 @@ export function ProtectionControl({ vault, ilkDataList }: ProtectionControlProps
   const automationTriggersDataWithError = useObservableWithError(autoTriggersData$)
   const collateralPricesWithError = useObservableWithError(collateralPrices$)
 
-  return (
+  return !vault.debt.isZero() ? (
     <WithErrorHandler
       error={[automationTriggersDataWithError.error, collateralPricesWithError.error]}
     >
@@ -53,5 +79,7 @@ export function ProtectionControl({ vault, ilkDataList }: ProtectionControlProps
         }}
       </WithLoadingIndicator>
     </WithErrorHandler>
+  ) : (
+    <ZeroDebtProtectionBanner />
   )
 }
