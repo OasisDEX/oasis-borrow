22baeff0b952013d9df070be6bcf20e030f280f3
sennett <anthony@oazoapps.com>
2022-02-15 16:36:29 +0000

    maybe this works

--- pages/errors/index.tsx
+++ pages/errors/index.tsx
@@ -11,6 +11,8 @@ export const getStaticProps = async ({ locale }: { locale: string }) => ({
 export default function ServerError() {
   return (
     <ul>
+      <li>{process.env.HEROKU_BRANCH}</li>
+      <li>{process.env.SENTRY_RELEASE}</li>
       <li>
         <button
           type="button"
