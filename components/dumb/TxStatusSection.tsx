f5c711700a664d69569241bc645e27df5542ac8b
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-02-17 08:52:18 +0100

    removed casting

--- components/dumb/TxStatusSection.tsx
+++ components/dumb/TxStatusSection.tsx
@@ -6,28 +6,37 @@ import { progressStatuses } from '../../features/automation/common/consts/txStat
 import { TxStatusCardProgress, TxStatusCardSuccess } from '../vault/TxStatusCard'
 
 export interface TxStatusSectionProps<A extends TxMeta> {
-  txState: TxState<A> | undefined
+  txState?: TxState<A>
   etherscan: string
 }
 
 export function TxStatusSection<A extends TxMeta>(props: TxStatusSectionProps<A>) {
   const { t } = useTranslation()
 
-  const txStatus = props.txState?.status
-  const txHash = (props.txState as any)?.txHash
+  if (props.txState) {
+    const txStatus = props.txState.status
+    const txHash =
+      txStatus === TxStatus.Propagating ||
+      txStatus === TxStatus.WaitingForConfirmation ||
+      txStatus === TxStatus.Success
+        ? props.txState.txHash
+        : ''
 
-  return (
-    <>
-      {txStatus && progressStatuses.includes(txStatus) && (
-        <TxStatusCardProgress text={t('waiting-confirmation')} etherscan={''} txHash={txHash} />
-      )}
-      {txStatus === TxStatus.Success && txHash && (
-        <TxStatusCardSuccess
-          text={t('vault-changed')}
-          etherscan={props.etherscan}
-          txHash={txHash}
-        />
-      )}
-    </>
-  )
+    return (
+      <>
+        {txStatus && progressStatuses.includes(txStatus) && (
+          <TxStatusCardProgress text={t('waiting-confirmation')} etherscan={''} txHash={txHash} />
+        )}
+        {txStatus === TxStatus.Success && txHash && (
+          <TxStatusCardSuccess
+            text={t('vault-changed')}
+            etherscan={props.etherscan}
+            txHash={txHash}
+          />
+        )}
+      </>
+    )
+  }
+
+  return null
 }
