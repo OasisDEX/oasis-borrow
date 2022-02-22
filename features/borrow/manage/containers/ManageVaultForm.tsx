0feeb2a05250fcd4cd8c3069bbb3af3fec9753f4
Fernando Tiberti <fertiberti@gmail.com>
2022-02-22 00:27:48 -0300

    I said, fix lint.

--- features/borrow/manage/containers/ManageVaultForm.tsx
+++ features/borrow/manage/containers/ManageVaultForm.tsx
@@ -1,3 +1,4 @@
+import React, { ReactNode } from 'react'
 import { Icon } from '@makerdao/dai-ui-icons'
 import { TextWithCheckmark } from 'components/TextWithCheckmark'
 import { VaultAllowanceStatus } from 'components/vault/VaultAllowance'
@@ -6,7 +7,6 @@ import { VaultFormContainer } from 'components/vault/VaultFormContainer'
 import { VaultProxyStatusCard } from 'components/vault/VaultProxy'
 import { ManageVaultFormHeader } from 'features/borrow/manage/containers/ManageVaultFormHeader'
 import { useTranslation } from 'next-i18next'
-import { ReactNode } from 'react'
 import { Box, Divider, Grid, Text } from 'theme-ui'
 
 import { ManageVaultCollateralAllowance } from '../../../../components/vault/commonMultiply/ManageVaultCollateralAllowance'
