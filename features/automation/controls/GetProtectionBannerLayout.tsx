470be712fa107031178e61635d785d9dbc6f3b76
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-02-22 09:15:13 +0100

    adjusted styles of get protection banner (#844)

--- features/automation/controls/GetProtectionBannerLayout.tsx
+++ features/automation/controls/GetProtectionBannerLayout.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'next-i18next'
 import React from 'react'
-import { Button, Grid, Heading, Image, Text } from 'theme-ui'
+import { Button, Flex, Heading, Image, Text } from 'theme-ui'
 
 import { Banner } from '../../../components/Banner'
 import { staticFilesRuntimeUrl } from '../../../helpers/staticPaths'
@@ -21,24 +21,44 @@ export function GetProtectionBannerLayout({
       close={() => {
         handleClose()
       }}
-      sx={{ marginBottom: 3 }}
+      sx={{ marginBottom: 3, height: '160px' }}
     >
-      <Grid columns={2}>
-        <Grid>
-          <Heading variant="header2" as="h1">
-            {t('protection.banner-header')}
-          </Heading>
-          <Text variant="subheader">{t('protection.banner-content')}</Text>
-          <Button
-            backgroundColor="selected"
-            sx={{ borderRadius: '6px', '&:hover': { backgroundColor: 'selected' } }}
-            onClick={handleClick}
+      <Flex sx={{ flexDirection: 'column' }}>
+        <Heading variant="header2" as="h1" sx={{ mb: 2, zIndex: 1 }}>
+          {t('protection.banner-header')}
+        </Heading>
+        <Text
+          variant="subheader"
+          sx={{ fontSize: 2, mb: '23px', fontWeight: 'semiBold', zIndex: 1 }}
+        >
+          {t('protection.banner-content')}
+        </Text>
+        <Button
+          backgroundColor="selected"
+          sx={{
+            borderRadius: '6px',
+            '&:hover': { backgroundColor: 'selected' },
+            height: '28px',
+            maxWidth: '168px',
+            px: '5px',
+            py: '2px',
+            zIndex: 1,
+          }}
+          onClick={handleClick}
+        >
+          <Text
+            color="link"
+            variant="subheader"
+            sx={{ fontSize: 1, fontWeight: 'semiBold', lineHeight: '100%' }}
           >
-            <Text color="#575CFE">{t('protection.banner-button')}</Text>
-          </Button>
-        </Grid>
-        <Image src={staticFilesRuntimeUrl('/static/img/automation.svg')} />
-      </Grid>
+            {t('protection.banner-button')}
+          </Text>
+        </Button>
+      </Flex>
+      <Image
+        src={staticFilesRuntimeUrl('/static/img/automation.svg')}
+        sx={{ position: 'absolute', right: 0, top: 0, opacity: [0.4, 1] }}
+      />
     </Banner>
   )
 }
