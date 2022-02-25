9ea6376075731a24a48ee96cc05d3c3331bfa7de
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-02-26 00:02:48 +0100

    guni history fix (#853)

--- features/generalManageVault/GeneralManageVaultView.tsx
+++ features/generalManageVault/GeneralManageVaultView.tsx
@@ -106,7 +106,7 @@ export function GeneralManageVaultView({ id }: { id: BigNumber }) {
                       header={GuniVaultHeader}
                       form={GuniManageMultiplyVaultForm}
                       history={VaultHistoryView}
-                      vaultHistory={[]}
+                      vaultHistory={vaultMultiplyHistory}
                     />
                   ) : (
                     <ManageMultiplyVaultContainer
