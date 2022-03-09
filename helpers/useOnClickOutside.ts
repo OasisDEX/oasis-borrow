27ca915973ad47898124d0cbb5661984ec9d4fcd
Fernando Tiberti <fertiberti@gmail.com>
2022-03-09 13:35:17 -0300

    Rename function.

--- helpers/useOnClickOutside.ts
+++ helpers/useOnClickOutside.ts
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react'
 
 export function useOnClickOutside(ref: React.RefObject<HTMLDivElement>, cb: Function) {
   useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
+    function handleClick(event: MouseEvent) {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         cb()
       }
     }
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener('mousedown', handleClick)
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
+      document.removeEventListener('mousedown', handleClick)
     }
   }, [])
 }
