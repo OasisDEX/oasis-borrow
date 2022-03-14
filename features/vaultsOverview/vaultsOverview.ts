00ac1ea734d90f5146b7d9bd6eda42a084c5e36d
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-03-14 10:08:01 +0100

    added stop loss label to vaults list (#879)
    * added stop loss label to vaults list
    
    * removed string

--- features/vaultsOverview/vaultsOverview.ts
+++ features/vaultsOverview/vaultsOverview.ts
@@ -1,11 +1,14 @@
+import { BigNumber } from 'bignumber.js'
 import { VaultWithType } from 'blockchain/vaults'
 import { IlkWithBalance } from 'features/ilks/ilksWithBalances'
 import { isEqual } from 'lodash'
 import { Observable } from 'rxjs'
 import { combineLatest } from 'rxjs'
 import { map } from 'rxjs/internal/operators/map'
-import { distinctUntilChanged } from 'rxjs/operators'
+import { distinctUntilChanged, switchMap } from 'rxjs/operators'
 
+import { extractStopLossData } from '../automation/common/StopLossTriggerDataExtractor'
+import { TriggersData } from '../automation/triggers/AutomationTriggersData'
 import { ilksWithFilter$, IlksWithFilters } from '../ilks/ilksFilters'
 import { vaultsWithFilter$, VaultsWithFilters } from './vaultsFilters'
 import { getVaultsSummary, VaultSummary } from './vaultSummary'
@@ -22,16 +25,33 @@ export interface VaultsOverview {
 export function createVaultsOverview$(
   vaults$: (address: string) => Observable<VaultWithType[]>,
   ilksListWithBalances$: Observable<IlkWithBalance[]>,
+  automationTriggersData$: (id: BigNumber) => Observable<TriggersData>,
   address: string,
 ): Observable<VaultsOverview> {
   const vaultsAddress$ = vaults$(address)
 
+  const vaultWithAutomationData$ = vaults$(address).pipe(
+    switchMap((vaults) => {
+      return combineLatest(
+        vaults.map((vault) => {
+          return automationTriggersData$(vault.id).pipe(
+            map((automationData) => ({ ...vault, ...extractStopLossData(automationData) })),
+          )
+        }),
+      )
+    }),
+  )
+
   return combineLatest(
     vaultsWithFilter$(
-      vaults$(address).pipe(map((vaults) => vaults.filter((vault) => vault.type === 'borrow'))),
+      vaultWithAutomationData$.pipe(
+        map((vaults) => vaults.filter((vault) => vault.type === 'borrow')),
+      ),
     ),
     vaultsWithFilter$(
-      vaults$(address).pipe(map((vaults) => vaults.filter((vault) => vault.type === 'multiply'))),
+      vaultWithAutomationData$.pipe(
+        map((vaults) => vaults.filter((vault) => vault.type === 'multiply')),
+      ),
     ),
     vaultsAddress$.pipe(map(getVaultsSummary)),
     ilksWithFilter$(ilksListWithBalances$),
