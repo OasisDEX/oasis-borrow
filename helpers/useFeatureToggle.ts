cbf54157364c91eea6c4ebe06a2737a31ed0782d
Fernando Tiberti <fertiberti@gmail.com>
2022-03-15 13:48:44 -0300

    Fix lint.

--- helpers/useFeatureToggle.ts
+++ helpers/useFeatureToggle.ts
@@ -3,7 +3,13 @@ export const FT_LOCAL_STORAGE_KEY = 'features'
 
 type ConfiguredFeatures = Record<Features, boolean>
 
-type Features = 'TestFeature' | 'AnotherTestFeature' | 'EarnProduct' | 'Automation' | 'steETH' | 'Exchange'
+type Features =
+  | 'TestFeature'
+  | 'AnotherTestFeature'
+  | 'EarnProduct'
+  | 'Automation'
+  | 'steETH'
+  | 'Exchange'
 
 const configuredFeatures: Record<Features, boolean> = {
   TestFeature: false, // used in unit tests
