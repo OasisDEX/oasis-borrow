8264dc58df8bc606619061b119dfc72ec9e4c4e5
sennett <anthony@oazoapps.com>
2022-03-23 09:56:26 +0000

    add typeform

--- pages/multiply.tsx
+++ pages/multiply.tsx
@@ -1,9 +1,13 @@
+import { Popover, Sidetab } from '@typeform/embed-react'
+import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 
 import { WithConnection } from '../components/connectWallet/ConnectWallet'
 import { ProductPagesLayout } from '../components/Layouts'
 import { MultiplyView } from '../features/multiply/MultiplyView'
+import { useBreakpointIndex } from '../theme/useBreakpointIndex'
+import { useTheme } from '../theme/useThemeUI'
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
   props: {
@@ -11,10 +15,41 @@ export const getStaticProps = async ({ locale }: { locale: string }) => ({
   },
 })
 
+function MultiplySurveyButtons() {
+  const breakpoint = useBreakpointIndex()
+  const { theme } = useTheme()
+  const btnColor = theme.colors.link
+  const { t } = useTranslation()
+
+  return (
+    <>
+      {breakpoint === 0 && (
+        <Popover
+          id="H52MeocX"
+          buttonColor={btnColor}
+          shareGaInstance={true}
+          tooltip={t('help-shape-the-future-of-multiply')}
+        />
+      )}
+      {breakpoint > 0 && (
+        <Sidetab
+          id="H52MeocX"
+          buttonText={t('help-shape-the-future-of-multiply')}
+          buttonColor={btnColor}
+          shareGaInstance={true}
+        >
+          Help shape the future of Multiply
+        </Sidetab>
+      )}
+    </>
+  )
+}
+
 export default function MultiplyPage() {
   return (
     <WithConnection>
       <MultiplyView />
+      <MultiplySurveyButtons />
     </WithConnection>
   )
 }
