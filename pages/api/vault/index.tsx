b0cdc999309185b466e4b28416706a87224f4eb2
Damian Paszkowski <paszko.damian@gmail.com>
2022-03-29 18:46:05 +0200

    scripts

--- pages/api/vault/index.tsx
+++ pages/api/vault/index.tsx
@@ -2,6 +2,11 @@ import { createOrUpdate } from 'handlers/vault/createOrUpdate'
 import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = async (req, res) => {
-  await createOrUpdate(req, res)
+  switch (req.method) {
+    case 'POST':
+      return await createOrUpdate(req, res)
+    default:
+      return res.status(405).end()
+  }
 }
 export default handler
