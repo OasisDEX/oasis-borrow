9c0c8078ea9053936b7f1e98389a69947105d26c
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-04-04 06:58:04 +0100

    Added automation events to multiply history, unified history handling (#900)
    * added automation events to multiply history
    
    * fixed conditions
    
    * removed borrow history pipe
    
    * clean up
    
    * events sorting
    
    * split translations
    
    * removed mocks, adjusted conditional expressions
    
    * removed comments and logs

--- features/automation/controls/StopLossTriggeredBannerControl.tsx
+++ features/automation/controls/StopLossTriggeredBannerControl.tsx
@@ -1,20 +1,7 @@
-import { BigNumber } from 'bignumber.js'
 import React, { useCallback } from 'react'
 
 import { useSessionStorage } from '../../../helpers/useSessionStorage'
 import { StopLossTriggeredBannerLayout } from './StopLossTriggeredBannerLayout'
-
-export const mockTriggeredEvent = {
-  kind: 'STOPLOSS-TRIGGERED',
-  daiAmount: new BigNumber(5000.34),
-  collateralAmount: new BigNumber(4),
-  oraclePrice: new BigNumber(2424.64),
-  gasFee: new BigNumber(200),
-  timestamp: '2022-02-11T10:09:02+00:00',
-  token: 'ETH',
-  slippage: new BigNumber(2),
-  triggerType: 1,
-}
 
 export function StopLossTriggeredBannerControl() {
   const [isBannerClosed, setIsBannerClosed] = useSessionStorage('stopLossTriggeredBanner', false)
--- features/multiply/manage/pipes/manageMultiplyVaultConditions.ts
+++ features/multiply/manage/pipes/manageMultiplyVaultConditions.ts
@@ -609,7 +609,8 @@ export function applyManageVaultConditions(
     'borrowTransitionFailure',
   ] as ManageMultiplyVaultStage[]).some((s) => s === stage)
 
-  const stopLossTriggered = !!vaultHistory.length && vaultHistory[0].kind === 'STOPLOSS-TRIGGERED'
+  const stopLossTriggered =
+    !!vaultHistory[1] && 'triggerId' in vaultHistory[1] && vaultHistory[1].eventType === 'executed'
 
   return {
     ...state,
