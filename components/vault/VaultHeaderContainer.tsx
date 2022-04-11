82d8ca0b770439e5db778609f5a21a880edc44c1
Piotr Konowrocki <piotr@oazoapps.com>
2022-04-11 11:25:36 +0200

    add vault redesign UI as feature and continue header changes

--- components/vault/VaultHeaderContainer.tsx
+++ components/vault/VaultHeaderContainer.tsx
@@ -3,9 +3,59 @@ import { Heading } from '@theme-ui/components'
 import { getToken } from 'blockchain/tokensMetadata'
 import { PriceInfo } from 'features/shared/priceInfo'
 import { formatAmount, formatPercent } from 'helpers/formatters/format'
+import { useFeatureToggle } from 'helpers/useFeatureToggle'
 import { useTranslation } from 'next-i18next'
 import React, { ReactNode } from 'react'
-import { Box, Flex, Grid } from 'theme-ui'
+import { Box, Flex, Grid, Text } from 'theme-ui'
+
+import { getPriceChangeColor } from './VaultDetails'
+
+function VaultHeaderPrice({
+  label,
+  value,
+  sub,
+  subColor,
+}: {
+  label: string
+  value: string
+  sub?: string
+  subColor?: string
+}) {
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        fontSize: 3,
+        ml: 3,
+        pl: 3,
+        '::before': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          width: '1px',
+          height: '16px',
+          margin: 'auto',
+          backgroundColor: 'lightIcon',
+        },
+        ':first-child': {
+          ml: 0,
+          pl: 0,
+          '::before': {
+            content: 'none',
+          }
+        }
+      }}
+    >
+      <Text as="span" sx={{ color: 'text.subtitle' }}>{label}</Text>
+      <Text as="span" sx={{ ml: 1, fontWeight: 'semiBold' }}>{value}</Text>
+      {sub && subColor && (
+        <Text as="span" sx={{ ml: 1, fontSize: 2, fontWeight: 'semiBold', color: subColor }}>{sub}</Text>
+      )}
+    </Box>
+  )
+}
 
 export function VaultHeaderContainer({
   children,
@@ -19,39 +69,61 @@ export function VaultHeaderContainer({
   priceInfo: PriceInfo
 }) {
   const { t } = useTranslation()
+  const vaultRedesignedEnabled = useFeatureToggle('vaultRedesigned')
+  const { currentCollateralPrice, nextCollateralPrice, collateralPricePercentageChange } = priceInfo
   const tokenInfo = getToken(token)
-  const currentPrice = formatAmount(priceInfo.currentCollateralPrice, 'USD')
-  const nextPrice = formatAmount(priceInfo.nextCollateralPrice, 'USD')
-  const priceChange = formatPercent(priceInfo.collateralPricePercentageChange.times(100), {
+  const currentPrice = formatAmount(currentCollateralPrice, 'USD')
+  const nextPrice = formatAmount(nextCollateralPrice, 'USD')
+  const priceChange = formatPercent(collateralPricePercentageChange.times(100), {
     precision: 2,
   })
+  const priceChangeColor = getPriceChangeColor({ collateralPricePercentageChange })
 
   return (
     <Grid mt={4}>
-      <Heading
-        as="h1"
-        variant="heading1"
-        sx={{
-          fontWeight: 'semiBold',
-          fontSize: '28px',
-          pb: 2,
-        }}
-      >
-        <Icon
-          name={tokenInfo.iconCircle}
-          size="32px"
-          sx={{ verticalAlign: 'text-bottom', mr: 2 }}
-        />
-        {header}
-      </Heading>
-      <Flex>
-        <Box>
-          {t('manage-vault.current-price', { token })} ${currentPrice}
-        </Box>
-        <Box>
-          {t('manage-vault.next-price', { token })} ${nextPrice} {priceChange}
-        </Box>
-      </Flex>
+      {vaultRedesignedEnabled ? (
+        <>
+          <Heading
+            as="h1"
+            variant="heading1"
+            sx={{
+              fontWeight: 'semiBold',
+              fontSize: '28px',
+              pb: 2,
+            }}
+          >
+            <Icon
+              name={tokenInfo.iconCircle}
+              size="32px"
+              sx={{ verticalAlign: 'text-bottom', mr: 2 }}
+            />
+            {header}
+          </Heading>
+          <Flex>
+            <VaultHeaderPrice
+              label={t('manage-vault.current-price', { token })}
+              value={`$${currentPrice}`}
+            />
+            <VaultHeaderPrice
+              label={t('manage-vault.next-price', { token })}
+              value={`$${nextPrice}`}
+              sub={priceChange}
+              subColor={priceChangeColor}
+            />
+          </Flex>
+        </>
+      ) : (
+        <Heading
+          as="h1"
+          variant="heading1"
+          sx={{
+            fontWeight: 'semiBold',
+            pb: 2,
+          }}
+        >
+          {header}
+        </Heading>
+      )}
 
       <Box
         sx={{
--- helpers/useFeatureToggle.ts
+++ helpers/useFeatureToggle.ts
@@ -10,6 +10,7 @@ type Features =
   | 'Automation'
   | 'Exchange'
   | 'AutomationBasicBuyAndSell'
+  | 'vaultRedesigned'
 
 const configuredFeatures: Record<Features, boolean> = {
   TestFeature: false, // used in unit tests
@@ -18,6 +19,7 @@ const configuredFeatures: Record<Features, boolean> = {
   Automation: false,
   Exchange: true,
   AutomationBasicBuyAndSell: false,
+  vaultRedesigned: false,
   // your feature here....
 }
 
