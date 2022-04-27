a6891cab607a693c4dd8b25c0ca10ea7ca995b7b
Piotr Konowrocki <piotr@oazoapps.com>
2022-04-27 16:31:49 +0200

    fix copypasta var names

--- components/vault/detailsSection/ContentCardCollateralizationRatio.tsx
+++ components/vault/detailsSection/ContentCardCollateralizationRatio.tsx
@@ -9,7 +9,7 @@ import { useTranslation } from 'next-i18next'
 import React from 'react'
 import { Card, Grid, Heading, Text } from 'theme-ui'
 
-interface ContentCardLiquidationPriceModalProps {
+interface ContentCardCollateralizationRatioModalProps {
   collateralizationRatioFormatted: string
   collateralizationRatioAtNextPriceFormated?: string
 }
@@ -23,7 +23,7 @@ interface ContentCardCollateralizationRatioProps {
 function ContentCardLiquidationPriceModal({
   collateralizationRatioFormatted,
   collateralizationRatioAtNextPriceFormated,
-}: ContentCardLiquidationPriceModalProps) {
+}: ContentCardCollateralizationRatioModalProps) {
   const { t } = useTranslation()
 
   return (
@@ -80,7 +80,7 @@ export function ContentCardCollateralizationRatio({
       }),
   }
 
-  const contentCardModalSettings: ContentCardLiquidationPriceModalProps = {
+  const contentCardModalSettings: ContentCardCollateralizationRatioModalProps = {
     collateralizationRatioFormatted: formatted.collateralizationRatio,
   }
 
