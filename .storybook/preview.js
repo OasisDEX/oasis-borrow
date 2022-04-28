fa0f1eb36fa7d0a7bb21651412717932396c4b6c
Fernando Tiberti <fertiberti@gmail.com>
2022-04-27 22:32:19 -0300

    Remove faulty i18n Storybook config.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,8 @@ import { addDecorator } from '@storybook/react'
 import React, { Suspense } from 'react'
 import { ThemeProvider } from 'theme-ui'
 import { theme } from '../theme'
-import { I18nextProvider } from 'next-i18next'
-import i18n from './i18n'
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 
-addDecorator((storyFn) => <I18nextProvider i18n={i18n}>{storyFn()}</I18nextProvider>)
 addDecorator((storyFn) => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>)
 addDecorator((story, context) => <Suspense fallback="Loading...">{story(context)}</Suspense>)
 
