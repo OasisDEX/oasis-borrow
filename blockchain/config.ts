6fa559374a19d0b14edeb36e577bb68619880171
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-04-29 13:23:22 +0100

    Add mainnet cache url env (#995)
    * Automaton feature toggle set true as default
    
    * added arg do docker file
    
    * prettier
    
    * added correct fallback

--- blockchain/config.ts
+++ blockchain/config.ts
@@ -49,6 +49,10 @@ const infuraProjectId =
   process.env.INFURA_PROJECT_ID || getConfig()?.publicRuntimeConfig?.infuraProjectId || ''
 const etherscanAPIKey =
   process.env.ETHERSCAN_API_KEY || getConfig()?.publicRuntimeConfig?.etherscan || ''
+const mainnetCacheUrl =
+  process.env.MAINNET_CACHE_URL ||
+  getConfig()?.publicRuntimeConfig?.mainnetCacheURL ||
+  'https://oazo-bcache.new.oasis.app/api/v1'
 
 export const charterIlks = ['INST-ETH-A', 'INST-WBTC-A']
 
@@ -171,7 +175,7 @@ const protoMain = {
   magicLink: {
     apiKey: '',
   },
-  cacheApi: 'https://oazo-bcache.new.oasis.app/api/v1',
+  cacheApi: mainnetCacheUrl,
 }
 
 export type NetworkConfig = typeof protoMain
