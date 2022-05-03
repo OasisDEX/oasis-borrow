25942c4ca5ae6335816aa6a19cecab0285215b46
Fernando Tiberti <fertiberti@gmail.com>
2022-05-03 01:13:42 -0300

    Add proper tooltip.

--- components/dumb/EarnTable.stories.tsx
+++ components/dumb/EarnTable.stories.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import { Text } from 'theme-ui'
+import { Box, Text, Link } from 'theme-ui'
 import { EarnTable } from './EarnTable'
 
-export const MissingHeader = () => {
-  return <EarnTable 
+export const MissingHeaders = () => (
+  <EarnTable 
     headerData={[{ label: 'Tokens', tooltip: 'These are tokens'}]}
     rows={[
       [<Text>ETH</Text>, <Text>Ethereum</Text>],
       [<Text>BTC</Text>, <Text>Bitcoin</Text>, <Text>Is the first coin</Text>]
     ]}
   />
-}
+)
+
+export const Numbers = () => (
+   <EarnTable 
+    headerData={[
+      { label: 'Collateral Ratio', tooltip: <Box>More info on collateral <Link href="#">here</Link></Box>},
+      { label: 'Dai Debt', tooltip: 'The Dai Debt' },
+      { label: 'Collateral Locked' }
+    ]}
+    rows={[
+      [<Text sx={{ color: 'red' }}>155.20%</Text>, <Text>1.32m</Text>, <Text>754.33 ETH</Text>],
+      [<Text sx={{ color: 'green'}}>223.55%</Text>, <Text>1.32m</Text>, <Text>754.33 GUI...</Text>]
+    ]}
+  />
+)
 
 // eslint-disable-next-line import/no-default-export
 export default {
--- components/dumb/EarnTable.tsx
+++ components/dumb/EarnTable.tsx
@@ -1,28 +1,36 @@
-import _ from 'lodash'
+import { StatefulTooltip } from 'components/Tooltip'
 import React from 'react'
-import { Box, Grid } from 'theme-ui'
+import { Flex, Grid, SxStyleProp, Text } from 'theme-ui'
+import { Icon } from '@makerdao/dai-ui-icons'
 
 export type EarnTableHeaderVM = {
   label: string,
-  tooltip: JSX.Element | string
+  tooltip?: JSX.Element | string
 }
 
 function EarnTableHeader({ label, tooltip }: EarnTableHeaderVM) {
-  return <Box>
-    {label} ({tooltip})
-  </Box>
+  return <Flex sx={{ alignItems: 'center' }}>
+    <Text sx={{ fontSize: 1, color: 'text.subtitle', fontWeight: 'medium' }}>{label}</Text> 
+    {tooltip && <StatefulTooltip
+      tooltip={
+        <Text sx={{ fontWeight: 'semiBold', mb: 1, fontSize: 2, textAlign: 'left' }}>
+          {tooltip}
+        </Text>
+      }
+    >
+      <Icon name="question_o" size="16px" sx={{ ml: 1 }} color="text.subtitle"/>
+    </StatefulTooltip>}
+  </Flex>
 }
 
 function pad(count: number) {
   return Array(count).fill(<div/>)
 }
 
-export function EarnTable({ headerData, rows }: { headerData: EarnTableHeaderVM[], rows: JSX.Element[][] }) {
-  // @ts-ignore
+export function EarnTable({ headerData, rows, sx }: { headerData: EarnTableHeaderVM[], rows: JSX.Element[][], sx?: SxStyleProp }) {
   const columnCount = Math.max(headerData.length, ...rows.map(row => row.length))
-  console.log(columnCount)
   const paddedRows = rows.map(row => row.concat(pad(columnCount - row.length)))
-  return <Grid sx={{ gridTemplateColumns: `repeat(${columnCount}, 1fr)`}}>
+  return <Grid sx={{ gridTemplateColumns: `repeat(${columnCount}, 1fr)`, ...sx }}>
     {headerData.map(headerVM => <EarnTableHeader {...headerVM} />)} {pad(columnCount - headerData.length)}
     {paddedRows.flat()}
   </Grid>
