1da6a079e0400e29232e0d2d87f418239ff7df4a
Fernando Tiberti <fertiberti@gmail.com>
2022-05-05 22:41:46 -0300

    Reworking earn table.

--- components/dumb/EarnTable.tsx
+++ components/dumb/EarnTable.tsx
@@ -1,15 +1,15 @@
 import { Icon } from '@makerdao/dai-ui-icons'
 import { StatefulTooltip } from 'components/Tooltip'
 import { WithChildren } from 'helpers/types'
+import _ from 'lodash'
+import { useTranslation } from 'next-i18next'
 import React, { Children, createContext, useContext } from 'react'
-import { Box, Flex, Grid, SxStyleProp, Text } from 'theme-ui'
+import { Box, Button, Flex, Grid, SxStyleProp, Text } from 'theme-ui'
 
-export type EarnTableHeaderVM = {
+function Header({ label, tooltip }: {
   label: string
   tooltip?: JSX.Element | string
-}
-
-function EarnTableHeader({ label, tooltip }: EarnTableHeaderVM) {
+}) {
   return (
     <Flex sx={{ alignItems: 'center' }}>
       <Text sx={{ fontSize: 1, color: 'text.subtitle', fontWeight: 'medium' }}>{label}</Text>
@@ -28,53 +28,154 @@ function EarnTableHeader({ label, tooltip }: EarnTableHeaderVM) {
   )
 }
 
-function pad(count: number) {
-  if (count < 0) {
-    throw  new Error('columnCount is less than amount of cells in row. ')
+function useHeaders() {
+  const { t } = useTranslation()
+
+  const renderHeader = (headerKey: string) => 
+    <Header label={t(`earn.position-headers.${headerKey}.label`)} tooltip={t(`earn.position-headers.${headerKey}.tooltip`)} />
+
+  return {
+    asset: renderHeader('asset'),
+    vaultID: renderHeader('vault-id'),
+    collateralRatio: renderHeader('collateral-ratio'),
+    daiDebt: renderHeader('dai-debt'),
+    collateralLocked: renderHeader('collateral-locked'),
+    variablePerc: renderHeader('variable-perc'),
+    automation: renderHeader('automation'),
+    netValue: renderHeader('net-value'),
+    multiple: renderHeader('multiple'),
+    liquidationPrice: renderHeader('liquidation-price'),
+    pnl: renderHeader('pnl'),
+    sevenDayYield: renderHeader('seven-day-yield'),
+    liquidity: renderHeader('liquidity'),
   }
+}
+
+function emptyDivs(count: number) {
   return Array(count).fill(<div />)
 }
 
-function EarnTableCell({ children }: WithChildren) {
+function Cell({ children, sx }: { sx?: SxStyleProp } & WithChildren) {
   return (
-    <Box sx={{ pt: 1, pb: 3 }}>
-      <Text sx={{ color: 'primary' }}>{children}</Text>
+    <Box sx={{ pt: 1, pb: 3, color: 'primary', ...sx }}>
+      <Text>{children}</Text>
     </Box>
   )
 }
 
 const ColumnCountContext = createContext<number>(0)
 
-export function EarnTable({
-  headerData,
-  rows,
-}: {
-  headerData: EarnTableHeaderVM[]
-  rows: (JSX.Element | string)[][]
-}) {
-  const columnCount = useContext(ColumnCountContext)
-  if (columnCount === 0) {
-    throw new Error('EarnTable should be used inside EarnTablesContainer')
-  }
-  const paddedRows = rows.map((row) => row.concat(pad(columnCount - row.length)))
-  return (
-    <>
-      {headerData.map((headerVM, index) => (
-        <EarnTableHeader key={index} {...headerVM} />
-      ))}{' '}
-      {pad(columnCount - headerData.length)}
-      {paddedRows.flat().map((cellContent, index) => (
-        <EarnTableCell key={index}>{cellContent}</EarnTableCell>
-      ))}
-    </>
-  )
-}
-
-export function EarnTablesContainer({ columnCount, children }: { columnCount: number } & WithChildren) {
+function TableGroup({ columnCount, children }: { columnCount: number } & WithChildren) {
   const arrayChildren = Children.toArray(children)
   return <ColumnCountContext.Provider value={columnCount}>
     <Grid sx={{ gridTemplateColumns: `repeat(${columnCount}, auto)`, gap: 4, alignItems: 'center' }}>
       {Children.map(arrayChildren, (child: any) => <>{child} <Box sx={{ gridColumn: `1 / span ${columnCount}` }}>------</Box> </>)}
     </Grid>
   </ColumnCountContext.Provider>
+}
+
+function Table({
+  headers,
+  rows,
+}: {
+  headers: JSX.Element[]
+  rows: (JSX.Element | string)[][]
+}) {
+  const columnCount = useContext(ColumnCountContext)
+  if (columnCount === 0) {
+    throw new Error('Table should be used inside TableGroup')
+  }
+  const paddedRows = rows.map((row) => row.concat(pad(columnCount - row.length)))
+  return (
+    <>
+      {headers}{' '}
+      {pad(columnCount - headers.length)}
+      {paddedRows.flat().map((cellContent, index) => (
+        <Cell key={index}>{cellContent}</Cell>
+      ))}
+    </>
+  )
+}
+
+type PositionCommonProps = {
+  type: 'borrow' | 'multiply' | 'earn',
+  icon: string,
+  ilk: string,
+  vaultID: string,
+  onEditClick: Function,
+}
+
+type BorrowPositionVM = {
+  type: 'borrow',
+  collateralRatio: string,
+  inDanger: boolean,
+  daiDebt: string,
+  collateralLocked: string,
+  variable: string,
+  automationEnabled: boolean,
+  onAutomationBtnClick: Function
+} & PositionCommonProps
+
+type MultiplyPositionVM = {
+  type: 'multiply',
+  netVaule: string,
+  multiple: string,
+  liquidationPrice: string,
+  fundingCost: string
+  automationEnabled: boolean,
+  onAutomationBtnClick: Function
+} & PositionCommonProps
+
+type EarnPositionVM = {
+  type: 'earn',
+  netVaule: string,
+  pnl: string,
+  sevenDayYield: string,
+  liquidity: string
+} & PositionCommonProps
+
+type PositionVM = BorrowPositionVM | MultiplyPositionVM | EarnPositionVM
+
+function IlkWithIcon({ icon, ilk }: { icon: string, ilk: string}) {
+  return <Flex sx={{ alignItems: 'center', minWidth: '180px' }}>
+    <Icon name={icon} size="36px" sx={{ mr: 2 }} /> {ilk}
+  </Flex>
+}
+
+export function PositionsList({ positions, columnCount = 8 }: { positions: PositionVM[], columnCount?: number}) {
+  const headers = useHeaders()
+  const positionsByType = _.groupBy(positions, 'type')
+
+  return <Grid sx={{ gridTemplateColumns: `repeat(${columnCount}, auto)`, gap: 4, alignItems: 'center' }}>
+  {Object.entries(positionsByType).map(([type, positions]) => {
+    switch(type) {
+      case 'borrow': return <>
+        {[
+          headers.asset,
+          headers.vaultID,
+          headers.collateralRatio,
+          headers.daiDebt,
+          headers.collateralLocked,
+          headers.variablePerc,
+          headers.automation,
+        ].concat(emptyDivs(columnCount - 7))}
+      {(positions as BorrowPositionVM[]).map(position => [
+        <IlkWithIcon icon={position.icon} ilk={position.ilk} />,
+        <Cell>{position.vaultID}</Cell>,
+        <Cell sx={{ color: position.inDanger ? '#D94A1E' : 'onSuccess' }}>{position.collateralRatio}</Cell>,
+        <Cell>{position.daiDebt}</Cell>,
+        <Cell>{position.collateralLocked}</Cell>,
+        <Cell>{position.variable}</Cell>,
+        position.automationEnabled ? 
+          <Button variant="outline" onClick={() => position.onAutomationBtnClick()}>On</Button> : 
+          <Button variant="outline" onClick={() => position.onAutomationBtnClick()}>Activate</Button>,
+        <Button variant="outline" onClick={() => position.onEditClick()}>Edit Vault</Button>
+      ])}
+      </>
+    }
+  })}
+  </Grid>
+  
+
+
 }
