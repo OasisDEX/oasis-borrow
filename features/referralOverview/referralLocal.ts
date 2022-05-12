336562ec29a05624dcaf4c2c1013ce979ebcb05b
halaprix <panicz.klos@gmail.com>
2022-05-12 13:13:39 +0200

    cleanup #3

--- features/referralOverview/referralLocal.ts
+++ features/referralOverview/referralLocal.ts
@@ -1,54 +1,7 @@
 import { Observable, of } from 'rxjs'
-import {  delay } from 'rxjs/operators'
+import { delay } from 'rxjs/operators'
 
 export function checkReferralLocalStorage$(): Observable<{ referrer: string }> {
   const referrer = localStorage.getItem(`referral`)
   return of({ referrer }).pipe(delay(500))
 }
-
-/* export function saveReferralLocalStorage$(
-  checkReferralLocalStorage$: Observable<{ referral: string }>,
-): Observable<void> {
-  return checkReferralLocalStorage$.pipe(
-    switchMap((checkReferralLocalStorage) => {
-      if (checkReferralLocalStorage.referral.length !== 0) {
-        const router = useRouter()
-        const linkReferral = router.query.ref as string
-        localStorage.setItem(`referral`, linkReferral)
-      }
-      return of(undefined).pipe(delay(500))
-    }),
-  )
-} */
-/* 
-export function checkReferral$(
-  checkReferralLocalStorage$: Observable<{referral: string}>
-):Observable<> {
-  return checkReferralLocalStorage$.pipe(
-    switchMap((checkReferralLocalStorage) => {
-      if (checkReferralLocalStorage.referral) {
-        return of({ stage: 'acceptanceAccepted' } as TermsAcceptanceState)
-      }
-
-      // const accept$ = new Subject<void>()
-      return accept$.pipe(
-        switchMap(() => {
-          return saveAcceptance$(token, version, email).pipe(
-            map(() => ({ stage: 'acceptanceAccepted' })),
-            catchError((error) =>
-              of({ stage: 'acceptanceSaveFailed', error } as TermsAcceptanceState),
-            ),
-            startWith({ stage: 'acceptanceSaveInProgress' }),
-          )
-        }),
-        startWith({
-          stage: 'acceptanceWaiting4TOSAcceptance',
-          acceptTOS: () => accept$.next(),
-          updated,
-        }),
-      )
-    }),
-    catchError((error) => withClose({ stage: 'acceptanceCheckFailed', error })),
-    startWith({ stage: 'acceptanceCheckInProgress' } as TermsAcceptanceState),
-  )
-} */
