f9ac9c203d8e435e92db207c94f7609bdaf2efdb
Sebastian Piekarczyk <sebastian@oazoapps.com>
2022-05-25 08:47:15 +0100

    Stop loss validation fix when vault has 0 debt (#1055)

--- features/multiply/manage/pipes/manageMultiplyVaultConditions.ts
+++ features/multiply/manage/pipes/manageMultiplyVaultConditions.ts
@@ -564,7 +564,6 @@ export function applyManageVaultConditions(
 
   const afterCollRatioBelowStopLossRatio =
     !!stopLossData?.isStopLossEnabled &&
-    otherAction !== 'closeVault' &&
     afterCollRatioBelowStopLossRatioValidator({
       afterCollateralizationRatio,
       afterCollateralizationRatioAtNextPrice,
