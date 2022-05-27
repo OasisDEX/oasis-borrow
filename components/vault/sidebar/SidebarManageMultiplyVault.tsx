3a14a9d8025b85e0119c32a1f6ab5ce97ea6a45e
Piotr Konowrocki <piotr@oazoapps.com>
2022-05-27 11:26:35 +0200

    SidebarManageMultiplyVault dropdown

--- components/vault/sidebar/SidebarManageMultiplyVault.tsx
+++ components/vault/sidebar/SidebarManageMultiplyVault.tsx
@@ -1,16 +1,100 @@
+import { getToken } from 'blockchain/tokensMetadata'
 import { SidebarSection, SidebarSectionProps } from 'components/sidebar/SidebarSection'
 import { ManageMultiplyVaultState } from 'features/multiply/manage/pipes/manageMultiplyVault'
+import { getSidebarTitle } from 'features/sidebar/getSidebarTitle'
+import { isDropdownDisabled } from 'features/sidebar/isDropdownDisabled'
+import { SidebarFlow } from 'features/types/vaults/sidebarLabels'
 import { useTranslation } from 'next-i18next'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Grid } from 'theme-ui'
 
 export function SidebarManageMultiplyVault(props: ManageMultiplyVaultState) {
   const { t } = useTranslation()
 
-  const { stage } = props
+  const {
+    stage,
+    otherAction,
+    toggle,
+    setOtherAction,
+    vault: { token },
+  } = props
+
+  const [forcePanel, setForcePanel] = useState<string>()
+  const flow: SidebarFlow = 'manageMultiply'
+
+  useEffect(() => {
+    switch (stage) {
+      case 'adjustPosition':
+        setForcePanel('adjust')
+        break
+      case 'otherActions':
+        if (['depositDai', 'paybackDai', 'withdrawDai'].includes(otherAction)) setForcePanel('dai')
+        else if (['depositCollateral', 'withdrawCollateral'].includes(otherAction))
+          setForcePanel('collateral')
+        else if (otherAction === 'closeVault') setForcePanel('close')
+        break
+      case 'borrowTransitionEditing':
+        setForcePanel('transition')
+        break
+    }
+  }, [stage, otherAction])
 
   const sidebarSectionProps: SidebarSectionProps = {
-    title: 'Title',
+    title: getSidebarTitle({ flow, stage, token }),
+    dropdown: {
+      forcePanel,
+      disabled: isDropdownDisabled({ stage }),
+      items: [
+        {
+          label: t('system.actions.multiply.adjust'),
+          icon: 'circle_slider',
+          iconShrink: 2,
+          panel: 'adjust',
+          action: () => {
+            toggle!('adjustPosition')
+          },
+        },
+        {
+          label: t('system.actions.borrow.edit-collateral', { token }),
+          shortLabel: token,
+          icon: getToken(token).iconCircle,
+          panel: 'collateral',
+          action: () => {
+            toggle!('otherActions')
+            setOtherAction!('depositCollateral')
+          },
+        },
+        {
+          label: t('system.actions.borrow.edit-dai'),
+          shortLabel: 'DAI',
+          icon: getToken('DAI').iconCircle,
+          panel: 'dai',
+          action: () => {
+            toggle!('otherActions')
+            setOtherAction!('depositDai')
+          },
+        },
+        {
+          label: t('system.actions.multiply.switch-to-borrow'),
+          icon: 'circle_exchange',
+          iconShrink: 2,
+          panel: 'transition',
+          action: () => {
+            toggle!('borrowTransitionEditing')
+          },
+        },
+        {
+          label: t('system.actions.common.close-vault'),
+          icon: 'circle_close',
+          iconShrink: 2,
+          panel: 'close',
+          action: () => {
+            toggle!('otherActions')
+            setOtherAction!('closeVault')
+          },
+        },
+      ],
+    },
     content: <Grid gap={3}>stage: {stage}</Grid>,
     primaryButton: {
       label: 'Button',
