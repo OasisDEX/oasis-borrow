a38044ad9c71e9e6443bf9bbe00cb35111bca75a
halaprix <panicz.klos@gmail.com>
2022-06-01 21:32:10 +0200

    clean up referral modal

--- components/NewReferralModal.tsx
+++ components/NewReferralModal.tsx
@@ -2,6 +2,7 @@ import { Icon } from '@makerdao/dai-ui-icons'
 import { UserReferralState } from 'features/referralOverview/user'
 import { createUserUsingApi$ } from 'features/referralOverview/userApi'
 import { jwtAuthGetToken } from 'features/termsOfService/jwt'
+import { useRedirect } from 'helpers/useRedirect'
 import { useTranslation } from 'next-i18next'
 import React, { useState } from 'react'
 import { Button, Flex, Grid, Heading, Image, Text } from 'theme-ui'
@@ -11,7 +12,7 @@ import { AppLink } from './Links'
 import { Modal } from './Modal'
 import { SuccessfulJoinModal } from './SuccessfullJoinModal'
 
-interface NewReferralProps {
+interface ReferralProps {
   account?: string | null
   userReferral?: UserReferralState | null
 }
@@ -21,10 +22,10 @@ interface UpsertUser {
   isReferred: boolean
 }
 
-export function NewReferralModal({ account, userReferral }: NewReferralProps) {
+export function NewReferralModal({ account, userReferral }: ReferralProps) {
   const { t } = useTranslation()
   const [success, setSuccess] = useState(false)
-
+  const { push } = useRedirect()
   const createUser = async (upsertUser: UpsertUser) => {
     const { hasAccepted, isReferred } = upsertUser
 
@@ -44,8 +45,20 @@ export function NewReferralModal({ account, userReferral }: NewReferralProps) {
     }
   }
 
-  const ConnectWalletModal = (
-    <Modal sx={{ maxWidth: '445px', margin: '0 auto' }} close={() => null}>
+  const ReferralModal = ({
+    heading,
+    topButtonText,
+    bottomButtonText,
+    topButtonFunc,
+    bottomButtonFunc,
+  }: {
+    heading: string
+    topButtonText: string
+    bottomButtonText?: string
+    topButtonFunc: () => void
+    bottomButtonFunc?: () => void
+  }) => (
+    <Modal sx={{ maxWidth: '445px', margin: '0 auto' }} close={close}>
       <Grid p={4} sx={{ py: '24px' }}>
         <Flex sx={{ flexDirection: 'column' }}>
           <Flex sx={{ flexDirection: 'column', alignItems: 'center' }}>
@@ -56,77 +69,7 @@ export function NewReferralModal({ account, userReferral }: NewReferralProps) {
             />
           </Flex>
           <Heading as="h3" sx={{ mb: '12px', lineHeight: 1.5 }} variant="text.headerSettings">
-            Welcome to the Oasis.app Referral Program
-          </Heading>
-          <Text variant="paragraph3" sx={{ color: 'lavender', mb: '8px' }}>
-            {t('ref.modal.body-text')}
-          </Text>
-          <Text variant="paragraph3" sx={{ color: 'lavender', my: '8px' }}>
-            <Icon name="checkmark" size="auto" width="14px" color="primary" sx={{ mr: '8px' }} />{' '}
-            <span style={{ fontWeight: 'bold', color: 'primary' }}> {t('ref.modal.p1_1')}</span>
-            {t('ref.modal.p1_2')}
-          </Text>
-          <Text variant="paragraph3" sx={{ color: 'lavender', my: '8px' }}>
-            <Icon name="checkmark" size="auto" width="14px" color="primary" sx={{ mr: '8px' }} />{' '}
-            {t('ref.modal.p2')}
-          </Text>
-          <Text variant="paragraph3" sx={{ color: 'lavender', my: '8px' }}>
-            <Icon name="checkmark" size="auto" width="14px" color="primary" sx={{ mr: '8px' }} />{' '}
-            {t('ref.modal.p3')}
-          </Text>
-          <Text variant="paragraph3" sx={{ color: 'lavender', my: '12px' }}>
-            {t('ref.modal.read')}{' '}
-            <AppLink href="#" sx={{ fontWeight: 'body' }}>
-              {' '}
-              {t('ref.modal.link')}
-            </AppLink>
-          </Text>
-          <>
-            <AppLink
-              href={'/connect'}
-              variant="primary"
-              sx={{
-                display: 'flex',
-                margin: '0 auto',
-                my: '20px',
-                px: '40px',
-                py: 2,
-                alignItems: 'center',
-                '&:hover svg': {
-                  transform: 'translateX(10px)',
-                },
-              }}
-            >
-              {t('connect-wallet')}
-              <Icon
-                name="arrow_right"
-                sx={{
-                  ml: 2,
-                  position: 'relative',
-                  left: 2,
-                  transition: '0.2s',
-                }}
-              />
-            </AppLink>
-          </>
-        </Flex>
-      </Grid>
-    </Modal>
-  )
-  const AcceptReferralModal = (userReferral: UserReferralState) => (
-    <Modal sx={{ maxWidth: '445px', margin: '0 auto' }} close={() => null}>
-      <Grid p={4} sx={{ py: '24px' }}>
-        <Flex sx={{ flexDirection: 'column' }}>
-          <Flex sx={{ flexDirection: 'column', alignItems: 'center' }}>
-            <Image
-              src={staticFilesRuntimeUrl('/static/img/referral.svg')}
-              mb="16px"
-              width="240px"
-            />
-          </Flex>
-          <Heading as="h3" sx={{ mb: '12px', lineHeight: 1.5 }} variant="text.headerSettings">
-            {t('ref.modal.you-have-been-ref')} {userReferral.referrer?.slice(0, 4)}...
-            {userReferral.referrer?.slice(-5)}
+            {heading}
           </Heading>
           <Text variant="paragraph3" sx={{ color: 'lavender', mb: '8px' }}>
             {t('ref.modal.body-text')}
@@ -167,9 +110,9 @@ export function NewReferralModal({ account, userReferral }: NewReferralProps) {
                   transform: 'translateX(10px)',
                 },
               }}
-              onClick={() => createUser({ hasAccepted: true, isReferred: true })}
+              onClick={topButtonFunc}
             >
-              {t('ref.modal.accept')}
+              {topButtonText}
               <Icon
                 name="arrow_right"
                 sx={{
@@ -180,25 +123,39 @@ export function NewReferralModal({ account, userReferral }: NewReferralProps) {
                 }}
               />
             </Button>
-            <Button
-              variant="textual"
-              sx={{ fontSize: 3, width: '100%', mt: '12px', py: 0 }}
-              onClick={() => createUser({ hasAccepted: false, isReferred: true })}
-            >
-              {t('ref.modal.later')}
-            </Button>
+            {bottomButtonText && (
+              <Button
+                variant="textual"
+                sx={{ fontSize: 3, width: '100%', mt: '12px', py: 0 }}
+                onClick={bottomButtonFunc}
+              >
+                {bottomButtonText}
+              </Button>
+            )}
           </>
         </Flex>
       </Grid>
     </Modal>
   )
+
   return (
     <>
-      {!success && !userReferral && ConnectWalletModal}
-      {!success &&
-        userReferral &&
-        userReferral.state === 'newUser' &&
-        AcceptReferralModal(userReferral)}
+      {!success && !userReferral && (
+        <ReferralModal
+          heading="Welcome to the Oasis.app Referral Program"
+          topButtonText={t('connect-wallet')}
+          topButtonFunc={() => push('/connect')}
+        />
+      )}
+      {!success && userReferral && userReferral.state === 'newUser' && (
+        <ReferralModal
+          heading="Welcome to the Oasis.app Referral Program"
+          topButtonText={t('ref.modal.accept')}
+          bottomButtonText={t('ref.modal.later')}
+          topButtonFunc={() => createUser({ hasAccepted: true, isReferred: true })}
+          bottomButtonFunc={() => createUser({ hasAccepted: false, isReferred: true })}
+        />
+      )}
       {success && userReferral && account && (
         <SuccessfulJoinModal
           account={account}
