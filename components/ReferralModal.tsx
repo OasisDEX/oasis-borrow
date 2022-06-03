eea708604fa3171b3e478b03f258cd294146e5c5
halaprix <panicz.klos@gmail.com>
2022-06-03 07:36:59 +0200

    add `import React`

--- components/ReferralModal.tsx
+++ components/ReferralModal.tsx
@@ -1,6 +1,7 @@
 import { Icon } from '@makerdao/dai-ui-icons'
 import { staticFilesRuntimeUrl } from 'helpers/staticPaths'
 import { useTranslation } from 'next-i18next'
+import React from 'react'
 import { Button, Flex, Grid, Heading, Image, Text } from 'theme-ui'
 
 import { AppLink } from './Links'
