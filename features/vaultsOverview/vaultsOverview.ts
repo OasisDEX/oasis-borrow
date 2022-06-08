219a5e17f1f502ff54fc3ca0625e4fe10d0b731a
James Tuckett <james@tuckett.me>
2022-06-08 12:43:05 +0100

    fix: lint issue

--- features/vaultsOverview/vaultsOverview.ts
+++ features/vaultsOverview/vaultsOverview.ts
@@ -5,7 +5,7 @@ import { isEqual } from 'lodash'
 import { iif, Observable } from 'rxjs'
 import { combineLatest, of } from 'rxjs'
 import { map } from 'rxjs/internal/operators/map'
-import { distinctUntilChanged, switchMap, startWith } from 'rxjs/operators'
+import { distinctUntilChanged, startWith, switchMap } from 'rxjs/operators'
 
 import { getToken } from '../../blockchain/tokensMetadata'
 import {
