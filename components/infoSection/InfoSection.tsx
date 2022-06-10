b40bf4729c009a70d4186580ca9032239f9a89d2
George Field <georgefield@georges-mbp.home>
2022-06-10 09:14:44 +0100

    Fix imports and lint fix

--- components/infoSection/InfoSection.tsx
+++ components/infoSection/InfoSection.tsx
@@ -1,33 +1,26 @@
-import React from 'react';
-import { theme } from 'theme';
-import { Box, Grid, Text } from 'theme-ui';
-import { Item, ItemProps } from './Item';
+import React from 'react'
+import { theme } from 'theme'
+import { Box, Grid, Text } from 'theme-ui'
+import { Item, ItemProps } from './Item'
 
 interface InfoSectionProps {
-  title: string;
+  title: string
   items: ItemProps[]
 }
 
-export function InfoSection({
-  title,
-  items
-}: InfoSectionProps) {
+export function InfoSection({ title, items }: InfoSectionProps) {
   return (
     <Box>
       <Text
         sx={{
           fontWeight: theme.fontWeights.semiBold,
-          fontSize: theme.fontSizes[2]
+          fontSize: theme.fontSizes[2],
         }}
       >
         {title}
       </Text>
       <Grid as="ul" gap={3} sx={{ m: 0, p: 0, listStyle: 'none', mt: 3 }}>
-        {items && items.map(item => (
-          <Item
-            {...item}
-          />
-        ))}
+        {items && items.map((item) => <Item {...item} />)}
       </Grid>
     </Box>
   )
