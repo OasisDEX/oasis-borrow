56f9903eabc9fdb71e15a713b6f7c7a71f6c81bf
Piotr Konowrocki <piotr@oazoapps.com>
2022-06-13 13:52:59 +0200

    remove renderLayout from OptimizationDetailsControl

--- features/automation/optimization/controls/OptimizationDetailsControl.tsx
+++ features/automation/optimization/controls/OptimizationDetailsControl.tsx
@@ -2,15 +2,10 @@ import { Vault } from 'blockchain/vaults'
 import React from 'react'
 
 import { OptimizationDetailsLayout } from './OptimizationDetailsLayout'
-
-function renderLayout({ vault }: { vault: Vault }) {
-  return <OptimizationDetailsLayout vault={vault} />
-}
-
 interface OptimizationDetailsControlProps {
   vault: Vault
 }
 
 export function OptimizationDetailsControl({ vault }: OptimizationDetailsControlProps) {
-  return renderLayout({ vault })
+  return <OptimizationDetailsLayout vault={vault} />
 }
