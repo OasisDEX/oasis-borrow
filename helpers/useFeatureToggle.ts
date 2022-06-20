c1ba18c77cfd0beb1530b38959069f84d29882e6
Piotr Konowrocki <piotr@oazoapps.com>
2022-06-20 11:59:46 +0200

    set NewComponents false by default

--- helpers/useFeatureToggle.ts
+++ helpers/useFeatureToggle.ts
@@ -21,7 +21,7 @@ const configuredFeatures: Record<Feature, boolean> = {
   EarnProduct: true,
   Automation: true,
   AutomationBasicBuyAndSell: false,
-  NewComponents: true,
+  NewComponents: false,
   StopLossRead: true,
   StopLossWrite: true,
   StopLossOpenFlow: false,
