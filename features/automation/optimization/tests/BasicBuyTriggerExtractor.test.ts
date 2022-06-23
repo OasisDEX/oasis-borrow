19111aa825a404084a3539fecfdfcbf42f077054
johnnieskywalker <lwolski0@gmail.com>
2022-06-23 15:25:16 +0200

    prepared draft test for extractor

--- features/automation/optimization/tests/BasicBuyTriggerExtractor.test.ts
+++ features/automation/optimization/tests/BasicBuyTriggerExtractor.test.ts
@@ -1,2 +1,14 @@
+/* eslint-disable func-style */
+import { expect } from 'chai'
+
 // TODO - ŁW prepare test for prepareBasicBuyTriggerCreationData
 // Get input data from successful trigger creation, compare if triggerData matches
+// https://goerli.etherscan.io/tx/0x81fe8ae6fd61e79a5fddb74f47f368d4609541c1782bca4f432a9941f14d61ac#eventlog
+
+describe('BasicBuyTriggerExtractor', () => {
+    beforeEach(() => {})
+    it('should prepare valid triggerData for Basic Buy', () => {
+        // const triggerId = 156
+        const commandAddress = '0x7c86781a95b7e55e6c2f7297ae6773e1dbceab13'
+    })
+})
