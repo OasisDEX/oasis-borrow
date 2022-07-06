83162b36e1ecfd75292ee2f127327aa15a1980f3
ferni <fertiberti@gmail.com>
2022-07-05 22:11:50 -0300

    Fix lint.

--- features/earn/aave/open/containers/AaveOpenView.tsx
+++ features/earn/aave/open/containers/AaveOpenView.tsx
@@ -1,45 +1,54 @@
 import { TabBar } from 'components/TabBar'
 import { aaveFaq } from 'features/content/faqs/aave'
 import { Survey } from 'features/survey'
-
-import React from 'react'
-import { Box, Card, Container, Grid } from 'theme-ui'
-
 import { VaultContainerSpinner, WithLoadingIndicator } from 'helpers/AppSpinner'
 import { WithErrorHandler } from 'helpers/errorHandlers/WithErrorHandler'
 import { useTranslation } from 'next-i18next'
+import React from 'react'
+import { Box, Card, Container, Grid } from 'theme-ui'
 
 export function AaveOpenView() {
-  const [openAave, openAaveError] = [{/* pipeline */}, [/* errors */]]
+  const [openAave, openAaveError] = [
+    {
+      /* pipeline */
+    },
+    [
+      /* errors */
+    ],
+  ]
   const { t } = useTranslation()
-  
+
   return (
     <WithErrorHandler error={openAaveError}>
       <WithLoadingIndicator value={openAave} customLoader={<VaultContainerSpinner />}>
-        {(openAave) => (
-          <Container variant="vaultPageContainer">
-            [HEADER]
-            <TabBar
-              variant="underline"
-              sections={[
-                {
-                  value: 'simulate',
-                  label: t('open-vault.simulate'),
-                  content: <Grid variant="vaultContainer">
-                    <Box>[OPEN AAVE DETAILS]</Box>
-                    <Box>[FORM]</Box>
-                  </Grid>,
+        {
+          (/* openAave */) => (
+            <Container variant="vaultPageContainer">
+              [HEADER]
+              <TabBar
+                variant="underline"
+                sections={[
+                  {
+                    value: 'simulate',
+                    label: t('open-vault.simulate'),
+                    content: (
+                      <Grid variant="vaultContainer">
+                        <Box>[OPEN AAVE DETAILS]</Box>
+                        <Box>[FORM]</Box>
+                      </Grid>
+                    ),
                   },
-                {
-                  value: 'position-info',
-                  label: t('system.position-info'),
-                  content: <Card variant="faq">{aaveFaq}</Card>,
-                },
-              ]}
-            />
-            <Survey for="earn" />
-          </Container>
-        )}
+                  {
+                    value: 'position-info',
+                    label: t('system.position-info'),
+                    content: <Card variant="faq">{aaveFaq}</Card>,
+                  },
+                ]}
+              />
+              <Survey for="earn" />
+            </Container>
+          )
+        }
       </WithLoadingIndicator>
     </WithErrorHandler>
   )
--- pages/earn/open/aave-steth.tsx
+++ pages/earn/open/aave-steth.tsx
@@ -1,12 +1,12 @@
 import { WithWalletConnection } from 'components/connectWallet/ConnectWallet'
 import { AppLayout } from 'components/Layouts'
+import { AaveOpenView } from 'features/earn/aave/open/containers/AaveOpenView'
 import { Survey } from 'features/survey'
 import { WithTermsOfService } from 'features/termsOfService/TermsOfService'
 import { GetServerSidePropsContext } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 import { BackgroundLight } from 'theme/BackgroundLight'
-import { AaveOpenView } from 'features/earn/aave/open/containers/AaveOpenView'
 
 export async function getStaticProps(ctx: GetServerSidePropsContext) {
   return {
