65e3c974b968a21cfc67030fd565c2800fb7eefb
johnnieskywalker <lwolski0@gmail.com>
2022-07-18 14:26:58 +0200

    added reducer for constant multiply

---
+++ features/automation/protection/common/UITypes/constantMultipleFormChange.ts
@@ -0,0 +1,58 @@
+import BigNumber from 'bignumber.js'
+import { AutomationChangeAction, AutomationFormChange } from './basicBSFormChange'
+
+export const CONSTANT_MULTIPLE_FORM_CHANGE = 'CONSTANT_MULTIPLE_FORM_CHANGE'
+
+export type ConstantMultipleChangeAction =
+  | AutomationChangeAction
+  | { type: 'max-buy-price'; maxBuyPrice?: BigNumber }
+  | { type: 'min-sell-price'; minSellPrice?: BigNumber }
+  | { type: 'buy-with-threshold'; buyWithThreshold: boolean }
+  | { type: 'sell-with-threshold'; sellWithThreshold: boolean }
+  | { type: 'multiplier'; multiplier: number }
+
+export type ConstantMultipleFormChange = AutomationFormChange & {
+  maxBuyPrice?: BigNumber
+  minSellPrice?: BigNumber
+  buyWithThreshold: boolean
+  sellWithThreshold: boolean
+  multiplier: number
+}
+
+export function basicBSFormChangeReducer(
+  state: ConstantMultipleFormChange,
+  action: ConstantMultipleChangeAction,
+): ConstantMultipleFormChange {
+  switch (action.type) {
+    case 'trigger-id':
+      return { ...state, triggerId: action.triggerId }
+    case 'execution-coll-ratio':
+      return { ...state, execCollRatio: action.execCollRatio }
+    case 'target-coll-ratio':
+      return { ...state, targetCollRatio: action.targetCollRatio }
+    case 'continuous':
+      return { ...state, continuous: action.continuous }
+    case 'deviation':
+      return { ...state, deviation: action.deviation }
+    case 'max-gas-fee-in-gwei':
+      return { ...state, maxBaseFeeInGwei: action.maxBaseFeeInGwei }
+    case 'current-form':
+      return { ...state, currentForm: action.currentForm }
+    case 'tx-details':
+      return { ...state, txDetails: action.txDetails }
+    case 'reset':
+      return { ...state, ...action.resetData }
+    case 'max-buy-price':
+      return { ...state, maxBuyPrice: action.maxBuyPrice }
+    case 'min-sell-price':
+      return { ...state, minSellPrice: action.minSellPrice }
+    case 'buy-with-threshold':
+      return { ...state, buyWithThreshold: action.buyWithThreshold }
+    case 'sell-with-threshold':
+      return { ...state, sellWithThreshold: action.sellWithThreshold }
+    case 'multiplier':
+      return { ...state, multiplier: action.multiplier }
+    default:
+      return state
+  }
+}
