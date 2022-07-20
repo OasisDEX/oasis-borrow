048d942c321424f2b582d7d89d5542d9d26018c3
johnnieskywalker <lwolski0@gmail.com>
2022-07-20 11:54:29 +0200

    triggersData as key value pair

--- features/automation/protection/useConstantMultipleStateInitialization.ts
+++ features/automation/protection/useConstantMultipleStateInitialization.ts
@@ -6,35 +6,38 @@ import { Vault } from 'blockchain/vaults'
 import { useAppContext } from 'components/AppContextProvider'
 import { useEffect } from 'react'
 import { BasicBSTriggerData, extractBasicBSData } from '../common/basicBSTriggerData'
-import { resolveWithThreshold } from '../common/helpers'
+import { resolveMaxBuyOrMinSellPrice, resolveWithThreshold } from '../common/helpers'
 
 import { CONSTANT_MULTIPLE_FORM_CHANGE } from './common/UITypes/constantMultipleFormChange'
 import { TriggersData } from './triggers/AutomationTriggersData'
 export const INITIAL_MULTIPLIER_SELECTED = 2
 
 export interface ConstantMultipleTriggerData {
-  triggerId: BigNumber
-  triggersData: [TriggerType, BasicBSTriggerData]
+  groupTriggerId: BigNumber
+  triggersData: {[key in TriggerType]: TriggersData}
 }
 
 export function useConstantMultipleStateInitialization(
   ilkData: IlkData,
   vault: Vault | InstiVault,
-  autoTriggersData: TriggersData,
+  autoTriggersData: ConstantMultipleTriggerData, //TODO ŁW how to make sure there will be 2 elements and which one will be for absic buy and whioch for basic sell?
 ) {
   const { uiChanges } = useAppContext()
 
-  const buyTriggerData = extractBasicBSData(autoTriggersData, TriggerType.BasicBuy)
-  const sellTriggerData = extractBasicBSData(autoTriggersData, TriggerType.BasicSell)
-  const  maxBuyPrice  = buyTriggerData.maxBuyOrMinSellPrice
+  const buyTriggerData = extractBasicBSData(autoTriggersData.triggersData[TriggerType.BasicBuy], TriggerType.BasicBuy)
+  const sellTriggerData = extractBasicBSData(autoTriggersData.triggersData[TriggerType.BasicSell], TriggerType.BasicSell)
+  const maxBuyPrice  = buyTriggerData.maxBuyOrMinSellPrice
   const buyWithThresholdResolved = resolveWithThreshold({ maxBuyOrMinSellPrice: maxBuyPrice, triggerId: buyTriggerData.triggerId })
   const minSellPrice = sellTriggerData.maxBuyOrMinSellPrice
   const sellWithThresholdResolved = resolveWithThreshold({maxBuyOrMinSellPrice: minSellPrice, triggerId: sellTriggerData.triggerId})
+  const maxBuyPriceResolved = resolveMaxBuyOrMinSellPrice(buyTriggerData.maxBuyOrMinSellPrice)
+  const minSellPriceResolved = resolveMaxBuyOrMinSellPrice(sellTriggerData.maxBuyOrMinSellPrice)
 
   const collateralizationRatio = vault.collateralizationRatio.toNumber()
+  const publishKey = CONSTANT_MULTIPLE_FORM_CHANGE
 
   useEffect(() => {
-    uiChanges.publish(CONSTANT_MULTIPLE_FORM_CHANGE, {
+    uiChanges.publish(publishKey, {
       type: 'multiplier',
       multiplier: INITIAL_MULTIPLIER_SELECTED,
     })
