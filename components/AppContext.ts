b069e5eefeb58a18d14491898ccf683247f1f616
sennett <anthony@oazoapps.com>
2022-07-20 13:00:32 +0200

    memoize oracle price data

--- components/AppContext.ts
+++ components/AppContext.ts
@@ -527,6 +527,9 @@ export function setupAppContext() {
 
   const oraclePriceData$ = memoize(
     curry(createOraclePriceData$)(context$, pipPeek$, pipPeep$, pipZzz$, pipHop$),
+    ({ token, requestedData }) => {
+      return `${token}-${requestedData.join(',')}`
+    },
   )
 
   const tokenBalance$ = observe(onEveryBlock$, context$, tokenBalance)
