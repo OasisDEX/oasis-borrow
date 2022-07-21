f4f1743904e64613cefc95c49b9628e0bfdc30ad
halaprix <panicz.klos@gmail.com>
2022-07-21 10:20:14 +0200

    fix

--- features/referralOverview/ReferralLayout.tsx
+++ features/referralOverview/ReferralLayout.tsx
@@ -54,7 +54,7 @@ export function ReferralLayout({ children }: WithChildren) {
             <AppLink
               variant="inText"
               target="_blank"
-              href="https://kb.oasis.app/help/a-step-by-step-guide-to-refer-a-friend?version=latest"
+              href="https://kb.oasis.app/help/a-step-by-step-guide-to-refer-a-friend"
               sx={{
                 pt: '8px',
                 fontSize: 2,
