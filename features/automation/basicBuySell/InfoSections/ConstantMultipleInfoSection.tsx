80833b24b81f8ce9522591aed0cc6863383ed4cd
Piotr Konowrocki <piotr@oazoapps.com>
2022-07-25 15:12:38 +0200

    empty data and translations

--- features/automation/basicBuySell/InfoSections/ConstantMultipleInfoSection.tsx
+++ features/automation/basicBuySell/InfoSections/ConstantMultipleInfoSection.tsx
@@ -7,5 +7,68 @@ interface ConstantMultipleProps {}
 export function ConstantMultipleInfoSection({}: ConstantMultipleProps) {
   const { t } = useTranslation()
 
-  return <InfoSection title={t('auto-buy.buy-title')} items={[]} />
+  return (
+    <InfoSection
+      title={t('constant-multiple.vault-changes.general-summary')}
+      items={[
+        {
+          label: t('constant-multiple.vault-changes.buy-sell-trigger-summary'),
+          value: '0',
+        },
+        {
+          label: t('constant-multiple.vault-changes.target-multiple-ratio-after-buy-sell'),
+          value: '0',
+        },
+        {
+          label: t('vault-changes.slippage-limit'),
+          value: '0',
+        },
+        {
+          label: t('constant-multiple.vault-changes.cost-per-adjustment'),
+          value: '0',
+        },
+        {
+          label: t('auto-sell.setup-transaction-cost'),
+          value: '0',
+        },
+        {
+          label: t('constant-multiple.vault-changes.buy-sell-trigger-summary'),
+          dropdownValues: [
+            {
+              label: t('auto-buy.trigger-col-ratio-to-perfrom-buy'),
+              value: '0',
+            },
+            {
+              label: t('auto-buy.next-buy-prices'),
+              value: '0',
+            },
+            {
+              label: t('constant-multiple.vault-changes.eth-at-next-buy'),
+              value: '0',
+            },
+            {
+              label: t('constant-multiple.vault-changes.max-price-buy'),
+              value: '0',
+            },
+            {
+              label: t('auto-sell.trigger-col-ratio-to-perfrom-sell'),
+              value: '0',
+            },
+            {
+              label: t('auto-sell.next-sell-prices'),
+              value: '0',
+            },
+            {
+              label: t('constant-multiple.vault-changes.eth-at-next-sell'),
+              value: '0',
+            },
+            {
+              label: t('constant-multiple.vault-changes.max-price-sell'),
+              value: '0',
+            },
+          ],
+        },
+      ]}
+    />
+  )
 }
