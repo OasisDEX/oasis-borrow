a27a101d9376f2ddb1fec4c1c2a15a5a3f050346
George Field <georgefield@georges-mbp.home>
2022-07-26 10:51:24 +0100

    formatting and lint

--- components/notifications/NotificationPrefrenceCard.tsx
+++ components/notifications/NotificationPrefrenceCard.tsx
@@ -14,7 +14,7 @@ export function NotificationPrefrenceCard({
   heading,
   description,
   checked,
-  onChangeHandler
+  onChangeHandler,
 }: NotificationPrefrenceCardProps) {
   return (
     <Card
