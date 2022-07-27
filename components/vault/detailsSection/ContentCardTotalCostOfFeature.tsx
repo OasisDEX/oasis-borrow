33353ed6bde1da453ca77de661ca442a34414411
Piotr Konowrocki <piotr@oazoapps.com>
2022-07-27 13:45:37 +0200

    prettier fix

--- components/vault/detailsSection/ContentCardTotalCostOfFeature.tsx
+++ components/vault/detailsSection/ContentCardTotalCostOfFeature.tsx
@@ -1,8 +1,5 @@
 import BigNumber from 'bignumber.js'
-import {
-  ContentCardProps,
-  DetailsSectionContentCard,
-} from 'components/DetailsSectionContentCard'
+import { ContentCardProps, DetailsSectionContentCard } from 'components/DetailsSectionContentCard'
 import { formatAmount, formatPercent } from 'helpers/formatters/format'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
