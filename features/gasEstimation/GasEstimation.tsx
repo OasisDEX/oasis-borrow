6e8c64d06cf55d9b13be40316d357c812830abd6
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-08-02 10:08:43 +0200

    fixed typescript issue

--- features/gasEstimation/GasEstimation.tsx
+++ features/gasEstimation/GasEstimation.tsx
@@ -1,5 +1,6 @@
 import { useGasEstimationContext } from 'components/GasEstimationContextProvider'
 import { getEstimatedGasFeeText } from 'components/vault/VaultChangesInformation'
+import React from 'react'
 
 export function GasEstimation() {
   const gasEstimation = useGasEstimationContext()
