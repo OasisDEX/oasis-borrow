1b4b8b02556158eca26507426fc92b6e109e4ecb
sennett <anthony@oazoapps.com>
2022-08-23 22:18:20 +0100

    add a comment

--- features/multiply/manage/pipes/manageMultiplyVaultTransactions.ts
+++ features/multiply/manage/pipes/manageMultiplyVaultTransactions.ts
@@ -653,7 +653,7 @@ export function applyEstimateGas(
           swap?.status === 'SUCCESS'
             ? exchangeAction === 'BUY_COLLATERAL'
               ? swap.daiAmount.div(one.minus(OAZO_FEE))
-              : swap.daiAmount.times(one.minus(OAZO_FEE)).times(one.minus(SLIPPAGE))
+              : swap.daiAmount.times(one.minus(OAZO_FEE)).times(one.minus(SLIPPAGE)) // accounts for proxy actions
             : zero
 
         const collateralAmount =
