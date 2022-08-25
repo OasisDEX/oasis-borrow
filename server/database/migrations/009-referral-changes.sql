1ff87c572040e4848189a1d3956460a838af884f
halaprix <panicz.klos@gmail.com>
2022-08-25 13:54:29 +0200

    new referral migration

---
+++ server/database/migrations/009-referral-changes.sql
@@ -0,0 +1,13 @@
+ALTER TABLE public.merkle_tree
+ADD COLUMN start_block decimal(78, 0);
+ALTER TABLE public.merkle_tree
+ADD COLUMN end_block decimal(78, 0);
+ALTER TABLE public.user
+    RENAME COLUMN "createdAt" TO timestamp;
+ALTER TABLE public.user DROP COLUMN total_amount;
+ALTER TABLE public.merkle_tree
+ADD COLUMN timestamp TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE public.weekly_claim
+ADD COLUMN timestamp TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE public.merkle_tree
+ADD COLUMN snapshot text;
