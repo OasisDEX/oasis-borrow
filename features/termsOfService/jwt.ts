9cc7824aba1bde0b4b2689e833b733e3273cdd97
Damian Paszkowski <paszko.damian@gmail.com>
2022-09-01 12:59:01 +0200

    investiage gnosis sdk

--- features/termsOfService/jwt.ts
+++ features/termsOfService/jwt.ts
@@ -1,3 +1,4 @@
+
 import { getNetworkId } from '@oasisdex/web3-context'
 import { decode } from 'jsonwebtoken'
 import getConfig from 'next/config'
@@ -6,6 +7,8 @@ import { ajax } from 'rxjs/ajax'
 import { fromPromise } from 'rxjs/internal-compatibility'
 import { map } from 'rxjs/operators'
 import Web3 from 'web3'
+import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk';
+import { recoverPersonalSignature } from 'eth-sig-util'
 
 const basePath = getConfig()?.publicRuntimeConfig.basePath || ''
 
@@ -37,20 +40,64 @@ export function jwtAuthSetupToken$(
 }
 
 async function requestJWT(web3: Web3, account: string, isGnosisSafe: boolean): Promise<string> {
-  const web3Instance = isGnosisSafe ? new Web3(web3.givenProvider) : web3
-  const addressForSignature = isGnosisSafe ? web3Instance.givenProvider.selectedAddress : account
+  console.log({
+    isGnosisSafe,
+    web3,
+    provider: web3.givenProvider,
+    cProvider: web3.currentProvider
+  })
+  const web3Instance = web3
+  const addressForSignature = account
+
   const challenge = await requestChallenge(account).toPromise()
-
-  console.log('Signing challenge: ', challenge)
-
   const chainId = getNetworkId()
 
-  const signature = await signTypedPayload(challenge, web3Instance, addressForSignature)
-  const jwt = await requestSignin({ challenge, signature, chainId }).toPromise()
+  if (isGnosisSafe) {
+    const sdk = new SafeAppsSDK();
+    console.log(sdk)
+    console.log('Signing challenge: ', challenge)
+    const dataToSign = getDataToSignFromChallenge(challenge)
+    const env = await sdk.safe.getEnvironmentInfo()
+    const info = await sdk.safe.getInfo()
+    const tx = await sdk.txs.signMessage(dataToSign)
 
-  localStorage.setItem(`token-b/${account}`, jwt)
+    console.log({ env, info, tx })
+    const { detailedExecutionInfo } = await sdk.txs.getBySafeTxHash(tx.safeTxHash)
 
-  return jwt
+    if (detailedExecutionInfo?.type === 'MULTISIG') {
+
+      const confirmation = detailedExecutionInfo.confirmations[0]
+      const signer = confirmation.signer.value
+      console.log({ confirmation })
+      try {
+
+        const test = await web3.eth.personal.sign(dataToSign, addressForSignature, '')
+        console.log(test)
+      } catch (e) {
+        console.log(e)
+      }
+
+      const isSigned = await sdk.safe.isMessageSigned(dataToSign, confirmation.signature)
+
+      const response = recoverPersonalSignature({ data: dataToSign, sig: confirmation.signature! })
+
+      console.log({ response, signer, confirmation, challenge, isSigned })
+      const safeJwt = await requestSignin({ challenge, signature: confirmation.signature!, chainId }).toPromise()
+
+      return safeJwt
+    }
+  } else {
+
+    const signature = await signTypedPayload(challenge, web3Instance, addressForSignature)
+    console.log({ signature });
+    const jwt = await requestSignin({ challenge, signature, chainId }).toPromise()
+
+    localStorage.setItem(`token-b/${account}`, jwt)
+
+    return jwt
+  }
+
+  throw new Error('')
 }
 
 async function signTypedPayload(challenge: string, web3: Web3, account: string): Promise<string> {
