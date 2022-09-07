8e0ec942f09acc7e3d5b812bf88c00de1df9f6fc
johnnieskywalker <lwolski0@gmail.com>
2022-09-07 12:39:43 +0200

    handled cases for as

--- features/automation/protection/controls/sidebar/SidebarAutoSellAddEditingStage.tsx
+++ features/automation/protection/controls/sidebar/SidebarAutoSellAddEditingStage.tsx
@@ -4,6 +4,7 @@ import { collateralPriceAtRatio } from 'blockchain/vault.maths'
 import { Vault } from 'blockchain/vaults'
 import { useAppContext } from 'components/AppContextProvider'
 import { AppLink } from 'components/Links'
+import { VaultViewMode } from 'components/vault/GeneralManageTabBar'
 import { MultipleRangeSlider } from 'components/vault/MultipleRangeSlider'
 import { SidebarResetButton } from 'components/vault/sidebar/SidebarResetButton'
 import { SidebarFormInfo } from 'components/vault/SidebarFormInfo'
@@ -13,6 +14,7 @@ import { VaultWarnings } from 'components/vault/VaultWarnings'
 import { AddAutoSellInfoSection } from 'features/automation/basicBuySell/InfoSections/AddAutoSellInfoSection'
 import { MaxGasPriceSection } from 'features/automation/basicBuySell/MaxGasPriceSection/MaxGasPriceSection'
 import { BasicBSTriggerData } from 'features/automation/common/basicBSTriggerData'
+import { MIX_MAX_COL_RATIO_TRIGGER_OFFSET } from 'features/automation/common/consts'
 import { prepareBasicBSResetData } from 'features/automation/common/helpers'
 import {
   BASIC_SELL_FORM_CHANGE,
@@ -22,10 +24,15 @@ import { VaultErrorMessage } from 'features/form/errorMessagesHandler'
 import { VaultWarningMessage } from 'features/form/warningMessagesHandler'
 import { handleNumericInput } from 'helpers/input'
 import { useFeatureToggle } from 'helpers/useFeatureToggle'
+import { useHash } from 'helpers/useHash'
 import { one } from 'helpers/zero'
-import { useTranslation } from 'next-i18next'
+import { Trans, useTranslation } from 'next-i18next'
 import React from 'react'
 import { Text } from 'theme-ui'
+
+import { StopLossTriggerData } from '../../common/stopLossTriggerData'
+import { AUTOMATION_CHANGE_FEATURE } from '../../common/UITypes/AutomationFeatureChange'
+import { TAB_CHANGE_SUBJECT } from '../../common/UITypes/TabChange'
 
 interface AutoSellInfoSectionControlProps {
   vault: Vault
@@ -85,6 +92,7 @@ interface SidebarAutoSellAddEditingStageProps {
   collateralDelta: BigNumber
   sliderMin: BigNumber
   sliderMax: BigNumber
+  stopLossTriggerData: StopLossTriggerData
 }
 
 export function SidebarAutoSellAddEditingStage({
@@ -99,6 +107,7 @@ export function SidebarAutoSellAddEditingStage({
   collateralDelta,
   sliderMin,
   sliderMax,
+  stopLossTriggerData,
 }: SidebarAutoSellAddEditingStageProps) {
   const { uiChanges } = useAppContext()
   const { t } = useTranslation()
@@ -109,6 +118,65 @@ export function SidebarAutoSellAddEditingStage({
   })
   const readOnlyBasicBSEnabled = useFeatureToggle('ReadOnlyBasicBS')
   const isVaultEmpty = vault.debt.isZero()
+
+  const { isStopLossEnabled, stopLossLevel } = stopLossTriggerData
+
+  if (!isStopLossEnabled) {
+    adjustDefaultValuesIfOutsideSlider()
+  }
+
+  const isCurrentCollRatioHigherThanSliderMax = vault.collateralizationRatio
+    .times(100)
+    .gt(sliderMax)
+
+  if (isCurrentCollRatioHigherThanSliderMax) {
+    return (
+      <Trans
+        i18nKey="auto-sell.coll-ratio-too-high"
+        components={[
+          <Text
+            as="span"
+            sx={{ fontWeight: 'semiBold', color: 'interactive100', cursor: 'pointer' }}
+            onClick={() => {
+              uiChanges.publish(TAB_CHANGE_SUBJECT, {
+                type: 'change-tab',
+                currentMode: VaultViewMode.Overview,
+              })
+            }}
+          />,
+        ]}
+        values={{
+          maxAutoBuyCollRatio: sliderMax,
+        }}
+      />
+    )
+  }
+
+  const [, setHash] = useHash()
+
+  if (isStopLossEnabled && stopLossLevel.times(100).lt(sliderMin)) {
+    return (
+      <Trans
+        i18nKey="auto-sell.sl-too-high"
+        components={[
+          <Text
+            as="span"
+            sx={{ fontWeight: 'semiBold', color: 'interactive100', cursor: 'pointer' }}
+            onClick={() => {
+              uiChanges.publish(AUTOMATION_CHANGE_FEATURE, {
+                type: 'Protection',
+                currentProtectionFeature: 'stopLoss',
+              })
+              setHash(VaultViewMode.Protection)
+            }}
+          />,
+        ]}
+        values={{
+          maxStopLoss: sliderMax.minus(MIX_MAX_COL_RATIO_TRIGGER_OFFSET.times(2)),
+        }}
+      />
+    )
+  }
 
   if (readOnlyBasicBSEnabled && !isVaultEmpty) {
     return (
@@ -274,4 +342,19 @@ export function SidebarAutoSellAddEditingStage({
       )}
     </>
   )
+
+  function adjustDefaultValuesIfOutsideSlider() {
+    if (basicSellState.targetCollRatio.gt(sliderMax)) {
+      uiChanges.publish(BASIC_SELL_FORM_CHANGE, {
+        type: 'target-coll-ratio',
+        targetCollRatio: sliderMin,
+      })
+    }
+    if (basicSellState.execCollRatio.lt(sliderMin)) {
+      uiChanges.publish(BASIC_SELL_FORM_CHANGE, {
+        type: 'execution-coll-ratio',
+        execCollRatio: sliderMax,
+      })
+    }
+  }
 }
