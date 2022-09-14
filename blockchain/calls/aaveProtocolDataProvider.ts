49f52415d0561d91ccd3e0d8e6e10090017f9762
sennett <anthony@oazoapps.com>
2022-09-14 09:30:00 +0100

    lint and format

--- blockchain/calls/aaveProtocolDataProvider.ts
+++ blockchain/calls/aaveProtocolDataProvider.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from 'bignumber.js'
+
 import { AaveProtocolDataProvider } from '../../types/web3-v1-contracts/aave-protocol-data-provider'
 import { CallDef } from './callsHelpers'
 
