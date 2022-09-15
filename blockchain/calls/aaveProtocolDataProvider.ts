420e9b58ea6a4c02d319f00e6999328551096fa4
Marcin Ciarka <marcin@oazoapps.com>
2022-09-15 18:41:19 +0200

    Pulling AAVE reserve data

--- blockchain/calls/aaveProtocolDataProvider.ts
+++ blockchain/calls/aaveProtocolDataProvider.ts
@@ -1,13 +1,30 @@
-import { AaveProtocolDataProvider } from '../../types/web3-v1-contracts/aave-protocol-data-provider'
+import { AaveProtocolDataProvider } from 'types/web3-v1-contracts/aave-protocol-data-provider'
+
 import { CallDef } from './callsHelpers'
 
 export interface AaveUserReserveDataParameters {
   token: string
   proxyAddress: string
 }
+export interface AaveReserveDataParameters {
+  token: AaveUserReserveDataParameters['token']
+}
 
 export interface AaveUserReserveData {
   currentATokenBalance: string
+}
+
+export type AaveReserveDataReply = {
+  availableLiquidity: string
+  totalStableDebt: string
+  totalVariableDebt: string
+  liquidityRate: string
+  variableBorrowRate: string
+  stableBorrowRate: string
+  averageStableBorrowRate: string
+  liquidityIndex: string
+  variableBorrowIndex: string
+  lastUpdateTimestamp: string
 }
 
 export const getAaveUserReserveData: CallDef<AaveUserReserveDataParameters, AaveUserReserveData> = {
@@ -17,4 +34,10 @@ export const getAaveUserReserveData: CallDef<AaveUserReserveDataParameters, Aave
   prepareArgs: ({ token, proxyAddress }, context) => {
     return [context.tokens[token].address, proxyAddress]
   },
+}
+
+export const getAaveReserveData: CallDef<AaveReserveDataParameters, AaveReserveDataReply> = {
+  call: (_, { contract, aaveProtocolDataProvider }) =>
+    contract<AaveProtocolDataProvider>(aaveProtocolDataProvider).methods.getReserveData,
+  prepareArgs: ({ token }, context) => [context.tokens[token].address],
 }
