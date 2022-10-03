2626c6bab71176f007d5bd452319024fdb85caba
johnnieskywalker <lwolski0@gmail.com>
2022-10-03 16:31:40 +0200

    atp add trigger data wip

---
+++ features/automation/optimization/autoTakeProfit/state/autoTakeProfitTriggerData.ts
@@ -0,0 +1,55 @@
+import { CommandContractType, encodeTriggerDataByType, TriggerType } from '@oasisdex/automation'
+import BigNumber from 'bignumber.js'
+import { AutomationBaseTriggerData, AutomationBotAddTriggerData } from 'blockchain/calls/automationBot'
+import { TxMetaKind } from 'blockchain/calls/txMeta'
+import { Vault } from 'blockchain/vaults'
+
+export interface AutoTakeProfitTriggerData {
+  // uint256 cdpId;
+  // uint16 triggerType;
+  executionPrice: BigNumber
+  maxBaseFeeInGwei: BigNumber
+  isToCollateral: boolean
+}
+
+export function prepareAutoTakeProfitTriggerData({
+  vaultData,
+  executionPrice,
+  maxBaseFeeInGwei,
+  isCloseToCollateral,
+}: {
+  vaultData: Vault
+  executionPrice: BigNumber
+  maxBaseFeeInGwei: BigNumber
+  isCloseToCollateral: boolean
+}): AutomationBaseTriggerData {
+  const triggerType = isCloseToCollateral
+    ? TriggerType.AutoTakeProfitToCollateral
+    : TriggerType.AutoTakeProfitToDai
+  return {
+    cdpId: vaultData.id,
+    triggerType,
+    proxyAddress: vaultData.owner,
+    triggerData: encodeTriggerDataByType(CommandContractType.AutoTakeProfitCommand, [
+        vaultData.id.toString(),
+        triggerType.toString(),
+        executionPrice.toString(),
+        maxBaseFeeInGwei.toString(),
+        ]),
+  }
+}
+
+export function prepareAddAutoTakeProfitTriggerData(
+    vaultData: Vault,
+    executionPrice: BigNumber,
+    maxBaseFeeInGwei: BigNumber,
+    isCloseToCollateral: boolean,
+    replacedTriggerId: number,
+): AutomationBotAddTriggerData {
+    const baseTriggerData = prepareAutoTakeProfitTriggerData({ vaultData, executionPrice, maxBaseFeeInGwei, isCloseToCollateral })
+    return {
+        ...baseTriggerData,
+        replacedTriggerId,
+        kind: TxMetaKind.addTrigger,
+    }
+}
