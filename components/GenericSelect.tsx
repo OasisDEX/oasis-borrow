b88863b01e45655176db60ca180e386b8d689775
Piotr Konowrocki <piotr@oazoapps.com>
2022-10-07 14:39:07 +0200

    GenericSelect custom styles

--- components/GenericSelect.tsx
+++ components/GenericSelect.tsx
@@ -2,12 +2,21 @@ import { ExpandableArrow } from 'components/dumb/ExpandableArrow'
 import React, { useState } from 'react'
 import ReactSelect from 'react-select'
 import { theme } from 'theme'
-import { Box } from 'theme-ui'
+import { Box, SxProps } from 'theme-ui'
+
+import { styleFn, Styles } from 'react-select/src/styles'
+
+type ReactSelectCSSProperties = ReturnType<styleFn>
+type ReactSelectSimplifiedStyles = {
+  [key in keyof Styles]: (state: any) => ReactSelectCSSProperties
+}
+
 interface GenericSelectOption {
   label: string
   value: string
 }
 interface GenericSelectProps {
+  customStyles?: ReactSelectSimplifiedStyles
   defaultValue?: GenericSelectOption
   isDisabled?: boolean
   isSearchable?: boolean
@@ -15,9 +24,12 @@ interface GenericSelectProps {
   onChange?: (value: GenericSelectOption) => void
   options: GenericSelectOption[]
   placeholder?: string
+  expandableArrowSize?: number
+  expandableArrowSx?: SxProps
 }
 
 export function GenericSelect({
+  customStyles = {},
   defaultValue,
   isDisabled = false,
   isSearchable = false,
@@ -25,78 +37,96 @@ export function GenericSelect({
   onChange,
   options,
   placeholder,
+  expandableArrowSize = 12,
+  expandableArrowSx,
 }: GenericSelectProps) {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [value, setValue] = useState<GenericSelectOption | undefined>(defaultValue)
 
+  const defaultStyles: ReactSelectSimplifiedStyles = {
+    control: ({ isFocused }) => ({
+      height: '54px',
+      border: `1px solid ${isFocused ? theme.colors.primary100 : theme.colors.secondary100}`,
+      boxShadow: 'none',
+      borderRadius: theme.radii.medium,
+      cursor: 'pointer',
+      transition: 'border-color 200ms',
+      '&:hover': {
+        borderColor: isFocused ? theme.colors.primary100 : theme.colors.neutral70,
+      },
+    }),
+    valueContainer: () => ({
+      paddingRight: theme.space[3],
+      paddingLeft: theme.space[3],
+    }),
+    singleValue: () => ({
+      margin: 0,
+      fontSize: theme.fontSizes[2],
+      fontWeight: theme.fontWeights.semiBold,
+      color: theme.colors.primary100,
+    }),
+    menu: () => ({
+      marginTop: theme.space[1],
+      marginBottom: 0,
+      border: `1px solid ${theme.colors.secondary100}`,
+      borderRadius: theme.radii.large,
+      boxShadow: theme.shadows.buttonMenu,
+      opacity: isOpen ? 1 : 0,
+      transform: isOpen ? 'translateY(0)' : 'translateY(-5px)',
+      pointerEvents: isOpen ? 'auto' : 'none',
+      transition: 'opacity 200ms, transform 200ms',
+    }),
+    menuList: () => ({
+      paddingTop: '12px',
+      paddingBottom: '12px',
+    }),
+    option: ({ isSelected }) => ({
+      padding: '12px 16px',
+      fontSize: theme.fontSizes[3],
+      fontWeight: theme.fontWeights.regular,
+      color: theme.colors.primary100,
+      backgroundColor: isSelected ? theme.colors.neutral30 : 'transparent',
+      cursor: 'pointer',
+      transition: 'background-color 200ms',
+      '&:hover': {
+        backgroundColor: theme.colors.neutral30,
+      },
+    }),
+  }
+  const combinedStyles: Styles = [...Object.keys(defaultStyles), ...Object.keys(customStyles)]
+    .filter((item, i, arr) => arr.indexOf(item) === i)
+    .reduce(
+      (o, key) => ({
+        ...o,
+        [key]: (provided: ReactSelectCSSProperties, state: any) => {
+          const styleKey = key as keyof Styles
+
+          return {
+            ...provided,
+            ...(defaultStyles[styleKey] && defaultStyles[styleKey]!(state)),
+            ...(customStyles[styleKey] && customStyles[styleKey]!(state)),
+          }
+        },
+      }),
+      {},
+    )
+
   return (
-    <Box sx={{ position: 'relative' }}>
+    <Box sx={{ position: 'relative', zIndex: 3 }}>
       <ReactSelect
         blurInputOnSelect={true}
+        components={{ DropdownIndicator: null }}
         isDisabled={isDisabled}
         isSearchable={isSearchable}
         menuIsOpen={true}
         options={options}
+        styles={combinedStyles}
         value={value}
         onBlur={() => {
           setIsOpen(false)
         }}
         onFocus={() => {
           setIsOpen(true)
-        }}
-        //
-        components={{
-          DropdownIndicator: null,
-        }}
-        styles={{
-          control: (provided, { isFocused }) => ({
-            ...provided,
-            height: '54px',
-            border: `1px solid ${isFocused ? theme.colors.primary100 : theme.colors.secondary100}`,
-            boxShadow: 'none',
-            borderRadius: theme.radii.medium,
-            cursor: 'pointer',
-            transition: 'border-color 200ms',
-            '&:hover': {
-              borderColor: isFocused ? theme.colors.primary100 : theme.colors.neutral70,
-            },
-          }),
-          singleValue: (provided) => ({
-            ...provided,
-            fontSize: theme.fontSizes[2],
-            fontWeight: theme.fontWeights.semiBold,
-            color: theme.colors.primary100,
-          }),
-          menu: (provided) => ({
-            ...provided,
-            marginTop: theme.space[1],
-            marginBottom: 0,
-            border: `1px solid ${theme.colors.secondary100}`,
-            borderRadius: theme.radii.large,
-            boxShadow: theme.shadows.buttonMenu,
-            opacity: isOpen ? 1 : 0,
-            transform: isOpen ? 'translateY(0)' : 'translateY(-5px)',
-            pointerEvents: isOpen ? 'auto' : 'none',
-            transition: 'opacity 200ms, transform 200ms',
-          }),
-          menuList: (provided) => ({
-            ...provided,
-            paddingTop: '12px',
-            paddingBottom: '12px',
-          }),
-          option: (provided, { isSelected }) => ({
-            ...provided,
-            padding: '12px 16px',
-            fontSize: theme.fontSizes[3],
-            fontWeight: theme.fontWeights.regular,
-            color: theme.colors.primary100,
-            backgroundColor: isSelected ? theme.colors.neutral30 : 'transparent',
-            cursor: 'pointer',
-            transition: 'background-color 200ms',
-            '&:hover': {
-              backgroundColor: theme.colors.neutral30,
-            },
-          }),
         }}
         {...(name && { name })}
         {...(placeholder && { placeholder })}
@@ -109,6 +139,7 @@ export function GenericSelect({
         }}
       />
       <ExpandableArrow
+        size={expandableArrowSize}
         direction={isOpen ? 'up' : 'down'}
         sx={{
           position: 'absolute',
@@ -117,6 +148,7 @@ export function GenericSelect({
           bottom: 0,
           my: 'auto',
           pointerEvents: 'none',
+          ...expandableArrowSx,
         }}
       />
     </Box>
