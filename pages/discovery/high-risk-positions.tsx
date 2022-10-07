7cf12614abf7e09a288176f3722cf3268a14b411
Piotr Konowrocki <piotr@oazoapps.com>
2022-10-07 15:33:04 +0200

    remo example

--- pages/discovery/high-risk-positions.tsx
+++ pages/discovery/high-risk-positions.tsx
@@ -1,4 +1,3 @@
-import { GenericSelect } from 'components/GenericSelect'
 import { DiscoveryNavigation } from 'features/discovery/common/DiscoveryNavigation'
 import { DiscoveryWrapperWithIntro } from 'features/discovery/common/DiscoveryWrapperWithIntro'
 import {
@@ -11,17 +10,9 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 
 function DiscoveryPage() {
-  const options = [
-    { value: '>100k', label: 'Over $100' },
-    { value: '75k-100k', label: '$75,000 - $100,000' },
-    { value: '50k-75k', label: '$50,000 - $75,000' },
-    { value: '<50k', label: 'Below $50,000' },
-  ]
-
   return (
     <DiscoveryWrapperWithIntro>
       <DiscoveryNavigation active={DiscoveryPages.HIGH_RISK_POSITIONS} />
-      <GenericSelect options={options} defaultValue={options[0]} />
     </DiscoveryWrapperWithIntro>
   )
 }
