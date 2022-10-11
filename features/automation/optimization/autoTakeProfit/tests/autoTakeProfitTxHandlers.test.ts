b2fd33fe2bdd0697bc38f11cfda7158ed373791f
johnnieskywalker <lwolski0@gmail.com>
2022-10-11 15:59:30 +0200

    prepared some scenarios to be reviewed

---
+++ features/automation/optimization/autoTakeProfit/tests/autoTakeProfitTxHandlers.test.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+
+describe('getAutoTakeProfitTxHandlers', () => {
+  describe('given correct, default auto take profit data, state and vault', () => {
+    it('should return correct tx handler', () => {
+      expect(true).to.be.true // this is just to pass the linter check
+    })
+
+    it('should return correct button handler extension', () => {})
+  })
+  // At the moment, the test is not implemented nor exception is being thrown. It is just a placeholder.
+  describe('given invalid autoTakeProfitTriggerData', () => {
+    it('should throw an error', () => {})
+  })
+
+  describe('given invalid autoTakeProfitState', () => {
+    it('should throw an error', () => {})
+  })
+})
