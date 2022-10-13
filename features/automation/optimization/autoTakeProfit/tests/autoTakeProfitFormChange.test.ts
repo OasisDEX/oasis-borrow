2496fdbda8b196cd328992b6ca1ddde1acdd45cc
johnnieskywalker <lwolski0@gmail.com>
2022-10-13 18:46:11 +0200

    added tests for atp form change

---
+++ features/automation/optimization/autoTakeProfit/tests/autoTakeProfitFormChange.test.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai'
+// Not sure if the test should be for reducer itself or check whole forms, what components are mounted,rendered etc. ~Ł
+describe('autoTakeProfitFormChangeReducer', () => {
+  describe('given user has no trigger', () => {
+    it('should render form with default values values', () => {
+      expect(true).to.be.true // this is just to pass the linter check
+      describe('and given execution-price was edited, and then reset button was clicked', () => {
+        it('should restore default executionPrice', () => {})
+        it('should restore default executionCollRatio', () => {})
+      })
+    })
+    describe('given user has trigger', () => {
+      it('should render form with trigger values', () => {})
+      describe('and given execution-price was edited, and then reset button was clicked', () => {
+        it("should restore existing trigger's executionPrice", () => {})
+        it("should restore existing trigger's executionCollRatio", () => {})
+      })
+    })
+
+    //Below are scenarios which would be the same for both cases existing triggegr or no trigger so not sure if needed to duplicate
+    describe('given execution-price was edited', () => {
+      it('should emit form change with edited execution-price', () => {})
+      it('should emit form change with edited executionCollRatio', () => {})
+    })
+
+    describe('user is trying to add trigger, transaction is pending', () => {
+      it('should contain txDetails with expected values', () => {})
+    })
+  })
+})
