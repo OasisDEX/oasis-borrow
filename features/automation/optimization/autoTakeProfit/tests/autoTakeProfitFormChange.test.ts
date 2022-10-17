b4438d77dc15e949e01a46174ae37972e26c3b9b
johnnieskywalker <lwolski0@gmail.com>
2022-10-17 14:17:56 +0200

    wip mocked uiChanges, can't make assertions this way as this is a hook, todo try using @testing-library/react

--- features/automation/optimization/autoTakeProfit/tests/autoTakeProfitFormChange.test.ts
+++ features/automation/optimization/autoTakeProfit/tests/autoTakeProfitFormChange.test.ts
@@ -1,9 +1,14 @@
 import BigNumber from 'bignumber.js'
 import { expect } from 'chai'
 import { initializeUIChanges } from 'components/AppContext'
+import {
+  AUTO_TAKE_PROFIT_FORM_CHANGE,
+  AutoTakeProfitFormChange,
+} from 'features/automation/optimization/autoTakeProfit/state/autoTakeProfitFormChange'
 import { defaultAutoTakeProfitData } from 'features/automation/optimization/autoTakeProfit/state/autoTakeProfitTriggerData'
 import { useAutoTakeProfitStateInitializator } from 'features/automation/optimization/autoTakeProfit/state/useAutoTakeProfitStateInitializator'
 import { mockVaults } from 'helpers/mocks/vaults.mock'
+import { useUIChanges } from 'helpers/uiChangesHook'
 // Not sure if the test should be for reducer itself or check whole forms, what components are mounted,rendered etc. ~Ł
 const hundredThousand = new BigNumber('100000')
 const fiftyMillion = new BigNumber('50000000')
@@ -17,22 +22,30 @@ const mockAutoTakeProfitTriggerDataNoTrigger = defaultAutoTakeProfitData
 const isAutoTakeProfitEnabled = useAutoTakeProfitStateInitializator(
   mockVault(),
   mockAutoTakeProfitTriggerDataNoTrigger,
-  mockUiChanges
+  mockUiChanges,
 )
 // prapare mock AutoTakeProfitTriggerData
 // mock return of useAppContext to return mock uiChanges?
 // then call useAutoTakeProfitStateInitializator
 describe('autoTakeProfitFormChangeReducer', () => {
   before(() => {})
-  beforeEach(() => {
-
-  })
+  beforeEach(() => {})
 
   describe('given user has no trigger', () => {
     // This scenario seems feature envy with tests for AutoTakeProfitFormControl ~Ł
-    it('should prepare default form values', () => {
+    // eslint-disable-next-line no-only-tests/no-only-tests
+    it.only('should prepare default form values', () => {
+      const [autoTakeProfitState] = useUIChanges<AutoTakeProfitFormChange>(
+        AUTO_TAKE_PROFIT_FORM_CHANGE,
+      )
+
       // here just assert that state contains correct default
-      expect(true).to.be.true // this is just to pass the linter check
+      expect(autoTakeProfitState.currentForm).to.equal('add')
+      expect(autoTakeProfitState.toCollateral).to.equal(defaultAutoTakeProfitData.isToCollateral)
+      expect(autoTakeProfitState.executionPrice).to.equal(defaultAutoTakeProfitData.executionPrice)
+      // expect(autoTakeProfitState.executionCollRatio).to.equal(defaultAutoTakeProfitData.)
+      expect(autoTakeProfitState.isEditing).to.equal(false)
+      expect(isAutoTakeProfitEnabled).to.equal(false)
       describe('and given execution-price was edited, and then reset button was clicked', () => {
         // here just call function that modifies state, check if it was modified, then call function used to reset, see if it worked
         it('should restore default executionPrice', () => {})
