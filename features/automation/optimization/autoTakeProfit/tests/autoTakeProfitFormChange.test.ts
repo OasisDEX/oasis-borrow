845092d91ccd20678d416e24ed1538434521c52e
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-10-17 10:18:54 +0200

    Sketch of ATP reducer test file

---
+++ features/automation/optimization/autoTakeProfit/tests/autoTakeProfitFormChange.test.ts
@@ -0,0 +1,10 @@
+describe('autoTakeProfitFormChangeReducer', () => {
+  it('should handle publish of close-type', () => {})
+  it('should handle publish of execution-price', () => {})
+  it('should handle publish of current-form', () => {})
+  it('should handle publish of form-defaults', () => {})
+  it('should handle publish of reset', () => {})
+  it('should handle publish of is-editing', () => {})
+  it('should handle publish of tx-details', () => {})
+  it('should handle invalid action type', () => {})
+})
