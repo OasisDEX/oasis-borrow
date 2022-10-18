b0336c172d35cf49aafce1db2f469062a7796f19
Piotr Konowrocki <piotr@oazoapps.com>
2022-10-18 12:27:14 +0200

    getRowKey for keys generation (#1580)

--- features/discovery/helpers.ts
+++ features/discovery/helpers.ts
@@ -1,5 +1,5 @@
 import { DiscoveryFiltersList } from 'features/discovery/meta'
-import { DiscoveryPages } from 'features/discovery/types'
+import { DiscoveryPages, DiscoveryTableRowData } from 'features/discovery/types'
 
 export const DISCOVERY_URL = '/discovery'
 
@@ -14,4 +14,8 @@ export function getDefaultSettingsState({
     ...Object.keys(filters).reduce((o, key) => ({ ...o, [key]: filters[key][0].value }), {}),
     table: kind,
   }
+}
+
+export function getRowKey(i: number, row: DiscoveryTableRowData) {
+  return [...(row.asset ? [row.asset] : []), ...(row.cdpId ? [row.cdpId] : []), i].join('-')
 }
