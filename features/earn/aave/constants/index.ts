9ba874c29de9c45a8e43bac2dcd756abbd9a07ec
Marcin Ciarka <marcin@oazoapps.com>
2022-10-24 10:56:27 +0200

    Set the default multiple for the simulation on open to 2x (2.01 to be precise, since the rounding shows 1.99 when set to 2.00)

--- features/earn/aave/constants/index.ts
+++ features/earn/aave/constants/index.ts
@@ -2,4 +2,4 @@ import { RiskRatio } from '@oasisdex/oasis-actions'
 import BigNumber from 'bignumber.js'
 
 export const aaveStrategiesList = ['stETHeth']
-export const aaveStETHMinimumRiskRatio = new RiskRatio(new BigNumber(1.1), RiskRatio.TYPE.MULITPLE)
+export const aaveStETHMinimumRiskRatio = new RiskRatio(new BigNumber(2.01), RiskRatio.TYPE.MULITPLE)
