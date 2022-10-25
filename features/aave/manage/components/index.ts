4c9b1beacee5a98e5af49531887583a27b43015b
sennett <anthony@oazoapps.com>
2022-10-25 11:25:40 -0500

    little cleanup

--- features/aave/manage/components/index.ts
+++ features/aave/manage/components/index.ts
@@ -1,3 +1,2 @@
 export * from '../../../earn/aave/components/ManageSectionComponent'
 export * from './ManageSectionModal'
-export { ManageSectionComponentProps } from '../../common/StrategyConfigTypes'
