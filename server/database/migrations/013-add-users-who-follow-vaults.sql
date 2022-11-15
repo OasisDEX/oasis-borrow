0e1f6dda21cb9b233c30db5aa6167e8b4bb40561
johnnieskywalker <lwolski0@gmail.com>
2022-11-15 13:00:38 +0100

    code review changes

--- server/database/migrations/013-add-users-who-follow-vaults.sql
+++ server/database/migrations/013-add-users-who-follow-vaults.sql
@@ -2,25 +2,16 @@ CREATE TABLE
     "users_who_follow_vaults" (
         "user_address" TEXT NOT NULL,
         "vault_id" INTEGER NOT NULL,
-        "vault_type" "vault_type" NOT NULL,
         "vault_chain_id" INTEGER NOT NULL,
-        CONSTRAINT "users_who_follow_vaults_pkey" PRIMARY KEY (
-            "user_address",
-            "vault_id",
-            "vault_type",
-            "vault_chain_id"
-        )
+        CONSTRAINT "users_who_follow_vaults_pkey" PRIMARY KEY ("user_address", "vault_id", "vault_chain_id")
     );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "vault_vault_id_chain_id_key" ON "vault" ("vault_id", "chain_id");
 
--- CreateIndex
-CREATE UNIQUE INDEX "vault_vault_id_type_chain_id_key" ON "vault" ("vault_id", "type", "chain_id");
-
 -- AddForeignKey
 ALTER TABLE
-    "users_who_follow_vaults" ADD CONSTRAINT "users_who_follow_vaults_vault_id_vault_type_vault_chain_id_fkey" FOREIGN KEY ("vault_id", "vault_type", "vault_chain_id") REFERENCES "vault" ("vault_id", "type", "chain_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+    "users_who_follow_vaults" ADD CONSTRAINT "users_who_follow_vaults_vault_id_vault_chain_id_fkey" FOREIGN KEY ("vault_id", "vault_chain_id") REFERENCES "vault" ("vault_id", "chain_id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE
