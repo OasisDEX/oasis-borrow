86532395c6fe985985686dbe01d05f3a8e797c8c
johnnieskywalker <lwolski0@gmail.com>
2022-11-15 15:26:21 +0100

    used tos approval instead of user in users who follow vaults

--- server/database/migrations/013-add-users-who-follow-vaults.sql
+++ server/database/migrations/013-add-users-who-follow-vaults.sql
@@ -7,12 +7,12 @@ CREATE TABLE
     );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "vault_vault_id_chain_id_key" ON "vault" ("vault_id", "chain_id");
+CREATE UNIQUE INDEX "tos_approval_address_chain_id_key" ON "tos_approval" ("address", "chain_id");
+
+-- AddForeignKey
+ALTER TABLE
+    "users_who_follow_vaults" ADD CONSTRAINT "users_who_follow_vaults_user_address_vault_chain_id_fkey" FOREIGN KEY ("user_address", "vault_chain_id") REFERENCES "tos_approval" ("address", "chain_id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE
     "users_who_follow_vaults" ADD CONSTRAINT "users_who_follow_vaults_vault_id_vault_chain_id_fkey" FOREIGN KEY ("vault_id", "vault_chain_id") REFERENCES "vault" ("vault_id", "chain_id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE
-    "users_who_follow_vaults" ADD CONSTRAINT "users_who_follow_vaults_user_address_fkey" FOREIGN KEY ("user_address") REFERENCES "user" ("address") ON DELETE RESTRICT ON UPDATE CASCADE;
