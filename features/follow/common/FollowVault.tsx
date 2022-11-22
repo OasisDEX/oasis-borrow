14382d251e50ee9eb9c3c4993c52b4541d6bdfb8
johnnieskywalker <lwolski0@gmail.com>
2022-11-22 10:50:02 +0100

    added empty star

--- features/follow/common/FollowVault.tsx
+++ features/follow/common/FollowVault.tsx
@@ -5,9 +5,9 @@ export function FollowVault() {
   return (
     <Box>
          <Icon
-                name="star"
+                name="star_empty"
                 size="15px"
-                sx={{ position: 'relative', left: '6px', transition: '0.2s' }}
+                sx={{ position: 'relative', left: '12px', top: '7px', width:'14px', height: '14px', transition: '0.2s' }}
               />
         <Button variant="menuButton">Follow</Button>
     </Box>
