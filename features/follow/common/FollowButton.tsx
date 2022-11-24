df267e6f8ebf7a3f3e022707b14c436bfed63238
johnnieskywalker <lwolski0@gmail.com>
2022-11-24 11:50:48 +0100

    css fix

--- features/follow/common/FollowButton.tsx
+++ features/follow/common/FollowButton.tsx
@@ -13,7 +13,19 @@ export function FollowButton(props: FollowButtonProps) {
   const { t } = useTranslation()
 
   return (
-    <Button variant="menuButton" disabled={props.isProcessing} onClick={props.buttonClickHandler}>
+    <Button
+      variant="menuButton"
+      disabled={props.isProcessing}
+      onClick={props.buttonClickHandler}
+      sx={{
+        padding: '4px, 12px, 4px, 12px',
+        border: '1px solid #EAEAEA',
+        borderRadius: '16px',
+        '&:hover': { border: '1px solid #25273D', color: '#25273D', background: 'white' },
+        color: '#626472',
+        boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.2)',
+      }}
+    >
       <Flex sx={{ justifyContent: 'space-between', gap: '8px', alignItems: 'center' }}>
         {props.isProcessing ? (
           <Spinner size="10px" />
--- features/homepage/HomepageView.tsx
+++ features/homepage/HomepageView.tsx
@@ -500,6 +500,7 @@ export function Hero({ sx, isConnected }: { sx?: SxStyleProp; isConnected: boole
           }}
         />
       </AppLink>
+      {/* <FollowButtonControl/> */}
     </Flex>
   )
 }
