60977496fea9736c6d8fc2f472f65bc7fa469eb4
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-11-30 15:43:48 +0100

    typo

--- blockchain/userDpmProxies.ts
+++ blockchain/userDpmProxies.ts
@@ -69,7 +69,7 @@ export function getUserDpmProxies$(
         ),
       )
 
-      const proxiesNotOwnerAnymore = userAssumedProxiesTransferredEvents
+      const proxiesNotOwnedAnymore = userAssumedProxiesTransferredEvents
         .flatMap((x) => x[x.length - 1])
         .filter((x) => x)
         .flatMap((event) => event)
@@ -78,7 +78,7 @@ export function getUserDpmProxies$(
         )
         .map((event) => event.returnValues.proxy)
 
-      return [...new Set(userAssumedProxies.filter((x) => !proxiesNotOwnerAnymore.includes(x)))]
+      return [...new Set(userAssumedProxies.filter((x) => !proxiesNotOwnedAnymore.includes(x)))]
     }),
   )
 }
