4c4d9072ae8838758853e7bece0db82f1ad64cf5
Piotr Konowrocki <piotr@oazoapps.com>
2022-12-01 13:02:19 +0100

    Feature toggles available outside components (#1754)
    * feature toggles available outside components
    
    * guard
    
    Co-authored-by: Marcin Ciarka <marcin@oazoapps.com>

--- helpers/useFeatureToggle.ts
+++ helpers/useFeatureToggle.ts
@@ -89,7 +89,15 @@ export function loadFeatureToggles(testFeaturesFlaggedEnabled: Array<Feature> = 
   }
 }
 
+export function getFeatureToggle(feature: Feature): boolean {
+  if (typeof localStorage !== 'undefined') {
+    const userEnabledFeatures = localStorage.getItem(FT_LOCAL_STORAGE_KEY)
+
+    return JSON.parse(userEnabledFeatures || '{}')[feature] || configuredFeatures[feature]
+  }
+  return false
+}
+
 export function useFeatureToggle(feature: Feature): boolean {
-  const userEnabledFeatures = localStorage.getItem(FT_LOCAL_STORAGE_KEY)
-  return JSON.parse(userEnabledFeatures || '{}')[feature] || configuredFeatures[feature]
+  return getFeatureToggle(feature)
 }
