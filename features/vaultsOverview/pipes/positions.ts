37ec7cfb67e96949c3ad6a9e124e8035c05b9d95
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-12-05 09:55:09 +0100

    removed liquidity

--- features/vaultsOverview/pipes/positions.ts
+++ features/vaultsOverview/pipes/positions.ts
@@ -96,7 +96,6 @@ export type AavePosition = Position & {
 
 export type AaveDpmPosition = Position & {
   netValue: BigNumber
-  liquidity: BigNumber
   liquidationPrice: BigNumber
   fundingCost: BigNumber
   lockedCollateral: BigNumber
