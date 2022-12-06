2188a6871dc279668a28a0a32f5c5bae73417911
Piotr Konowrocki <piotr@oazoapps.com>
2022-12-06 13:27:24 +0100

    example of not overwritten theme props

--- pages/ajna/index.tsx
+++ pages/ajna/index.tsx
@@ -6,7 +6,12 @@ function AjnaPage() {
   return (
     <WithFeatureToggleRedirect feature="Ajna">
       <Box sx={{ width: '100%' }}>
-        <Text sx={{ backgroundColor: 'interactive100' }}>Text component with background set to interactive100</Text>
+        <Text sx={{ backgroundColor: 'interactive100' }}>
+          Text component with background set to interactive100
+        </Text>
+        <Text sx={{ backgroundColor: 'interactive50' }}>
+          Text component with background set to interactive50
+        </Text>
         <ThemeProvider
           theme={{
             colors: {
@@ -14,7 +19,12 @@ function AjnaPage() {
             },
           }}
         >
-          <Text sx={{ backgroundColor: 'interactive100' }}>Text component with background set to interactive100, with overwriting ThemeProvider</Text>
+          <Text sx={{ backgroundColor: 'interactive100' }}>
+            Text component with background set to interactive100, with overwriting ThemeProvider
+          </Text>
+          <Text sx={{ backgroundColor: 'interactive50' }}>
+            Text component with background set to interactive100, with overwriting ThemeProvider
+          </Text>
         </ThemeProvider>
       </Box>
     </WithFeatureToggleRedirect>
