6b8470fca3329e03c45c586977ca6dfef33f68c2
halaprix <panicz.klos@gmail.com>
2022-12-08 14:02:38 +0100

    remove console.log

--- pages/api/rpc.tsx
+++ pages/api/rpc.tsx
@@ -257,7 +257,6 @@ export async function rpc(req: NextApiRequest, res: NextApiResponse) {
       console.log(errStack)
 
       counters.bypassedPayloadSize += JSON.stringify(req.body).length
-      console.log(JSON.stringify(req.body))
       console.log('RPC call failed, falling back to individual calls')
       finalResponse = await makeCall(req.query.network.toString(), req.body)
       console.log('RPC call failed fallback successful')
