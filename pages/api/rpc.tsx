9308e83c9fc4e3b1d1a2edf412d1247931044d82
Adam Skrodzki <adam@oazoapps.com>
2022-12-09 11:13:23 +0100

    refactor

--- pages/api/rpc.tsx
+++ pages/api/rpc.tsx
@@ -142,7 +142,6 @@ export async function rpc(req: NextApiRequest, res: NextApiResponse) {
   const network = req.query.network.toString()
   const clientId = req.query.clientId.toString()
   //withCache = req.query.withCache.toString() === "true"
-  withCache = false;
 
   const rpcNode = getRpcNode(network)
   const provider = new ethers.providers.JsonRpcProvider(rpcNode)
