9c6504027e90c66dfc41daec368f54a0f01955ab
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-12-11 23:01:02 +0100

    added dsr card redirection

--- components/productCards/ProductCardEarnDsr.tsx
+++ components/productCards/ProductCardEarnDsr.tsx
@@ -1,16 +1,30 @@
-
+import { useAppContext } from 'components/AppContextProvider'
+import { redirectState$ } from 'features/router/redirectState'
+import { useObservable } from 'helpers/observableHook'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
 
 import { ProductCard, ProductCardProtocolLink } from './ProductCard'
 
+export const dsrLink = '/earn/dsr'
+
 export function ProductCardEarnDsr() {
   const { t } = useTranslation()
+  const { connectedContext$ } = useAppContext()
+  const [connectedContext] = useObservable(connectedContext$)
+
+  function handleClick() {
+    if (!connectedContext) {
+      redirectState$.next(dsrLink)
+    }
+  }
+
+  const link = connectedContext ? `${dsrLink}/${connectedContext.account}` : '/connect'
 
   return (
     <ProductCard
-      tokenImage='/static/img/tokens/maker_dai.png'
-      tokenGif='/static/img/tokens/maker_dai.gif'
+      tokenImage="/static/img/tokens/maker_dai.png"
+      tokenGif="/static/img/tokens/maker_dai.gif"
       title={t(`DAI Savings Rate`)}
       description={t(`dsr.product-card.description`)}
       banner={{
@@ -20,27 +34,25 @@ export function ProductCardEarnDsr() {
       labels={[
         {
           title: 'Current APY',
-          value: '1.00%'
+          value: '1.00%',
         },
         {
           title: 'Total Value Locked',
-          value: '125.04m'
+          value: '125.04m',
         },
         {
           title: t('system.protocol'),
-          value: (
-            <ProductCardProtocolLink ilk={'DAI'} protocol='maker' />
-          ),
+          value: <ProductCardProtocolLink ilk={'DAI'} protocol="maker" />,
         },
       ]}
-      
       button={{
         // TODO: Add the correct link here. Needs to be /earn/dsr/{walletAddress}
-        link: '',
+        link,
         text: t('nav.earn'),
+        onClick: handleClick,
       }}
-      background='#E0F9F0'
-      protocol='maker'
+      background="#E0F9F0"
+      protocol="maker"
       isFull={false}
     />
   )
