4334e56f8651a487dc4993982b69c346b0086685
johnnieskywalker <lwolski0@gmail.com>
2022-12-12 14:48:47 +0100

    proper query to load followed vaults

--- handlers/follow/follow.ts
+++ handlers/follow/follow.ts
@@ -4,15 +4,9 @@ export async function selectVaultsFollowedByAddress(
   prisma: PrismaClient,
   { address }: { address: string },
 ): Promise<UsersWhoFollowVaults[] | undefined> {
-  // TODO why is wrong with this query
-  // const results = await prisma.usersWhoFollowVaults.findMany({
-  //   where: { user_address: address },
-  // })
-
   const results = await prisma.usersWhoFollowVaults.findMany({
     where: { user_address: { equals: address.toLocaleLowerCase() } },
   })
 
-  console.log('results', results)
   return results
 }
--- handlers/follow/get.ts
+++ handlers/follow/get.ts
@@ -5,26 +5,19 @@ import { prisma } from 'server/prisma'
 import * as z from 'zod'
 
 const paramsSchema = z.object({
-  address: z.string()
+  address: z.string(),
 })
 
 export async function get(req: NextApiRequest, res: NextApiResponse) {
-    console.log('paramsSchema.parse(req.query)')
-    console.log(paramsSchema.parse(req.query))
   const { address } = paramsSchema.parse(req.query)
-  console.log('address',address)
   const followedVaults = await selectVaultsFollowedByAddress(prisma, {
     address,
   })
-  console.log('followedVaults', followedVaults)
 
   if (followedVaults === undefined || !followedVaults.length) {
     console.log(404)
     return res.status(404).send('Not Found')
   } else {
-    console.log(200)
-    console.log('...followedVaults')
-    console.log(...followedVaults)
     return res.status(200).json({
       ...followedVaults,
     })
