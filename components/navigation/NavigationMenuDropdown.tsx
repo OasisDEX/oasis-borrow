304567309d0a364454334ac23c60050e2ba26992
Piotr Konowrocki <piotr@oazoapps.com>
2022-12-15 17:51:03 +0100

    fix animation transitions and positioning

--- components/navigation/NavigationMenuDropdown.tsx
+++ components/navigation/NavigationMenuDropdown.tsx
@@ -10,10 +10,20 @@ import { Box, Flex, Grid, Heading, Text } from 'theme-ui'
 
 export interface NavigationMenuDropdownProps {
   arrowPosition: number
-  currentPanel?: string
+  currentPanel: string
   isPanelOpen: boolean
   isPanelSwitched: boolean
   panels: NavigationMenuPanelType[]
+}
+
+function getDropdownTranslation(
+  labelsMap: string[],
+  activePanel: string,
+  currentPanel: string,
+): string {
+  if (activePanel === currentPanel) return '0'
+  else if (labelsMap.indexOf(currentPanel) > labelsMap.indexOf(activePanel)) return '-20%'
+  else return '20%'
 }
 
 export function NavigationMenuDropdown({
@@ -27,12 +37,14 @@ export function NavigationMenuDropdown({
   const [width, setWidth] = useState<number>(0)
   const [height, setHeight] = useState<number>(0)
 
+  const labelsMap = panels.map((panel) => panel.label)
+
   useEffect(() => {
     if (ref.current) {
       setWidth(ref.current.offsetWidth)
       setHeight(ref.current.offsetHeight)
     }
-  }, [currentPanel])
+  }, [currentPanel, isPanelOpen])
 
   return (
     <Flex
@@ -47,7 +59,7 @@ export function NavigationMenuDropdown({
           position: 'absolute',
           top: '100%',
           left: '-7px',
-          mt: '10px',
+          mt: '-6px',
           transform: isPanelOpen ? 'translateY(0)' : 'translateY(20px)',
           transition: 'transform 200ms',
           zIndex: 2,
@@ -68,124 +80,164 @@ export function NavigationMenuDropdown({
         sx={{
           position: 'absolute',
           top: '100%',
-          left: '-500px',
-          right: '-500px',
+          left: '-100%',
+          right: '-100%',
           justifyContent: 'center',
-          pt: 3,
           transform: isPanelOpen ? 'translateY(0)' : 'translateY(-5px)',
           transition: 'transform 200ms',
+          pointerEvents: 'none',
         }}
       >
         <Flex
           sx={{
+            position: 'relative',
+            p: 4,
             borderRadius: 'large',
             bg: 'neutral10',
             boxShadow: 'buttonMenu',
-            height: '600px',
-            ...(width > 0 && { width: width + 64 }),
-            ...(height > 0 && { height: height + 64 }),
-            p: 4,
-            ...(isPanelSwitched && { transition: 'width 200ms, height 200ms' }),
+            pointerEvents: 'auto',
+            overflow: 'hidden',
           }}
         >
-          {panels.map(({ description, label, learn, links, otherAssets }, i) => (
-            <Flex
-              key={i}
-              sx={{
-                position: 'absolute',
-                flexDirection: 'column',
-                rowGap: 4,
-                ...(isPanelSwitched && { transition: 'opacity 200ms' }),
-                ...(currentPanel === label
-                  ? { opacity: 1, pointerEvents: 'auto' }
-                  : { opacity: 0, pointerEvents: 'none' }),
-              }}
-              {...(currentPanel === label && { ref })}
-            >
-              <Box>
-                <Heading
+          <Flex
+            sx={{
+              flexDirection: 'column',
+              ...(width > 0 && { width }),
+              ...(height > 0 && { height }),
+              ...(isPanelSwitched && { transition: 'width 200ms, height 200ms' }),
+            }}
+          >
+            {panels.map(({ description, label, learn, links, otherAssets }, i) => (
+              <Flex sx={{ ml: '-100%', transform: 'translateX(50%)' }}>
+                <Flex
+                  key={i}
                   sx={{
-                    fontSize: '28px',
-                    fontWeight: 'bold',
-                    mb: 1,
-                    color: 'primary100',
-                    letterSpacing: '-0.02em',
+                    position: 'absolute',
+                    flexDirection: 'column',
+                    rowGap: 4,
+                    // ml: '100%',
+                    ...(isPanelSwitched && { transition: 'opacity 200ms, transform 200ms' }),
+                    ...(currentPanel === label
+                      ? { opacity: 1, pointerEvents: 'auto' }
+                      : { opacity: 0, pointerEvents: 'none' }),
+                    transform: `translateX(${getDropdownTranslation(
+                      labelsMap,
+                      label,
+                      currentPanel,
+                    )})`,
                   }}
+                  {...(currentPanel === label && { ref })}
                 >
-                  {label}
-                </Heading>
-                <Text as="p" sx={{ fontSize: '14px', color: 'neutral80' }}>
-                  {description}{' '}
-                  {learn && (
-                    <AppLink href={learn.link}>
-                      <WithArrow
-                        gap={1}
-                        sx={{ display: 'inline-block', ...getAjnaWithArrowColorScheme() }}
-                      >
-                        {learn.label}
-                      </WithArrow>
-                    </AppLink>
-                  )}
-                </Text>
-              </Box>
-              <Grid
-                as="ul"
-                sx={{
-                  gap: 4,
-                  gridTemplateColumns: 'repeat(2, 1fr)',
-                  p: '0',
-                  listStyle: 'none',
-                }}
-              >
-                {links.map(({ icon, footnote, link, title }, i) => (
-                  <Flex key={i} as="li">
-                    <AppLink
-                      href={link}
-                      sx={{ display: 'flex', alignItems: 'center', fontWeight: 'regular' }}
+                  <Box>
+                    <Heading
+                      sx={{
+                        fontSize: '28px',
+                        fontWeight: 'bold',
+                        mb: 1,
+                        color: 'primary100',
+                        letterSpacing: '-0.02em',
+                      }}
                     >
-                      <Icon size={48} name={icon} sx={{ flexShrink: 0 }} />
-                      <Flex sx={{ flexDirection: 'column', ml: 3 }}>
-                        <Text
-                          as="span"
-                          sx={{ fontSize: 4, fontWeight: 'semiBold', color: 'primary100' }}
-                        >
-                          {title}
-                        </Text>
-                        <Text as="span" sx={{ fontSize: 2, color: 'neutral80' }}>
-                          {footnote}
-                        </Text>
-                      </Flex>
-                    </AppLink>
-                  </Flex>
-                ))}
-              </Grid>
-              {otherAssets && otherAssets?.length > 0 && (
-                <Box sx={{ pt: '24px', borderTop: '1px solid', borderColor: 'neutral20' }}>
-                  <Heading
-                    as="h3"
+                      {label}
+                    </Heading>
+                    <Flex sx={{ columnGap: 1 }}>
+                      <Text
+                        as="p"
+                        sx={{ fontSize: '14px', color: 'neutral80', lineHeight: '22px' }}
+                      >
+                        {description}
+                      </Text>
+                      {learn && (
+                        <AppLink href={learn.link}>
+                          <WithArrow
+                            gap={1}
+                            sx={{ display: 'inline-block', ...getAjnaWithArrowColorScheme() }}
+                          >
+                            {learn.label}
+                          </WithArrow>
+                        </AppLink>
+                      )}
+                    </Flex>
+                  </Box>
+                  <Grid
+                    as="ul"
                     sx={{
-                      fontSize: '16px',
-                      fontWeight: 'semiBold',
-                      mb: 3,
-                      color: 'primary100',
+                      gap: 4,
+                      gridTemplateColumns: 'repeat( 2, 1fr )',
+                      p: '0',
+                      listStyle: 'none',
                     }}
                   >
-                    Other assets you can borrow against
-                  </Heading>
-                  <Flex
-                    as="ul"
-                    sx={{ flexWrap: 'wrap', columnGap: 3, rowGap: 2, listStyle: 'none', p: 0 }}
-                  >
-                    {otherAssets.map(({ link, token }, i) => (
-                      <Box key={i} as="li">
-                        <AssetPill icon={getToken(token).iconCircle} label={token} link={link} />
-                      </Box>
+                    {links.map(({ icon, footnote, link, title }, i) => (
+                      <Flex key={i} as="li" sx={{ flexGrow: 1, flexBasis: 0 }}>
+                        <AppLink
+                          href={link}
+                          sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            fontWeight: 'regular',
+                            '&:hover': {
+                              '.dropdownLinkTitle': {
+                                color: 'neutral80',
+                              },
+                            },
+                          }}
+                        >
+                          <Icon size={48} name={icon} sx={{ flexShrink: 0 }} />
+                          <Flex sx={{ flexDirection: 'column', ml: 3 }}>
+                            <Text
+                              as="p"
+                              className="dropdownLinkTitle"
+                              sx={{
+                                fontSize: 4,
+                                fontWeight: 'semiBold',
+                                color: 'primary100',
+                                transition: 'color 200ms',
+                              }}
+                            >
+                              {title}
+                            </Text>
+                            <Text
+                              as="p"
+                              sx={{ fontSize: 2, color: 'neutral80', whiteSpace: 'nowrap' }}
+                            >
+                              {footnote}
+                            </Text>
+                          </Flex>
+                        </AppLink>
+                      </Flex>
                     ))}
-                  </Flex>
-                </Box>
-              )}
-            </Flex>
-          ))}
+                  </Grid>
+                  {otherAssets && otherAssets?.length > 0 && (
+                    <Box sx={{ pt: '24px', borderTop: '1px solid', borderColor: 'neutral20' }}>
+                      <Heading
+                        as="h3"
+                        sx={{
+                          fontSize: '16px',
+                          fontWeight: 'semiBold',
+                          mb: 3,
+                          color: 'primary100',
+                        }}
+                      >
+                        Other assets you can borrow against
+                      </Heading>
+                      <Flex as="ul" sx={{ columnGap: 3, rowGap: 2, listStyle: 'none', p: 0 }}>
+                        {otherAssets.map(({ link, token }, i) => (
+                          <Box key={i} as="li">
+                            <AssetPill
+                              icon={getToken(token).iconCircle}
+                              label={token}
+                              link={link}
+                            />
+                          </Box>
+                        ))}
+                      </Flex>
+                    </Box>
+                  )}
+                </Flex>
+              </Flex>
+            ))}
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
