62e77e17b859dd3b1bc3b1c8b98417cd2557999d
Piotr Konowrocki <piotr@oazoapps.com>
2022-12-15 18:23:57 +0100

    split into smaller components

--- components/navigation/NavigationMenuDropdown.tsx
+++ components/navigation/NavigationMenuDropdown.tsx
@@ -1,12 +1,8 @@
-import { Icon } from '@makerdao/dai-ui-icons'
-import { getToken } from 'blockchain/tokensMetadata'
-import { AssetPill } from 'components/AssetPill'
-import { AppLink } from 'components/Links'
+import { NavigationMenuDropdownContent } from 'components/navigation/NavigationMenuDropdownContent'
 import { NavigationMenuPanelType } from 'components/navigation/NavigationMenuPanel'
-import { WithArrow } from 'components/WithArrow'
-import { getAjnaWithArrowColorScheme } from 'features/ajna/common/helpers'
+import { NavigationMenuPointer } from 'components/navigation/NavigationMenuPointer'
 import React, { useEffect, useRef, useState } from 'react'
-import { Box, Flex, Grid, Heading, Text } from 'theme-ui'
+import { Flex } from 'theme-ui'
 
 export interface NavigationMenuDropdownProps {
   arrowPosition: number
@@ -54,28 +50,11 @@ export function NavigationMenuDropdown({
         transition: 'opacity 200ms',
       }}
     >
-      <Box
-        sx={{
-          position: 'absolute',
-          top: '100%',
-          left: '-7px',
-          mt: '-6px',
-          transform: isPanelOpen ? 'translateY(0)' : 'translateY(20px)',
-          transition: 'transform 200ms',
-          zIndex: 2,
-        }}
-      >
-        <Box
-          sx={{
-            width: '14px',
-            height: '14px',
-            borderRadius: 2,
-            bg: 'neutral10',
-            transform: `translateX(${arrowPosition}px) scaleY(1.3) rotate(45deg)`,
-            ...(isPanelSwitched && { transition: 'transform 200ms' }),
-          }}
-        />
-      </Box>
+      <NavigationMenuPointer
+        arrowPosition={arrowPosition}
+        isPanelOpen={isPanelOpen}
+        isPanelSwitched={isPanelSwitched}
+      />
       <Flex
         sx={{
           position: 'absolute',
@@ -95,8 +74,8 @@ export function NavigationMenuDropdown({
             borderRadius: 'large',
             bg: 'neutral10',
             boxShadow: 'buttonMenu',
-            pointerEvents: 'auto',
             overflow: 'hidden',
+            pointerEvents: 'auto',
           }}
         >
           <Flex
@@ -107,7 +86,7 @@ export function NavigationMenuDropdown({
               ...(isPanelSwitched && { transition: 'width 200ms, height 200ms' }),
             }}
           >
-            {panels.map(({ description, label, learn, links, otherAssets }, i) => (
+            {panels.map(({ label, ...panel }, i) => (
               <Flex sx={{ ml: '-100%', transform: 'translateX(50%)' }}>
                 <Flex
                   key={i}
@@ -126,112 +105,7 @@ export function NavigationMenuDropdown({
                   }}
                   {...(currentPanel === label && { ref })}
                 >
-                  <Box>
-                    <Heading
-                      sx={{
-                        fontSize: '28px',
-                        fontWeight: 'bold',
-                        mb: 1,
-                        color: 'primary100',
-                        letterSpacing: '-0.02em',
-                      }}
-                    >
-                      {label}
-                    </Heading>
-                    <Flex sx={{ columnGap: 1 }}>
-                      <Text
-                        as="p"
-                        sx={{ fontSize: '14px', color: 'neutral80', lineHeight: '22px' }}
-                      >
-                        {description}
-                      </Text>
-                      {learn && (
-                        <AppLink href={learn.link}>
-                          <WithArrow
-                            gap={1}
-                            sx={{ display: 'inline-block', ...getAjnaWithArrowColorScheme() }}
-                          >
-                            {learn.label}
-                          </WithArrow>
-                        </AppLink>
-                      )}
-                    </Flex>
-                  </Box>
-                  <Grid
-                    as="ul"
-                    sx={{
-                      gap: 4,
-                      gridTemplateColumns: 'repeat( 2, 1fr )',
-                      p: '0',
-                      listStyle: 'none',
-                    }}
-                  >
-                    {links.map(({ icon, footnote, link, title }, i) => (
-                      <Flex key={i} as="li" sx={{ flexGrow: 1, flexBasis: 0 }}>
-                        <AppLink
-                          href={link}
-                          sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            fontWeight: 'regular',
-                            '&:hover': {
-                              '.dropdownLinkTitle': {
-                                color: 'neutral80',
-                              },
-                            },
-                          }}
-                        >
-                          <Icon size={48} name={icon} sx={{ flexShrink: 0 }} />
-                          <Flex sx={{ flexDirection: 'column', ml: 3 }}>
-                            <Text
-                              as="p"
-                              className="dropdownLinkTitle"
-                              sx={{
-                                fontSize: 4,
-                                fontWeight: 'semiBold',
-                                color: 'primary100',
-                                transition: 'color 200ms',
-                              }}
-                            >
-                              {title}
-                            </Text>
-                            <Text
-                              as="p"
-                              sx={{ fontSize: 2, color: 'neutral80', whiteSpace: 'nowrap' }}
-                            >
-                              {footnote}
-                            </Text>
-                          </Flex>
-                        </AppLink>
-                      </Flex>
-                    ))}
-                  </Grid>
-                  {otherAssets && otherAssets?.length > 0 && (
-                    <Box sx={{ pt: '24px', borderTop: '1px solid', borderColor: 'neutral20' }}>
-                      <Heading
-                        as="h3"
-                        sx={{
-                          fontSize: '16px',
-                          fontWeight: 'semiBold',
-                          mb: 3,
-                          color: 'primary100',
-                        }}
-                      >
-                        Other assets you can borrow against
-                      </Heading>
-                      <Flex as="ul" sx={{ columnGap: 3, rowGap: 2, listStyle: 'none', p: 0 }}>
-                        {otherAssets.map(({ link, token }, i) => (
-                          <Box key={i} as="li">
-                            <AssetPill
-                              icon={getToken(token).iconCircle}
-                              label={token}
-                              link={link}
-                            />
-                          </Box>
-                        ))}
-                      </Flex>
-                    </Box>
-                  )}
+                  <NavigationMenuDropdownContent label={label} {...panel} />
                 </Flex>
               </Flex>
             ))}
---
+++ components/navigation/NavigationMenuPointer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Box } from 'theme-ui'
+
+export interface NavigationMenuPointerProps {
+  arrowPosition: number
+  isPanelOpen: boolean
+  isPanelSwitched: boolean
+}
+
+export function NavigationMenuPointer({
+  arrowPosition,
+  isPanelOpen,
+  isPanelSwitched,
+}: NavigationMenuPointerProps) {
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        top: '100%',
+        left: '-7px',
+        mt: '-6px',
+        transform: isPanelOpen ? 'translateY(0)' : 'translateY(20px)',
+        transition: 'transform 200ms',
+        zIndex: 2,
+      }}
+    >
+      <Box
+        sx={{
+          width: '14px',
+          height: '14px',
+          borderRadius: 2,
+          bg: 'neutral10',
+          transform: `translateX(${arrowPosition}px) scaleY(1.3) rotate(45deg)`,
+          ...(isPanelSwitched && { transition: 'transform 200ms' }),
+        }}
+      />
+    </Box>
+  )
+}
