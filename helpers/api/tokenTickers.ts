ecefdf59334ea665bb839f0f577ef1347d4a9be7
sennett <anthony@oazoapps.com>
2022-12-20 18:38:03 +0000

    remove vestigial code

--- helpers/api/tokenTickers.ts
+++ helpers/api/tokenTickers.ts
@@ -2,8 +2,6 @@ import { getCoinbaseTickers } from 'server/services/coinbase'
 import { getCoingeckoTickers } from 'server/services/coingecko'
 import { getCoinPaprikaTickers } from 'server/services/coinPaprika'
 
-import devResponse from './devResponse.json'
-
 export async function tokenTickers() {
   const results = await Promise.all([
     getCoinPaprikaTickers(),
