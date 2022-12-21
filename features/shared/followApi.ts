588508eb3ef9e03de8c51bbe60ea6fc2f6e512b7
johnnieskywalker <lwolski0@gmail.com>
2022-12-21 14:30:46 +0100

    changed follow function from observable to async

--- features/shared/followApi.ts
+++ features/shared/followApi.ts
@@ -1,34 +1,38 @@
 import { UsersWhoFollowVaults } from '@prisma/client'
 import BigNumber from 'bignumber.js'
 import getConfig from 'next/config'
-import { Observable } from 'rxjs'
-import { ajax } from 'rxjs/ajax'
-import { map } from 'rxjs/operators'
 
 const basePath = getConfig()?.publicRuntimeConfig?.basePath || ''
 
-// TODO ŁW rewrite to fetch and async function
-export function followVaultUsingApi$(
+export async function followVaultUsingApi(
   vaultId: BigNumber,
   followerAddress: string,
   docVersion: string,
   chainId: number,
   token: string,
-): Observable<void> {
-  return ajax({
-    url: `${basePath}/api/follow`,
+): Promise<UsersWhoFollowVaults[]> {
+  return fetch(`${basePath}/api/follow`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       authorization: 'Bearer ' + token,
     },
-    body: {
+    body: JSON.stringify({
       user_address: followerAddress,
       vault_id: parseInt(vaultId.toFixed(0)),
       tos_doc_version: docVersion,
       vault_chain_id: chainId,
-    },
-  }).pipe(map((_) => {}))
+    }),
+  })
+    .then((resp) => {
+      return resp.json() as UsersWhoFollowVaults[]
+    })
+    .catch((err) => {
+      if (err.status === 404) {
+        return [] as UsersWhoFollowVaults[]
+      }
+      throw err
+    })
 }
 
 export function getFollowFromApi(address: string): Promise<UsersWhoFollowVaults[]> {
