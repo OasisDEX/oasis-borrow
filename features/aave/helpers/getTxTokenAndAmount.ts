87f9b67aa526e34ae4843c7525ee76340d5ead55
Jakub Swierczek <jakub.swierczek@outlook.com>
2022-12-21 17:49:27 +0100

    payback debt work

--- features/aave/helpers/getTxTokenAndAmount.ts
+++ features/aave/helpers/getTxTokenAndAmount.ts
@@ -2,21 +2,25 @@ import BigNumber from 'bignumber.js'
 import { zero } from 'helpers/zero'
 
 import { BaseAaveContext } from '../common/BaseAaveContext'
-import { ManageDebtActionsEnum } from '../strategyConfig'
+import { ManageCollateralActionsEnum, ManageDebtActionsEnum } from '../strategyConfig'
 
 export function getTxTokenAndAmount(context: BaseAaveContext) {
+  const isDepositingAction =
+    context.manageTokenInput?.manageTokenAction ===
+      ManageCollateralActionsEnum.DEPOSIT_COLLATERAL ||
+    context.manageTokenInput?.manageTokenAction === ManageDebtActionsEnum.PAYBACK_DEBT
+
   const isBorrowOrPaybackDebt = context.manageTokenInput
     ? [ManageDebtActionsEnum.BORROW_DEBT, ManageDebtActionsEnum.PAYBACK_DEBT].includes(
         context.manageTokenInput.manageTokenAction as ManageDebtActionsEnum,
       )
     : false
-  const isAmountFromUserInputNeeded = (context.transactionToken || context.tokens.deposit) === 'ETH'
 
-  console.log(`isAmountFromUserInputNeeded: ${isAmountFromUserInputNeeded}`)
+  const isAmountFromUserInputNeeded = (context.transactionToken || context.tokens.deposit) === 'ETH'
 
   return {
     amount:
-      isAmountFromUserInputNeeded && !isBorrowOrPaybackDebt
+      isAmountFromUserInputNeeded && isDepositingAction
         ? context.userInput.amount || context.manageTokenInput!.manageTokenActionValue
         : zero,
     token: context.userInput.amount
