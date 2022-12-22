bca583d85145806d4aad28e7de00f3443c16ac1b
Jakub Swierczek <jakub.swierczek@outlook.com>
2022-12-22 14:45:33 +0100

    [SC-7318] Validations adn small changes (#1848)

--- features/aave/common/components/informationContainer/StrategyInformationContainer.tsx
+++ features/aave/common/components/informationContainer/StrategyInformationContainer.tsx
@@ -5,11 +5,14 @@ import React from 'react'
 
 import { VaultChangesInformationContainer } from '../../../../../components/vault/VaultChangesInformation'
 import { HasGasEstimation } from '../../../../../helpers/form'
+import { zero } from '../../../../../helpers/zero'
 import { UserSettingsState } from '../../../../userSettings/userSettings'
 import { FeesInformation } from './FeesInformation'
 import { LtvInformation } from './LtvInformation'
-import { MultiplyInformation } from './MultiplyInformation'
-import { OutstandingDebtInformation } from './OutstandingDebtInformation'
+import {
+  OutstandingDebtInformation,
+  TotalCollateralInformation,
+} from './OutstandingDebtInformation'
 import { PriceImpact } from './PriceImpact'
 import { SlippageInformation } from './SlippageInformation'
 import { TransactionTokenAmount } from './TransactionTokenAmount'
@@ -37,17 +40,25 @@ export function StrategyInformationContainer({ state }: OpenAaveInformationConta
 
   const { strategy, currentPosition } = state.context
 
+  const simulationHasSwap = strategy?.simulation.swap.toTokenAmount.gt(zero)
+
   return strategy && currentPosition ? (
     <VaultChangesInformationContainer title={t('vault-changes.order-information')}>
-      <TransactionTokenAmount {...state.context} transactionParameters={strategy} />
-      <PriceImpact {...state.context} transactionParameters={strategy} />
-      <SlippageInformation {...state.context.userSettings!} />
-      <MultiplyInformation
-        {...state.context}
-        transactionParameters={strategy}
-        currentPosition={currentPosition}
-      />
+      {simulationHasSwap && (
+        <TransactionTokenAmount {...state.context} transactionParameters={strategy} />
+      )}
+      {simulationHasSwap && <PriceImpact {...state.context} transactionParameters={strategy} />}
+      {simulationHasSwap && <SlippageInformation {...state.context.userSettings!} />}
+      {/*<MultiplyInformation*/}
+      {/*  {...state.context}*/}
+      {/*  transactionParameters={strategy}*/}
+      {/*  currentPosition={currentPosition}*/}
+      {/*/>*/}
       <OutstandingDebtInformation
+        currentPosition={currentPosition}
+        newPosition={strategy.simulation.position}
+      />
+      <TotalCollateralInformation
         currentPosition={currentPosition}
         newPosition={strategy.simulation.position}
       />
