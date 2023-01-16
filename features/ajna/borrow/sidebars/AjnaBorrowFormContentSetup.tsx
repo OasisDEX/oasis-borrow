a12e43859b16f0d4994228139ad029e3bd242900
Piotr Konowrocki <piotr@oazoapps.com>
2023-01-16 12:55:04 +0100

    input with fake props

--- features/ajna/borrow/sidebars/AjnaBorrowFormContentSetup.tsx
+++ features/ajna/borrow/sidebars/AjnaBorrowFormContentSetup.tsx
@@ -1,3 +1,25 @@
+import BigNumber from 'bignumber.js'
+import { VaultActionInput } from 'components/vault/VaultActionInput'
+
 export function AjnaBorrowFormContentSetup() {
-  return <>Setup stage</>
+  return (
+    <>
+      <VaultActionInput
+        action="Deposit"
+        amount={new BigNumber(0)}
+        auxiliaryAmount={new BigNumber(1500)}
+        hasAuxiliary={true}
+        hasError={false}
+        maxAmount={new BigNumber(100)}
+        maxAmountLabel={'Balance'}
+        maxAuxiliaryAmount={new BigNumber(10000)}
+        onChange={() => {}}
+        onSetMax={() => {}}
+        showMax={true}
+        currencyCode={'WBTC'}
+        tokenUsdPrice={new BigNumber(1500)}
+        disabled={false}
+      />
+    </>
+  )
 }
