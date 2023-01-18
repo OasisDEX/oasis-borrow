787e3a9d44fd8e58ac9facc22f835af480a798e8
Piotr Konowrocki <piotr@oazoapps.com>
2023-01-18 20:22:00 +0100

    minor bugfixes

--- features/follow/api.ts
+++ features/follow/api.ts
@@ -4,7 +4,7 @@ import { ajax } from 'rxjs/ajax'
 import { map } from 'rxjs/operators'
 
 export function followedVaults$(address: string): Observable<UsersWhoFollowVaults[]> {
-  const url = `http://localhost:3000/api/follow/${address}`
+  const url = `/api/follow/${address}`
 
   return ajax({ url, method: 'GET' }).pipe(map(({ response }) => response))
 }
