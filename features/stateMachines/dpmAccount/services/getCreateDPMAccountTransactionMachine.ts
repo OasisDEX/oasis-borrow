eeae759883990934bf166d74c0a037729fb63a01
sennett <anthony@oazoapps.com>
2023-01-19 10:59:21 +0000

    remove any

--- features/stateMachines/dpmAccount/services/getCreateDPMAccountTransactionMachine.ts
+++ features/stateMachines/dpmAccount/services/getCreateDPMAccountTransactionMachine.ts
@@ -10,6 +10,9 @@ import {
   createTransactionStateMachine,
   startTransactionService,
 } from '../../transaction'
+import { TxState } from '@oasisdex/transactions'
+import { SuccessTxState } from '@oasisdex/transactions/lib/src/types'
+import { UserDpmProxy } from '../../../../blockchain/userDpmProxies'
 
 export function getCreateDPMAccountTransactionMachine(
   txHelpers$: Observable<TxHelpers>,
@@ -31,27 +34,37 @@ export function getCreateDPMAccountTransactionMachine(
   })
 }
 
-function extractDpmProxyFromTxnReceipt(context: ContextConnected, txnReceipt: any) {
-  const iface = new ethers.utils.Interface(context.accountFactory.abi.default)
+function extractDpmProxyFromTxnReceipt(
+  context: ContextConnected,
+  txnReceipt: SuccessTxState | TxState<CreateDPMAccount>,
+): UserDpmProxy | undefined {
+  if ('receipt' in txnReceipt) {
+    const logParser = new ethers.utils.Interface(context.accountFactory.abi.default)
 
-  // find proxy address event
-  const proxy = txnReceipt.receipt.logs.reduce((discoveredProxy: string | undefined, log: any) => {
-    if (discoveredProxy) {
-      return discoveredProxy
-    }
-    try {
-      return iface.parseLog(log).args // args is the proxy
-    } catch (e) {
-      // throws when reading an event from a non AccountFactory ABI - assume no proxy from this event
-      return undefined
-    }
-  }, undefined)
-
-  if (!proxy) {
-    throw new Error(
-      `could not read DPM proxy address from txnReceipt ${JSON.stringify(txnReceipt)}`,
+    // find proxy address event
+    const proxy = txnReceipt.receipt.logs.reduce(
+      (discoveredProxy: string | undefined, log: any) => {
+        if (discoveredProxy) {
+          return discoveredProxy
+        }
+        try {
+          return (logParser.parseLog(log).args as unknown) as UserDpmProxy // args is the proxy
+        } catch (e) {
+          // throws when reading an event from a non AccountFactory ABI - assume no proxy from this event
+          return undefined
+        }
+      },
+      undefined,
     )
-  }
 
-  return proxy
+    if (!proxy) {
+      throw new Error(
+        `could not read DPM proxy address from txnReceipt ${JSON.stringify(txnReceipt)}`,
+      )
+    }
+
+    return proxy
+  } else {
+    return undefined
+  }
 }
