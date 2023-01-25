33cd82a77a7040bab5761a2080a6dd2776334f79
Piotr Konowrocki <piotr@oazoapps.com>
2023-01-25 18:54:13 +0100

    share on vault headline

--- features/follow/common/ShareButton.tsx
+++ features/follow/common/ShareButton.tsx
@@ -7,7 +7,7 @@ interface ShareButtonPropsWithLink {
   hashtags?: never
   link: string
   text?: never
-  url: never
+  url?: never
   via?: never
 }
 
@@ -40,90 +40,35 @@ export function getTwitterShareUrl({
 
 export function ShareButton({ link, ...rest }: ShareButtonProps) {
   return (
-    <AppLink href={link || getTwitterShareUrl(rest)}>
+    <AppLink href={link || getTwitterShareUrl(rest as ShareButtonPropsWithParts)}>
       <Button
-        className="discover-action"
-        variant="tertiary"
-        sx={{ height: '36px', px: 2, pt: '5px', pb: 0 }}
+        sx={{
+          height: '28px',
+          width: '28px',
+          p: 0,
+          pt: '3px',
+          fontSize: 1,
+          border: '1px solid',
+          borderColor: 'neutral20',
+          borderRadius: 'ellipse',
+          backgroundColor: 'neutral10',
+          boxShadow: ['none', null, null, 'surface'],
+          transition: 'border-color 200ms, background-color 200ms, color 200ms',
+          '&:hover': {
+            backgroundColor: 'neutral10',
+            borderColor: 'primary100',
+            svg: {
+              color: 'primary100',
+            },
+          },
+          svg: {
+            color: 'primary60',
+            transition: 'color 200ms'
+          },
+        }}
       >
-        <Icon name="share" size={20} />
+        <Icon name="share" size={16} />
       </Button>
     </AppLink>
-    // <Button
-    //   disabled={isProcessing}
-    //   onClick={buttonClickHandler}
-    //   sx={{
-    //     position: 'relative',
-    //     py: 0,
-    //     pr: isShort ? 0 : '12px',
-    //     pl: isShort ? 0 : '30px',
-    //     fontSize: 1,
-    //     width: isShort ? ['32px', null, null, '36px'] : 'auto',
-    //     height: isShort ? ['32px', null, null, '36px'] : 'auto',
-    //     lineHeight: '26px',
-    //     color: isFollowing ? 'primary100' : 'primary60',
-    //     border: '1px solid',
-    //     borderColor: 'neutral20',
-    //     borderRadius: isShort ? 'ellipse' : 'large',
-    //     backgroundColor: 'neutral10',
-    //     boxShadow: isShort ? 'none' : 'surface',
-    //     transition: 'border-color 200ms, background-color 200ms, color 200ms',
-    //     '&:hover': {
-    //       backgroundColor: 'neutral10',
-    //       borderColor: 'primary100',
-    //       color: isFollowing ? 'primary60' : 'primary100',
-    //       '.star': {
-    //         fill: isFollowing ? 'interactive50' : 'neutral10',
-    //         stroke: isFollowing ? 'interactive50' : 'primary100',
-    //         strokeWidth: isFollowing ? 0 : '1.5px',
-    //       },
-    //     },
-    //     '&:disabled': {
-    //       backgroundColor: 'neutral10',
-    //       color: 'primary60',
-    //     },
-    //     '.star': {
-    //       fill: isFollowing ? 'interactive100' : 'neutral10',
-    //       stroke: isFollowing ? 'interactive100' : 'primary60',
-    //       strokeWidth: isFollowing ? 0 : '1px',
-    //       transition: 'stroke 200ms, stroke-width 200ms, fill 200ms',
-    //     },
-    //     ...sx,
-    //   }}
-    //   onMouseOver={handleMouseOver}
-    //   onMouseOut={handleMouseOut}
-    // >
-    //   <Box
-    //     sx={{
-    //       position: 'absolute',
-    //       top: isShort ? ['5px', null, null, '7px'] : '2px',
-    //       left: isShort ? ['8px', null, null, '10px'] : '13px',
-    //       margin: 'auto',
-    //     }}
-    //   >
-    //     {isProcessing ? (
-    //       <Spinner
-    //         size={isShort ? 20 : 15}
-    //         color="#878BFC"
-    //         sx={{ position: 'relative', top: isShort ? 0 : '1px', left: '-3px' }}
-    //       />
-    //     ) : (
-    //       <Box className="star">
-    //         <Icon name="star" size={isShort ? 14 : 12} />
-    //       </Box>
-    //     )}
-    //   </Box>
-    //   {!isShort && (
-    //     <>
-    //       {isProcessing
-    //         ? t('loading')
-    //         : isHovering && isFollowing
-    //         ? t('unfollow')
-    //         : isFollowing
-    //         ? t('following')
-    //         : t('follow')}
-    //     </>
-    //   )}
-    // </Button>
   )
 }
