dd8f7cffa2e3d5cdb7c3a3cb4966f80372451a9d
johnnieskywalker <lwolski0@gmail.com>
2023-01-26 13:38:24 +0100

    added multiple protocols support for follow

--- server/database/migrations/019-protocol-column.sql
+++ server/database/migrations/019-protocol-column.sql
@@ -1,4 +1,6 @@
 CREATE TYPE "protocol" AS ENUM ('AAVE', 'AJNA', 'MAKERDAO');
 
-ALTER TABLE "users_who_follow_vaults" ADD COLUMN     "protocol" 
-ALTER TABLE "user_who_follow_vaults" ALTER COLUMN "protocol" SET NOT NULL;
+ALTER TABLE "users_who_follow_vaults" ADD COLUMN "protocol" protocol; 
+UPDATE "users_who_follow_vaults" SET "protocol" = 'MAKERDAO';
+
+ALTER TABLE "users_who_follow_vaults" ALTER COLUMN "protocol" SET NOT NULL;
