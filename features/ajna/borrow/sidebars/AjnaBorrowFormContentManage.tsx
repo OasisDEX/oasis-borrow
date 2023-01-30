0f64f44f1d14ba3b1590726d5534a0d4aa973ae2
Piotr Konowrocki <piotr@oazoapps.com>
2023-01-30 20:17:22 +0100

    pills on manage

--- features/ajna/borrow/sidebars/AjnaBorrowFormContentManage.tsx
+++ features/ajna/borrow/sidebars/AjnaBorrowFormContentManage.tsx
@@ -1,5 +1,7 @@
-import { AjnaBorrowPanel } from 'features/ajna/common/types'
-import React from 'react'
+import { ActionPills } from 'components/ActionPills'
+import { AjnaBorrowAction, AjnaBorrowPanel } from 'features/ajna/common/types'
+import { useTranslation } from 'next-i18next'
+import React, { useEffect, useState } from 'react'
 import { Grid } from 'theme-ui'
 
 interface AjnaBorrowFormContentManageProps {
@@ -7,5 +9,48 @@ interface AjnaBorrowFormContentManageProps {
 }
 
 export function AjnaBorrowFormContentManage({ panel }: AjnaBorrowFormContentManageProps) {
-  return <Grid gap={3}>Manage step on {panel} panel</Grid>
+  const { t } = useTranslation()
+  const [active, setActive] = useState<AjnaBorrowAction>('deposit')
+
+  useEffect(() => {
+    setActive(panel === 'collateral' ? 'deposit' : 'generate')
+  }, [panel])
+
+  return (
+    <Grid gap={3}>
+      <ActionPills
+        active={active}
+        {...(panel === 'collateral'
+          ? {
+              items: [
+                {
+                  id: 'deposit',
+                  label: t('vault-actions.deposit'),
+                  action: () => setActive('deposit'),
+                },
+                {
+                  id: 'withdraw',
+                  label: t('vault-actions.withdraw'),
+                  action: () => setActive('withdraw'),
+                },
+              ],
+            }
+          : {
+              items: [
+                {
+                  id: 'generate',
+                  label: t('vault-actions.generate'),
+                  action: () => setActive('generate'),
+                },
+                {
+                  id: 'payback',
+                  label: t('vault-actions.payback'),
+                  action: () => setActive('payback'),
+                },
+              ],
+            })}
+      />
+      Manage step on {panel} panel with {active} tab.
+    </Grid>
+  )
 }
