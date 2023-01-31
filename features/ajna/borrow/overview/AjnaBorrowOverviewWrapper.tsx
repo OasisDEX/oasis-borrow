80294c09e4e84e801092137cec1bb570470a3651
Piotr Konowrocki <piotr@oazoapps.com>
2023-01-31 12:53:34 +0100

    liquidation card

--- features/ajna/borrow/overview/AjnaBorrowOverviewWrapper.tsx
+++ features/ajna/borrow/overview/AjnaBorrowOverviewWrapper.tsx
@@ -1,15 +1,30 @@
 import { DetailsSection } from 'components/DetailsSection'
 import { DetailsSectionContentCardWrapper } from 'components/DetailsSectionContentCard'
+import { ContentCardLiquidationPrice } from 'features/ajna/borrow/overview/ContentCardLiquidationPrice'
+import { useAjnaBorrowContext } from 'features/ajna/contexts/AjnaProductContext'
+import { zero } from 'helpers/zero'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
 
 export function AjnaBorrowOverviewWrapper() {
   const { t } = useTranslation()
+  const {
+    environment: { collateralToken, quoteToken },
+  } = useAjnaBorrowContext()
 
   return (
     <DetailsSection
       title={t('system.overview')}
-      content={<DetailsSectionContentCardWrapper>Lorem ipsum</DetailsSectionContentCardWrapper>}
+      content={
+        <DetailsSectionContentCardWrapper>
+          <ContentCardLiquidationPrice
+            collateralToken={collateralToken}
+            quoteToken={quoteToken}
+            liquidationPrice={zero}
+            belowCurrentPrice={zero}
+          />
+        </DetailsSectionContentCardWrapper>
+      }
     />
   )
 }
