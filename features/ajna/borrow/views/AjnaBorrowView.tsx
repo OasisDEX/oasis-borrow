6752ef60eaca2dcd9b056b20b9531d3f20a8866a
Piotr Konowrocki <piotr@oazoapps.com>
2023-01-31 11:29:01 +0100

    empty history tab

--- features/ajna/borrow/views/AjnaBorrowView.tsx
+++ features/ajna/borrow/views/AjnaBorrowView.tsx
@@ -5,9 +5,10 @@ import { AjnaBorrowOverviewWrapper } from 'features/ajna/borrow/overview/AjnaBor
 import { AjnaBorrowFormWrapper } from 'features/ajna/borrow/sidebars/AjnaBorrowFormWrapper'
 import { useAjnaBorrowContext } from 'features/ajna/contexts/AjnaProductContext'
 import { formatCryptoBalance } from 'helpers/formatters/format'
+import { staticFilesRuntimeUrl } from 'helpers/staticPaths'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
-import { Card, Container, Grid } from 'theme-ui'
+import { Card, Container, Flex, Grid, Heading, Image, Text } from 'theme-ui'
 
 export function AjnaBorrowView() {
   const { t } = useTranslation()
@@ -50,6 +51,33 @@ export function AjnaBorrowView() {
               <Grid variant="vaultContainer">
                 <Card variant="faq">FAQ</Card>
               </Grid>
+            ),
+          },
+          {
+            value: 'history',
+            label: t('system.history'),
+            content: (
+              <Flex
+                sx={{
+                  maxWidth: '600px',
+                  flexDirection: 'column',
+                  alignItems: ['flex-start', 'center'],
+                  mx: 'auto',
+                  pt: 5,
+                  textAlign: ['left', 'center'],
+                }}
+              >
+                <Image
+                  src={staticFilesRuntimeUrl('/static/img/no-positions.svg')}
+                  sx={{ alignSelf: 'center' }}
+                />
+                <Heading variant="boldParagraph2" sx={{ mt: 4, mb: 1 }}>
+                  {t('ajna.history.title')}
+                </Heading>
+                <Text as="p" variant="paragraph2" sx={{ m: 0, color: 'neutral80' }}>
+                  {t('ajna.history.notice')}
+                </Text>
+              </Flex>
             ),
           },
         ]}
