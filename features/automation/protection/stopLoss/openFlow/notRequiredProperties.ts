3d455e1b15bb7b93a40189689fb627bed7421f17
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-01-31 10:00:23 +0100

    addressed code review comments v2

--- features/automation/protection/stopLoss/openFlow/notRequiredProperties.ts
+++ features/automation/protection/stopLoss/openFlow/notRequiredProperties.ts
@@ -1,9 +1,15 @@
-import { VaultType } from '@prisma/client'
 import { AutomationBotV2AddTriggerData } from 'blockchain/calls/automationBot'
 import { AutomationBotRemoveTriggersData } from 'blockchain/calls/automationBotAggregator'
 import { TransactionDef } from 'blockchain/calls/callsHelpers'
+import {
+  AutomationCommonData,
+  AutomationContextProviderProps,
+  AutomationPositionData,
+} from 'components/AutomationContextProvider'
 import { AutomationAddTriggerData } from 'features/automation/common/txDefinitions'
+import { AutomationContracts, StopLossMetadata } from 'features/automation/metadata/types'
 import { StopLossResetData } from 'features/automation/protection/stopLoss/state/StopLossFormChange'
+import { VaultType } from 'features/generalManageVault/vaultType'
 import { VaultProtocol } from 'helpers/getVaultProtocol'
 import { zero } from 'helpers/zero'
 
@@ -13,24 +19,40 @@ import { zero } from 'helpers/zero'
  * it to mock required properties and ensure type safety.
  * */
 
-export const notRequiredMethods = {
+export const notRequiredMethods: Pick<
+  StopLossMetadata['methods'],
+  'getExecutionPrice' | 'prepareAddStopLossTriggerData'
+> = {
   getExecutionPrice: () => zero,
   prepareAddStopLossTriggerData: () => ({} as AutomationAddTriggerData),
 }
 
-export const notRequiredAaveTranslations = {
+export const notRequiredAaveTranslations: Pick<
+  StopLossMetadata['translations'],
+  'stopLossLevelCardFootnoteKey' | 'bannerStrategiesKey'
+> = {
   stopLossLevelCardFootnoteKey: '',
   bannerStrategiesKey: '',
 }
 
-export const notRequiredValidations = {
+export const notRequiredValidations: Pick<
+  StopLossMetadata['validation'],
+  'getAddErrors' | 'getAddWarnings' | 'cancelErrors' | 'cancelWarnings'
+> = {
   getAddErrors: () => ({}),
   getAddWarnings: () => ({}),
   cancelErrors: [],
   cancelWarnings: [],
 }
 
-export const notRequiredValues = {
+export const notRequiredValues: Pick<
+  StopLossMetadata['values'],
+  | 'initialSlRatioWhenTriggerDoesntExist'
+  | 'triggerMaxToken'
+  | 'dynamicStopLossPrice'
+  | 'removeTxData'
+  | 'resetData'
+> = {
   initialSlRatioWhenTriggerDoesntExist: zero,
   triggerMaxToken: zero,
   dynamicStopLossPrice: zero,
@@ -38,12 +60,23 @@ export const notRequiredValues = {
   resetData: {} as StopLossResetData,
 }
 
-export const notRequiredContracts = {
+export const notRequiredContracts: AutomationContracts = {
   addTrigger: {} as TransactionDef<AutomationBotV2AddTriggerData>,
   removeTrigger: {} as TransactionDef<AutomationBotRemoveTriggersData>,
 }
 
-export const notRequiredPositionData = {
+export const notRequiredPositionData: Pick<
+  AutomationPositionData,
+  | 'owner'
+  | 'nextPositionRatio'
+  | 'debtFloor'
+  | 'debtOffset'
+  | 'liquidationRatio'
+  | 'liquidationPenalty'
+  | 'liquidationPrice'
+  | 'lockedCollateral'
+  | 'vaultType'
+> = {
   owner: '',
   nextPositionRatio: zero,
   debtFloor: zero,
@@ -55,17 +88,20 @@ export const notRequiredPositionData = {
   vaultType: '' as VaultType,
 }
 
-export const notRequiredCommonData = {
+export const notRequiredCommonData: AutomationCommonData = {
   controller: '',
   nextCollateralPrice: zero,
   token: '',
 }
 
-export const notRequiredAutomationContext = {
+export const notRequiredAutomationContext: Pick<
+  AutomationContextProviderProps,
+  'protocol' | 'commonData'
+> = {
   protocol: ('' as unknown) as VaultProtocol,
   commonData: notRequiredCommonData,
 }
 
-export const notRequiredStopLossMetadata = {
+export const notRequiredStopLossMetadata: Pick<StopLossMetadata, 'stopLossWriteEnabled'> = {
   stopLossWriteEnabled: false,
 }
