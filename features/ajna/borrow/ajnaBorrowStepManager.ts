92be9fff5dc64a7ce4646ad0f61c10a7e2c16a73
Piotr Konowrocki <piotr@oazoapps.com>
2023-02-08 13:11:58 +0100

    disable primary button when inputs are empty

--- features/ajna/borrow/ajnaBorrowStepManager.ts
+++ features/ajna/borrow/ajnaBorrowStepManager.ts
@@ -5,11 +5,18 @@ interface StepManagerWithBorrowForm extends GeneralStepManager {
   formState: AjnaBorrowFormState
 }
 
-export function isBorrowStepValid({ currentStep, formState }: StepManagerWithBorrowForm) {
-  switch (currentStep) {
-    case 'setup':
+export function isBorrowStepValid({ formState }: StepManagerWithBorrowForm) {
+  switch (formState.action) {
+    case 'open':
+    case 'deposit':
       return !!formState.depositAmount?.gt(0)
+    case 'withdraw':
+      return !!formState.withdrawAmount?.gt(0)
+    case 'generate':
+      return !!formState.generateAmount?.gt(0)
+    case 'payback':
+      return !!formState.paybackAmount?.gt(0)
     default:
-      return true
+      return false
   }
 }
