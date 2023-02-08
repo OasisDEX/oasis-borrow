b9e6343804c32c5f6a6bc1f4d551f4572a5d44fc
Piotr Konowrocki <piotr@oazoapps.com>
2023-02-08 13:25:04 +0100

    isBorrowStepValid update

--- features/ajna/borrow/ajnaBorrowStepManager.ts
+++ features/ajna/borrow/ajnaBorrowStepManager.ts
@@ -5,18 +5,24 @@ interface StepManagerWithBorrowForm extends GeneralStepManager {
   formState: AjnaBorrowFormState
 }
 
-export function isBorrowStepValid({ formState }: StepManagerWithBorrowForm) {
-  switch (formState.action) {
-    case 'open':
-    case 'deposit':
-      return !!formState.depositAmount?.gt(0)
-    case 'withdraw':
-      return !!formState.withdrawAmount?.gt(0)
-    case 'generate':
-      return !!formState.generateAmount?.gt(0)
-    case 'payback':
-      return !!formState.paybackAmount?.gt(0)
+export function isBorrowStepValid({ currentStep, formState }: StepManagerWithBorrowForm) {
+  switch (currentStep) {
+    case 'setup':
+    case 'manage':
+      switch (formState.action) {
+        case 'open':
+        case 'deposit':
+          return !!formState.depositAmount?.gt(0)
+        case 'withdraw':
+          return !!formState.withdrawAmount?.gt(0)
+        case 'generate':
+          return !!formState.generateAmount?.gt(0)
+        case 'payback':
+          return !!formState.paybackAmount?.gt(0)
+        default:
+          return false
+      }
     default:
-      return false
+      return true
   }
 }
