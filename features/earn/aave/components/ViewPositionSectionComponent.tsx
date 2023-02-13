372e133d8b90ae183383d938180d9b8796c4ceba
sennett <anthony@oazoapps.com>
2023-02-13 16:54:32 +0200

    manage position values

--- features/earn/aave/components/ViewPositionSectionComponent.tsx
+++ features/earn/aave/components/ViewPositionSectionComponent.tsx
@@ -1,12 +1,9 @@
 import { AaveV2ReserveConfigurationData } from 'blockchain/aave/aaveV2ProtocolDataProvider'
 import { IStrategyConfig } from 'features/aave/common/StrategyConfigTypes'
 import { useSimulationYields } from 'helpers/useSimulationYields'
+import { AaveProtocolData, PreparedAaveReserveData } from 'lendingProtocols/aave-v2/pipelines'
 import React from 'react'
 
-import {
-  AaveProtocolData,
-  PreparedAaveReserveData,
-} from '../../../../lendingProtocols/aave-v2/pipelines'
 import { PositionInfoComponent } from './PositionInfoComponent'
 
 export type ViewPositionSectionComponentProps = {
@@ -24,7 +21,6 @@ export function ViewPositionSectionComponent({
   const { accountData, oraclePrice, position } = aaveProtocolData!
 
   const simulations = useSimulationYields({
-    amount: accountData?.totalCollateralETH,
     riskRatio: position?.riskRatio,
     fields: ['7Days'],
     strategy: strategyConfig,
@@ -33,9 +29,7 @@ export function ViewPositionSectionComponent({
   return (
     <PositionInfoComponent
       aaveReserveDataDebtToken={aaveReserveDataDebtToken}
-      accountData={accountData}
       apy={simulations?.apy}
-      tokens={strategyConfig.tokens}
       oraclePrice={oraclePrice}
       position={position}
     />
