3ad8a1098f83b91403095aa794ad5e7fbf339576
johnnieskywalker <lwolski0@gmail.com>
2023-02-14 18:00:10 +0100

    updated db model

--- server/database/migrations/019-follow-add-protocol.sql
+++ server/database/migrations/019-follow-add-protocol.sql
@@ -1,7 +1,8 @@
-CREATE TYPE "protocol" AS ENUM ('maker', 'aavev2', 'aavev3', 'ajna');
+CREATE TYPE "protocol" AS ENUM ('maker', 'aave', 'ajna');
 
 ALTER TABLE "users_who_follow_vaults"
 ADD COLUMN "protocol" "protocol";
+ADD COLUMN "version" text;
 
 UPDATE "users_who_follow_vaults"
 SET
@@ -10,3 +11,10 @@ ALTER TABLE "users_who_follow_vaults"
 ALTER column "protocol"
 SET
     NOT NULL;
+
+ALTER TABLE "users_who_follow_vaults" ADD CONSTRAINT "users_who_follow_vaults_pkey" PRIMARY KEY (
+    "user_address",
+    "vault_id",
+    "vault_chain_id",
+    "protocol"
+);
