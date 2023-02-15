6061e2232831c68d5f4ec6280a4b432a6625277e
johnnieskywalker <lwolski0@gmail.com>
2023-02-15 15:58:46 +0100

    added helper function

---
+++ features/aave/helpers/createFollowButton.ts
@@ -0,0 +1,23 @@
+import BigNumber from "bignumber.js"
+import { PositionId } from "features/aave/types"
+import { FollowButtonControlProps } from "features/follow/controllers/FollowButtonControl"
+import { useAccount } from "helpers/useAccount"
+import { useChainId } from "helpers/useChainId"
+import { useFeatureToggle } from "helpers/useFeatureToggle"
+
+export function createFollowButton(positionId: PositionId) {
+    console.log('create follow button')
+    const followAaveVaultsEnabled = useFeatureToggle('FollowAAVEVaults')
+    const { walletAddress: connectedWalletAddress } = useAccount()
+    const chainId = useChainId()
+  
+    const followButton: FollowButtonControlProps | undefined =
+      chainId && connectedWalletAddress && positionId.vaultId && followAaveVaultsEnabled
+        ? {
+            chainId,
+            followerAddress: connectedWalletAddress,
+            vaultId: new BigNumber(positionId.vaultId),
+          }
+        : undefined
+    return followButton
+  }
---
+++ helpers/useChainId.ts
@@ -0,0 +1,9 @@
+import { useAppContext } from "components/AppContextProvider"
+import { useObservable } from "helpers/observableHook"
+
+export function useChainId(): number | undefined {
+  const { context$ } = useAppContext()
+  const [context] = useObservable(context$)
+
+  return context?.chainId
+}
