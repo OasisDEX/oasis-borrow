f44c2d4703fc230b6d7e3c13c0f86405631e84c2
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-02-15 13:44:31 +0100

    [SC-7719] Blank page when simulating on disconnected page. (#2035)

--- features/aave/SetupAaveV3Context.ts
+++ features/aave/SetupAaveV3Context.ts
@@ -91,9 +91,8 @@ export function setupAaveV3Context(appContext: AppContext): AaveContext {
     ),
   )
 
-  const tokenBalances$: Observable<TokenBalances | undefined> = context$.pipe(
+  const tokenBalances$: Observable<TokenBalances> = context$.pipe(
     switchMap(({ account }) => {
-      if (!account) return of(undefined)
       return aaveSupportedTokenBalances$(account)
     }),
   )
