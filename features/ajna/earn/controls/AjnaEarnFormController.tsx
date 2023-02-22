5d0bd7b7803c381ccd35779c69cc886231ddced3
Piotr Konowrocki <piotr@oazoapps.com>
2023-02-22 14:54:59 +0100

    🧿 Ajna manage form (#2074)
    * empty components
    
    * accordion
    
    * manage pills and dropdown content
    
    * manage overview footer
    
    * final touches
    
    * prettier fix

--- features/ajna/earn/controls/AjnaEarnFormController.tsx
+++ features/ajna/earn/controls/AjnaEarnFormController.tsx
@@ -4,7 +4,8 @@ import { useAjnaProductContext } from 'features/ajna/common/contexts/AjnaProduct
 import { AjnaFormContentRisk } from 'features/ajna/common/sidebars/AjnaFormContentRisk'
 import { AjnaFormContentTransaction } from 'features/ajna/common/sidebars/AjnaFormContentTransaction'
 import { AjnaFormView } from 'features/ajna/common/views/AjnaFormView'
-import { AjnaEarnFormContentDeposit } from 'features/ajna/earn/sidebars/AjnaEarnFormContentDeposit'
+import { AjnaEarnFormContentManage } from 'features/ajna/earn/sidebars/AjnaEarnFormContentManage'
+import { AjnaEarnFormContentOpen } from 'features/ajna/earn/sidebars/AjnaEarnFormContentOpen'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
 
@@ -59,8 +60,8 @@ export function AjnaEarnFormController() {
       })}
     >
       {currentStep === 'risk' && <AjnaFormContentRisk />}
-      {currentStep === 'setup' && <AjnaEarnFormContentDeposit />}
-      {currentStep === 'manage' && 'Manage form'}
+      {currentStep === 'setup' && <AjnaEarnFormContentOpen />}
+      {currentStep === 'manage' && <AjnaEarnFormContentManage />}
       {currentStep === 'transaction' && <AjnaFormContentTransaction />}
     </AjnaFormView>
   )
