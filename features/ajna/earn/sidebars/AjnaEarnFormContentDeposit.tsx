80e9f6121a5e6b3182016100b2db781b7b713601
Piotr Konowrocki <piotr@oazoapps.com>
2023-02-22 12:35:49 +0100

    empty components

--- features/ajna/earn/sidebars/AjnaEarnFormContentDeposit.tsx
+++ features/ajna/earn/sidebars/AjnaEarnFormContentDeposit.tsx
@@ -1,39 +1,5 @@
-import { SidebarResetButton } from 'components/vault/sidebar/SidebarResetButton'
-import { AjnaValidationMessages } from 'features/ajna/common/components/AjnaValidationMessages'
-import { useAjnaProductContext } from 'features/ajna/common/contexts/AjnaProductContext'
-import { AjnaFormFieldDeposit } from 'features/ajna/common/sidebars/AjnaFormFields'
-import {
-  AjnaEarnSlider,
-  ajnaSliderDefaults,
-  ajnaSliderHighRange,
-  ajnaSliderLowRange,
-} from 'features/ajna/earn/components/AjnaEarnSlider'
-import { AjnaEarnFormOrder } from 'features/ajna/earn/sidebars/AjnaEarnFormOrder'
 import React from 'react'
 
 export function AjnaEarnFormContentDeposit() {
-  const {
-    form: {
-      dispatch,
-      state: { depositAmount },
-    },
-    validation: { errors, warnings },
-  } = useAjnaProductContext('earn')
-
-  return (
-    <>
-      <AjnaFormFieldDeposit dispatchAmount={dispatch} resetOnClear />
-      <AjnaEarnSlider {...ajnaSliderDefaults} />
-      <AjnaEarnSlider {...ajnaSliderLowRange} />
-      <AjnaEarnSlider {...ajnaSliderHighRange} />
-      {depositAmount && (
-        <>
-          <SidebarResetButton clear={() => dispatch({ type: 'reset' })} />
-          <AjnaValidationMessages {...errors} />
-          <AjnaValidationMessages {...warnings} />
-          <AjnaEarnFormOrder />
-        </>
-      )}
-    </>
-  )
+  return <>Deposit</>
 }
