6f4b4d7e2332822e0d109e7c369973162b709dd9
Piotr Konowrocki <piotr@oazoapps.com>
2023-03-01 11:10:45 +0100

    🧿 Ajna Earn manage overview changes (#2105)
    * price in headline
    
    * content cards
    
    * prettier fix

--- features/ajna/borrow/controls/AjnaBorrowPositionController.tsx
+++ features/ajna/borrow/controls/AjnaBorrowPositionController.tsx
@@ -4,29 +4,13 @@ import {
   AjnaPositionViewHistoryPlaceholder,
   AjnaPositionViewInfoPlaceholder,
 } from 'features/ajna/common/components/AjnaPositionViewPlaceholders'
-import { useAjnaGeneralContext } from 'features/ajna/common/contexts/AjnaGeneralContext'
 import { AjnaPositionView } from 'features/ajna/common/views/AjnaPositionView'
-import { formatCryptoBalance } from 'helpers/formatters/format'
 import React from 'react'
-import { useTranslation } from 'react-i18next'
 import { Grid } from 'theme-ui'
 
 export function AjnaBorrowPositionController() {
-  const { t } = useTranslation()
-  const {
-    environment: { collateralPrice, collateralToken, quotePrice, quoteToken },
-  } = useAjnaGeneralContext()
-
   return (
     <AjnaPositionView
-      headlineDetails={[
-        {
-          label: t('ajna.borrow.common.headline.current-market-price', { collateralToken }),
-          value: `${formatCryptoBalance(
-            collateralPrice.dividedBy(quotePrice),
-          )} ${collateralToken}/${quoteToken}`,
-        },
-      ]}
       tabs={{
         position: (
           <Grid variant="vaultContainer">
