994dcfc28cbeabe552dd7291cf1abd4e24724879
James Tuckett <james@tuckett.me>
2023-03-16 09:06:43 +0000

    Remove .on expiry handler from cacheObject  (#2166)
    * refactor: remove .on expiry handler from cacheObject
    
    * refactor: update cache name from statsCache to generic cache given is used in various situations

--- helpers/api/cacheObject.tsx
+++ helpers/api/cacheObject.tsx
@@ -4,24 +4,21 @@ interface Cached<T> {
   data: T
   time: number
 }
+
 export function cacheObject<R>(
   fetchFunction: () => Promise<R>,
   stdTTL: number, // seconds
 ): () => Promise<Cached<R>> {
-  const statsCache = new NodeCache({ stdTTL })
+  const cache = new NodeCache({ stdTTL })
 
   async function cacheFreshData() {
     const response = { data: await fetchFunction(), time: Date.now() }
-    statsCache.set('data', JSON.stringify(response))
+    cache.set('data', JSON.stringify(response))
     return JSON.stringify(response)
   }
 
-  statsCache.on('expired', async () => {
-    return cacheFreshData()
-  })
-
   return async () => {
-    let storedData: string | undefined = statsCache.get('data')
+    let storedData: string | undefined = cache.get('data')
 
     if (!storedData) {
       storedData = await cacheFreshData()
