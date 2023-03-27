8eecbe7e76cadb0c4a4b24f8e5420e625045b7fa
Marcin Ciarka <marcin@oazoapps.com>
2023-03-27 14:20:10 +0200

    fix isSupportedAutomationIlk

--- blockchain/tokensMetadata.ts
+++ blockchain/tokensMetadata.ts
@@ -896,7 +896,7 @@ export const ETH_TOKENS = tokens
 export const ONLY_MULTIPLY_TOKENS = ['GUNIV3DAIUSDC1', 'GUNIV3DAIUSDC2']
 
 const ALLOWED_AUTOMATION_ILKS: Record<string, string[]> = {
-  main: [
+  ethereumMainnet: [
     'ETH-A',
     'ETH-B',
     'ETH-C',
@@ -912,7 +912,7 @@ const ALLOWED_AUTOMATION_ILKS: Record<string, string[]> = {
     'MANA-A',
     'RETH-A',
   ],
-  goerli: ['ETH-A', 'ETH-B', 'ETH-C', 'WSTETH-A', 'WBTC-A', 'WBTC-B', 'WBTC-C', 'RETH-A'],
+  ethereumGoerli: ['ETH-A', 'ETH-B', 'ETH-C', 'WSTETH-A', 'WBTC-A', 'WBTC-B', 'WBTC-C', 'RETH-A'],
 }
 
 export function isSupportedAutomationIlk(network: string, ilk: string) {
