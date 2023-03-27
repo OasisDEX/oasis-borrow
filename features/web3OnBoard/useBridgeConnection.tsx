ab33721f52952d75297c378bf6bab2fd1ba8a575
Marcin Ciarka <marcin@oazoapps.com>
2023-03-27 12:23:05 +0200

    auto reconnecting

--- features/web3OnBoard/useBridgeConnection.tsx
+++ features/web3OnBoard/useBridgeConnection.tsx
@@ -1,29 +1,42 @@
+import { useConnectWallet, useSetChain } from '@web3-onboard/react'
 import { useAppContext } from 'components/AppContextProvider'
 import { useObservable } from 'helpers/observableHook'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
+import { BridgeConnector } from './BridgeConnector'
 import { useBridgeConnector } from './useBridgeConnector'
 
 export function useBridgeConnection() {
   const createBridgeConnector = useBridgeConnector()
+  const [{ wallet }] = useConnectWallet()
+  const [{ chains }] = useSetChain()
   const { web3Context$ } = useAppContext()
   const [web3Context] = useObservable(web3Context$)
+  const walletIsNotConnected =
+    web3Context?.status === 'error' ||
+    web3Context?.status === 'notConnected' ||
+    web3Context?.status === 'connectedReadonly'
+
+  useEffect(() => {
+    async function autoConnect() {
+      if (walletIsNotConnected && wallet?.accounts.length && chains.length) {
+        const bridgeConnector = new BridgeConnector(wallet, chains)
+        await web3Context.connect(bridgeConnector!, bridgeConnector!.connectionKind)
+      }
+    }
+    void autoConnect()
+  }, [createBridgeConnector, wallet, walletIsNotConnected, web3Context, chains])
 
   const connect = useCallback(async () => {
     const bridgeConnector = await createBridgeConnector()
-    if (
-      bridgeConnector &&
-      (web3Context?.status === 'error' ||
-        web3Context?.status === 'notConnected' ||
-        web3Context?.status === 'connectedReadonly')
-    ) {
+    if (bridgeConnector && walletIsNotConnected) {
       try {
         await web3Context.connect(bridgeConnector, bridgeConnector.connectionKind)
       } catch (error) {
         console.error(error)
       }
     }
-  }, [createBridgeConnector, web3Context])
+  }, [createBridgeConnector, walletIsNotConnected, web3Context])
 
   return { connect }
 }
