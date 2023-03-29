9bf72ca56eab7268c4c55ccb6aeb05dedfc6b8f7
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-03-29 09:43:02 +0200

    Ajna earn overview modals

--- features/ajna/positions/earn/overview/ContentPositionLendingPrice.tsx
+++ features/ajna/positions/earn/overview/ContentPositionLendingPrice.tsx
@@ -8,12 +8,55 @@ import { formatCryptoBalance, formatDecimalAsPercent } from 'helpers/formatters/
 import { zero } from 'helpers/zero'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
+import { Card, Grid, Heading, Text } from 'theme-ui'
+
+interface ContentPositionLendingPriceModalProps {
+  positionLendingPrice: string
+  highestThresholdPrice: string
+  collateralToken: string
+  quoteToken: string
+}
+
+function ContentPositionLendingPriceModal({
+  positionLendingPrice,
+  highestThresholdPrice,
+  collateralToken,
+  quoteToken,
+}: ContentPositionLendingPriceModalProps) {
+  const { t } = useTranslation()
+
+  return (
+    <Grid gap={2}>
+      <Heading variant="header3">
+        {t('ajna.position-page.earn.manage.overview.position-lending-price')}
+      </Heading>
+      <Text variant="paragraph2" as="p" sx={{ pb: 2 }}>
+        {t('ajna.position-page.earn.manage.overview.position-lending-price-modal-desc', { quoteToken })}
+      </Text>
+      <Card variant="vaultDetailsCardModal" sx={{ my: 2 }}>
+        {positionLendingPrice} {quoteToken}
+      </Card>
+      <Heading variant="header3">
+        {t('ajna.position-page.earn.manage.overview.minimum-yield-price-modal')}
+      </Heading>
+      <Text variant="paragraph2" as="p" sx={{ pb: 2 }}>
+        {t('ajna.position-page.earn.manage.overview.minimum-yield-price-modal-desc', {
+          quoteToken,
+        })}
+      </Text>
+      <Card variant="vaultDetailsCardModal" sx={{ my: 2 }}>
+        {highestThresholdPrice} {collateralToken}/{quoteToken}
+      </Card>
+    </Grid>
+  )
+}
 
 interface ContentPositionLendingPriceProps {
   isLoading?: boolean
   collateralToken: string
   quoteToken: string
   positionLendingPrice: BigNumber
+  highestThresholdPrice: BigNumber
   afterPositionLendingPrice?: BigNumber
   relationToMarketPrice: BigNumber
   changeVariant?: ChangeVariantType
@@ -24,6 +67,7 @@ export function ContentPositionLendingPrice({
   collateralToken,
   quoteToken,
   positionLendingPrice,
+  highestThresholdPrice,
   afterPositionLendingPrice,
   relationToMarketPrice,
   changeVariant = 'positive',
@@ -34,6 +78,7 @@ export function ContentPositionLendingPrice({
     positionLendingPrice: formatCryptoBalance(positionLendingPrice),
     afterPositionLendingPrice:
       afterPositionLendingPrice && formatCryptoBalance(afterPositionLendingPrice),
+    highestThresholdPrice: formatCryptoBalance(highestThresholdPrice),
     relationToMarketPrice: t(
       relationToMarketPrice.gte(zero)
         ? 'ajna.position-page.earn.manage.overview.above-market-price'
@@ -54,6 +99,14 @@ export function ContentPositionLendingPrice({
       variant: changeVariant,
     },
     footnote: formatted.relationToMarketPrice,
+    modal: (
+      <ContentPositionLendingPriceModal
+        collateralToken={collateralToken}
+        quoteToken={quoteToken}
+        positionLendingPrice={formatted.positionLendingPrice}
+        highestThresholdPrice={formatted.highestThresholdPrice}
+      />
+    ),
   }
 
   return <DetailsSectionContentCard {...contentCardSettings} />
