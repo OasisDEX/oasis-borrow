a45a568422ebd40d6f6a2fe9830695c63b33e7eb
Piotr Konowrocki <piotr@oazoapps.com>
2023-04-11 10:39:44 +0200

    getAjnaBorrowDebtMax comment

--- features/ajna/positions/borrow/helpers/getAjnaBorrowDebtMax.ts
+++ features/ajna/positions/borrow/helpers/getAjnaBorrowDebtMax.ts
@@ -1,6 +1,5 @@
 import { AjnaPosition } from '@oasisdex/oasis-actions-poc'
 import BigNumber from 'bignumber.js'
-import { zero } from 'helpers/zero'
 
 interface AjnaBorrowDebtMaxParams {
   digits: number
@@ -9,26 +8,24 @@ interface AjnaBorrowDebtMaxParams {
   simulation?: AjnaPosition
 }
 
-// export function getAjnaBorrowDebtMax({
-//   digits,
-//   interestRate,
-//   position: {
-//     collateralAmount,
-//     debtAmount,
-//     pool: { lowestUtilizedPrice },
-//   },
-//   simulation,
-// }: AjnaBorrowDebtMaxParams) {
-// const resolveedCollateralAmount = simulation?.collateralAmount || collateralAmount
-// const resolvedLowestUtilizedPrice = simulation?.pool.lowestUtilizedPrice || lowestUtilizedPrice
-// const maxDebt = resolveedCollateralAmount.times(resolvedLowestUtilizedPrice).minus(debtAmount)
-// const oneDayInterest = maxDebt.times(interestRate.div(new BigNumber(365)))
+export function getAjnaBorrowDebtMax({
+  // digits,
+  // interestRate,
+  position: {
+    collateralAmount,
+    debtAmount,
+    pool: { lowestUtilizedPrice },
+  },
+  simulation,
+}: AjnaBorrowDebtMaxParams) {
+  const resolveedCollateralAmount = simulation?.collateralAmount || collateralAmount
+  const resolvedLowestUtilizedPrice = simulation?.pool.lowestUtilizedPrice || lowestUtilizedPrice
+  const maxDebt = resolveedCollateralAmount.times(resolvedLowestUtilizedPrice).minus(debtAmount)
+  // const oneDayInterest = maxDebt.times(interestRate.div(new BigNumber(365)))
 
-// return maxDebt
-//   .minus(oneDayInterest)
-//   .decimalPlaces(digits, BigNumber.ROUND_DOWN)
-// }
-// eslint-disable-next-line no-empty-pattern
-export function getAjnaBorrowDebtMax({}: AjnaBorrowDebtMaxParams) {
-  return zero
+  // TODO: temporary hack to return 0, but keep interface intact in method
+  // so I don't have to comment out passed params in every place where it's used
+  return maxDebt.minus(maxDebt)
+  // .minus(oneDayInterest)
+  // .decimalPlaces(digits, BigNumber.ROUND_DOWN)
 }
