0d2217c366e64c8fb2edd9383ee6eedc161b5c88
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-04-13 18:29:09 +0200

    Fix not connected issue

--- components/connectWallet/Connection.tsx
+++ components/connectWallet/Connection.tsx
@@ -2,13 +2,14 @@ import { useWeb3OnBoardConnection } from 'features/web3OnBoard'
 import { INTERNAL_LINKS } from 'helpers/applicationLinks'
 import { WithChildren } from 'helpers/types'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export function Connection({ children, walletConnect }: WithChildren & { walletConnect: boolean }) {
   const { executeConnection, connected, connecting, autoConnect } = useWeb3OnBoardConnection({
     walletConnect,
   })
   const { replace, reload } = useRouter()
+  const [connectionExecuted, setConnectionExecuted] = useState<boolean>(false)
   const handleConnected = useCallback(
     async (account?: string) => {
       if (!account && walletConnect) {
@@ -23,14 +24,19 @@ export function Connection({ children, walletConnect }: WithChildren & { walletC
   )
 
   useEffect(() => {
-    if (!connecting && !connected) {
+    if (!connected && !connecting) {
+      void executeConnection(handleConnected)
       if (walletConnect) {
-        void executeConnection(handleConnected)
-      } else {
-        void autoConnect()
+        setConnectionExecuted(true)
       }
     }
-  })
+  }, [connected, executeConnection, connecting, handleConnected, walletConnect])
+
+  useEffect(() => {
+    if (!connecting && !walletConnect && !connectionExecuted) {
+      void autoConnect()
+    }
+  }, [connecting, walletConnect, autoConnect, connectionExecuted])
 
   return children
 }
