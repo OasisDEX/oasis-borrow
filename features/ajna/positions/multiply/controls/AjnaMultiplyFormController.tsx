55ba8dbad6be197a8b69f56d27e921667dfa71f3
Piotr Konowrocki <piotr@oazoapps.com>
2023-04-14 11:29:30 +0200

    manage multiply drop and pills

--- features/ajna/positions/multiply/controls/AjnaMultiplyFormController.tsx
+++ features/ajna/positions/multiply/controls/AjnaMultiplyFormController.tsx
@@ -1,21 +1,91 @@
+import { getToken } from 'blockchain/tokensMetadata'
 import { useAjnaGeneralContext } from 'features/ajna/positions/common/contexts/AjnaGeneralContext'
+import { useAjnaProductContext } from 'features/ajna/positions/common/contexts/AjnaProductContext'
 import { AjnaFormContentRisk } from 'features/ajna/positions/common/sidebars/AjnaFormContentRisk'
 import { AjnaFormContentTransaction } from 'features/ajna/positions/common/sidebars/AjnaFormContentTransaction'
 import { AjnaFormView } from 'features/ajna/positions/common/views/AjnaFormView'
+import { AjnaMultiplyFormContentManage } from 'features/ajna/positions/multiply/sidebars/AjnaMultiplyFormContentManage'
 import { AjnaMultiplyFormContentOpen } from 'features/ajna/positions/multiply/sidebars/AjnaMultiplyFormContentOpen'
 import { AjnaMultiplyFormOrder } from 'features/ajna/positions/multiply/sidebars/AjnaMultiplyFormOrder'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 export function AjnaMultiplyFormController() {
+  const { t } = useTranslation()
   const {
+    environment: { collateralToken, flow, quoteToken },
     steps: { currentStep },
   } = useAjnaGeneralContext()
+  const {
+    form: {
+      dispatch,
+      state: { uiDropdown },
+      updateState,
+    },
+  } = useAjnaProductContext('multiply')
 
   return (
-    <AjnaFormView>
+    <AjnaFormView
+      {...(flow === 'manage' && {
+        dropdown: {
+          forcePanel: uiDropdown,
+          disabled: currentStep !== 'manage',
+          items: [
+            {
+              label: t('system.manage-collateral-token', {
+                token: collateralToken,
+              }),
+              panel: 'collateral',
+              shortLabel: collateralToken,
+              icon: getToken(collateralToken).iconCircle,
+              action: () => {
+                dispatch({ type: 'reset' })
+                updateState('uiDropdown', 'collateral')
+                updateState('uiPill', 'deposit-multiply')
+                updateState('action', 'deposit-multiply')
+              },
+            },
+            {
+              label: t('system.manage-debt-token', {
+                token: quoteToken,
+              }),
+              panel: 'quote',
+              shortLabel: quoteToken,
+              icon: getToken(quoteToken).iconCircle,
+              action: () => {
+                dispatch({ type: 'reset' })
+                updateState('uiDropdown', 'quote')
+                updateState('uiPill', 'buy-collateral-multiply')
+                updateState('action', 'buy-collateral-multiply')
+              },
+            },
+            {
+              label: t('system.actions.multiply.switch-to-borrow'),
+              icon: 'circle_exchange',
+              iconShrink: 2,
+              panel: 'switch',
+              action: () => {
+                dispatch({ type: 'reset' })
+                updateState('uiDropdown', 'switch')
+              },
+            },
+            {
+              label: t('system.actions.common.close-position'),
+              icon: 'circle_close',
+              iconShrink: 2,
+              panel: 'close',
+              action: () => {
+                dispatch({ type: 'reset' })
+                updateState('uiDropdown', 'close')
+              },
+            },
+          ],
+        },
+      })}
+    >
       {currentStep === 'risk' && <AjnaFormContentRisk />}
       {currentStep === 'setup' && <AjnaMultiplyFormContentOpen />}
-      {currentStep === 'manage' && <>Multiply Manage UI</>}
+      {currentStep === 'manage' && <AjnaMultiplyFormContentManage />}
       {currentStep === 'transaction' && (
         <AjnaFormContentTransaction orderInformation={AjnaMultiplyFormOrder} />
       )}
---
+++ features/ajna/positions/multiply/sidebars/AjnaMultiplyFormContentManage.tsx
@@ -0,0 +1,91 @@
+import { ActionPills } from 'components/ActionPills'
+import { useAjnaProductContext } from 'features/ajna/positions/common/contexts/AjnaProductContext'
+import { useTranslation } from 'next-i18next'
+import React from 'react'
+
+export function AjnaMultiplyFormContentManage() {
+  const { t } = useTranslation()
+  const {
+    form: {
+      dispatch,
+      state: { uiDropdown, uiPill },
+      updateState,
+    },
+  } = useAjnaProductContext('multiply')
+
+  return (
+    <>
+      {uiDropdown === 'collateral' && (
+        <>
+          <ActionPills
+            active={uiPill}
+            items={[
+              {
+                id: 'deposit-multiply',
+                label: t('vault-actions.deposit'),
+                action: () => {
+                  dispatch({ type: 'reset' })
+                  updateState('uiPill', 'deposit-multiply')
+                  updateState('action', 'deposit-multiply')
+                },
+              },
+              {
+                id: 'withdraw-multiply',
+                label: t('vault-actions.withdraw'),
+                action: () => {
+                  dispatch({ type: 'reset' })
+                  updateState('uiPill', 'withdraw-multiply')
+                  updateState('action', 'withdraw-multiply')
+                },
+              },
+            ]}
+          />
+          {uiPill === 'deposit-multiply' && <>Deposit</>}
+          {uiPill === 'withdraw-multiply' && <>Withdraw</>}
+        </>
+      )}
+      {uiDropdown === 'quote' && (
+        <>
+          <ActionPills
+            active={uiPill}
+            items={[
+              {
+                id: 'buy-collateral-multiply',
+                label: t('system.actions.multiply.buy-coll'),
+                action: () => {
+                  dispatch({ type: 'reset' })
+                  updateState('uiPill', 'buy-collateral-multiply')
+                  updateState('action', 'buy-collateral-multiply')
+                },
+              },
+              {
+                id: 'payback-multiply',
+                label: t('system.actions.multiply.reduce-debt'),
+                action: () => {
+                  dispatch({ type: 'reset' })
+                  updateState('uiPill', 'payback-multiply')
+                  updateState('action', 'payback-multiply')
+                },
+              },
+              {
+                id: 'withdraw-quote-multiply',
+                label: t('vault-actions.withdraw'),
+                action: () => {
+                  dispatch({ type: 'reset' })
+                  updateState('uiPill', 'withdraw-quote-multiply')
+                  updateState('action', 'withdraw-quote-multiply')
+                },
+              },
+            ]}
+          />
+          {uiPill === 'buy-collateral-multiply' && <>Buy</>}
+          {uiPill === 'payback-multiply' && <>Payback</>}
+          {uiPill === 'withdraw-quote-multiply' && <>Withdraw</>}
+        </>
+      )}
+
+      {uiDropdown === 'switch' && <>Switch</>}
+      {uiDropdown === 'close' && <>Close</>}
+    </>
+  )
+}
