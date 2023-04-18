3f86ba8becaa91c6f43c4697a7da76dc8bc8dabe
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-04-18 13:09:21 +0200

    Liquidation overview notifications

--- features/ajna/positions/common/notifications.ts
+++ features/ajna/positions/common/notifications.ts
@@ -29,6 +29,10 @@ type EmptyPositionParams = {
   quoteToken: string
 }
 
+type TimeToLiquidationParams = {
+  time: string
+}
+
 type NotificationCallbackWithParams<P> = (
   params: P,
   action?: () => void,
@@ -38,6 +42,12 @@ const ajnaNotifications: {
   depositIsNotWithdrawable: NotificationCallbackWithParams<DepositIsNotWithdrawableParams>
   earningNoApy: NotificationCallbackWithParams<EarningNoApyParams>
   emptyPosition: NotificationCallbackWithParams<EmptyPositionParams>
+  timeToLiquidation: NotificationCallbackWithParams<TimeToLiquidationParams>
+  beingLiquidated: NotificationCallbackWithParams<null>
+  gotLiquidated: NotificationCallbackWithParams<null>
+  gotPartiallyLiquidated: NotificationCallbackWithParams<null>
+  lendingPriceFrozen: NotificationCallbackWithParams<null>
+  bucketLiquidated: NotificationCallbackWithParams<null>
 } = {
   depositIsNotWithdrawable: ({ action, message }) => ({
     title: {
@@ -85,6 +95,73 @@ const ajnaNotifications: {
     type: 'warning',
     closable: true,
   }),
+  timeToLiquidation: ({ time }) => ({
+    title: {
+      translationKey: 'ajna.position-page.common.notifications.time-to-liquidation.title',
+      params: { time },
+    },
+    message: {
+      translationKey: 'ajna.position-page.common.notifications.time-to-liquidation.message',
+    },
+    icon: 'coins_cross',
+    type: 'warning',
+    closable: true,
+  }),
+  beingLiquidated: () => ({
+    title: {
+      translationKey: 'ajna.position-page.common.notifications.being-liquidated.title',
+    },
+    message: {
+      translationKey: 'ajna.position-page.common.notifications.being-liquidated.message',
+    },
+    icon: 'coins_cross',
+    type: 'error',
+    closable: true,
+  }),
+  gotLiquidated: () => ({
+    title: {
+      translationKey: 'ajna.position-page.common.notifications.got-liquidated.title',
+    },
+    message: {
+      translationKey: 'ajna.position-page.common.notifications.got-liquidated.message',
+    },
+    icon: 'coins_cross',
+    type: 'error',
+    closable: true,
+  }),
+  gotPartiallyLiquidated: () => ({
+    title: {
+      translationKey: 'ajna.position-page.common.notifications.got-partially-liquidated.title',
+    },
+    message: {
+      translationKey: 'ajna.position-page.common.notifications.got-partially-liquidated.message',
+    },
+    icon: 'coins_cross',
+    type: 'error',
+    closable: true,
+  }),
+  lendingPriceFrozen: () => ({
+    title: {
+      translationKey: 'ajna.position-page.common.notifications.got-partially-liquidated.title',
+    },
+    message: {
+      translationKey: 'ajna.position-page.common.notifications.got-partially-liquidated.message',
+    },
+    icon: 'coins_cross',
+    type: 'error',
+    closable: true,
+  }),
+  bucketLiquidated: () => ({
+    title: {
+      translationKey: 'ajna.position-page.earn.manage.notifications.bucket-liquidated.title',
+    },
+    message: {
+      translationKey: 'ajna.position-page.earn.manage.notifications.bucket-liquidated.message',
+    },
+    icon: 'coins_cross',
+    type: 'error',
+    closable: true,
+  }),
 }
 
 export function getAjnaNotifications({
@@ -113,6 +190,11 @@ export function getAjnaNotifications({
   switch (product) {
     case 'multiply':
     case 'borrow':
+      // TODO adjust it when data source available
+      // notifications.push(ajnaNotifications.timeToLiquidation({ time: '4 days' }))
+      // notifications.push(ajnaNotifications.beingLiquidated(null))
+      // notifications.push(ajnaNotifications.gotLiquidated(null))
+      // notifications.push(ajnaNotifications.gotPartiallyLiquidated(null))
       break
     case 'earn': {
       const {
@@ -145,6 +227,10 @@ export function getAjnaNotifications({
       if (emptyPosition) {
         notifications.push(ajnaNotifications.emptyPosition({ quoteToken }))
       }
+
+      // TODO adjust it when data source available
+      // notifications.push(ajnaNotifications.lendingPriceFrozen(null))
+      // notifications.push(ajnaNotifications.bucketLiquidated(null))
 
       break
     }
