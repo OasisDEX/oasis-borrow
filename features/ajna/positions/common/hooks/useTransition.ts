b94670c198ed01b52aeaf12d06619a9673725a6a
Piotr Konowrocki <piotr@oazoapps.com>
2023-04-25 14:48:21 +0200

    reload on success

--- features/ajna/positions/common/hooks/useTransition.ts
+++ features/ajna/positions/common/hooks/useTransition.ts
@@ -1,9 +1,11 @@
 import BigNumber from 'bignumber.js'
 import { AjnaFormAction } from 'features/ajna/common/types'
 import { VaultType } from 'features/generalManageVault/vaultType'
+import { jwtAuthGetToken } from 'features/shared/jwt'
 import { saveVaultUsingApi$ } from 'features/shared/vaultApi'
 import { useAccount } from 'helpers/useAccount'
 import { LendingProtocol } from 'lendingProtocols'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 interface TransitionProps {
@@ -14,21 +16,24 @@ interface TransitionProps {
 }
 
 export function useTransition({ action, positionId, product, protocol }: TransitionProps) {
+  const { reload } = useRouter()
   const { chainId, walletAddress } = useAccount()
   const [isTransitionWairingForApproval, setIsTransitionWairingForApproval] =
     useState<boolean>(false)
   const [isTransitionInProgress, setIsTransitionInProgress] = useState<boolean>(false)
 
   const transitionHandler = () => {
-    if (positionId && walletAddress && product && chainId && protocol) {
+    const jwtToken = jwtAuthGetToken(walletAddress as string)
+
+    if (jwtToken && positionId && product && chainId && protocol) {
       setIsTransitionInProgress(true)
       saveVaultUsingApi$(
         new BigNumber(positionId),
-        walletAddress,
+        jwtToken,
         product,
         chainId,
-        protocol,
-      ).subscribe()
+        protocol.toLowerCase(),
+      ).subscribe(reload)
     } else throw new Error(`Not enough position data provided`)
   }
 
