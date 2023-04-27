5465374af54e195da8b7c90b0b56efe2e91c750a
Marcin Ciarka <marcin@oazoapps.com>
2023-04-27 16:52:47 +0200

    quick network switcher changes

--- components/navigation/NavigationNetworkSwitcher.tsx
+++ components/navigation/NavigationNetworkSwitcher.tsx
@@ -22,7 +22,7 @@ const filterNetworks = (network: NetworkConfig) => {
   return false
 }
 
-export function NavigationPickNetwork() {
+export function NavigationNetworkSwitcher() {
   const { web3Context$ } = useAppContext()
   const [{ chains: usableChains, settingChain, connectedChain }, setChain] = useSetChain()
   const currentNetworkName = useNetworkName()
@@ -57,81 +57,78 @@ export function NavigationPickNetwork() {
     [connectedChain, setChain, setCustomNetwork, web3Context?.status],
   )
   return (
-    <Box sx={{ mr: 2 }}>
-      <NavigationOrb
-        customIcon={NavigationPickNetworkIcon}
-        outline={{
-          outline: '1px solid',
-          outlineColor: 'primary100',
-        }}
-      >
-        {(_isOpen) => (
-          <>
-            <Box
-              sx={{
-                width: ['100%', '260px'],
-                gap: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                padding: 3,
-                overflow: 'hidden',
-              }}
-            >
-              {networks.filter(filterNetworks).map((network) => {
-                const isCurrentNetwork = network.name === currentNetworkName
-                return (
-                  <Button
-                    variant="networkPicker"
+    <NavigationOrb
+      customIcon={NavigationNetworkSwitcherIcon}
+      outline={{
+        outline: '1px solid',
+        outlineColor: 'primary100',
+      }}
+    >
+      {(_isOpen) => (
+        <>
+          <Box
+            sx={{
+              width: ['100%', '260px'],
+              gap: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              padding: 3,
+              overflow: 'hidden',
+            }}
+          >
+            {networks.filter(filterNetworks).map((network) => {
+              const isCurrentNetwork = network.name === currentNetworkName
+              return (
+                <Button
+                  variant="networkPicker"
+                  sx={{
+                    fontWeight: isCurrentNetwork ? '600' : '400',
+                    whiteSpace: 'pre',
+                    color: isCurrentNetwork ? 'primary100' : 'neutral80',
+                    ':hover': {
+                      color: 'primary100',
+                    },
+                  }}
+                  onClick={changeChain(network.name)}
+                  disabled={
+                    !usableChains.map(({ label }) => label).includes(network.label) || settingChain
+                  }
+                  key={network.hexId}
+                >
+                  <Image
+                    src={network.icon}
                     sx={{
-                      fontWeight: isCurrentNetwork ? '600' : '400',
-                      whiteSpace: 'pre',
-                      color: isCurrentNetwork ? 'primary100' : 'neutral80',
-                      ':hover': {
-                        color: 'primary100',
-                      },
+                      mr: 3,
+                      minWidth: 4,
+                      minHeight: 4,
                     }}
-                    onClick={changeChain(network.name)}
-                    disabled={
-                      !usableChains.map(({ label }) => label).includes(network.label) ||
-                      settingChain
-                    }
-                    key={network.hexId}
+                  />
+                  {network.label}
+                  <Box
+                    sx={{
+                      width: '100%',
+                      textAlign: 'right',
+                      position: 'relative',
+                      opacity: isCurrentNetwork ? 1 : 0,
+                      left: isCurrentNetwork ? 0 : 2,
+                      transition: '0.2s opacity, 0.2s left',
+                      mb: '-3px',
+                    }}
                   >
-                    <Image
-                      src={network.icon}
-                      sx={{
-                        mr: 3,
-                        minWidth: 4,
-                        minHeight: 4,
-                      }}
-                    />
-                    {network.label}
-                    <Box
-                      sx={{
-                        width: '100%',
-                        textAlign: 'right',
-                        position: 'relative',
-                        opacity: isCurrentNetwork ? 1 : 0,
-                        left: isCurrentNetwork ? 0 : 2,
-                        transition: '0.2s opacity, 0.2s left',
-                        mb: '-3px',
-                      }}
-                    >
-                      <Icon name="tick" color="interactive100" />
-                    </Box>
-                  </Button>
-                )
-              })}
-            </Box>
-            {settingChain && <AppSpinnerWholePage />}
-          </>
-        )}
-      </NavigationOrb>
-    </Box>
+                    <Icon name="tick" color="interactive100" />
+                  </Box>
+                </Button>
+              )
+            })}
+          </Box>
+          {settingChain && <AppSpinnerWholePage />}
+        </>
+      )}
+    </NavigationOrb>
   )
 }
 
-function NavigationPickNetworkIcon(_isOpen: boolean) {
+function NavigationNetworkSwitcherIcon(_isOpen: boolean) {
   const [{ connecting: isWalletConnecting }] = useConnectWallet()
   const currentNetworkName = useNetworkName()
   return isWalletConnecting ? (
