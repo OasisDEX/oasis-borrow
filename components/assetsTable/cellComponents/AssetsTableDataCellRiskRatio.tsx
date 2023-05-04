44683f20dcbaa47c7a9f1c8ff4184551bd39f77f
Piotr Konowrocki <piotr@oazoapps.com>
2023-05-04 11:34:09 +0200

    remove duplicated AssetsTableDataCellProtocols

--- components/assetsTable/cellComponents/AssetsTableDataCellRiskRatio.tsx
+++ components/assetsTable/cellComponents/AssetsTableDataCellRiskRatio.tsx
@@ -3,15 +3,6 @@ import { formatPercent } from 'helpers/formatters/format'
 import React from 'react'
 import { Text } from 'theme-ui'
 
-const assetsTableDataCellProtocolBackgrounds = {
-  'Aave v2': 'linear-gradient(229.92deg, #B6509E 15.42%, #2EBAC6 84.42%)',
-  'Aave v3': 'linear-gradient(229.92deg, #B6509E 15.42%, #2EBAC6 84.42%)',
-  Ajna: 'linear-gradient(90deg, #F154DB 0%, #974EEA 100%)',
-  Maker: 'linear-gradient(152.36deg, #218F6F 17.19%, #66C5A9 95.07%)',
-}
-
-export type AssetsTableDataCellProtocols = keyof typeof assetsTableDataCellProtocolBackgrounds
-
 interface AssetsTableDataCellRiskRatioProps {
   level: number
   isAtRiskDanger: boolean
