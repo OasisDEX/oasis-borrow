257e361fb8c625b278e347d7526a94c69fc68b95
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-06-05 11:40:19 +0200

    Make smaller sample. Make larger `largePageDataBytes` (#2355)

--- next.config.js
+++ next.config.js
@@ -182,7 +182,9 @@ const baseConfig = {
     ]
   },
   transpilePackages: ['@lifi/widget', '@lifi/wallet-management'],
-  largePageDataBytes: 200 * 1024, // 200 KB. The default one is 128 KB, but we have a lot of that kind of errors, so we increase it.
+  experimental: {
+    largePageDataBytes: 256 * 1024, // 256 KB. The default one is 128 KB, but we have a lot of that kind of errors, so we increase it.
+  },
 }
 
 module.exports = withBundleAnalyzer(withMDX(baseConfig))
