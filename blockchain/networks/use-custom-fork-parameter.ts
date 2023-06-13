bc2170edfe036c30b671d1715988302d0ef55bf2
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-06-13 22:19:07 +0200

    guard for fork parameters

--- blockchain/networks/use-custom-fork-parameter.ts
+++ blockchain/networks/use-custom-fork-parameter.ts
@@ -1,6 +1,6 @@
 import { useLocalStorage } from 'helpers/useLocalStorage'
 
-import { NetworkNames } from './network-names'
+import { isSupportedNetwork, NetworkNames } from './network-names'
 
 export type CustomForkParameterFieldsType = 'url' | 'id' | 'isAddedToWallet'
 export type CustomForkParameterType = Partial<
@@ -9,9 +9,30 @@ export type CustomForkParameterType = Partial<
 
 export const CustomForkStorageKey = 'ForkNetwork'
 
+export function isValidCustomForkParameter(
+  element?: CustomForkParameterType,
+): element is CustomForkParameterType {
+  if (!element) {
+    return false
+  }
+  if (typeof element !== 'object') {
+    return false
+  }
+  return Object.entries(element).every(([key, value]) => {
+    if (!isSupportedNetwork(key)) {
+      return false
+    }
+    if (typeof value !== 'object') {
+      return false
+    }
+    return value.hasOwnProperty('url') && value.hasOwnProperty('id')
+  })
+}
+
 export function useCustomForkParameter() {
   return useLocalStorage<CustomForkParameterType>(
     CustomForkStorageKey,
     {} as CustomForkParameterType,
+    isValidCustomForkParameter,
   )
 }
