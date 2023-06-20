2d893636343f48cfffcedded875a1b47c5517dfd
Marcin Ciarka <marcin@oazoapps.com>
2023-06-20 16:40:07 +0200

    fix getStaticPaths on assets pages

--- pages/[networkOrProduct]/[token].tsx
+++ pages/[networkOrProduct]/[token].tsx
@@ -1,12 +1,39 @@
+import { ALL_ASSETS, productHubOptionsMap } from 'features/productHub/meta'
+import { ProductHubProductType } from 'features/productHub/types'
 import ProductHubRouteHandler from 'features/productHub/views/ProductHubRouteHandler'
-import {
-  getStaticPaths as indexGetStaticPaths,
-  getStaticProps as indexGetStaticProps,
-} from 'pages/[networkOrProduct]/index'
+import { GetStaticPaths } from 'next'
+import { getStaticProps as indexGetStaticProps } from 'pages/[networkOrProduct]/index'
 
 export default ProductHubRouteHandler
 
-// actual logic is in index.tsx
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
+  const paths =
+    locales?.flatMap((locale) =>
+      Object.values(ProductHubProductType)
+        .flatMap((product) =>
+          Object.values(productHubOptionsMap[product].tokens).map((token) =>
+            token.value !== ALL_ASSETS
+              ? [product, ...(token.value !== ALL_ASSETS ? [token.value] : [])]
+              : undefined,
+          ),
+        )
+        .filter((param) => param !== undefined)
+        .map((param) => ({
+          params: {
+            networkOrProduct: param![0],
+            token: param![1],
+          },
+          locale,
+        })),
+    ) || []
 
-export const getStaticPaths = indexGetStaticPaths
+  console.log('paths', JSON.stringify(paths, null, 2))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+// actual logic is in index.tsx
 export const getStaticProps = indexGetStaticProps
--- pages/[networkOrProduct]/index.tsx
+++ pages/[networkOrProduct]/index.tsx
@@ -1,5 +1,4 @@
 import { getProductHubStaticProps } from 'features/productHub/helpers/getProductHubStaticProps'
-import { ALL_ASSETS, productHubOptionsMap } from 'features/productHub/meta'
 import { ProductHubProductType } from 'features/productHub/types'
 import ProductHubRouteHandler from 'features/productHub/views/ProductHubRouteHandler'
 import { GetStaticPaths, GetStaticProps } from 'next'
@@ -9,18 +8,15 @@ export default ProductHubRouteHandler
 export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
   const paths =
     locales?.flatMap((locale) =>
-      Object.values(ProductHubProductType)
-        .flatMap((product) =>
-          Object.values(productHubOptionsMap[product].tokens).map((token) => [
-            product,
-            ...(token.value !== ALL_ASSETS ? [token.value] : []),
-          ]),
-        )
-        .map((param) => ({
-          params: { networkOrProduct: param[0], token: param[1] || '' },
-          locale,
-        })),
+      Object.values(ProductHubProductType).flatMap((product) => ({
+        params: {
+          networkOrProduct: product,
+        },
+        locale,
+      })),
     ) || []
+
+  console.log('paths', JSON.stringify(paths, null, 2))
 
   return {
     paths,
