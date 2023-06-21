764aa1511d1a363b0e112f0fb997e40e35d794cb
Piotr Konowrocki <piotr@oazoapps.com>
2023-06-21 14:38:01 +0200

    format fix

--- components/assetsTable/cellComponents/AssetsTableDataCellInactive.tsx
+++ components/assetsTable/cellComponents/AssetsTableDataCellInactive.tsx
@@ -2,5 +2,9 @@ import React, { PropsWithChildren } from 'react'
 import { Text } from 'theme-ui'
 
 export function AssetsTableDataCellInactive({ children = 'n/a' }: PropsWithChildren<{}>) {
-  return <Text as="span" sx={{ color: 'neutral80' }}>{children}</Text>
+  return (
+    <Text as="span" sx={{ color: 'neutral80' }}>
+      {children}
+    </Text>
+  )
 }
