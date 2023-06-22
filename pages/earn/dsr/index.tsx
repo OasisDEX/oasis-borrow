9e1dfb87bfc0f5cac2d9f6a9aefe93fa149d8b73
Marcin Ciarka <marcin@oazoapps.com>
2023-06-22 19:24:21 +0200

    Add urls in the product hub table for: aave v2, v3 and maker

---
+++ pages/earn/dsr/index.tsx
@@ -0,0 +1,28 @@
+import { AppLayout } from 'components/Layouts'
+import { useAccount } from 'helpers/useAccount'
+import { GetServerSidePropsContext } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useRouter } from 'next/router'
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale!, ['common'])),
+      walletAddress: ctx.query.wallet || null,
+    },
+  }
+}
+
+function DsrProxyPage({ walletAddress }: { walletAddress: string }) {
+  const { walletAddress: walletContextAddress } = useAccount()
+  const { replace } = useRouter()
+  if (!walletAddress && walletContextAddress) {
+    void replace(`/earn/dsr/${walletContextAddress}`)
+  }
+
+  return null
+}
+
+DsrProxyPage.layout = AppLayout
+
+export default DsrProxyPage
