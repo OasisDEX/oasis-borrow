c34f4fbdce87830685c37fcea9ef1ff022449aeb
Piotr Konowrocki <piotr@oazoapps.com>
2023-06-23 11:06:29 +0200

    start multiply cards

--- handlers/product-hub/promo-card-collections-parsers/home.ts
+++ handlers/product-hub/promo-card-collections-parsers/home.ts
@@ -1,30 +1,47 @@
+import { NetworkNames } from 'blockchain/networks'
 import {
   ProductHubItem,
   ProductHubProductType,
   ProductHubPromoCards,
 } from 'features/productHub/types'
-import { findByIlk } from 'handlers/product-hub/helpers'
 import {
   automationEnabledPill,
   earnStakingRewardsPill,
+  findByIlk,
+  findByTokenPair,
   highestAvailableLtvPill,
+  highestMultiplePill,
   lowestBorrowingCostPill,
+  parseAaveV3MultiplyPromoCard,
   parseMakerBorrowPromoCard,
-} from 'handlers/product-hub/helpers/parseMakerPromoCards'
+} from 'handlers/product-hub/helpers'
 import { EXTERNAL_LINKS } from 'helpers/applicationLinks'
 import { LendingProtocol } from 'lendingProtocols'
 
 export default function (table: ProductHubItem[]): ProductHubPromoCards {
   const makerProducts = table.filter((product) => product.protocol === LendingProtocol.Maker)
-  const borrowishProducts = makerProducts.filter(({ product }) =>
+  const aaveV3EthereumProducts = table.filter(
+    ({ network, protocol }) =>
+      protocol === LendingProtocol.AaveV3 && network === NetworkNames.ethereumMainnet,
+  )
+  const makerBorrowishProducts = makerProducts.filter(({ product }) =>
     product.includes(ProductHubProductType.Borrow),
   )
+  const aaveV3EthereumMultiplyProducts = aaveV3EthereumProducts.filter(({ product }) =>
+    product.includes(ProductHubProductType.Multiply),
+  )
 
-  const ETHCProduct = findByIlk(borrowishProducts, 'ETH-C')
-  const WSTETHBProduct = findByIlk(borrowishProducts, 'WSTETH-B')
-  const RETHAProduct = findByIlk(borrowishProducts, 'RETH-A')
-  const WBTCBProduct = findByIlk(borrowishProducts, 'WBTC-B')
-  const WBTCCProduct = findByIlk(borrowishProducts, 'WBTC-C')
+  const ETHCProduct = findByIlk(makerBorrowishProducts, 'ETH-C')
+  const WSTETHAProduct = findByIlk(makerBorrowishProducts, 'WSTETH-A')
+  const WSTETHBProduct = findByIlk(makerBorrowishProducts, 'WSTETH-B')
+  const RETHAProduct = findByIlk(makerBorrowishProducts, 'RETH-A')
+  const WBTCBProduct = findByIlk(makerBorrowishProducts, 'WBTC-B')
+  const WBTCCProduct = findByIlk(makerBorrowishProducts, 'WBTC-C')
+
+  const ETHUSDCAaveV3BorrowishProduct = findByTokenPair(aaveV3EthereumMultiplyProducts, [
+    'ETH',
+    'USDC',
+  ])
 
   const promoCardLearnAboutBorrow = {
     icon: 'selectBorrow',
@@ -33,38 +50,54 @@ export default function (table: ProductHubItem[]): ProductHubPromoCards {
     link: { href: EXTERNAL_LINKS.KB.WHAT_IS_BORROW, label: { key: 'Learn more' } },
   }
 
-  const promoCardETHC = {
+  const promoCardETHCBorrow = {
     ...parseMakerBorrowPromoCard('ETH', 'DAI', ETHCProduct),
     pills: [lowestBorrowingCostPill, automationEnabledPill],
   }
-  const promoCardWSTETHB = {
+  const promoCardWSTETHBBorrow = {
     ...parseMakerBorrowPromoCard('WSTETH', 'DAI', WSTETHBProduct),
     pills: [earnStakingRewardsPill, automationEnabledPill],
   }
-  const promoCardRETHA = {
+  const promoCardRETHABorrow = {
     ...parseMakerBorrowPromoCard('RETH', 'DAI', RETHAProduct),
     pills: [earnStakingRewardsPill, automationEnabledPill],
   }
-  const promoCardWBTCB = {
+  const promoCardWBTCBBorrow = {
     ...parseMakerBorrowPromoCard('WBTC', 'DAI', WBTCBProduct),
     pills: [highestAvailableLtvPill, automationEnabledPill],
   }
-  const promoCardWBTCC = {
+  const promoCardWBTCCBorrow = {
     ...parseMakerBorrowPromoCard('WBTC', 'DAI', WBTCCProduct),
     pills: [lowestBorrowingCostPill, automationEnabledPill],
+  }
+  const promoCardWSTETHABorrow = {
+    ...parseMakerBorrowPromoCard('WSTETH', 'DAI', WSTETHAProduct),
+    pills: [earnStakingRewardsPill, automationEnabledPill],
+  }
+
+  const promoCardETHUSDCAaveV3Multiply = {
+    ...parseAaveV3MultiplyPromoCard('ETH', 'USDC', ETHUSDCAaveV3BorrowishProduct),
+    pills: [
+      highestMultiplePill,
+      {
+        label: { key: 'product-hub.promo-cards.long-token', props: { token: 'ETH' } },
+      },
+    ],
   }
 
   return {
     [ProductHubProductType.Borrow]: {
-      default: [promoCardETHC, promoCardWSTETHB, promoCardWBTCC],
+      default: [promoCardETHCBorrow, promoCardWSTETHBBorrow, promoCardWBTCCBorrow],
       tokens: {
-        ETH: [promoCardETHC, promoCardWSTETHB, promoCardRETHA],
-        WBTC: [promoCardWBTCC, promoCardWBTCB, promoCardLearnAboutBorrow],
+        ETH: [promoCardETHCBorrow, promoCardWSTETHBBorrow, promoCardRETHABorrow],
+        WBTC: [promoCardWBTCCBorrow, promoCardWBTCBBorrow, promoCardLearnAboutBorrow],
       },
     },
     [ProductHubProductType.Multiply]: {
       default: [],
-      tokens: {},
+      tokens: {
+        ETH: [promoCardETHUSDCAaveV3Multiply, promoCardWSTETHABorrow],
+      },
     },
     [ProductHubProductType.Earn]: {
       default: [],
