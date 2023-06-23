adc91eb75e36aaffd6fa69a1f52be8ea032d846a
Piotr Konowrocki <piotr@oazoapps.com>
2023-06-23 11:57:46 +0200

    move swap and discover

--- components/navigation/content/SwapWidgetOrb.tsx
+++ components/navigation/content/SwapWidgetOrb.tsx
@@ -1,13 +1,11 @@
 import { useAppContext } from 'components/AppContextProvider'
 import { NavigationOrb } from 'components/navigation/NavigationMenuOrb'
-import { SwapWidgetShowHide } from 'components/swapWidget/SwapWidgetShowHide'
 import {
   SWAP_WIDGET_CHANGE_SUBJECT,
   SwapWidgetChangeAction,
 } from 'features/swapWidget/SwapWidgetChange'
 import { useOnboarding } from 'helpers/useOnboarding'
 import React, { useState } from 'react'
-import { Box } from 'theme-ui'
 
 export function SwapWidgetOrb() {
   const { uiChanges } = useAppContext()
@@ -15,19 +13,16 @@ export function SwapWidgetOrb() {
   const [exchangeOpened, setExchangeOpened] = useState(false)
 
   return (
-    <Box>
-      <NavigationOrb
-        beacon={!exchangeOnboarded && !exchangeOpened}
-        icon="exchange"
-        iconSize={18}
-        onClick={() => {
-          setExchangeOpened(true)
-          uiChanges.publish<SwapWidgetChangeAction>(SWAP_WIDGET_CHANGE_SUBJECT, {
-            type: 'open',
-          })
-        }}
-      />
-      <SwapWidgetShowHide />
-    </Box>
+    <NavigationOrb
+      beacon={!exchangeOnboarded && !exchangeOpened}
+      icon="exchange"
+      iconSize={18}
+      onClick={() => {
+        setExchangeOpened(true)
+        uiChanges.publish<SwapWidgetChangeAction>(SWAP_WIDGET_CHANGE_SUBJECT, {
+          type: 'open',
+        })
+      }}
+    />
   )
 }
