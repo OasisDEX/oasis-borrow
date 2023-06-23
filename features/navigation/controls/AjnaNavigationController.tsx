ceff534ed9db1773b3b9db2e34f92fce62ee57aa
Piotr Konowrocki <piotr@oazoapps.com>
2023-06-23 12:06:03 +0200

    add swap widget to ajna

--- features/navigation/controls/AjnaNavigationController.tsx
+++ features/navigation/controls/AjnaNavigationController.tsx
@@ -1,6 +1,7 @@
 import { useAppContext } from 'components/AppContextProvider'
 import { MyPositionsLink } from 'components/navigation/content/MyPositionsLink'
 import { Navigation, navigationBreakpoints } from 'components/navigation/Navigation'
+import { SwapWidgetShowHide } from 'components/swapWidget/SwapWidgetShowHide'
 import { NavigationActionsController } from 'features/navigation/controls/NavigationActionsController'
 import {
   SWAP_WIDGET_CHANGE_SUBJECT,
@@ -17,43 +18,46 @@ export function AjnaNavigationController() {
   const isViewBelowXl = useMediaQuery(`(max-width: ${navigationBreakpoints[3]})`)
 
   return (
-    <Navigation
-      pill={{ label: 'Ajna', color: ['#f154db', '#974eea'] }}
-      links={[
-        {
-          label: 'Borrow',
-          link: INTERNAL_LINKS.ajnaBorrow,
-        },
-        {
-          label: 'Multiply',
-          link: INTERNAL_LINKS.ajnaMultiply,
-        },
-        {
-          label: 'Earn',
-          link: INTERNAL_LINKS.ajnaEarn,
-        },
-        ...(!isViewBelowXl
-          ? [
-              {
-                label: 'Swap',
-                onClick: () => {
-                  uiChanges.publish<SwapWidgetChangeAction>(SWAP_WIDGET_CHANGE_SUBJECT, {
-                    type: 'open',
-                  })
+    <>
+      <Navigation
+        pill={{ label: 'Ajna', color: ['#f154db', '#974eea'] }}
+        links={[
+          {
+            label: 'Borrow',
+            link: INTERNAL_LINKS.ajnaBorrow,
+          },
+          {
+            label: 'Multiply',
+            link: INTERNAL_LINKS.ajnaMultiply,
+          },
+          {
+            label: 'Earn',
+            link: INTERNAL_LINKS.ajnaEarn,
+          },
+          ...(!isViewBelowXl
+            ? [
+                {
+                  label: 'Swap',
+                  onClick: () => {
+                    uiChanges.publish<SwapWidgetChangeAction>(SWAP_WIDGET_CHANGE_SUBJECT, {
+                      type: 'open',
+                    })
+                  },
                 },
-              },
-              ...(isConnected
-                ? [
-                    {
-                      label: <MyPositionsLink />,
-                      link: `/owner/${walletAddress}`,
-                    },
-                  ]
-                : []),
-            ]
-          : []),
-      ]}
-      actions={<NavigationActionsController isConnected={isConnected} />}
-    />
+                ...(isConnected
+                  ? [
+                      {
+                        label: <MyPositionsLink />,
+                        link: `/owner/${walletAddress}`,
+                      },
+                    ]
+                  : []),
+              ]
+            : []),
+        ]}
+        actions={<NavigationActionsController isConnected={isConnected} />}
+      />
+      <SwapWidgetShowHide />
+    </>
   )
 }
