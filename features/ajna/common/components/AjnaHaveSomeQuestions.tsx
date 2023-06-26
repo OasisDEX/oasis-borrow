7e3c605dfe5d4a2b3caa2eee615600c34ab577be
Marcin Ciarka <marcin@oazoapps.com>
2023-06-26 18:20:55 +0200

    Revert the twitter handle change (#2435)

--- features/ajna/common/components/AjnaHaveSomeQuestions.tsx
+++ features/ajna/common/components/AjnaHaveSomeQuestions.tsx
@@ -65,7 +65,7 @@ export function AjnaHaveSomeQuestions() {
               text: t('ajna.learn.oasis-discord'),
             },
             {
-              href: 'https://twitter.com/summerfi_',
+              href: 'https://twitter.com/summerfinance_',
               text: t('ajna.learn.oasis-twitter'),
             },
           ]}
