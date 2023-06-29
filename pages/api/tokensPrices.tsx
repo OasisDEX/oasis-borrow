fa37a03059fe0bd39a57a8fce7e8c65722f2da57
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-06-29 11:36:08 +0200

    Add second layer cache and better error messages

--- pages/api/tokensPrices.tsx
+++ pages/api/tokensPrices.tsx
@@ -2,14 +2,23 @@ import { cacheObject } from 'helpers/api/cacheObject'
 import { tokenTickers } from 'helpers/api/tokenTickers'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const getTicker = cacheObject(tokenTickers, 2 * 60)
+const getTicker = cacheObject(tokenTickers, 2 * 60, 'token-tickers')
 
 async function tokensPricesHandler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case 'GET':
-      const tickers = await getTicker()
-      res.setHeader('Cache-Control', 'public, s-maxage=90, stale-while-revalidate=119')
-      return res.status(200).json(tickers.data)
+      try {
+        const tickers = await getTicker()
+        if (tickers?.data) {
+          res.setHeader('Cache-Control', 'public, s-maxage=90, stale-while-revalidate=119')
+          return res.status(200).json(tickers.data)
+        }
+
+        return res.status(500).json({ error: 'Token tickers unavailable' })
+      } catch (e) {
+        console.error(e)
+        return res.status(500).end()
+      }
     default:
       return res.status(405).end()
   }
