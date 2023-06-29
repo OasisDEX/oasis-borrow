995bfd5ddf9fdf62f74dd6ef3d0d177b0b464d00
piekczyk <sebastian@oazoapps.com>
2023-06-29 10:45:56 +0200

    Remove long / short wording in setup & manage flow (#2461)
    * Remove long / short wording in setup & manage flow
    
    * remove unnecessary params

--- features/ajna/positions/common/views/AjnaFormView.tsx
+++ features/ajna/positions/common/views/AjnaFormView.tsx
@@ -39,7 +39,7 @@ export function AjnaFormView({
   const [context] = useObservable(context$)
   const { walletAddress } = useAccount()
   const {
-    environment: { collateralToken, dpmProxy, flow, isOwner, isShort, product, quoteToken },
+    environment: { collateralToken, dpmProxy, flow, isOwner, product, quoteToken },
     steps: {
       currentStep,
       editingStep,
@@ -163,7 +163,7 @@ export function AjnaFormView({
       isTxSuccess
         ? `ajna.position-page.common.form.transaction.success-${flow}`
         : `ajna.position-page.common.form.transaction.progress-${flow}`,
-      { collateralToken, quoteToken, strategy: t(isShort ? 'short' : 'long') },
+      { collateralToken, quoteToken },
     ),
     txDetails,
   })
