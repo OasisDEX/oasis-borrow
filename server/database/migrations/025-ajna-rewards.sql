1b30d6869d5f645be3a608f53418c930c8069a77
halaprix <panicz.klos@gmail.com>
2023-07-05 09:28:44 +0200

    feat: add `chain_id`

--- server/database/migrations/025-ajna-rewards.sql
+++ server/database/migrations/025-ajna-rewards.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "ajna_rewards_weekly_claim" (
     "id" SERIAL NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "chain_id" INTEGER NOT NULL,
     "user_address" TEXT NOT NULL,
     "amount" TEXT NOT NULL,
     "week_number" INTEGER NOT NULL,
@@ -14,6 +15,7 @@ CREATE TABLE "ajna_rewards_weekly_claim" (
 CREATE TABLE "ajna_rewards_daily_claim" (
     "id" SERIAL NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "chain_id" INTEGER NOT NULL,
     "user_address" TEXT NOT NULL,
     "amount" TEXT NOT NULL,
     "week_number" INTEGER NOT NULL,
@@ -24,17 +26,21 @@ CREATE TABLE "ajna_rewards_daily_claim" (
 
 -- CreateTable
 CREATE TABLE "ajna_rewards_merkle_tree" (
-    "week_number" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "chain_id" INTEGER NOT NULL,
+    "week_number" INTEGER NOT NULL,
     "tree_root" TEXT NOT NULL,
-    "tx_processed" BOOLEAN NOT NULL DEFAULT false
+    "tx_processed" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "ajna_rewards_merkle_tree_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ajna_rewards_weekly_claim_week_number_user_address_key" ON "ajna_rewards_weekly_claim"("week_number", "user_address");
+CREATE UNIQUE INDEX "ajna_rewards_weekly_claim_week_number_user_address_chain_id_key" ON "ajna_rewards_weekly_claim"("week_number", "user_address", "chain_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ajna_rewards_daily_claim_day_number_user_address_key" ON "ajna_rewards_daily_claim"("day_number", "user_address");
+CREATE UNIQUE INDEX "ajna_rewards_daily_claim_day_number_user_address_chain_id_key" ON "ajna_rewards_daily_claim"("day_number", "user_address", "chain_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ajna_rewards_merkle_tree_week_number_key" ON "ajna_rewards_merkle_tree"("week_number");
+CREATE UNIQUE INDEX "ajna_rewards_merkle_tree_week_number_chain_id_key" ON "ajna_rewards_merkle_tree"("week_number", "chain_id");
