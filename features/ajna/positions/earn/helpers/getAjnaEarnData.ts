0819be05cb7594cf5c162203e4e3d10e1d06a3ca
Piotr Konowrocki <piotr@oazoapps.com>
2023-07-11 13:07:58 +0200

    🧿 Fix passing earn subgraph for empty positions (#2510)

--- features/ajna/positions/earn/helpers/getAjnaEarnData.ts
+++ features/ajna/positions/earn/helpers/getAjnaEarnData.ts
@@ -3,31 +3,45 @@ import BigNumber from 'bignumber.js'
 import { loadSubgraph } from 'features/subgraphLoader/useSubgraphLoader'
 import { zero } from 'helpers/zero'
 
+const defaultResponse = {
+  lps: zero,
+  priceIndex: null,
+  nftID: null,
+  cumulativeDeposit: zero,
+  cumulativeFees: zero,
+  cumulativeWithdraw: zero,
+}
+
 export const getAjnaEarnData: GetEarnData = async (proxy: string) => {
   const { response } = await loadSubgraph('Ajna', 'getEarnData', {
     dpmProxyAddress: proxy.toLowerCase(),
   })
 
-  if (response && 'account' in response && response.account) {
-    const earnPosition = response.account.earnPositions.find((position) => position.lps > 0)
+  if (
+    response &&
+    'account' in response &&
+    response.account &&
+    response.account.earnPositions.length
+  ) {
+    const earnPosition = response.account.earnPositions[0]
+    const cumulativeValues = {
+      cumulativeDeposit: new BigNumber(earnPosition.account.cumulativeDeposit),
+      cumulativeFees: new BigNumber(earnPosition.account.cumulativeFees),
+      cumulativeWithdraw: new BigNumber(earnPosition.account.cumulativeWithdraw),
+    }
 
-    if (earnPosition)
-      return {
-        lps: new BigNumber(earnPosition.lps),
-        priceIndex: new BigNumber(earnPosition.index),
-        nftID: earnPosition.nft?.id || null,
-        cumulativeDeposit: new BigNumber(earnPosition.account.cumulativeDeposit),
-        cumulativeFees: new BigNumber(earnPosition.account.cumulativeFees),
-        cumulativeWithdraw: new BigNumber(earnPosition.account.cumulativeWithdraw),
-      }
+    return earnPosition.lps > 0
+      ? {
+          lps: new BigNumber(earnPosition.lps),
+          priceIndex: new BigNumber(earnPosition.index),
+          nftID: earnPosition.nft?.id || null,
+          ...cumulativeValues,
+        }
+      : {
+          ...defaultResponse,
+          ...cumulativeValues,
+        }
   }
 
-  return {
-    lps: zero,
-    priceIndex: null,
-    nftID: null,
-    cumulativeDeposit: zero,
-    cumulativeFees: zero,
-    cumulativeWithdraw: zero,
-  }
+  return defaultResponse
 }
