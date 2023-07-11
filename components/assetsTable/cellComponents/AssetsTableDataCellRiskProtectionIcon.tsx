9851da4fc3bb71c32803343b703a9d1d5310759b
Marcin Ciarka <marcin@oazoapps.com>
2023-07-11 17:45:16 +0200

    change the icon back

--- components/assetsTable/cellComponents/AssetsTableDataCellRiskProtectionIcon.tsx
+++ components/assetsTable/cellComponents/AssetsTableDataCellRiskProtectionIcon.tsx
@@ -21,27 +21,7 @@ export function AssetsTableDataCellRiskProtectionIcon({
         justifyContent: 'flex-end',
       }}
     >
-      <Button
-        variant="action"
-        sx={{
-          pointerEvents: 'none',
-          backgroundColor: 'success100',
-          borderRadius: 'round',
-          color: 'transparent', // text is hidden, its only needed to set width and height
-          position: 'relative',
-          overflow: 'hidden',
-        }}
-      >
-        {
-          // all of this is just to have the button as same width as all of the other buttons
-          isOwner ? t('discover.table.activate') : t('discover.table.inactive')
-        }
-        <Icon
-          name="stop_loss_active"
-          size={38}
-          sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
-        />
-      </Button>
+      <Icon name="stop_loss_active" size={38} />
     </Flex>
   ) : (
     <Button
