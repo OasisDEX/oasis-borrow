3a85bb9fdc0f4088474b155389db9ddfb8001563
piekczyk <sebastian@oazoapps.com>
2023-07-13 16:30:25 +0200

    Ajna earn input fix (#2544)

--- features/ajna/positions/earn/helpers/resolveLendingPriceIfOutsideRange.ts
+++ features/ajna/positions/earn/helpers/resolveLendingPriceIfOutsideRange.ts
@@ -1,28 +1,10 @@
 import BigNumber from 'bignumber.js'
-import {
-  mappedAjnaBuckets,
-  snapToPredefinedValues,
-} from 'features/ajna/positions/earn/helpers/snapToPredefinedValues'
-import { zero } from 'helpers/zero'
+import { snapToPredefinedValues } from 'features/ajna/positions/earn/helpers/snapToPredefinedValues'
 
 export const resolveLendingPriceIfOutsideRange = ({
   manualAmount,
-  min,
-  max,
-  fallbackValue,
 }: {
   manualAmount: BigNumber
-  min: BigNumber
-  max: BigNumber
-  fallbackValue: BigNumber
 }) => {
-  const snappedValue = snapToPredefinedValues(manualAmount)
-  const index = mappedAjnaBuckets.indexOf(snappedValue)
-  const selectedValue = mappedAjnaBuckets.at(index) || zero
-
-  if (selectedValue.gt(max) || selectedValue.lt(min)) {
-    return fallbackValue
-  }
-
-  return selectedValue
+  return snapToPredefinedValues(manualAmount)
 }
