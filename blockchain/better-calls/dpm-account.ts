456d6d0dd7ddc8827db2026e120f87aba8984627
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-07-19 09:00:25 +0200

    Merge pull request #2571
    * Revert "Add access list to gas estimation and execution"
    
    * Revert "Add access list to gas estimation and execution (#2568)"

--- blockchain/better-calls/dpm-account.ts
+++ blockchain/better-calls/dpm-account.ts
@@ -73,17 +73,6 @@ export async function estimateGasOnDpm({
     ])
     const result = await dpm.estimateGas.execute(operationExecutor.address, encodedCallDAta, {
       value: ethers.utils.parseEther(value.toString()).toHexString(),
-      type: 1,
-      accessList: [
-        {
-          address: '0xB694119b6dAa4fAc888A3FdbD1A8Da0398A8a438',
-          storageKeys: ['0x0000000000000000000000000000000000000000000000000000000000000000'],
-        },
-        {
-          address: '0xd9db270c1b5e3bd161e8c8503c55ceabee709552',
-          storageKeys: [],
-        },
-      ],
     })
     return {
       estimatedGas: new BigNumber(result.toString()).multipliedBy(GasMultiplier).toFixed(0),
@@ -123,17 +112,6 @@ export async function createExecuteTransaction({
     return await dpm.execute(operationExecutor.address, encodedCallDAta, {
       value: ethers.utils.parseEther(value.toString()).toHexString(),
       gasLimit: ethers.BigNumber.from(dangerTransactionEnabled.gasLimit),
-      type: 1,
-      accessList: [
-        {
-          address: '0xB694119b6dAa4fAc888A3FdbD1A8Da0398A8a438',
-          storageKeys: ['0x0000000000000000000000000000000000000000000000000000000000000000'],
-        },
-        {
-          address: '0xd9db270c1b5e3bd161e8c8503c55ceabee709552',
-          storageKeys: [],
-        },
-      ],
     })
   }
   const gasLimit = await estimateGasOnDpm({
