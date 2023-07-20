a93a30bb2617d852487df45610b21ee711b37df4
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-07-20 14:16:17 +0200

    suppressed slider when depositing quote

--- features/ajna/positions/multiply/sidebars/AjnaMultiplyFormContentDepositQuote.tsx
+++ features/ajna/positions/multiply/sidebars/AjnaMultiplyFormContentDepositQuote.tsx
@@ -1,15 +1,15 @@
-import { PillAccordion } from 'components/PillAccordion'
+// import { PillAccordion } from 'components/PillAccordion'
 import { useAjnaGeneralContext } from 'features/ajna/positions/common/contexts/AjnaGeneralContext'
 import { useAjnaProductContext } from 'features/ajna/positions/common/contexts/AjnaProductContext'
 import { AjnaFormContentSummary } from 'features/ajna/positions/common/sidebars/AjnaFormContentSummary'
 import { AjnaFormFieldDeposit } from 'features/ajna/positions/common/sidebars/AjnaFormFields'
-import { AjnaMultiplySlider } from 'features/ajna/positions/multiply/components/AjnaMultiplySlider'
+// import { AjnaMultiplySlider } from 'features/ajna/positions/multiply/components/AjnaMultiplySlider'
 import { AjnaMultiplyFormOrder } from 'features/ajna/positions/multiply/sidebars/AjnaMultiplyFormOrder'
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
 
 export function AjnaMultiplyFormContentDepositQuote() {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
   const {
     environment: { quoteBalance, quotePrice, quoteToken },
   } = useAjnaGeneralContext()
@@ -29,9 +29,10 @@ export function AjnaMultiplyFormContentDepositQuote() {
         token={quoteToken}
         tokenPrice={quotePrice}
       />
-      <PillAccordion title={t('adjust-your-position-additional')}>
-        <AjnaMultiplySlider disabled={!depositAmount} />
-      </PillAccordion>
+      {/* TODO uncomment once action will be handled */}
+      {/*<PillAccordion title={t('adjust-your-position-additional')}>*/}
+      {/*  <AjnaMultiplySlider disabled={!depositAmount} />*/}
+      {/*</PillAccordion>*/}
       {depositAmount && (
         <AjnaFormContentSummary>
           <AjnaMultiplyFormOrder />
