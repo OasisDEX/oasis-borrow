2f5cdd325ab76cbf94f2849fc01a1576cbd0cdcf
Marcin Ciarka <marcin@oazoapps.com>
2023-07-20 17:19:53 +0200

    Ajna overview lending price color fix

--- features/ajna/positions/earn/helpers/getLendingPriceColor.ts
+++ features/ajna/positions/earn/helpers/getLendingPriceColor.ts
@@ -15,7 +15,7 @@ export const getLendingPriceColor = ({
   price,
 }: GetLendingPriceColorParams): { color: string; index: number } => {
   if (price.lte(highestThresholdPrice)) return { color: lendingPriceColors.belowHtp, index: 0 }
-  if (price.lte(lowestUtilizedPrice)) return { color: lendingPriceColors.belowLup, index: 1 }
+  if (price.lt(lowestUtilizedPrice)) return { color: lendingPriceColors.belowLup, index: 1 }
   if (price.lte(mostOptimisticMatchingPrice))
     return { color: lendingPriceColors.belowMomp, index: 2 }
   else return { color: lendingPriceColors.aboveMomp, index: 3 }
