5bb572d44c9f8fc239b36787c20a458f22aad1ad
piekczyk <sebastian@oazoapps.com>
2023-07-25 12:00:31 +0200

    Add missing max values to ajna multiply inputs (#2608)

--- features/ajna/positions/multiply/sidebars/AjnaMultiplyFormContentPayback.tsx
+++ features/ajna/positions/multiply/sidebars/AjnaMultiplyFormContentPayback.tsx
@@ -1,3 +1,6 @@
+import { getToken } from 'blockchain/tokensMetadata'
+import { getAjnaBorrowPaybackMax } from 'features/ajna/positions/borrow/helpers/getAjnaBorrowPaybackMax'
+import { useAjnaGeneralContext } from 'features/ajna/positions/common/contexts/AjnaGeneralContext'
 import { useAjnaProductContext } from 'features/ajna/positions/common/contexts/AjnaProductContext'
 import { AjnaFormContentSummary } from 'features/ajna/positions/common/sidebars/AjnaFormContentSummary'
 import { AjnaFormFieldPayback } from 'features/ajna/positions/common/sidebars/AjnaFormFields'
@@ -6,18 +9,30 @@ import React from 'react'
 
 export function AjnaMultiplyFormContentPayback() {
   const {
+    environment: { quoteBalance, quoteToken },
+  } = useAjnaGeneralContext()
+  const {
     form: {
       dispatch,
       state: { paybackAmount },
     },
+    position: {
+      currentPosition: { position },
+    },
   } = useAjnaProductContext('multiply')
+
+  const paybackMax = getAjnaBorrowPaybackMax({
+    balance: quoteBalance,
+    digits: getToken(quoteToken).digits,
+    position,
+  })
 
   return (
     <>
       <AjnaFormFieldPayback
         dispatchAmount={dispatch}
-        // maxAmount={paybackMax}
-        // maxAmountLabel={quoteBalance.lt(debtAmount) ? 'balance' : 'max'}
+        maxAmount={paybackMax}
+        maxAmountLabel={quoteBalance.lt(position.debtAmount) ? 'balance' : 'max'}
         resetOnClear
       />
       {paybackAmount && (
