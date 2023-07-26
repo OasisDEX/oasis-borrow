0403dd9b0cbee3fa271739da14d6b4a8ccc49727
Piotr Konowrocki <piotr@oazoapps.com>
2023-07-26 09:04:15 +0200

    Ajna Multiply on Product Finder (#2617)
    * ajna multiply cards
    
    * homepage promo cards
    
    * filter out ajna multiply results
    
    * remove console
    
    * added feature toggle handling in other places
    
    * prettier
    
    ---------
    
    Co-authored-by: sebastianPiekarczyk <sebastian@oazoapps.com>

--- features/ajna/common/components/AjnaProductHubIntro.tsx
+++ features/ajna/common/components/AjnaProductHubIntro.tsx
@@ -2,8 +2,10 @@ import { AppLink } from 'components/Links'
 import { WithArrow } from 'components/WithArrow'
 import { productHubLinksMap } from 'features/productHub/meta'
 import { ProductHubProductType } from 'features/productHub/types'
+import { INTERNAL_LINKS } from 'helpers/applicationLinks'
+import { useFeatureToggle } from 'helpers/useFeatureToggle'
 import React, { FC } from 'react'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import { Box, Text } from 'theme-ui'
 
 interface AjnaProductHubIntroProps {
@@ -14,31 +16,61 @@ interface AjnaProductHubIntroProps {
 export const AjnaProductHubIntro: FC<AjnaProductHubIntroProps> = ({ selectedProduct }) => {
   const { t } = useTranslation()
 
+  // TODO: remove when Ajna Multiply feature flag is no longer needed
+  const ajnaMultiplyEnabled = useFeatureToggle('AjnaMultiply')
+
   return (
     <Box sx={{ my: 5 }}>
-      <Text
-        as="p"
-        variant="paragraph2"
-        sx={{
-          mx: 'auto',
-          mt: '24px',
-        }}
-      >
-        {t(`product-hub.intro.${selectedProduct}`)}{' '}
-        <AppLink href={productHubLinksMap[selectedProduct]}>
-          <WithArrow
-            variant="paragraph2"
-            sx={{
-              display: 'inline-block',
-              fontSize: 3,
-              color: 'interactive100',
-              fontWeight: 'regular',
-            }}
-          >
-            Summer.fi {t(`nav.${selectedProduct}`)}
-          </WithArrow>
-        </AppLink>
-      </Text>
+      {selectedProduct === ProductHubProductType.Multiply && !ajnaMultiplyEnabled ? (
+        <>
+          <Text as="p" variant="header5" sx={{ mb: 1 }}>
+            {t('ajna.product-finder.multiply-soon-title')}
+          </Text>
+          <Text as="p" variant="paragraph2">
+            <Trans
+              i18nKey="ajna.product-finder.multiply-soon-intro"
+              components={{
+                1: (
+                  <AppLink
+                    href={INTERNAL_LINKS.ajnaBorrow}
+                    sx={{ fontSize: 'inherit', fontWeight: 'inherit' }}
+                  />
+                ),
+                2: (
+                  <AppLink
+                    href={INTERNAL_LINKS.ajnaEarn}
+                    sx={{ fontSize: 'inherit', fontWeight: 'inherit' }}
+                  />
+                ),
+              }}
+            />
+          </Text>
+        </>
+      ) : (
+        <Text
+          as="p"
+          variant="paragraph2"
+          sx={{
+            mx: 'auto',
+            mt: '24px',
+          }}
+        >
+          {t(`product-hub.intro.${selectedProduct}`)}{' '}
+          <AppLink href={productHubLinksMap[selectedProduct]}>
+            <WithArrow
+              variant="paragraph2"
+              sx={{
+                display: 'inline-block',
+                fontSize: 3,
+                color: 'interactive100',
+                fontWeight: 'regular',
+              }}
+            >
+              Summer.fi {t(`nav.${selectedProduct}`)}
+            </WithArrow>
+          </AppLink>
+        </Text>
+      )}
     </Box>
   )
 }
