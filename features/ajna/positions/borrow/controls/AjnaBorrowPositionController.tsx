c25029c4e5afde47788e32d63ccacb7944641385
James Tuckett <james@tuckett.me>
2023-07-26 07:36:32 +0100

    chore: lint and format

--- features/ajna/positions/borrow/controls/AjnaBorrowPositionController.tsx
+++ features/ajna/positions/borrow/controls/AjnaBorrowPositionController.tsx
@@ -9,17 +9,16 @@ import { Grid } from 'theme-ui'
 
 export function AjnaBorrowPositionController() {
   return (
-
     <AjnaPositionView
       tabs={{
         position: (
           <Grid variant="vaultContainer">
-            <AjnaBorrowOverviewController/>
-            <AjnaBorrowFormController/>
+            <AjnaBorrowOverviewController />
+            <AjnaBorrowFormController />
           </Grid>
         ),
-        info: <AjnaFaqController content={{ en }}/>,
-        history: <AjnaPositionHistoryController/>,
+        info: <AjnaFaqController content={{ en }} />,
+        history: <AjnaPositionHistoryController />,
       }}
     />
   )
