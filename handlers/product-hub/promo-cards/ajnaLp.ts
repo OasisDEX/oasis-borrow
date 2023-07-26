4c06f093a1aa7e870ae3826536a4a9c989a9a6f9
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-07-26 08:41:11 +0200

    prettier

--- handlers/product-hub/promo-cards/ajnaLp.ts
+++ handlers/product-hub/promo-cards/ajnaLp.ts
@@ -79,7 +79,7 @@ export default function (table: ProductHubItem[]): ProductHubPromoCards {
           promoCardWBTCDAIAjnaMultiply,
           promoCardWSTETHDAIAjnaMultiply,
           promoCardsWhatAreAjnaRewards,
-        ]
+        ],
       },
     },
     [ProductHubProductType.Earn]: {
