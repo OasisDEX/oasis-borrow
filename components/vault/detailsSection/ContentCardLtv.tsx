e9839d1e5efc01175535c15bcf7f6a1ba0b1b723
Marcin Ciarka <marcin@oazoapps.com>
2023-08-01 16:04:35 +0200

    Aave Position overview Card - Add Stop Loss information (#2650)

--- components/vault/detailsSection/ContentCardLtv.tsx
+++ components/vault/detailsSection/ContentCardLtv.tsx
@@ -1,8 +1,12 @@
 import BigNumber from 'bignumber.js'
 import { ContentCardProps, DetailsSectionContentCard } from 'components/DetailsSectionContentCard'
+import { VaultViewMode } from 'components/vault/GeneralManageTabBar'
+import { AppSpinner } from 'helpers/AppSpinner'
 import { formatDecimalAsPercent, formatPercent } from 'helpers/formatters/format'
+import { useModal } from 'helpers/modalHook'
+import { useHash } from 'helpers/useHash'
 import { zero } from 'helpers/zero'
-import { useTranslation } from 'next-i18next'
+import { Trans, useTranslation } from 'next-i18next'
 import React from 'react'
 import { theme } from 'theme'
 import { Card, Grid, Heading, Text } from 'theme-ui'
@@ -27,14 +31,25 @@ interface ContentCardLtvModalProps {
   loanToValue: BigNumber
   liquidationThreshold: BigNumber
   maxLoanToValue?: BigNumber
+  stopLossLevel?: BigNumber
+  stopLossLevelLoading?: boolean
 }
 
 function ContentCardLtvModal({
   loanToValue,
   liquidationThreshold,
   maxLoanToValue,
+  stopLossLevel,
+  stopLossLevelLoading,
 }: ContentCardLtvModalProps) {
+  const { close: closeModal } = useModal()
   const { t } = useTranslation()
+  const [, setHash] = useHash()
+
+  const goToProtection = () => {
+    closeModal!()
+    setHash(VaultViewMode.Protection)
+  }
 
   return (
     <Grid gap={2}>
@@ -63,6 +78,29 @@ function ContentCardLtvModal({
       <Card as="p" variant="vaultDetailsCardModal">
         {formatDecimalAsPercent(liquidationThreshold)}
       </Card>
+      <Heading variant="header4">{t('aave-position-modal.ltv.fourth-header')}</Heading>
+      <Text as="p" variant="paragraph3" sx={{ mb: 1 }}>
+        <Trans
+          i18nKey="aave-position-modal.ltv.fourth-description-line"
+          components={[
+            <Text
+              as="span"
+              variant="boldParagraph3"
+              onClick={goToProtection}
+              sx={{ cursor: 'pointer' }}
+            />,
+          ]}
+        />
+      </Text>
+      <Card as="p" variant="vaultDetailsCardModal">
+        {stopLossLevel ? (
+          formatDecimalAsPercent(stopLossLevel)
+        ) : (
+          <Text as="p" variant="paragraph3" onClick={goToProtection} sx={{ cursor: 'pointer' }}>
+            {stopLossLevelLoading ? <AppSpinner /> : t('aave-position-modal.ltv.stop-loss-not-set')}
+          </Text>
+        )}
+      </Card>
     </Grid>
   )
 }
@@ -72,6 +110,8 @@ interface ContentCardLtvProps {
   liquidationThreshold: BigNumber
   maxLoanToValue?: BigNumber
   afterLoanToValue?: BigNumber
+  stopLossLevel?: BigNumber
+  stopLossLevelLoading?: boolean
 }
 
 export function ContentCardLtv({
@@ -79,6 +119,8 @@ export function ContentCardLtv({
   liquidationThreshold,
   afterLoanToValue,
   maxLoanToValue,
+  stopLossLevel,
+  stopLossLevelLoading,
 }: ContentCardLtvProps) {
   const { t } = useTranslation()
 
@@ -86,20 +128,27 @@ export function ContentCardLtv({
     loanToValue: formatDecimalAsPercent(loanToValue),
     afterLoanToValue: afterLoanToValue && formatDecimalAsPercent(afterLoanToValue),
     liquidationThreshold: formatPercent(liquidationThreshold.times(100)),
+    stopLossLevel: stopLossLevel && formatPercent(stopLossLevel.times(100)),
   }
 
   const contentCardModalSettings: ContentCardLtvModalProps = {
     loanToValue,
     maxLoanToValue,
     liquidationThreshold,
+    stopLossLevel,
+    stopLossLevelLoading,
   }
 
   const contentCardSettings: ContentCardProps = {
     title: t('system.loan-to-value'),
     value: formatted.loanToValue,
-    footnote: `${t('manage-earn-vault.liquidation-threshold', {
-      percentage: formatted.liquidationThreshold,
-    })}`,
+    footnote: stopLossLevel
+      ? t('manage-earn-vault.stop-loss-ltv', {
+          percentage: formatted.stopLossLevel,
+        })
+      : t('manage-earn-vault.liquidation-threshold', {
+          percentage: formatted.liquidationThreshold,
+        }),
     customBackground:
       afterLoanToValue && !liquidationThreshold.eq(zero)
         ? getLTVRatioColor(liquidationThreshold.minus(loanToValue).times(100))
