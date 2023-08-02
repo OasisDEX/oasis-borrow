31a5b82c4ba286e041920a44a2dc234c71d42020
Piotr Konowrocki <piotr@oazoapps.com>
2023-08-02 16:31:37 +0200

    normalize sdai factory import

--- server/services/sdaiOracle.ts
+++ server/services/sdaiOracle.ts
@@ -2,19 +2,17 @@ import BigNumber from 'bignumber.js'
 import { getNetworkContracts } from 'blockchain/contracts'
 import { getRpcProvider,NetworkIds } from 'blockchain/networks'
 import { PriceServiceResponse } from 'helpers/types'
-import { SdaiPriceOracle__factory } from 'types/ethers-contracts'
-
-const SdaiPriceOracleFactory = SdaiPriceOracle__factory
+import { SdaiPriceOracle__factory as SdaiPriceOracleFactory } from 'types/ethers-contracts'
 
 export async function getSDaiOracleTicker(): Promise<PriceServiceResponse> {
   const rpcProvider = getRpcProvider(NetworkIds.MAINNET)
-  const SdaiPriceOracleContractAddress = getNetworkContracts(NetworkIds.MAINNET).SdaiOracle.address
-  const SdaiPriceOracleContract = SdaiPriceOracleFactory.connect(
-    SdaiPriceOracleContractAddress,
+  const sdaiPriceOracleContractAddress = getNetworkContracts(NetworkIds.MAINNET).SdaiOracle.address
+  const sdaiPriceOracleContract = SdaiPriceOracleFactory.connect(
+    sdaiPriceOracleContractAddress,
     rpcProvider,
   )
 
-  const response = await SdaiPriceOracleContract.latestAnswer()
+  const response = await sdaiPriceOracleContract.latestAnswer()
   const sdai = new BigNumber(response.toString()).div(new BigNumber(10 ** 8)).toNumber()
 
   return {
