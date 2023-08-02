b1cd77830e116b397a538471ffea2a57b28edb45
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-08-02 10:21:52 +0200

    Ajna rewards banner adjustment

--- features/ajna/positions/earn/controls/AjnaEarnOverviewController.tsx
+++ features/ajna/positions/earn/controls/AjnaEarnOverviewController.tsx
@@ -1,4 +1,5 @@
 import { useAjnaGeneralContext } from 'features/ajna/positions/common/contexts/AjnaGeneralContext'
+import { useAjnaProductContext } from 'features/ajna/positions/common/contexts/AjnaProductContext'
 import { AjnaTokensBannerController } from 'features/ajna/positions/common/controls/AjnaTokensBannerController'
 import { isPoolWithRewards } from 'features/ajna/positions/common/helpers/isPoolWithRewards'
 import { AjnaEarnOverviewManageController } from 'features/ajna/positions/earn/controls/AjnaEarnOverviewManageController'
@@ -10,13 +11,23 @@ export function AjnaEarnOverviewController() {
   const {
     environment: { collateralToken, flow, quoteToken },
   } = useAjnaGeneralContext()
+  const {
+    position: {
+      currentPosition: {
+        position: { pool, price },
+      },
+    },
+  } = useAjnaProductContext('earn')
+
+  const isPriceBelowLup =
+    price.lt(pool.lowestUtilizedPrice) && !pool.lowestUtilizedPriceIndex.isZero()
 
   return (
     <Grid gap={2}>
       {flow === 'open' && <AjnaEarnOverviewOpenController />}
       {flow === 'manage' && <AjnaEarnOverviewManageController />}
       {isPoolWithRewards({ collateralToken, quoteToken }) && (
-        <AjnaTokensBannerController flow={flow} />
+        <AjnaTokensBannerController flow={flow} isPriceBelowLup={isPriceBelowLup} />
       )}
     </Grid>
   )
