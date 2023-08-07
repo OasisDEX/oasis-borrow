e0c5e10b8a45b776e923d8184246300c7b411c6a
Piotr Konowrocki <piotr@oazoapps.com>
2023-08-07 12:52:24 +0200

    ui

--- features/ajna/common/controls/AjnaPoolFinderController.tsx
+++ features/ajna/common/controls/AjnaPoolFinderController.tsx
@@ -1,14 +1,56 @@
 import { AnimatedWrapper } from 'components/AnimatedWrapper'
 import { WithConnection } from 'components/connectWallet'
+import { AjnaHeader } from 'features/ajna/common/components/AjnaHeader'
 import { searchAjnaPool } from 'features/ajna/positions/common/helpers/searchAjnaPool'
-import React from 'react'
-import { Button } from 'theme-ui'
+import { useDebouncedEffect } from 'helpers/useDebouncedEffect'
+import React, { useState } from 'react'
+import { Box, Button, Flex, Grid, Input, SxStyleProp } from 'theme-ui'
+
+const inputStyles: SxStyleProp = {
+  height: '50px',
+  p: 3,
+  fontSize: 2,
+  border: '1px solid',
+  borderColor: 'neutral20',
+  borderRadius: 'medium',
+}
 
 export function AjnaPoolFinderController() {
+  const [poolAddress, setPoolAddress] = useState<string>('')
+
+  useDebouncedEffect(
+    () => {
+      console.log('new')
+    },
+    [poolAddress],
+    250,
+  )
+
   return (
     <WithConnection>
-      <AnimatedWrapper sx={{ mb: 5 }}>
-        Seach
+      <AnimatedWrapper>
+        <AjnaHeader title="Ajna pool finder" intro="Lorem ipsum dolor sit amet" />
+        <Grid
+          sx={{
+            gridTemplateColumns: 'minmax(20px, 460px) max-content minmax(20px, 460px)',
+            columnGap: 3,
+            justifyContent: 'center',
+          }}
+        >
+          <Flex>
+            <Input
+              sx={inputStyles}
+              placeholder="Pool address"
+              value={poolAddress}
+              onChange={(e) => setPoolAddress(e.target.value)}
+            />
+          </Flex>
+          <Box sx={{ pt: '12px' }}>OR</Box>
+          <Flex sx={{ flexDirection: 'column', rowGap: 3 }}>
+            <Input sx={inputStyles} placeholder="Collateral token symbol or address" />
+            <Input sx={inputStyles} placeholder="Quote token symbol or address" />
+          </Flex>
+        </Grid>
         <Button
           onClick={async () => {
             const data = await searchAjnaPool({
