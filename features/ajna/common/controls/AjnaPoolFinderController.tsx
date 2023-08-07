c895fa982d5787321fddcac97317fee26fd7909f
Piotr Konowrocki <piotr@oazoapps.com>
2023-08-07 16:37:38 +0200

    partial ui

--- features/ajna/common/controls/AjnaPoolFinderController.tsx
+++ features/ajna/common/controls/AjnaPoolFinderController.tsx
@@ -1,16 +1,26 @@
-
 import { AnimatedWrapper } from 'components/AnimatedWrapper'
 import { useAppContext } from 'components/AppContextProvider'
 import { WithConnection } from 'components/connectWallet'
+import { AppLink } from 'components/Links'
+import { TokensGroup } from 'components/TokensGroup'
 import { isAddress } from 'ethers/lib/utils'
 import { AjnaHeader } from 'features/ajna/common/components/AjnaHeader'
 import { searchAjnaPool } from 'features/ajna/positions/common/helpers/searchAjnaPool'
 import { useDebouncedEffect } from 'helpers/useDebouncedEffect'
-import React, {   useState } from 'react'
-import { Box, Flex, Grid, Input, SxStyleProp, Text } from 'theme-ui'
+import { uniq } from 'lodash'
+import { useState } from 'react'
+import { Box, Button, Flex, Input, Spinner, SxStyleProp, Text } from 'theme-ui'
+
+interface OraclessPoolResult {
+  collateralAddress: string
+  collateralToken: string
+  quoteAddress: string
+  quoteToken: string
+}
 
 const inputStyles: SxStyleProp = {
   height: '50px',
+  maxWidth: '460px',
   p: 3,
   fontSize: 2,
   border: '1px solid',
@@ -44,6 +54,7 @@ function validateParams({
 export function AjnaPoolFinderController() {
   const { identifiedTokens$ } = useAppContext()
 
+  const [results, setResults] = useState<{ [key: string]: OraclessPoolResult[] }>({})
   const [poolAddress, setPoolAddress] = useState<string>('')
   const [collateralAddress, setCollateralAddress] = useState<string>('')
   const [quoteAddress, setQuoteAddress] = useState<string>('')
@@ -58,15 +69,50 @@ export function AjnaPoolFinderController() {
       })
 
       setErrors(validation)
-      if (validation.length === 0) {
-        const data = await searchAjnaPool({
+      if (
+        !results[`${poolAddress}-${collateralAddress}-${quoteAddress}`] &&
+        validation.length === 0
+      ) {
+        const { pools, size } = await searchAjnaPool({
           collateralAddress,
           poolAddress,
           quoteAddress,
         })
-        // const identifiedTokens = identifiedTokens$.sub
-
-        console.log(data)
+        if (size > 0) {
+          const identifiedTokensSubscription = identifiedTokens$(
+            uniq(
+              pools.flatMap(({ collateralAddress, quoteTokenAddress }) => [
+                collateralAddress,
+                quoteTokenAddress,
+              ]),
+            ),
+          ).subscribe(
+            (identifiedTokens) => {
+              console.log(pools)
+              console.log(identifiedTokens)
+              setResults({
+                ...results,
+                [`${poolAddress}-${collateralAddress}-${quoteAddress}`]: pools
+                  .filter(
+                    (pool) =>
+                      Object.keys(identifiedTokens).includes(pool.collateralAddress) &&
+                      Object.keys(identifiedTokens).includes(pool.quoteTokenAddress),
+                  )
+                  .map((pool) => ({
+                    collateralAddress: pool.collateralAddress,
+                    collateralToken: identifiedTokens[pool.collateralAddress].symbol,
+                    quoteAddress: pool.quoteTokenAddress,
+                    quoteToken: identifiedTokens[pool.quoteTokenAddress].symbol,
+                  })),
+              })
+              identifiedTokensSubscription.unsubscribe()
+            },
+            undefined,
+            () => {
+              console.log('complete?')
+            },
+          )
+        }
       }
     },
     [collateralAddress, poolAddress, quoteAddress],
@@ -77,38 +123,62 @@ export function AjnaPoolFinderController() {
     <WithConnection>
       <AnimatedWrapper>
         <AjnaHeader title="Ajna pool finder" intro="Lorem ipsum dolor sit amet" />
-        <Grid
+        <Flex
           sx={{
-            gridTemplateColumns: 'minmax(20px, 460px) max-content minmax(20px, 460px)',
-            columnGap: 3,
-            justifyContent: 'center',
+            flexDirection: 'column',
+            rowGap: 2,
+            justifyItems: 'center',
+            alignItems: 'center',
+            mb: '48px',
           }}
         >
-          <Flex>
-            <Input
-              sx={inputStyles}
-              placeholder="Pool address"
-              value={poolAddress}
-              onChange={(e) => setPoolAddress(e.target.value)}
-            />
+          <Input
+            sx={inputStyles}
+            placeholder="Pool address"
+            value={poolAddress}
+            onChange={(e) => setPoolAddress(e.target.value)}
+          />
+          <Box>OR</Box>
+          <Input
+            sx={inputStyles}
+            placeholder="Collateral token address"
+            value={collateralAddress}
+            onChange={(e) => setCollateralAddress(e.target.value)}
+          />
+          <Input
+            sx={inputStyles}
+            placeholder="Quote token address"
+            value={quoteAddress}
+            onChange={(e) => setQuoteAddress(e.target.value)}
+          />
+        </Flex>
+        {results[`${poolAddress}-${collateralAddress}-${quoteAddress}`] ? (
+          <Flex sx={{ flexDirection: 'column', rowGap: 2 }}>
+            {results[`${poolAddress}-${collateralAddress}-${quoteAddress}`].map((pool) => (
+              <Flex sx={{ alignItems: 'center', columnGap: 2 }}>
+                <TokensGroup tokens={[pool.collateralToken, pool.quoteToken]} />
+                <Text as="p" sx={{ fontWeight: 'semiBold' }}>
+                  {pool.collateralToken}/{pool.quoteToken}
+                </Text>
+                <AppLink
+                  href={`/ethereum/ajna/borrow/${pool.collateralAddress}/${pool.quoteAddress}`}
+                >
+                  <Button variant="tertiary">Borrow</Button>
+                </AppLink>
+                or
+                <AppLink
+                  href={`/ethereum/ajna/earn/${pool.collateralAddress}/${pool.quoteAddress}`}
+                >
+                  <Button variant="tertiary">Earn</Button>
+                </AppLink>
+              </Flex>
+            ))}
           </Flex>
-          <Box sx={{ pt: '12px' }}>OR</Box>
-          <Flex sx={{ flexDirection: 'column', rowGap: 3 }}>
-            <Input
-              sx={inputStyles}
-              placeholder="Collateral token address"
-              value={collateralAddress}
-              onChange={(e) => setCollateralAddress(e.target.value)}
-            />
-            <Input
-              sx={inputStyles}
-              placeholder="Quote token address"
-              value={quoteAddress}
-              onChange={(e) => setQuoteAddress(e.target.value)}
-            />
-          </Flex>
-        </Grid>
-        {errors.length > 0 && errors.map((error) => <Text as="p">{error}</Text>)}
+        ) : (
+          <>
+            {errors.length > 0 ? errors.map((error) => <Text as="p">{error}</Text>) : <Spinner />}
+          </>
+        )}
       </AnimatedWrapper>
     </WithConnection>
   )
