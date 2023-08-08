615166d057792fc2e7acc7039705d4b285ace88f
Piotr Konowrocki <piotr@oazoapps.com>
2023-08-08 11:53:08 +0200

    modal

--- features/ajna/positions/common/components/contentCards/ContentCardThresholdPrice.tsx
+++ features/ajna/positions/common/components/contentCards/ContentCardThresholdPrice.tsx
@@ -8,6 +8,11 @@ import { AjnaDetailsSectionContentSimpleModal } from 'features/ajna/common/compo
 import { formatCryptoBalance } from 'helpers/formatters/format'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
+import { Card, Text } from 'theme-ui'
+
+interface ContentCardThresholdPriceModalProps {
+  thresholdPrice: string
+}
 
 interface ContentCardThresholdPriceProps {
   isLoading?: boolean
@@ -16,6 +21,47 @@ interface ContentCardThresholdPriceProps {
   afterThresholdPrice?: BigNumber
   lup?: BigNumber
   changeVariant?: ChangeVariantType
+}
+
+function ContentCardThresholdPriceModal({ thresholdPrice }: ContentCardThresholdPriceModalProps) {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <Text variant="paragraph3" as="p" sx={{ color: 'neutral80' }}>
+        {t('ajna.position-page.borrow.common.overview.threshold-price-modal-desc-part-1')}
+      </Text>
+      <Card
+        variant="vaultDetailsCardModal"
+        sx={{ my: 2, fontSize: 2, fontWeight: 'regular', lineHeight: 'body' }}
+      >
+        <code>
+          TP ={' '}
+          {t('ajna.position-page.borrow.common.overview.threshold-price-modal-desc-formula-part-1')}
+          <br />
+          {'\u00A0'}{'\u00A0'} ={' '}
+          {t('ajna.position-page.borrow.common.overview.threshold-price-modal-desc-formula-part-2')}
+          <br />
+          {'\u00A0'}{'\u00A0'} = {thresholdPrice}
+        </code>
+      </Card>
+      <Text variant="paragraph3" as="p" sx={{ color: 'neutral80' }}>
+        {t('ajna.position-page.borrow.common.overview.threshold-price-modal-desc-part-2')}
+      </Text>
+      <Text variant="boldParagraph3" as="p" sx={{ color: 'neutral80' }}>
+        {t('ajna.position-page.borrow.common.overview.threshold-price-modal-desc-part-3')}
+      </Text>
+      <Card variant="vaultDetailsCardModal" sx={{ mt: 2 }}>
+        {t(
+          'ajna.position-page.borrow.common.overview.threshold-price-modal-desc-explanation-part-1',
+        )}
+        <br />
+        {t(
+          'ajna.position-page.borrow.common.overview.threshold-price-modal-desc-explanation-part-2',
+        )}
+      </Card>
+    </>
+  )
 }
 
 export function ContentCardThresholdPrice({
@@ -46,11 +92,9 @@ export function ContentCardThresholdPrice({
     modal: (
       <AjnaDetailsSectionContentSimpleModal
         title={t('ajna.position-page.borrow.common.overview.threshold-price')}
-        description={t(
-          'ajna.position-page.borrow.common.overview.threshold-price-modal-desc',
-        )}
-        value={`${formatted.lup} ${lup ? priceFormat : ''}`}
-      />
+      >
+        <ContentCardThresholdPriceModal thresholdPrice={formatted.thresholdPrice} />
+      </AjnaDetailsSectionContentSimpleModal>
     ),
   }
 
