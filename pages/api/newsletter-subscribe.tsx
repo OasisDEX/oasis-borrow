c42f43c43b0d54a19cf55586c2f39696108b65f9
Marcin Ciarka <marcin@oazoapps.com>
2023-08-17 11:17:57 +0200

    disable newsletter

--- pages/api/newsletter-subscribe.tsx
+++ pages/api/newsletter-subscribe.tsx
@@ -1,4 +1,4 @@
-import md5 from 'crypto-js/md5'
+// import md5 from 'crypto-js/md5'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const AUTHORIZATION_HEADER = `Basic ${Buffer.from(
@@ -14,60 +14,61 @@ type UserStatus = 'pending' | 'subscribed' | 'unsubscribed' | 'cleaned' | 'trans
 const INITIAL_USER_STATUS: UserStatus = 'subscribed'
 
 const handler = async function (req: NextApiRequest, res: NextApiResponse) {
-  const { email } = req.body
+  return res.status(500).json({ error: 'unknown' })
+  // const { email } = req.body
 
-  try {
-    const emailMd5Hash = md5(email)
-    const MEMBER_ENDPOINT = `${SUBSCRIBERS_ENDPOINT}/${emailMd5Hash}`
+  // try {
+  //   const emailMd5Hash = md5(email)
+  //   const MEMBER_ENDPOINT = `${SUBSCRIBERS_ENDPOINT}/${emailMd5Hash}`
 
-    // GET request first to check if user exists OR is subscribed
-    const response = await fetch(MEMBER_ENDPOINT, {
-      headers: {
-        Authorization: AUTHORIZATION_HEADER,
-      },
-      method: 'GET',
-    }).then((res) => res.json())
+  //   // GET request first to check if user exists OR is subscribed
+  //   const response = await fetch(MEMBER_ENDPOINT, {
+  //     headers: {
+  //       Authorization: AUTHORIZATION_HEADER,
+  //     },
+  //     method: 'GET',
+  //   }).then((res) => res.json())
 
-    if (response.status === 'pending') {
-      return res.status(409).json({ error: 'emailPending' })
-    }
+  //   if (response.status === 'pending') {
+  //     return res.status(409).json({ error: 'emailPending' })
+  //   }
 
-    if (response.status === 'subscribed') {
-      return res.status(409).json({ error: 'emailAlreadyExists' })
-    }
+  //   if (response.status === 'subscribed') {
+  //     return res.status(409).json({ error: 'emailAlreadyExists' })
+  //   }
 
-    if (response.status === 'unsubscribed' || response.status === 404) {
-      const bodyData = {
-        email_address: email,
-        status: INITIAL_USER_STATUS,
-        status_if_new: INITIAL_USER_STATUS,
-      }
+  //   if (response.status === 'unsubscribed' || response.status === 404) {
+  //     const bodyData = {
+  //       email_address: email,
+  //       status: INITIAL_USER_STATUS,
+  //       status_if_new: INITIAL_USER_STATUS,
+  //     }
 
-      const response = await fetch(MEMBER_ENDPOINT, {
-        body: JSON.stringify(bodyData),
-        headers: {
-          Authorization: AUTHORIZATION_HEADER,
-        },
-        method: 'PUT',
-      }).then((res) => res.json())
+  //     const response = await fetch(MEMBER_ENDPOINT, {
+  //       body: JSON.stringify(bodyData),
+  //       headers: {
+  //         Authorization: AUTHORIZATION_HEADER,
+  //       },
+  //       method: 'PUT',
+  //     }).then((res) => res.json())
 
-      // Might happen eg. if user who was permanently deleted (not archived) tries to resubscribe
-      if (response.status !== INITIAL_USER_STATUS) {
-        console.error(response)
-        return res.status(500).json({ error: 'unknown', response })
-      }
+  //     // Might happen eg. if user who was permanently deleted (not archived) tries to resubscribe
+  //     if (response.status !== INITIAL_USER_STATUS) {
+  //       console.error(response)
+  //       return res.status(500).json({ error: 'unknown', response })
+  //     }
 
-      return res.status(200).json({})
-    }
+  //     return res.status(200).json({})
+  //   }
 
-    return res.status(500).json({
-      error: 'unknown',
-      response,
-    })
-  } catch (error) {
-    // @ts-ignore
-    return res.status(500).json({ error: error.message || error.toString() })
-  }
+  //   return res.status(500).json({
+  //     error: 'unknown',
+  //     response,
+  //   })
+  // } catch (error) {
+  //   // @ts-ignore
+  //   return res.status(500).json({ error: error.message || error.toString() })
+  // }
 }
 
 export default handler
