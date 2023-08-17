1cc5617e45b5904a9093d3e06c927231e43dd567
Piotr Konowrocki <piotr@oazoapps.com>
2023-08-17 17:22:27 +0200

    loading state

--- features/ajna/common/controls/AjnaPoolCreatorController.tsx
+++ features/ajna/common/controls/AjnaPoolCreatorController.tsx
@@ -2,6 +2,7 @@ import { AnimatedWrapper } from 'components/AnimatedWrapper'
 import { WithConnection } from 'components/connectWallet'
 import { DetailsSection } from 'components/DetailsSection'
 import { AjnaHeader } from 'features/ajna/common/components/AjnaHeader'
+import { PoolCreatorLoadingState } from 'features/poolCreator/components/PoolCreatorLoadingState'
 import { DEFAULT_POOL_INTEREST_RATE } from 'features/poolCreator/consts'
 import { PoolCreatorActionController } from 'features/poolCreator/controls/PoolCreatorActionController'
 import { PoolCreatorFormController } from 'features/poolCreator/controls/PoolCreatorFormController'
@@ -46,7 +47,10 @@ export function AjnaPoolCreatorController() {
       <AnimatedWrapper sx={{ mb: 5 }}>
         <AjnaHeader title={t('pool-creator.header.title')} intro={t('pool-creator.header.intro')} />
         <Box sx={{ maxWidth: '584px', mx: 'auto' }}>
-          <WithLoadingIndicator value={[boundries, isFormReady]} customLoader={<>Loading</>}>
+          <WithLoadingIndicator
+            value={[boundries, isFormReady]}
+            customLoader={<PoolCreatorLoadingState />}
+          >
             {([{ min, max }]) => (
               <DetailsSection
                 title={t('pool-creator.form.title')}
