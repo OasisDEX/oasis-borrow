97d722628db51b6fc939c1cafd28140595001881
halaprix <panicz.klos@gmail.com>
2023-09-04 17:09:14 +0200

    fix: fix non ENS referrals

--- features/referralOverview/referralLocal.ts
+++ features/referralOverview/referralLocal.ts
@@ -1,17 +1,73 @@
+import { ensNameToAddressMainnet } from 'blockchain/ens'
+import { getAddress } from 'ethers/lib/utils'
 import { Observable, of } from 'rxjs'
 
 export function checkReferralLocalStorage$(): Observable<string | null> {
-  let referrer = localStorage.getItem(`referral`)
-  if (referrer === 'null') {
-    referrer = null
-  }
-  return of(referrer)
+  const referrer = localStorage.getItem(`referral`)
+  return checkReferrer(referrer)
 }
 
 export function checkReferralLocalStorage(): string | null {
   let referrer = localStorage.getItem(`referral`)
-  if (referrer === 'null') {
+  if (referrer) {
+    try {
+      const address = getAddress(referrer)
+      if (address) {
+        referrer = address
+      }
+      return referrer
+    } catch (err) {
+      console.warn(`Referrals: not an address.`)
+    }
+
+    ensNameToAddressMainnet(referrer)
+      .then((ensAddress) => {
+        if (ensAddress) {
+          localStorage.setItem(`referral`, ensAddress)
+          return ensAddress
+        } else {
+          return null
+        }
+      })
+      .catch((err: Error) => {
+        console.warn(`Error looking up ENS name for address: ${err.message}`)
+        localStorage.removeItem(`referral`)
+        return null
+      })
+  } else if (referrer === 'null') {
     referrer = null
   }
-  return referrer
+  return null
+}
+
+function checkReferrer(referrer: string | null): Observable<string | null> {
+  if (referrer) {
+    try {
+      const address = getAddress(referrer)
+      if (address) {
+        referrer = address
+      }
+      return of(referrer)
+    } catch (err) {
+      console.warn(`Referrals: not an address.`)
+    }
+
+    ensNameToAddressMainnet(referrer)
+      .then((ensAddress) => {
+        if (ensAddress) {
+          localStorage.setItem(`referral`, ensAddress)
+          return of(ensAddress)
+        } else {
+          return of(null)
+        }
+      })
+      .catch((err: Error) => {
+        console.warn(`Error looking up ENS name for address: ${err.message}`)
+        localStorage.removeItem(`referral`)
+        return of(null)
+      })
+  } else if (referrer === 'null') {
+    referrer = null
+  }
+  return of(null)
 }
