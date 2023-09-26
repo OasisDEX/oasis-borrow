1de793aab9f379f5bc8f1b4bd094b12de5310262
Piotr Konowrocki <piotr@oazoapps.com>
2023-09-26 14:32:26 +0200

    type fix

--- features/navigation/controls/NavigationController.tsx
+++ features/navigation/controls/NavigationController.tsx
@@ -5,7 +5,7 @@ import { SwapWidgetShowHide } from 'components/swapWidget/SwapWidgetShowHide'
 import { NavigationActionsController } from 'features/navigation/controls/NavigationActionsController'
 import {
   SWAP_WIDGET_CHANGE_SUBJECT,
-  SwapWidgetChangeAction,
+  type SwapWidgetChangeAction,
 } from 'features/swapWidget/SwapWidgetChange'
 import { INTERNAL_LINKS } from 'helpers/applicationLinks'
 import { useAppConfig } from 'helpers/config'
