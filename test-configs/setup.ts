5f6627b74313cd39f91688de67cede3fbba66637
Marcin Ciarka <marcin@oazoapps.com>
2023-09-26 12:07:36 +0300

    Package updates + lifi widget config update (#2892)
    Co-authored-by: Jakub Swierczek <jakub.swierczek@outlook.com>

--- test-configs/setup.ts
+++ test-configs/setup.ts
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom'
+import '@inrupt/jest-jsdom-polyfills' // Some libraries are using certain parts of the Web API that do not appear in jsdom (which is used in the Jest framework).
 
 import { LocalStorage } from './mock-local-storage'
 
