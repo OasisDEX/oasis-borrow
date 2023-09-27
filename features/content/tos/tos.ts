28ffe3d9c3574c0c4f37aa3434b094fa80664c2d
Marcin Ciarka <marcin@oazoapps.com>
2023-09-27 11:04:13 +0200

    remove not translated pages

--- features/content/tos/tos.ts
+++ features/content/tos/tos.ts
@@ -1,10 +1,8 @@
 import type { ContentType } from '..'
 import en from './en'
-import es from './es'
-import pt from './pt'
 
 export const content: ContentType = {
   en,
-  es,
-  pt,
+  es: en,
+  pt: en,
 }
