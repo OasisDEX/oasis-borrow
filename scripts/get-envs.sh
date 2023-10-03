4ca1c89ae618c76f7831af536ae9b9d966307ddb
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-10-03 19:38:58 +0200

    Env config in repo

---
+++ scripts/get-envs.sh
@@ -0,0 +1,13 @@
+# RUN it from root directory of the project or directly from package.json
+# in root use `run get-envs` or `./scripts/get-envs.sh`
+
+borrowDir=$(basename "$(pwd)")
+cd ..
+if [ ! -d "borrow-envs" ]; then
+  echo "Directory borrow-envs doesn't exist, cloning..."
+  git clone git@github.com:OasisDEX/borrow-envs.git
+fi
+cd borrow-envs
+echo "Pulling latest changes..."
+git pull
+cp .env ../${borrowDir}/.env
