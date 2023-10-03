8b2b4e2372daab54edac059b488fba08c98827ea
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-10-03 09:44:26 +0200

    Swap widget predefining variant

--- features/swapWidget/SwapWidgetChange.ts
+++ features/swapWidget/SwapWidgetChange.ts
@@ -1,10 +1,15 @@
+import type { WidgetConfig } from '@lifi/widget'
+
 export const SWAP_WIDGET_CHANGE_SUBJECT = 'swapWidgetChange'
 
-export type SwapWidgetChangeAction = { type: 'open'; token?: string } | { type: 'close' }
+export type SwapWidgetChangeAction =
+  | { type: 'open'; token?: string; variant?: WidgetConfig['subvariantOptions'] }
+  | { type: 'close' }
 
 export interface SwapWidgetState {
   token?: string
   isOpen: boolean
+  variant: WidgetConfig['subvariantOptions']
 }
 
 export function swapWidgetChangeReducer(
@@ -13,7 +18,7 @@ export function swapWidgetChangeReducer(
 ): SwapWidgetState {
   switch (action.type) {
     case 'open':
-      return { ...state, token: action.token, isOpen: true }
+      return { ...state, token: action.token, isOpen: true, variant: action.variant }
     case 'close':
       return { ...state, isOpen: false, token: undefined }
     default:
