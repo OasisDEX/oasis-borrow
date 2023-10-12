d36e649714fbeac0f7733a88af7e125c4b1a438d
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-10-12 10:27:14 +0200

    disable sentry

--- next.config.js
+++ next.config.js
@@ -30,6 +30,10 @@ const baseConfig = {
   productionBrowserSourceMaps: true,
   pageExtensions: ['mdx', 'tsx', 'ts'],
   publicRuntimeConfig: publicRuntimeConfig,
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
   webpack: function (config, { isServer, dev }) {
     config.module.rules.push({
       test: /\.(svg|png|jpg|gif)$/,
