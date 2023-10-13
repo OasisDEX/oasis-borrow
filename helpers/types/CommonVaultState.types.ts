4b2fe7cb15bd027f230754f6cd202ccc62fa7b41
Damian Paszkowski <paszko.damian@gmail.com>
2023-10-13 23:55:33 +0200

    fix: type error

--- helpers/types/CommonVaultState.types.ts
+++ helpers/types/CommonVaultState.types.ts
@@ -1,8 +1,10 @@
+import { OpenVaultState } from 'features/borrow/open/pipes/openVault.types'
 import type { OpenGuniVaultState } from 'features/earn/guni/open/pipes/openGuniVault.types'
 import type { ManageMultiplyVaultState } from 'features/multiply/manage/pipes/ManageMultiplyVaultState.types'
 import type { OpenMultiplyVaultState } from 'features/multiply/open/pipes/openMultiplyVault.types'
 
 export type CommonVaultState =
+  | OpenVaultState
   | OpenMultiplyVaultState
   | ManageMultiplyVaultState
   | OpenGuniVaultState
