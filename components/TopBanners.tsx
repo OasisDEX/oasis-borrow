9b306f1c31595a831ce832620540f46c3a5575d2
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-10-24 09:23:59 +0200

    Refactored TopBanners component and fixed typo
    The unnecessary fragment wrapping the Box component in OneBanner function in the TopBanners component has been removed to simplify the component. This has no effect on the functionality or the appearance of the component. In addition, there was a typo in a variable name "loccationBasedBanner" which has been corrected to "locationBasedBanner". This makes the code cleaner and easier to read.

--- components/TopBanners.tsx
+++ components/TopBanners.tsx
@@ -15,62 +15,61 @@ import { AppLink } from './Links'
 import { WithArrow } from './WithArrow'
 
 type TopBanner = ConfiguredAppParameters['topBanner']
+
 function OneBanner({ banner }: { banner: TopBanner }) {
   const [bannerClosed, setBannerClosed] = useLocalStorage(`TopBanner_${banner.name}_closed`, false)
 
   const isClosed = banner.closeable && bannerClosed
   return !banner.enabled || isClosed || !banner.name ? null : (
-    <>
-      <Box
-        sx={{
-          position: 'relative',
-          textAlign: 'center',
-          background: 'linear-gradient(90.6deg, #D3F5FF 0%, #F2FCFF 39.53%, #FFE7D8 99.87%)',
-          '&:hover svg.arrow': {
-            transform: 'translateX(10px)',
-          },
-          height: ['auto', 0],
-          overflow: 'hidden',
-          ...rollDownTopBannerAnimation,
+    <Box
+      sx={{
+        position: 'relative',
+        textAlign: 'center',
+        background: 'linear-gradient(90.6deg, #D3F5FF 0%, #F2FCFF 39.53%, #FFE7D8 99.87%)',
+        '&:hover svg.arrow': {
+          transform: 'translateX(10px)',
+        },
+        height: ['auto', 0],
+        overflow: 'hidden',
+        ...rollDownTopBannerAnimation,
+      }}
+    >
+      <AppLink
+        href={banner.url as string}
+        onClick={() => {
+          trackingEvents.topBannerEvent(MixpanelTopBannerEvents.TopBannerClicked, 'rebranding')
         }}
+        sx={{ display: 'inline', padding: 3 }}
       >
-        <AppLink
-          href={banner.url as string}
-          onClick={() => {
-            trackingEvents.topBannerEvent(MixpanelTopBannerEvents.TopBannerClicked, 'rebranding')
-          }}
-          sx={{ display: 'inline', padding: 3 }}
-        >
-          <WithArrow variant="boldParagraph2" sx={{ fontSize: '16px', display: 'inline' }}>
-            <Icon
-              icon={loudspeaker}
-              sx={{ mr: 2, position: 'relative', top: '2px', transition: '0.2s transform' }}
-            />
-            {banner.message}
-          </WithArrow>
-        </AppLink>
-        {banner.closeable && (
+        <WithArrow variant="boldParagraph2" sx={{ fontSize: '16px', display: 'inline' }}>
           <Icon
-            icon={close}
-            sx={{
-              position: 'absolute',
-              top: '4px',
-              right: '20px',
-              cursor: 'pointer',
-              padding: 3,
-              boxSizing: 'content-box',
-            }}
-            onClick={() => {
-              trackingEvents.topBannerEvent(
-                MixpanelTopBannerEvents.TopBannerClosed,
-                banner.name as string,
-              )
-              setBannerClosed(true)
-            }}
+            icon={loudspeaker}
+            sx={{ mr: 2, position: 'relative', top: '2px', transition: '0.2s transform' }}
           />
-        )}
-      </Box>
-    </>
+          {banner.message}
+        </WithArrow>
+      </AppLink>
+      {banner.closeable && (
+        <Icon
+          icon={close}
+          sx={{
+            position: 'absolute',
+            top: '4px',
+            right: '20px',
+            cursor: 'pointer',
+            padding: 3,
+            boxSizing: 'content-box',
+          }}
+          onClick={() => {
+            trackingEvents.topBannerEvent(
+              MixpanelTopBannerEvents.TopBannerClosed,
+              banner.name as string,
+            )
+            setBannerClosed(true)
+          }}
+        />
+      )}
+    </Box>
   )
 }
 
@@ -89,11 +88,12 @@ export default function TopBanners() {
   const { topBanner, locationBanner } = useAppConfig('parameters')
   const [viewerCountry] = useCookie('country')
 
-  const loccationBasedBanner = getLocationBanner(viewerCountry, locationBanner)
+  const locationBasedBanner = getLocationBanner(viewerCountry, locationBanner)
+
   return (
     <>
       <OneBanner banner={topBanner} />
-      {loccationBasedBanner && <OneBanner banner={loccationBasedBanner} />}
+      {locationBasedBanner && <OneBanner banner={locationBasedBanner} />}
     </>
   )
 }
