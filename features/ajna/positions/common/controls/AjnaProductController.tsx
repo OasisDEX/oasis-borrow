3b1ec7d91fdfd2f77fc345e44025eb0eadb65756
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-10-25 13:42:06 +0200

    prettier

--- features/ajna/positions/common/controls/AjnaProductController.tsx
+++ features/ajna/positions/common/controls/AjnaProductController.tsx
@@ -21,9 +21,7 @@ import { getAjnaEarnDefaultAction } from 'features/ajna/positions/earn/helpers/g
 import { getAjnaEarnDefaultUiDropdown } from 'features/ajna/positions/earn/helpers/getAjnaEarnDefaultUiDropdown'
 import { getEarnDefaultPrice } from 'features/ajna/positions/earn/helpers/getEarnDefaultPrice'
 import { useAjnaEarnFormReducto } from 'features/ajna/positions/earn/state/ajnaEarnFormReducto'
-import {
-  AjnaMultiplyPositionController,
-} from 'features/ajna/positions/multiply/controls/AjnaMultiplyPositionController'
+import { AjnaMultiplyPositionController } from 'features/ajna/positions/multiply/controls/AjnaMultiplyPositionController'
 import { useAjnaMultiplyFormReducto } from 'features/ajna/positions/multiply/state/ajnaMultiplyFormReducto'
 import { WithTermsOfService } from 'features/termsOfService/TermsOfService'
 import { WithWalletAssociatedRisk } from 'features/walletAssociatedRisk/WalletAssociatedRisk'
