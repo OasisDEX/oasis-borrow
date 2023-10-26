f0fc613d068c63c59299c2f120e1d6d2d94fc733
Marcin Ciarka <marcin@oazoapps.com>
2023-10-26 15:45:04 +0200

    New portfolio empty page

---
+++ pages/portfolio/[address].tsx
@@ -0,0 +1,26 @@
+import { AppLayout } from 'components/layouts/AppLayout'
+import { useRedirect } from 'helpers/useRedirect'
+import type { GetServerSidePropsContext } from 'next'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import React, { useEffect } from 'react'
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale ?? 'en', ['portfolio', 'navigation', 'system'])),
+      address: ctx.query?.address ?? null,
+    },
+  }
+}
+
+export default function PortfolioView({ address }: { address: string }) {
+  const { t: tPortfolio } = useTranslation('portfolio')
+  const { replace } = useRedirect()
+  useEffect(() => {
+    if (!address) {
+      replace('/')
+    }
+  }, [address, replace])
+  return address ? <AppLayout>{tPortfolio('portfolio-view', { address })}</AppLayout> : null
+}
