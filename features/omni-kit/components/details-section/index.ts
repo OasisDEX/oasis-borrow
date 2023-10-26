4db1c0e4c5c979ef17c06788d7078a04978d0ee6
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-10-26 12:29:54 +0200

    omni details cards

---
+++ features/omni-kit/components/details-section/index.ts
@@ -0,0 +1,4 @@
+export { OmniContentCardLtv } from './OmniContentCardLtv'
+export { OmniContentCardNetBorrowCost } from './OmniContentCardNetBorrowCost'
+export { OmniContentCardLiquidationPrice } from './OmniContentCardLiquidationPrice'
+export { OmniContentCardNetValue } from './OmniContentCardNetValue'
