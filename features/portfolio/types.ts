a55fbead14219c10800aa236ece5ad61258653bc
Marcin Ciarka <marcin@oazoapps.com>
2023-10-30 12:05:41 +0200

    🍿 [Portfolio] Wallet endpoint (#3038)
    * Added api/portfolio/wallet endpoint, some types, logging on the new empty portfolio page
    
    * add debank env
    
    * remove switch

---
+++ features/portfolio/types.ts
@@ -0,0 +1,38 @@
+import type { NetworkNames } from 'blockchain/networks'
+
+export type DebankTokenReply = {
+  id: string
+  chain: string
+  name: string
+  symbol: string
+  display_symbol: string
+  optimized_symbol: string
+  decimals: number
+  logo_url: string
+  protocol_id: string
+  price: number
+  price_24h_change: number
+  is_verified: boolean
+  is_core: boolean
+  is_wallet: boolean
+  time_at: number
+  amount: number
+  raw_amount: number
+  raw_amount_hex_str: string
+}
+export type DebankTokensReply = DebankTokenReply[]
+
+export type PortfolioAssetsToken = {
+  name: string
+  network: NetworkNames
+  priceUSD: number
+  price24hChange: number
+  balance: number
+  balanceUSD: number
+}
+
+export type PortfolioAssetsReply = {
+  totalUSDAssets: number
+  totalUSDAssets24hChange: number
+  assets: PortfolioAssetsToken[]
+}
--- pages/portfolio/[address].tsx
+++ pages/portfolio/[address].tsx
@@ -1,9 +1,12 @@
 import { AppLayout } from 'components/layouts/AppLayout'
+import type { PortfolioAssetsReply } from 'features/portfolio/types'
 import { useRedirect } from 'helpers/useRedirect'
 import type { GetServerSidePropsContext } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React, { useEffect } from 'react'
+import { Box, Flex } from 'theme-ui'
+import { useFetch } from 'usehooks-ts'
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   return {
@@ -22,5 +25,17 @@ export default function PortfolioView({ address }: { address: string }) {
       replace('/')
     }
   }, [address, replace])
-  return address ? <AppLayout>{tPortfolio('portfolio-view', { address })}</AppLayout> : null
+  const { data: portfolioData } = useFetch<PortfolioAssetsReply>(`/api/portfolio/wallet/${address}`)
+  return address ? (
+    <AppLayout>
+      <Flex sx={{ flexDirection: 'column' }}>
+        <Box>
+          <h3>{tPortfolio('portfolio-view', { address })}</h3>
+        </Box>
+        <Box>
+          <pre>{JSON.stringify(portfolioData, null, 2)}</pre>
+        </Box>
+      </Flex>
+    </AppLayout>
+  ) : null
 }
