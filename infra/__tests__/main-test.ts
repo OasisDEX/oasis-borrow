69c2997225cba9d83328de717a4ef725f7e3801a
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2023-11-02 14:24:58 +0100

    tsc check

--- infra/__tests__/main-test.ts
+++ infra/__tests__/main-test.ts
@@ -1,6 +1,6 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import 'cdktf/lib/testing/adapters/jest' // Load types for expect matchers
+// import 'cdktf/lib/testing/adapters/jest' // Load types for expect matchers
 // import { Testing } from "cdktf";
 
 describe('My CDKTF Application', () => {
