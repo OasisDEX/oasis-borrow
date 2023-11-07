3ae05f7f0630102c6bf7e1d808c42968d28ad01b
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2023-11-07 09:58:27 +0100

    wip

--- lambdas/src/portfolio-positions/index.ts
+++ lambdas/src/portfolio-positions/index.ts
@@ -5,6 +5,15 @@ import { getDefaultErrorMessage } from '../shared/helpers'
 import { ResponseBadRequest, ResponseOk } from '../shared/responses'
 import { getAddressFromRequest } from '../shared/validators'
 import { mockPositionsResponse } from './mock'
+import { PortfolioPosition, PortfolioPositionsResponse } from '../shared/domain-types'
+import { DebankComplexProtocol } from '../shared/debank-types'
+
+const { DEBANK_API_KEY: debankApiKey, DEBANK_API_URL: serviceUrl } = process.env
+if (!debankApiKey) {
+  throw new Error('Missing DEBANK_API_KEY')
+}
+const debankAuthHeaderKey = 'Accesskey'
+const headers = { [debankAuthHeaderKey]: debankApiKey }
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
   // validate the query
@@ -18,7 +27,34 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
     return ResponseBadRequest(message)
   }
 
-  return ResponseOk({ body: mockPositionsResponse.response })
+  const reqUrl = new URL(`${serviceUrl}/v1/user/all_token_list?id=${address}&chain`)
+  const response: DebankComplexProtocol[] = await fetch(reqUrl.toString(), {
+    headers,
+  })
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error(error)
+      throw new Error('Failed to fetch wallet assets')
+    })
+
+  const positionsPerProtocol = response.map((protocol): PortfolioPosition => {
+    const { idchain, name, logo_url, tvl } = protocol
+    return {
+      name: protocol.name,
+      symbol: protocol.symbol,
+      network: chain,
+      priceUSD: price,
+      price24hChange: price_24h_change,
+      balance: amount,
+      balanceUSD: amount * price,
+    }
+  })
+
+  const body: PortfolioPositionsResponse = {
+    positions: positionsPerProtocol.flat(),
+  }
+
+  return ResponseOk({ body })
 }
 
 export default handler
