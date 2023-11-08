506428e43e79e0f60f0e2a69741db2478a940c79
Marcin Ciarka <marcin@oazoapps.com>
2023-11-08 16:34:24 +0100

    fix

--- handlers/portfolio/positions/index.ts
+++ handlers/portfolio/positions/index.ts
@@ -5,7 +5,6 @@ export const portfolioPositionsHandler = async (
   req: NextApiRequest,
 ): Promise<{ positions: PortfolioPosition[]; address: string }> => {
   const { address } = req.query
-  console.log('portfolioPositionsHandler address', address)
   const positions = {} as PortfolioPosition[]
   return { positions, address: address as string }
 }
