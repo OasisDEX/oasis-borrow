3213c2cb83d42fdd37ba0d6bf525481aa15d9e68
Marcin Ciarka <marcin@oazoapps.com>
2023-11-08 16:41:07 +0100

    fix

--- helpers/clients/portfolio-client.ts
+++ helpers/clients/portfolio-client.ts
@@ -18,7 +18,11 @@ export const usePortfolioClient = (baseUrl: string, headers: HeadersInit) => {
       section: 'overview' | 'assets' | 'positions',
       address: string,
     ): Promise<ResponseType> => {
-      const response = await fetch(`${baseUrl}/portfolio-${section}?address=${address}`, {
+      const callUrl =
+        section === 'positions'
+          ? `/api/positions/${address}`
+          : `${baseUrl}/portfolio-${section}?address=${address}`
+      const response = await fetch(callUrl, {
         headers,
       })
         .then((res) => res.json())
