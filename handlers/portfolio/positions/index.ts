68d3463471de334c88e8408214855898afd4d6d0
Marcin Ciarka <marcin@oazoapps.com>
2023-11-08 16:57:41 +0100

    empty handler

--- handlers/portfolio/positions/index.ts
+++ handlers/portfolio/positions/index.ts
@@ -1,10 +1,18 @@
+import { ajnaPositionsHandler } from 'handlers/portfolio/positions/handlers/ajna'
 import type { PortfolioPosition } from 'handlers/portfolio/types'
 import type { NextApiRequest } from 'next'
 
 export const portfolioPositionsHandler = async (
   req: NextApiRequest,
-): Promise<{ positions: PortfolioPosition[]; address: string }> => {
-  const { address } = req.query
-  const positions = [] as PortfolioPosition[]
-  return { positions, address: address as string }
+): Promise<{ positions: PortfolioPosition[]; address: string; error?: string }> => {
+  const { address } = req.query as { address: string }
+  const positionsReply = await Promise.all([ajnaPositionsHandler({ address })])
+    .then(([ajnaPositions]) => {
+      return { positions: [...ajnaPositions], address: address as string }
+    })
+    .catch((error) => {
+      console.error(error)
+      return { positions: [], address: address as string, error: JSON.stringify(error) }
+    })
+  return positionsReply
 }
