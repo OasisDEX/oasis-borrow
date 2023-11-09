794fefcdcf92407f96f1f09d58f43eb976a75f21
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-09 12:04:17 +0100

    added price tickers to handlers

--- handlers/portfolio/positions/index.ts
+++ handlers/portfolio/positions/index.ts
@@ -1,15 +1,24 @@
+import BigNumber from 'bignumber.js'
+import type { Tickers } from 'blockchain/prices.types'
 import { aaveV3PositionsHandler } from 'handlers/portfolio/positions/handlers/aaveV3'
 import { ajnaPositionsHandler } from 'handlers/portfolio/positions/handlers/ajna'
 import type { PortfolioPosition } from 'handlers/portfolio/types'
+import { tokenTickers } from 'helpers/api/tokenTickers'
 import type { NextApiRequest } from 'next'
 
 export const portfolioPositionsHandler = async (
   req: NextApiRequest,
 ): Promise<{ positions: PortfolioPosition[]; address: string; error?: string }> => {
+  const tickersResponse = await tokenTickers()
+  const tickers = Object.entries(tickersResponse).reduce((acc, [key, value]) => {
+    acc[key.toLowerCase()] = new BigNumber(value)
+    return acc
+  }, {} as Tickers)
+
   const { address } = req.query as { address: string }
   const positionsReply = await Promise.all([
-    ajnaPositionsHandler({ address }),
-    aaveV3PositionsHandler({ address }),
+    ajnaPositionsHandler({ address, tickers }),
+    aaveV3PositionsHandler({ address, tickers }),
   ])
     .then(([{ positions: ajnaPositions }, { positions: aaveV3Positions }]) => {
       return { positions: [...ajnaPositions, ...aaveV3Positions], address: address as string }
@@ -18,5 +27,6 @@ export const portfolioPositionsHandler = async (
       console.error(error)
       return { positions: [], address: address as string, error: JSON.stringify(error) }
     })
+
   return positionsReply
 }
