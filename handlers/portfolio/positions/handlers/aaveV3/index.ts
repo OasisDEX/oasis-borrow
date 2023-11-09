1728314366899181bc22aa0616dbc9c58cc4cbd6
Marcin Ciarka <marcin@oazoapps.com>
2023-11-09 14:21:57 +0200

    🍿 [Portfolio] DPM list on every network (#3105)

--- handlers/portfolio/positions/handlers/aaveV3/index.ts
+++ handlers/portfolio/positions/handlers/aaveV3/index.ts
@@ -1,9 +1,10 @@
 import type { PortfolioPosition, PortfolioPositionsHandler } from 'handlers/portfolio/types'
 
-export const aaveV3PositionsHandler: PortfolioPositionsHandler = async ({ address }) => {
+export const aaveV3PositionsHandler: PortfolioPositionsHandler = async ({ address, dpmList }) => {
   const positions = [] as PortfolioPosition[]
   return {
     positions,
     address,
+    dpmList,
   }
 }
--- handlers/portfolio/positions/index.ts
+++ handlers/portfolio/positions/index.ts
@@ -2,13 +2,22 @@ import BigNumber from 'bignumber.js'
 import type { Tickers } from 'blockchain/prices.types'
 import { aaveV3PositionsHandler } from 'handlers/portfolio/positions/handlers/aaveV3'
 import { ajnaPositionsHandler } from 'handlers/portfolio/positions/handlers/ajna'
+import type { DpmList } from 'handlers/portfolio/positions/handlers/dpm'
+import { getAllDpmsForWallet } from 'handlers/portfolio/positions/handlers/dpm'
 import type { PortfolioPosition } from 'handlers/portfolio/types'
 import { tokenTickers } from 'helpers/api/tokenTickers'
 import type { NextApiRequest } from 'next'
 
+type PortfolioPositionsReply = {
+  positions: PortfolioPosition[]
+  address: string
+  error?: string
+  dpmList: DpmList
+}
+
 export const portfolioPositionsHandler = async (
   req: NextApiRequest,
-): Promise<{ positions: PortfolioPosition[]; address: string; error?: string }> => {
+): Promise<PortfolioPositionsReply> => {
   const tickersResponse = await tokenTickers()
   const tickers = Object.entries(tickersResponse).reduce<Tickers>(
     (acc, [key, value]) => ({
@@ -19,16 +28,21 @@ export const portfolioPositionsHandler = async (
   )
 
   const { address } = req.query as { address: string }
+  const dpmList = await getAllDpmsForWallet({ address })
   const positionsReply = await Promise.all([
-    ajnaPositionsHandler({ address, tickers }),
-    aaveV3PositionsHandler({ address, tickers }),
+    ajnaPositionsHandler({ address, tickers, dpmList }),
+    aaveV3PositionsHandler({ address, tickers, dpmList }),
   ])
     .then(([{ positions: ajnaPositions }, { positions: aaveV3Positions }]) => {
-      return { positions: [...ajnaPositions, ...aaveV3Positions], address: address as string }
+      return {
+        positions: [...ajnaPositions, ...aaveV3Positions],
+        address: address as string,
+        dpmList,
+      }
     })
     .catch((error) => {
       console.error(error)
-      return { positions: [], address: address as string, error: JSON.stringify(error) }
+      return { positions: [], address: address as string, error: JSON.stringify(error), dpmList }
     })
 
   return positionsReply
