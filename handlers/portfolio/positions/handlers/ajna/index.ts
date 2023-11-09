cc7efd7d0fd6dd8242299f8f1b9e4d7f4856e136
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-09 13:17:27 +0100

    load position

--- handlers/portfolio/positions/handlers/ajna/index.ts
+++ handlers/portfolio/positions/handlers/ajna/index.ts
@@ -1,7 +1,35 @@
+import { views } from '@oasisdex/dma-library'
+import { getNetworkContracts } from 'blockchain/contracts'
+import { getRpcProvider, NetworkIds } from 'blockchain/networks'
+import { getTokenPrice } from 'blockchain/prices'
+import { getAjnaCumulatives } from 'features/ajna/positions/common/helpers/getAjnaCumulatives'
+import { getAjnaPoolData } from 'features/ajna/positions/common/helpers/getAjnaPoolData'
 import type { PortfolioPosition, PortfolioPositionsHandler } from 'handlers/portfolio/types'
 
-export const ajnaPositionsHandler: PortfolioPositionsHandler = async ({ address }) => {
+export const ajnaPositionsHandler: PortfolioPositionsHandler = async ({ address, tickers }) => {
   const positions = [] as PortfolioPosition[]
+
+  const { ajnaPoolPairs, ajnaPoolInfo } = getNetworkContracts(NetworkIds.MAINNET)
+
+  const commonPayload = {
+    collateralPrice: getTokenPrice('SDAI', tickers),
+    quotePrice: getTokenPrice('USDC', tickers),
+    proxyAddress: '0x07aab97fbaf2a8fc7d209e342fb161f3cb7e7a7b',
+    poolAddress: ajnaPoolPairs['SDAI-USDC' as keyof typeof ajnaPoolPairs].address,
+  }
+
+  const commonDependency = {
+    poolInfoAddress: ajnaPoolInfo.address,
+    provider: getRpcProvider(NetworkIds.MAINNET),
+    getPoolData: getAjnaPoolData(NetworkIds.MAINNET),
+    getCumulatives: getAjnaCumulatives(NetworkIds.MAINNET),
+  }
+
+  const position = await views.ajna.getPosition(commonPayload, commonDependency)
+
+  console.log(position.collateralAmount.toString())
+  console.log(position.debtAmount.toString())
+
   return {
     positions,
     address,
