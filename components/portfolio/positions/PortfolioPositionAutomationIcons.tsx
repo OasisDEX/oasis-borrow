4ecf16fc5ec36643c38bba36f719390a84294de7
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-13 12:52:54 +0100

    linking blocks

--- components/portfolio/positions/PortfolioPositionAutomationIcons.tsx
+++ components/portfolio/positions/PortfolioPositionAutomationIcons.tsx
@@ -1,25 +1,36 @@
 import { AutomationIcon } from 'components/AutomationIcon'
+import { AppLink } from 'components/Links'
 import type { PortfolioPosition } from 'handlers/portfolio/types'
 import React from 'react'
 
 export const PortfolioPositionAutomationIcons = ({
   automations = {},
+  url,
 }: {
   automations: PortfolioPosition['automations']
+  url: string
 }) => {
   return (
     <>
       {automations.stopLoss && (
-        <AutomationIcon enabled={automations.stopLoss.enabled} type="stopLoss" />
+        <AppLink href={url} hash="protection">
+          <AutomationIcon enabled={automations.stopLoss.enabled} type="stopLoss" />
+        </AppLink>
       )}
       {automations.autoBuy && (
-        <AutomationIcon enabled={automations.autoBuy.enabled} type="autoBuy" />
+        <AppLink href={url} hash="optimization">
+          <AutomationIcon enabled={automations.autoBuy.enabled} type="autoBuy" />
+        </AppLink>
       )}
       {automations.autoSell && (
-        <AutomationIcon enabled={automations.autoSell.enabled} type="autoSell" />
+        <AppLink href={url} hash="protection">
+          <AutomationIcon enabled={automations.autoSell.enabled} type="autoSell" />
+        </AppLink>
       )}
       {automations.takeProfit && (
-        <AutomationIcon enabled={automations.takeProfit.enabled} type="takeProfit" />
+        <AppLink href={url} hash="optimization">
+          <AutomationIcon enabled={automations.takeProfit.enabled} type="takeProfit" />
+        </AppLink>
       )}
     </>
   )
