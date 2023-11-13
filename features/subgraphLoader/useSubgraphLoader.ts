2eb5c4ae7ea9239977b684901a876d818560747c
Marcin Ciarka <marcin@oazoapps.com>
2023-11-13 22:22:43 +0100

    rm

--- features/subgraphLoader/useSubgraphLoader.ts
+++ features/subgraphLoader/useSubgraphLoader.ts
@@ -1,11 +1,9 @@
 import type { NetworkIds } from 'blockchain/networks'
-import { subgraphMethodsRecord, subgraphsRecord } from 'features/subgraphLoader/consts'
 import type {
   SubgraphBaseResponse,
   Subgraphs,
   SubgraphsResponses,
 } from 'features/subgraphLoader/types'
-import request from 'graphql-request'
 import { useEffect, useState } from 'react'
 
 interface UseSubgraphLoader<R> {
@@ -24,19 +22,6 @@ export async function loadSubgraph<
   networkId: NetworkIds,
   params: P = {} as P,
 ): Promise<SubgraphsResponses[S][keyof SubgraphsResponses[S]]> {
-  if (global.window === undefined) {
-    const subgraphUrl = subgraphsRecord[subgraph][networkId]
-    const subgraphQuery = subgraphMethodsRecord[method as keyof typeof subgraphMethodsRecord]
-    const subgraphParams = params as {}
-
-    const backendResponse = await request(subgraphUrl, subgraphQuery, subgraphParams)
-
-    return {
-      success: !!backendResponse,
-      error: undefined,
-      response: backendResponse,
-    } as SubgraphsResponses[S][keyof SubgraphsResponses[S]]
-  }
   const resolvedUrl = global.window?.location.origin || 'http://localhost:3000'
 
   const response = await fetch(`${resolvedUrl}/api/subgraph`, {
