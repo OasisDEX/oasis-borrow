26e8ff8a059bae5a9fcbf6474140c9cd3feeda68
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-13 18:04:31 +0100

    caching price reponse

--- handlers/portfolio/positions/index.ts
+++ handlers/portfolio/positions/index.ts
@@ -5,6 +5,7 @@ import { makerPositionsHandler } from 'handlers/portfolio/positions/handlers/mak
 import { getPositionsFromDatabase, getTokensPrices } from 'handlers/portfolio/positions/helpers'
 import { getAllDpmsForWallet } from 'handlers/portfolio/positions/helpers/getAllDpmsForWallet'
 import type { PortfolioPosition } from 'handlers/portfolio/types'
+import { cacheObject } from 'helpers/api/cacheObject'
 import type { NextApiRequest } from 'next'
 
 type PortfolioPositionsReply = {
@@ -12,37 +13,53 @@ type PortfolioPositionsReply = {
   error?: string
 }
 
+export const getCachedTokensPrices = cacheObject(getTokensPrices, 2 * 60, 'portfolio-prices')
+
 export const portfolioPositionsHandler = async (
   req: NextApiRequest,
 ): Promise<PortfolioPositionsReply> => {
   const { address } = req.query as { address: string }
 
   const apiVaults = await getPositionsFromDatabase({ address })
-  const prices = await getTokensPrices()
-
   const dpmList = await getAllDpmsForWallet({ address })
-  const positionsReply = await Promise.all([
-    aaveLikePositionsHandler({ address, dpmList, apiVaults, prices }),
-    ajnaPositionsHandler({ address, dpmList, apiVaults, prices }),
-    dsrPositionsHandler({ address, dpmList, apiVaults, prices }),
-    makerPositionsHandler({ address, dpmList, apiVaults, prices }),
-  ])
-    .then(
-      ([
-        { positions: aaveV3Positions },
-        { positions: ajnaPositions },
-        { positions: dsrPositions },
-        { positions: makerPositions },
-      ]) => {
-        return {
-          positions: [...aaveV3Positions, ...ajnaPositions, ...dsrPositions, ...makerPositions],
-        }
-      },
-    )
-    .catch((error) => {
-      console.error(error)
-      return { positions: [], address, error: JSON.stringify(error), dpmList }
-    })
+  const prices = await getCachedTokensPrices()
 
-  return positionsReply
+  if (prices) {
+    const payload = {
+      address,
+      dpmList,
+      apiVaults,
+      prices: prices.data,
+    }
+
+    const positionsReply = await Promise.all([
+      aaveLikePositionsHandler(payload),
+      ajnaPositionsHandler(payload),
+      dsrPositionsHandler(payload),
+      makerPositionsHandler(payload),
+    ])
+      .then(
+        ([
+          { positions: aaveV3Positions },
+          { positions: ajnaPositions },
+          { positions: dsrPositions },
+          { positions: makerPositions },
+        ]) => {
+          return {
+            positions: [...aaveV3Positions, ...ajnaPositions, ...dsrPositions, ...makerPositions],
+          }
+        },
+      )
+      .catch((error) => {
+        console.error(error)
+        return { positions: [], address, error: JSON.stringify(error), dpmList }
+      })
+
+    return positionsReply
+  } else {
+    return {
+      positions: [],
+      error: 'Unable to load token prices',
+    }
+  }
 }
