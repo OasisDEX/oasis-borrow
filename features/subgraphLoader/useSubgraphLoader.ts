67e1c27a14c3c64eed4b9c0a1934ce91912fa28d
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-13 22:27:50 +0100

    Server side subgraph loader (#3125)

--- features/subgraphLoader/useSubgraphLoader.ts
+++ features/subgraphLoader/useSubgraphLoader.ts
@@ -1,9 +1,11 @@
 import type { NetworkIds } from 'blockchain/networks'
+import { subgraphMethodsRecord, subgraphsRecord } from 'features/subgraphLoader/consts'
 import type {
   SubgraphBaseResponse,
   Subgraphs,
   SubgraphsResponses,
 } from 'features/subgraphLoader/types'
+import request from 'graphql-request'
 import { useEffect, useState } from 'react'
 
 interface UseSubgraphLoader<R> {
@@ -22,19 +24,33 @@ export async function loadSubgraph<
   networkId: NetworkIds,
   params: P = {} as P,
 ): Promise<SubgraphsResponses[S][keyof SubgraphsResponses[S]]> {
-  const resolvedUrl = global.window?.location.origin || 'http://localhost:3000'
+  if (global.window === undefined) {
+    const response = await request(
+      subgraphsRecord[subgraph][networkId],
+      subgraphMethodsRecord[method as keyof typeof subgraphMethodsRecord],
+      params as P,
+    )
 
-  const response = await fetch(`${resolvedUrl}/api/subgraph`, {
-    method: 'POST',
-    body: JSON.stringify({
-      subgraph,
-      method,
-      params,
-      networkId,
-    }),
-  })
+    return {
+      success: !!response,
+      error: undefined,
+      response,
+    } as SubgraphsResponses[S][keyof SubgraphsResponses[S]]
+  } else {
+    const resolvedUrl = global.window?.location.origin || 'http://localhost:3000'
 
-  return await response.json()
+    const response = await fetch(`${resolvedUrl}/api/subgraph`, {
+      method: 'POST',
+      body: JSON.stringify({
+        subgraph,
+        method,
+        params,
+        networkId,
+      }),
+    })
+
+    return await response.json()
+  }
 }
 
 export function useSubgraphLoader<
