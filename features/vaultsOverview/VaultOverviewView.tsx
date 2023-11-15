54fead3fcda30426afab1c00af1037f6eaac44b0
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-15 12:49:11 +0100

    new porttfolio banner

--- features/vaultsOverview/VaultOverviewView.tsx
+++ features/vaultsOverview/VaultOverviewView.tsx
@@ -1,23 +1,31 @@
 import { Announcement } from 'components/Announcement'
 import { useAccountContext } from 'components/context/AccountContextProvider'
 import { useMainContext } from 'components/context/MainContextProvider'
+import { Icon } from 'components/Icon'
+import { AppLink } from 'components/Links'
 import { getAddress } from 'ethers/lib/utils'
+import { VaultNotice } from 'features/notices/VaultsNoticesView'
 import { AssetsAndPositionsOverview } from 'features/vaultsOverview/containers/AssetsAndPositionsOverview'
 import { ConnectWalletPrompt } from 'features/vaultsOverview/containers/ConnectWalletPrompt'
 import { PositionsTable } from 'features/vaultsOverview/containers/PositionsTable'
 import { EXTERNAL_LINKS } from 'helpers/applicationLinks'
 import { useAppConfig } from 'helpers/config'
+import { getPortfolioLink } from 'helpers/get-portfolio-link'
 import { useObservable } from 'helpers/observableHook'
 import { useAccount } from 'helpers/useAccount'
 import React, { useMemo } from 'react'
-import { Grid } from 'theme-ui'
+import { useTranslation } from 'react-i18next'
+import { bannerWallet } from 'theme/icons'
+import { Flex, Grid, Text } from 'theme-ui'
 
 import { VaultOwnershipNotice } from './containers/VaultOwnershipNotice'
 import { useOwnerPositions } from './owner-positions'
 
 export function VaultsOverviewView({ address }: { address: string }) {
-  const { AjnaSafetySwitch: ajnaSafetySwitchOn } = useAppConfig('features')
+  const { AjnaSafetySwitch: ajnaSafetySwitchOn, NewPortfolio: newPortfolioEnabled } =
+    useAppConfig('features')
 
+  const { t } = useTranslation()
   // calculating positions
   const mainContext = useMainContext()
   const accountContext = useAccountContext()
@@ -34,6 +42,35 @@ export function VaultsOverviewView({ address }: { address: string }) {
 
   return (
     <Grid sx={{ flex: 1, zIndex: 1, gap: '48px', mt: [0, 4], mb: 5 }} key={address}>
+      {newPortfolioEnabled && (
+        <VaultNotice
+          status={
+            <Flex
+              sx={{
+                border: 'bold',
+                borderRadius: 'circle',
+                width: '56px',
+                height: '56px',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Icon size="auto" width="24" height="24" icon={bannerWallet} />
+            </Flex>
+          }
+          header={t('vaults-overview.portfolio-banner-header')}
+          subheader={
+            <Text>
+              {t('vaults-overview.portfolio-banner-content')}{' '}
+              <AppLink href={getPortfolioLink(address)} sx={{ fontSize: 3 }}>
+                {t('here')}
+              </AppLink>
+            </Text>
+          }
+          color="primary100"
+          withClose={false}
+        />
+      )}
       {ajnaSafetySwitchOn &&
         isOwner &&
         positionsListData?.ajnaPositions &&
