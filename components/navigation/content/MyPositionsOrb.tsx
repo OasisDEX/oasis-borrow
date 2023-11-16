937d90c50bdaa0a5576d592e3fae7fa4e4c158f2
Marcin Ciarka <marcin@oazoapps.com>
2023-11-16 15:55:39 +0100

    Add portfolio positions count and local app config to MyPositionsOrb

--- components/navigation/content/MyPositionsOrb.tsx
+++ components/navigation/content/MyPositionsOrb.tsx
@@ -1,11 +1,18 @@
 import { NavigationOrb } from 'components/navigation/NavigationMenuOrb'
+import { usePortfolioPositionsCount } from 'helpers/clients/portfolio-positions-count'
+import { getLocalAppConfig } from 'helpers/config'
 import { getPortfolioLink } from 'helpers/get-portfolio-link'
 import { useAccount } from 'helpers/useAccount'
 import React from 'react'
 import { home } from 'theme/icons'
 
 export function MyPositionsOrb() {
-  const { amountOfPositions, walletAddress } = useAccount()
+  const { NewPortfolio } = getLocalAppConfig('features')
+  const { amountOfPositions: oldAmountOfPositions, walletAddress } = useAccount()
+  const { amountOfPositions: newAmountOfPositions } = usePortfolioPositionsCount({
+    address: walletAddress?.toLowerCase(),
+  })
+  const amountOfPositions = NewPortfolio ? newAmountOfPositions : oldAmountOfPositions
 
   return (
     <NavigationOrb
