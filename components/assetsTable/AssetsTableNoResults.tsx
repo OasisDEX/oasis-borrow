5aa5e5b122ca40a5ef30d8eb0a5b327b9bdcd242
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-20 15:29:04 +0100

    Portfolio page empty and loading states (#3161)
    * positions empty state
    
    * better loading states
    
    * empty featured section
    
    * wallet empty states

--- components/assetsTable/AssetsTableNoResults.tsx
+++ components/assetsTable/AssetsTableNoResults.tsx
@@ -1,35 +1,12 @@
-import { staticFilesRuntimeUrl } from 'helpers/staticPaths'
+import { EmptyState } from 'components/EmptyState'
 import type { ReactNode } from 'react'
 import React from 'react'
-import { Flex, Heading, Image, Text } from 'theme-ui'
 
 interface AssetsTableNoResultsProps {
-  header: ReactNode
+  header: string
   content: ReactNode
 }
 
 export function AssetsTableNoResults({ header, content }: AssetsTableNoResultsProps) {
-  return (
-    <Flex
-      sx={{
-        flexDirection: 'column',
-        alignItems: ['flex-start', 'center'],
-        px: ['24px', 4],
-        pt: 5,
-        pb: '96px',
-        textAlign: ['left', 'center'],
-      }}
-    >
-      <Image
-        src={staticFilesRuntimeUrl('/static/img/no-positions.svg')}
-        sx={{ alignSelf: 'center' }}
-      />
-      <Heading variant="boldParagraph2" sx={{ mt: 4, mb: 1 }}>
-        {header}
-      </Heading>
-      <Text as="p" variant="paragraph2" sx={{ m: 0, maxWidth: '804px', color: 'neutral80' }}>
-        {content}
-      </Text>
-    </Flex>
-  )
+  return <EmptyState header={header}>{content}</EmptyState>
 }
