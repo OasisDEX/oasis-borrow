c56128b1b1ae0e2a751191ddaf46d9452f61d797
Marcin Ciarka <marcin@oazoapps.com>
2023-11-23 14:47:04 +0100

    Remove WBTC-DAI token pair from aaveTokenPairsAllowedAutomation (duplicated)

--- features/automation/common/consts.ts
+++ features/automation/common/consts.ts
@@ -84,7 +84,6 @@ export const aaveTokenPairsAllowedAutomation = [
   ['WSTETH', 'DAI'],
   ['RETH', 'DAI'],
   ['CBETH', 'DAI'],
-  ['WBTC', 'DAI'],
 ]
 
 export const vaultIdsThatAutoBuyTriggerShouldBeRecreated = [
