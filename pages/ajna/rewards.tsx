e1b2f0e32fb9c0bc5b72140d5c3ce72669c3ef40
Piotr Konowrocki <piotr@oazoapps.com>
2023-11-27 14:21:43 +0100

    redirect

--- pages/ajna/rewards.tsx
+++ pages/ajna/rewards.tsx
@@ -1,33 +1,17 @@
-import { WithConnection } from 'components/connectWallet'
-import { FunctionalContextHandler } from 'components/context'
-import { AjnaRewardsController } from 'features/ajna/common/controls/AjnaRewardsController'
-import { AjnaLayout, ajnaPageSeoTags } from 'features/ajna/common/layout'
-import { WithTermsOfService } from 'features/termsOfService/TermsOfService'
-import { WithWalletAssociatedRisk } from 'features/walletAssociatedRisk/WalletAssociatedRisk'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { INTERNAL_LINKS } from 'helpers/applicationLinks'
 import React from 'react'
 
 function AjnaRewardsPage() {
-  return (
-    <FunctionalContextHandler>
-      <WithConnection>
-        <WithTermsOfService>
-          <WithWalletAssociatedRisk>
-            <AjnaRewardsController />
-          </WithWalletAssociatedRisk>
-        </WithTermsOfService>
-      </WithConnection>
-    </FunctionalContextHandler>
-  )
+  return <></>
 }
-
-AjnaRewardsPage.layout = AjnaLayout
-AjnaRewardsPage.seoTags = ajnaPageSeoTags
 
 export default AjnaRewardsPage
 
-export const getStaticProps = async ({ locale }: { locale: string }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['common'])),
-  },
-})
+export async function getStaticProps() {
+  return {
+    redirect: {
+      permanent: true,
+      destination: `${INTERNAL_LINKS.appUrl}/${INTERNAL_LINKS.ajnaRewards}`,
+    },
+  }
+}
