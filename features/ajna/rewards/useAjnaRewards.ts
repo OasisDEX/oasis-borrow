3e63c4c799ae516df839b3041065b6782ab315da
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-12-04 15:29:02 +0100

    Removed old-ajna rewards handling

--- features/ajna/rewards/useAjnaRewards.ts
+++ features/ajna/rewards/useAjnaRewards.ts
@@ -1,11 +1,5 @@
-import BigNumber from 'bignumber.js'
 import { Rewards } from 'features/ajna/common/components/AjnaRewardCard'
-import { getAjnaRewards } from 'features/ajna/positions/common/helpers/getAjnaRewards'
-import { useWalletManagement } from 'features/web3OnBoard'
-import { getAjnaRewardsData } from 'handlers/ajna-rewards/getAjnaRewardsData'
-import { useAccount } from 'helpers/useAccount'
 import { zero } from 'helpers/zero'
-import { useCallback, useEffect, useMemo, useState } from 'react'
 
 interface AjnaRewardsParamsState {
   rewards: Rewards
@@ -18,74 +12,14 @@ const defaultRewards = {
   tokens: zero,
   usd: zero,
 }
-const errorState = {
-  rewards: defaultRewards,
-  isError: true,
-  isLoading: false,
-}
 
-export const useAjnaRewards = (address?: string): AjnaRewardsParamsState => {
-  const { walletAddress } = useAccount()
-  const { chainId } = useWalletManagement()
-  const resolvedAddress = useMemo(() => address || walletAddress, [address, walletAddress])
-  const [state, setState] = useState<AjnaRewardsParamsState>({
+export const useAjnaRewards = (_?: string): AjnaRewardsParamsState => {
+  // Returning static object with zero rewards since we won't support rewards claiming on old-ajna
+  // and user are redirected to main domain if they want to claim rewards
+  return {
     rewards: defaultRewards,
     isError: false,
-    isLoading: true,
-    refetch: () => {},
-  })
-
-  const fetchData = useCallback(async (): Promise<void> => {
-    setState({
-      ...state,
-      isLoading: true,
-    })
-
-    if (resolvedAddress) {
-      Promise.all([
-        fetch(
-          `/api/ajna-rewards?address=${resolvedAddress.toLocaleLowerCase()}&networkId=${chainId}`,
-        ),
-        getAjnaRewards(resolvedAddress, chainId),
-      ])
-        .then(async ([apiResponse, subgraphResponse]) => {
-          const parseApiResponse = (await apiResponse.json()) as Awaited<
-            ReturnType<typeof getAjnaRewardsData>
-          >
-          const claimed = subgraphResponse.reduce((total, current) => total + current.amount, 0)
-
-          if (parseApiResponse.amount) {
-            setState({
-              ...state,
-              rewards: {
-                tokens: new BigNumber(parseApiResponse.amount).minus(claimed),
-                // TODO: recalculate when Ajna Token value is available
-                usd: zero,
-              },
-              isError: false,
-              isLoading: false,
-            })
-          }
-          if (parseApiResponse.error) {
-            setState({
-              ...state,
-              ...errorState,
-            })
-          }
-        })
-        .catch(() => {
-          setState({
-            ...state,
-            ...errorState,
-          })
-        })
-    }
-  }, [resolvedAddress])
-
-  useEffect(() => void fetchData(), [fetchData])
-
-  return {
-    ...state,
-    refetch: fetchData,
+    isLoading: false,
+    refetch: () => null,
   }
 }
