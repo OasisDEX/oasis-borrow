b5af261e6bacfd97155124b81d8e3018f842fc7f
Piotr Konowrocki <piotr@oazoapps.com>
2023-12-07 16:23:34 +0100

    Fix Ajna Base Pool Creator network contract (#3250)

--- blockchain/calls/ajnaErc20PoolFactory.constants.ts
+++ blockchain/calls/ajnaErc20PoolFactory.constants.ts
@@ -1,15 +1,16 @@
 import type { TransactionDef } from 'blockchain/calls/callsHelpers'
 import { getNetworkContracts } from 'blockchain/contracts'
-import { NetworkIds } from 'blockchain/networks'
 import type { AjnaErc20PoolFactory } from 'types/web3-v1-contracts'
 
 import type { DeployAjnaPoolTxData } from './ajnaErc20PoolFactory.types'
 
 export const deployAjnaPool: TransactionDef<DeployAjnaPoolTxData> = {
   call: (_, { contract, chainId }) => {
-    return contract<AjnaErc20PoolFactory>(
-      getNetworkContracts(NetworkIds.MAINNET, chainId).ajnaERC20PoolFactory,
-    ).methods.deployPool
+    const networkContracts = getNetworkContracts(chainId)
+
+    return 'ajnaERC20PoolFactory' in networkContracts
+      ? contract<AjnaErc20PoolFactory>(networkContracts.ajnaERC20PoolFactory).methods.deployPool
+      : { abi: {}, address: '' }
   },
   prepareArgs: (data) => {
     const { collateralAddress, quoteAddress, interestRate } = data
