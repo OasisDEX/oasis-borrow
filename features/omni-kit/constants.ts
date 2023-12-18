afcd18f857144f3bf2e31f47dddc32b939f4d0ce
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-12-18 16:06:56 +0100

    Fix Ajna L1 gas fees

--- features/omni-kit/constants.ts
+++ features/omni-kit/constants.ts
@@ -54,3 +54,5 @@ export const omniSwapVersionMap: Record<OmniSupportedNetworkIds, 'v4.0' | 'v5.0'
   [NetworkIds.ARBITRUMMAINNET]: 'v5.0',
   [NetworkIds.GOERLI]: 'v4.0', // doesn't matter
 }
+
+export const omniL2SupportedNetworks = [NetworkIds.BASEMAINNET, NetworkIds.OPTIMISMMAINNET]
--- features/omni-kit/observables/estimateOmniGas.ts
+++ features/omni-kit/observables/estimateOmniGas.ts
@@ -4,12 +4,14 @@ import { validateParameters } from 'blockchain/better-calls/dpm-account'
 import { GasMultiplier } from 'blockchain/better-calls/utils'
 import { getOverrides } from 'blockchain/better-calls/utils/get-overrides'
 import type { GasPriceParams } from 'blockchain/prices.types'
+import { getOptimismTransactionFee } from 'blockchain/transaction-fee'
 import type { ethers } from 'ethers'
+import { omniL2SupportedNetworks } from 'features/omni-kit/constants'
 import type { OmniTxData } from 'features/omni-kit/hooks'
 import type { OmniSupportedNetworkIds } from 'features/omni-kit/types'
 import { zero } from 'helpers/zero'
 import { combineLatest, from, of } from 'rxjs'
-import { catchError, first, map, startWith, switchMap } from 'rxjs/operators'
+import { catchError, first, startWith, switchMap } from 'rxjs/operators'
 
 export const estimateOmniGas$ = ({
   signer,
@@ -41,12 +43,36 @@ export const estimateOmniGas$ = ({
             new BigNumber(val.toString()).multipliedBy(GasMultiplier).decimalPlaces(0),
           ),
       ).pipe(
-        map((gasAmount) => ({
-          usdValue: amountFromWei(gasPrice.maxFeePerGas.times(gasAmount)).times(ethPrice),
-          gasAmount,
-          isSuccessful: true,
-          isCompleted: true,
-        })),
+        switchMap(async (gasAmount) => {
+          let usdValue = amountFromWei(gasPrice.maxFeePerGas.times(gasAmount)).times(ethPrice)
+
+          if (omniL2SupportedNetworks.includes(networkId)) {
+            const optimisTxFeeData = await getOptimismTransactionFee({
+              estimatedGas: gasAmount.toString(),
+              transactionData: txData.data,
+            })
+
+            if (!optimisTxFeeData) {
+              return {
+                usdValue: zero,
+                gasAmount,
+                isSuccessful: false,
+                isCompleted: true,
+              }
+            }
+
+            usdValue = amountFromWei(
+              new BigNumber(optimisTxFeeData.l1Fee).plus(optimisTxFeeData.l2Fee),
+            ).times(ethPrice)
+          }
+
+          return {
+            usdValue,
+            gasAmount,
+            isSuccessful: true,
+            isCompleted: true,
+          }
+        }),
       ),
     ),
     first(),
