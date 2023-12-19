4de9c44b6bdebd3566e28dec268082ad771fa366
Marcin Ciarka <marcin@oazoapps.com>
2023-12-19 12:47:31 +0100

    Remove unused code

--- features/vaultsOverview/components/VaultSuggestionsLoadingState.tsx
+++
@@ -1,6 +0,0 @@
-import { Skeleton } from 'components/Skeleton'
-import React from 'react'
-
-export function VaultSuggestionsLoadingState() {
-  return <Skeleton cols={3} count={3} height="640px" gap={4} />
-}
