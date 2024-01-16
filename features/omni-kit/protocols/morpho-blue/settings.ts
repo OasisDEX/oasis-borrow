ab2370b66634ed4e9b3f6dc2e2013e4c89115d94
Piotr Konowrocki <piotr@oazoapps.com>
2024-01-16 17:30:26 +0100

    Add supported Morpho Blue token pairs to Product Finder (#3379)
    * load basic data
    
    * calculate rate
    
    * display in table
    
    * types fix

--- features/omni-kit/protocols/morpho-blue/settings.ts
+++ features/omni-kit/protocols/morpho-blue/settings.ts
@@ -27,6 +27,7 @@ export const settings: OmniProtocolSettings = {
 export const morphoMarkets: NetworkIdsWithValues<{ [key: string]: string }> = {
   [NetworkIds.MAINNET]: {
     'WSTETH-ETH': '0xc54d7acf14de29e0e5527cabd7a576506870346a78a11a6762e2cca66322ec41',
+    'WSTETH-USDC': '0xb323495f7e4148be5643a4ea4a8221eef163e4bccfdedc2a6f4696baacbc86cc',
   },
   [NetworkIds.GOERLI]: {
     'WSTETH-ETH': '0x44f9ccdefcee6c2b8dc7bce454ea3a9dfc48ed3be1ef79ce4bc9696932b56ddd',
