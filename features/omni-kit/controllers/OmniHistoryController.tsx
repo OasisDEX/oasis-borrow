6860c99d49d44e4e176bc03731563a35c4451db5
Piotr Konowrocki <piotr@oazoapps.com>
2024-01-19 17:08:09 +0100

    Protocol name from context in history screen (#3405)

--- features/omni-kit/controllers/OmniHistoryController.tsx
+++ features/omni-kit/controllers/OmniHistoryController.tsx
@@ -1,6 +1,7 @@
 import { PositionHistory } from 'components/history/PositionHistory'
 import { useOmniGeneralContext, useOmniProductContext } from 'features/omni-kit/contexts'
 import { staticFilesRuntimeUrl } from 'helpers/staticPaths'
+import { LendingProtocolLabel } from 'lendingProtocols'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
 import { Flex, Grid, Heading, Image, Text } from 'theme-ui'
@@ -9,13 +10,14 @@ export function OmniHistoryController() {
   const { t } = useTranslation()
   const {
     environment: {
-      isOracless,
-      productType,
-      isShort,
-      priceFormat,
       collateralToken,
-      quoteToken,
+      isOracless,
+      isShort,
       networkId,
+      priceFormat,
+      productType,
+      protocol,
+      quoteToken,
     },
   } = useOmniGeneralContext()
   const {
@@ -50,10 +52,10 @@ export function OmniHistoryController() {
         sx={{ alignSelf: 'center' }}
       />
       <Heading variant="boldParagraph2" sx={{ mt: 4, mb: 1 }}>
-        {t('ajna.history.title')}
+        {t('omni-kit.history.title')}
       </Heading>
       <Text as="p" variant="paragraph2" sx={{ m: 0, color: 'neutral80' }}>
-        {t('ajna.history.notice')}
+        {t('omni-kit.history.notice', { protocol: LendingProtocolLabel[protocol] })}
       </Text>
     </Flex>
   )
