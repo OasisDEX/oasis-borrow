d2ab15c37a265da397fd513bd7bb135c48d441f4
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-01-23 12:26:02 +0100

    Added new token icons

---
+++ theme/icons/ldo_circle.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+export const ldo_circle = {
+  path: (
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_14079_5412)">
+        <path
+          d="M16 29C23.1797 29 29 23.1797 29 16C29 8.8203 23.1797 3 16 3C8.8203 3 3 8.8203 3 16C3 23.1797 8.8203 29 16 29Z"
+          fill="#F69988"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M16.0061 6.80859L20.5663 13.9299L16.0059 16.5812L11.446 13.9297L16.0061 6.80859ZM12.842 13.593L16.0061 8.6518L19.1703 13.593L16.0059 15.4328L12.842 13.593Z"
+          fill="white"
+        />
+        <path
+          d="M15.9993 18.1466L10.7094 15.0706L10.565 15.2962C8.93571 17.8405 9.29956 21.1726 11.4398 23.3074C13.9583 25.8195 18.0417 25.8195 20.5602 23.3074C22.7004 21.1726 23.0643 17.8405 21.435 15.2962L21.2905 15.0706L15.9995 18.1467L15.9993 18.1466Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_14079_5412">
+          <rect width="26" height="26" fill="white" transform="translate(3 3)" />
+        </clipPath>
+      </defs>
+    </svg>
+  ),
+}
