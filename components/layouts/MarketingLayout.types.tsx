684f6dd2cdffe49fa237308ea6ca504940d659aa
Piotr Konowrocki <piotr@oazoapps.com>
2024-01-24 13:34:38 +0100

    Landing page layout (#3429)
    * MarketingLayout with gradient color
    
    * formatting fix

--- components/layouts/MarketingLayout.types.tsx
+++ components/layouts/MarketingLayout.types.tsx
@@ -3,6 +3,7 @@ import type { PropsWithChildren } from 'react'
 import type { marketingBackgrounds } from './backgrounds'
 
 export interface MarketingLayoutProps extends PropsWithChildren<{}> {
-  variant?: string
+  backgroundGradient?: string[]
   topBackground?: keyof typeof marketingBackgrounds
+  variant?: string
 }
--- features/marketing-layouts/helpers/index.ts
+++ features/marketing-layouts/helpers/index.ts
@@ -1,1 +1,2 @@
+export * from './renderCssGradient'
 export * from './renderLinearGradientStops'
