2c03fe6559f0d00571833ee5ba7aaec70fa58e7e
Piotr Konowrocki <piotr@oazoapps.com>
2024-01-24 17:15:24 +0100

    remove unused icons

--- features/marketing-layouts/icons/index.ts
+++ features/marketing-layouts/icons/index.ts
@@ -1,7 +1,5 @@
 import { sleep } from 'features/marketing-layouts/icons/sleep'
-import { stack } from 'features/marketing-layouts/icons/stack'
 
 export const marketingTemplatesIcons = {
   sleep,
-  stack,
 }
