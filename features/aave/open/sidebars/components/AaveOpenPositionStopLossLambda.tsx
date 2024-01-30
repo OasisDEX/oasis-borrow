d8d798bef1e52ca5503fc7b7546e541a036fef31
Marcin Ciarka <marcin@oazoapps.com>
2024-01-30 14:03:06 +0100

    Reorganise aave like open sidebars

---
+++ features/aave/open/sidebars/components/AaveOpenPositionStopLossLambda.tsx
@@ -0,0 +1,6 @@
+import type { OpenAaveStateProps } from 'features/aave/open/sidebars/sidebar.types'
+import React from 'react'
+
+export function AaveOpenPositionStopLossLambda(_props: OpenAaveStateProps) {
+  return <div>new stop loss sidebar</div>
+}
