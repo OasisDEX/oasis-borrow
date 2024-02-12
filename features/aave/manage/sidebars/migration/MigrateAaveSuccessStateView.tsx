1ec79fda4062bde97dd2c0f0752e257bef7d620b
Marcin Ciarka <marcin@oazoapps.com>
2024-02-12 17:49:23 +0100

    Add MigrateAaveSuccessStateView, tick_circle icon and update MigrateToSummerIcons component (#3535)
    * Add MigrateAaveSuccessStateView, tick_circle icon and update MigrateToSummerIcons component
    
    * Update MigrateAaveSuccessStateView.tsx with optional url parameter

--- features/aave/manage/sidebars/migration/MigrateAaveSuccessStateView.tsx
+++ features/aave/manage/sidebars/migration/MigrateAaveSuccessStateView.tsx
@@ -1,17 +1,57 @@
+import { Icon } from 'components/Icon'
+import { AppLink } from 'components/Links'
 import type { SidebarSectionProps } from 'components/sidebar/SidebarSection'
 import { ConnectedSidebarSection } from 'features/aave/components'
+import { MigrateToSummerIcons } from 'features/aave/components/MigrateToSummerIcons'
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { Grid } from 'theme-ui'
+import { Trans, useTranslation } from 'react-i18next'
+import { dot } from 'theme/icons'
+import { Box, Flex, Grid, Heading, Text } from 'theme-ui'
 
 import type { MigrateAaveStateProps } from './migrateAaveStateProps'
 
-export function MigrateAaveSuccessStateView({ state }: MigrateAaveStateProps) {
+export function MigrateAaveSuccessStateView({
+  state,
+  url,
+}: MigrateAaveStateProps & { url?: string }) {
   const { t } = useTranslation()
+  const { protocol } = state.context.strategyConfig
 
   const sidebarSectionProps: SidebarSectionProps = {
     title: t('migrate.vault-form.success-title'),
-    content: <Grid gap={3}></Grid>,
+    content: (
+      <Grid gap={3}>
+        <MigrateToSummerIcons fromProtocol={protocol} success />
+        <Box sx={{ mb: 3 }}>
+          <Heading as="h3">{t('migrate.success-state.title-1')}</Heading>
+          <Flex sx={{ flexDirection: 'column', mt: 2 }}>
+            {[
+              ['migrate.success-state.description-points-1', url ?? `${url}#protection`],
+              ['migrate.success-state.description-points-2', url ?? `${url}#adjust`],
+              ['migrate.success-state.description-points-3'],
+            ].map(([descriptionPoint, descriptionPointUrl]) => (
+              <Flex
+                key={descriptionPoint}
+                sx={{ flexDirection: 'row', alignItems: 'center', mt: 2 }}
+              >
+                <Box sx={{ mx: 3 }}>
+                  <Icon icon={dot} size={'4px'} color="neutral80" />
+                </Box>
+                <Text variant="paragraph3" sx={{ display: 'block', color: 'neutral80' }}>
+                  <Trans
+                    i18nKey={descriptionPoint}
+                    components={{
+                      1: <AppLink href={descriptionPointUrl || ''} sx={{ fontSize: 2 }} />,
+                      strong: <Text sx={{ fontWeight: 'semiBold' }} />,
+                    }}
+                  />
+                </Text>
+              </Flex>
+            ))}
+          </Flex>
+        </Box>
+      </Grid>
+    ),
     primaryButton: {
       label: t('migrate.go-to-position'),
     },
---
+++ theme/icons/tick_circle.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+export const tick_circle = {
+  path: (
+    <>
+      <g filter="url(#filter0_d_2532_43622)">
+        <circle cx="25.5" cy="25.5" r="17" stroke="white" fill="currentColor" />
+      </g>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M33.7149 19.6938C34.3331 20.2216 34.4055 21.1494 33.8766 21.7663L24.8026 32.3492C24.522 32.6765 24.1117 32.8645 23.6801 32.8635C23.2485 32.8626 22.839 32.6728 22.5598 32.3444L18.6709 27.7688C18.1447 27.1497 18.2211 26.2222 18.8416 25.6971C19.4621 25.172 20.3916 25.2483 20.9178 25.8674L23.6882 29.127L31.638 19.8552C32.1669 19.2383 33.0967 19.1661 33.7149 19.6938Z"
+        fill="white"
+      />
+      <defs>
+        <filter
+          id="filter0_d_2532_43622"
+          x="0"
+          y="0"
+          width="51"
+          height="51"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation="4" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2532_43622" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2532_43622"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </>
+  ),
+  viewBox: '0 0 51 51',
+}
