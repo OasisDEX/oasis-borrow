5f7e300dba7dfbd8fd625c9e8a4281d928c41392
Jakub Swierczek <jakub.swierczek@outlook.com>
2024-02-23 10:45:31 +0100

    New Pools for Ajna (#3592)
    * New Pools for Ajna
    
    * fix icons
    
    * add multiply

---
+++ theme/icons/mpeth.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+export const mpeth = {
+  path: (
+    <>
+      <circle cx="16" cy="16" r="13" fill="url(#paint0_linear_14480_4433)" />
+      <path
+        d="M10.7019 14.2981L9 16L16 23L23 16L21.2981 14.2981L16 19.5962L10.7019 14.2981Z"
+        fill="white"
+      />
+      <path
+        d="M20.2981 13.2981L16 17.5962L11.7019 13.2981L13.2019 11.7981L16 14.5962L18.7981 11.7981L20.2981 13.2981Z"
+        fill="white"
+      />
+      <path d="M17.7981 10.7981L16 12.5962L14.2019 10.7981L16 9L17.7981 10.7981Z" fill="white" />
+      <defs>
+        <linearGradient
+          id="paint0_linear_14480_4433"
+          x1="26"
+          y1="8.5"
+          x2="6.5"
+          y2="24.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#6271AD" />
+          <stop offset="1" stop-color="#8A92B2" />
+        </linearGradient>
+      </defs>
+    </>
+  ),
+  viewBox: '0 0 32 32',
+}
