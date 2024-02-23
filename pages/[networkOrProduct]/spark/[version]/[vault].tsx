388ff97c2c564005050243e6414c05890969793c
Marcin Ciarka <marcin@oazoapps.com>
2024-02-23 11:28:05 +0100

    Fix position not loading and add not-found page constant (#3594)
    Add INTERNAL_LINKS constant for not-found page

--- pages/[networkOrProduct]/spark/[version]/[vault].tsx
+++ pages/[networkOrProduct]/spark/[version]/[vault].tsx
@@ -44,7 +44,7 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   return {
     redirect: {
       permanent: false,
-      destination: '/not-found',
+      destination: INTERNAL_LINKS.notFound,
     },
   }
 }
