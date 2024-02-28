bd737233adddbdb16ab2b877acd43ea6ec0ddafc
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-02-28 12:31:26 +0100

    break even copy

--- features/omni-kit/components/details-section/parsers/useOmniCardDataBreakEven.tsx
+++ features/omni-kit/components/details-section/parsers/useOmniCardDataBreakEven.tsx
@@ -2,6 +2,7 @@ import BigNumber from 'bignumber.js'
 import { Skeleton } from 'components/Skeleton'
 import type { OmniContentCardBase } from 'features/omni-kit/components/details-section'
 import { zero } from 'helpers/zero'
+import { useTranslation } from 'next-i18next'
 import React from 'react'
 
 interface OmniCardDataBreakEvenParams {
@@ -13,6 +14,11 @@ export function useOmniCardDataBreakEven({
   breakEven,
   isSimulationLoading,
 }: OmniCardDataBreakEvenParams): OmniContentCardBase {
+  const { t } = useTranslation()
+
+  const formatted = {
+    breakeven: breakEven?.gt(zero) ? breakEven.toFixed(0, BigNumber.ROUND_UP) : '1',
+  }
   return {
     title: { key: 'omni-kit.content-card.est-break-even.title' },
     ...(!breakEven || isSimulationLoading
@@ -20,7 +26,7 @@ export function useOmniCardDataBreakEven({
           extra: <Skeleton width="120px" height="20px" sx={{ mt: 1 }} />,
         }
       : {
-          value: breakEven.gt(zero) ? breakEven.toFixed(0, BigNumber.ROUND_UP) : '1',
+          value: `${t('system.est-break-even-value', { days: formatted.breakeven })}`,
         }),
   }
 }
