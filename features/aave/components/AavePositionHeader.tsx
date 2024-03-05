76ba84a7c2a40a5e4cbb947a25177808e581c05e
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-03-05 08:59:15 +0100

    revert changes in aave component

--- features/aave/components/AavePositionHeader.tsx
+++ features/aave/components/AavePositionHeader.tsx
@@ -1,17 +1,22 @@
 import type { IRiskRatio } from '@oasisdex/dma-library'
 import { RiskRatio } from '@oasisdex/dma-library'
 import type { Protocol } from '@prisma/client'
+import type BigNumber from 'bignumber.js'
+import { getPriceChangeColor } from 'components/vault/VaultDetails'
 import { VaultHeadline } from 'components/vault/VaultHeadline'
 import { useAaveContext } from 'features/aave'
 import { createFollowButton } from 'features/aave/helpers/createFollowButton'
+import { useAaveEarnYields } from 'features/aave/hooks'
 import type { IStrategyConfig, ManageAaveHeaderProps } from 'features/aave/types'
 import type { FollowButtonControlProps } from 'features/follow/controllers/FollowButtonControl'
-import { useAaveLikeHeadlineDetails } from 'features/omni-kit/protocols/aave-like/hooks'
 import { AppSpinner, WithLoadingIndicator } from 'helpers/AppSpinner'
 import { WithErrorHandler } from 'helpers/errorHandlers/WithErrorHandler'
+import { formatCryptoBalance, formatPercent } from 'helpers/formatters/format'
 import { useObservable } from 'helpers/observableHook'
+import { zero } from 'helpers/zero'
 import type { AaveLikeLendingProtocol } from 'lendingProtocols'
 import { LendingProtocol } from 'lendingProtocols'
+import { has7daysYield, has90daysYield } from 'lendingProtocols/aave-like-common'
 import type { PreparedAaveTotalValueLocked } from 'lendingProtocols/aave-v2/pipelines'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
@@ -56,6 +61,7 @@ function AavePositionHeader({
   maxRisk,
   strategy,
   aaveTVL,
+  minimumRiskRatio,
 }: {
   maxRisk?: IRiskRatio
   strategy: IStrategyConfig
@@ -64,11 +70,86 @@ function AavePositionHeader({
 }) {
   const { t } = useTranslation()
 
-  const { headlineDetails } = useAaveLikeHeadlineDetails({
-    maxRiskRatio: maxRisk,
-    protocol: strategy.protocol,
-    network: strategy.network,
-  })
+  const isSparkProtocol = strategy.protocol === LendingProtocol.SparkV3
+
+  const minYields = useAaveEarnYields(
+    !isSparkProtocol ? minimumRiskRatio : undefined,
+    strategy.protocol,
+    strategy.network,
+    ['7Days'],
+  )
+  const maxYields = useAaveEarnYields(
+    !isSparkProtocol ? maxRisk || minimumRiskRatio : undefined,
+    strategy.protocol,
+    strategy.network,
+    ['7Days', '7DaysOffset', '90Days', '90DaysOffset'],
+  )
+
+  const headlineDetails = []
+  if (minYields && maxYields) {
+    const formatYield = (yieldVal: BigNumber) =>
+      formatPercent(yieldVal, {
+        precision: 2,
+      })
+
+    if (has7daysYield(minYields, false) && has7daysYield(maxYields)) {
+      const yield7DaysMin = minYields.annualisedYield7days ?? zero
+      const yield7DaysMax = maxYields.annualisedYield7days ?? zero
+
+      const yield7DaysDiff = maxYields.annualisedYield7days.minus(
+        maxYields.annualisedYield7daysOffset,
+      )
+
+      headlineDetails.push({
+        label: t('open-earn.aave.product-header.current-yield'),
+        value: `${formatYield(yield7DaysMin).toString()} - ${formatYield(
+          yield7DaysMax,
+        ).toString()}`,
+        sub: formatPercent(yield7DaysDiff, {
+          precision: 2,
+          plus: true,
+        }),
+        subColor: getPriceChangeColor({
+          collateralPricePercentageChange: yield7DaysDiff,
+        }),
+      })
+    } else {
+      headlineDetails.push({
+        label: t('open-earn.aave.product-header.current-yield'),
+        value: `n/a`,
+      })
+    }
+  }
+
+  if (maxYields && has90daysYield(maxYields)) {
+    const yield90DaysDiff = maxYields.annualisedYield90daysOffset.minus(
+      maxYields.annualisedYield90days,
+    )
+    headlineDetails.push({
+      label: t('open-earn.aave.product-header.90-day-avg-yield'),
+      value: formatPercent(maxYields.annualisedYield90days, {
+        precision: 2,
+      }),
+      sub: formatPercent(yield90DaysDiff, {
+        precision: 2,
+        plus: true,
+      }),
+      subColor: getPriceChangeColor({
+        collateralPricePercentageChange: yield90DaysDiff,
+      }),
+    })
+  } else {
+    headlineDetails.push({
+      label: t('open-earn.aave.product-header.90-day-avg-yield'),
+      value: `n/a`,
+    })
+  }
+
+  aaveTVL?.totalValueLocked &&
+    headlineDetails.push({
+      label: t('open-earn.aave.product-header.total-value-locked'),
+      value: formatCryptoBalance(aaveTVL.totalValueLocked),
+    })
 
   return (
     <VaultHeadline
