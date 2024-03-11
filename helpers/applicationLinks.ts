bc65ae693b254fe3c6d791c5e915f6c724da6350
Marcin Ciarka <marcin@oazoapps.com>
2024-03-11 17:25:39 +0100

    Update application links

--- helpers/applicationLinks.ts
+++ helpers/applicationLinks.ts
@@ -85,7 +85,7 @@ export const EXTERNAL_LINKS = {
     DSR_RATE_HIKE: 'https://blog.summer.fi/dsr-rate-hike/',
     MAKER_STABILITY_FEE_CHANGES: 'https://blog.summer.fi/major-stability-fee-changes/',
     LEARN: 'https://blog.summer.fi/tag/learn',
-    HOW_TO_SET_UP_AUTO_TAKE_PROFIT: 'https://youtu.be/ZqnvxeccJDQ?feature=shared',
+    HOW_TO_SET_UP_AUTO_TAKE_PROFIT: 'https://blog.summer.fi/how-to-set-up-auto-take-profit',
   },
   DOCS: {
     AJNA: {
@@ -132,5 +132,5 @@ export const EXTERNAL_LINKS = {
   LIDO_FI_STETH_LIQUIDITY:
     'https://blog.lido.fi/providing-steth-liquidity-via-curve-to-receive-rewards',
   AAVE_SDAI_YIELD_DUNE: 'https://dune.com/Lucianken/aave-v3-sdai-yield-multiple',
-  HOW_TO_SET_UP_AUTO_TAKE_PROFIT_VIDEO: 'https://blog.summer.fi/how-to-set-up-auto-take-profit',
+  HOW_TO_SET_UP_AUTO_TAKE_PROFIT_VIDEO: 'https://youtu.be/ZqnvxeccJDQ?feature=shared',
 }
