4ab93eabec3fe6d21c7edce9770685ad601cbd3a
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-03-12 09:24:13 +0100

    adjust auto bs hooks

--- features/omni-kit/automation/components/OmniAutoBSOverviewDetailsSection.tsx
+++ features/omni-kit/automation/components/OmniAutoBSOverviewDetailsSection.tsx
@@ -1,14 +1,17 @@
+import type { LendingPosition } from '@oasisdex/dma-library'
 import BigNumber from 'bignumber.js'
 import { DetailsSection } from 'components/DetailsSection'
 import { DetailsSectionContentCardWrapper } from 'components/DetailsSectionContentCard'
-import {
-  ContentCardTriggerExecutionLTV,
-  ContentCardTriggerTargetLTV,
-} from 'features/aave/components/BasicAutomationDetailsView'
 import { AutomationFeatures } from 'features/automation/common/types'
+import {
+  OmniContentCard,
+  useOmniCardDataAutoBSTriggerExecutionLtv,
+  useOmniCardDataAutoBSTriggerTargetLtv,
+} from 'features/omni-kit/components/details-section'
 import type { OmniAutomationSimulationResponse } from 'features/omni-kit/contexts'
 import { useOmniGeneralContext, useOmniProductContext } from 'features/omni-kit/contexts'
 import type { SetupBasicAutoResponse } from 'helpers/triggers'
+import { one } from 'helpers/zero'
 import { useTranslation } from 'next-i18next'
 import type { FC } from 'react'
 import React from 'react'
@@ -32,7 +35,7 @@ export const OmniAutoBSOverviewDetailsSection: FC<OmniAutoBSOverviewDetailsSecti
 }) => {
   const { t } = useTranslation()
   const {
-    environment: { productType, collateralToken, priceFormat },
+    environment: { productType, collateralToken, priceFormat, isShort },
   } = useOmniGeneralContext()
   const {
     dynamicMetadata: {
@@ -42,7 +45,12 @@ export const OmniAutoBSOverviewDetailsSection: FC<OmniAutoBSOverviewDetailsSecti
       simulationData,
       automationForm: { state },
     },
+    position: {
+      currentPosition: { position },
+    },
   } = useOmniProductContext(productType)
+
+  const castedPosition = position as LendingPosition
 
   if (!isAutoBSSimulationResponse(simulationData?.simulationResponse)) {
     throw new Error('Wrong auto BS simulation response type')
@@ -66,33 +74,58 @@ export const OmniAutoBSOverviewDetailsSection: FC<OmniAutoBSOverviewDetailsSecti
     [AutomationFeatures.AUTO_BUY]: automation?.triggers.autoBuy,
   }[type]
 
+  const resolvedThresholdPrice = {
+    [AutomationFeatures.AUTO_SELL]:
+      automation?.triggers.autoSell &&
+      new BigNumber(automation?.triggers.autoSell?.decodedParams.minSellPrice),
+    [AutomationFeatures.AUTO_BUY]:
+      automation?.triggers.autoBuy &&
+      new BigNumber(automation?.triggers.autoBuy?.decodedParams.maxBuyPrice),
+  }[type]
+
+  const currentExecutionLTV =
+    resolvedTrigger && new BigNumber(resolvedTrigger.decodedParams.executionLtv).div(10000)
+  const currentTargetLTV =
+    resolvedTrigger && new BigNumber(resolvedTrigger.decodedParams.targetLtv).div(10000)
+
+  const debtToCollateralRatio =
+    currentExecutionLTV &&
+    castedPosition.debtAmount.div(
+      castedPosition.collateralAmount.times(currentExecutionLTV.div(100)),
+    )
+
+  const nextPrice = debtToCollateralRatio
+    ? isShort
+      ? one.div(debtToCollateralRatio)
+      : debtToCollateralRatio
+    : undefined
+
+  const autoBSTriggerExecutionLtvContentCardCommonData = useOmniCardDataAutoBSTriggerExecutionLtv({
+    automationFeature: type,
+    collateralToken: collateralToken,
+    currentExecutionLTV: currentExecutionLTV,
+    afterTxExecutionLTV: afterTriggerLtv,
+    nextPrice: nextPrice,
+    denomination: priceFormat,
+  })
+
+  const autoBSTriggerTargetLtvContentCardCommonData = useOmniCardDataAutoBSTriggerTargetLtv({
+    automationFeature: type,
+    collateralToken: collateralToken,
+    currentTargetLTV: currentTargetLTV,
+    afterTxTargetLTV: afterTargetLtv,
+    thresholdPrice: resolvedThresholdPrice,
+    denomination: priceFormat,
+  })
+
   return (
     <DetailsSection
       title={resolvedTitle}
       badge={resolvedFlag}
       content={
         <DetailsSectionContentCardWrapper>
-          <ContentCardTriggerExecutionLTV
-            automationFeature={AutomationFeatures.AUTO_SELL}
-            collateralToken={collateralToken}
-            currentExecutionLTV={
-              resolvedTrigger && new BigNumber(resolvedTrigger.decodedParams.executionLtv).div(100)
-            }
-            afterTxExecutionLTV={afterTriggerLtv}
-            // nextPrice={nextPrice?.price}
-            denomination={priceFormat}
-          />
-          <ContentCardTriggerTargetLTV
-            automationFeature={AutomationFeatures.AUTO_SELL}
-            collateralToken={collateralToken}
-            currentTargetLTV={
-              resolvedTrigger && new BigNumber(resolvedTrigger.decodedParams.targetLtv).div(100)
-            }
-            afterTxTargetLTV={afterTargetLtv}
-            // thresholdPrice={thresholdPrice}
-            // denomination={nextPrice?.denomination || getDenomination(position)}
-            denomination={priceFormat}
-          />
+          <OmniContentCard {...autoBSTriggerExecutionLtvContentCardCommonData} />
+          <OmniContentCard {...autoBSTriggerTargetLtvContentCardCommonData} />
         </DetailsSectionContentCardWrapper>
       }
     />
