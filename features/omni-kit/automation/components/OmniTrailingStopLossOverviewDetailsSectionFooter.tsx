2206b75dd98ccc0bf4209476d3c48da599d57da2
piekczyk <sebastian@oazoapps.com>
2024-03-13 13:37:22 +0100

    Add trailing stop loss char to overview footer (#3659)

---
+++ features/omni-kit/automation/components/OmniTrailingStopLossOverviewDetailsSectionFooter.tsx
@@ -0,0 +1,33 @@
+import { staticFilesRuntimeUrl } from 'helpers/staticPaths'
+import { useTranslation } from 'next-i18next'
+import type { FC } from 'react'
+import React from 'react'
+import { Box, Divider, Image, Text } from 'theme-ui'
+
+interface OmniTrailingStopLossOvereviewDetailsSectionFooterProps {
+  simpleView: boolean
+}
+
+export const OmniTrailingStopLossOverviewDetailsSectionFooter: FC<
+  OmniTrailingStopLossOvereviewDetailsSectionFooterProps
+> = ({ simpleView }) => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      {!simpleView && (
+        <>
+          <Box>
+            <Text variant="paragraph3" sx={{ color: 'neutral80', mb: 4 }}>
+              {t('protection.stop-loss-type-select.trailing-stop-loss-description')}
+            </Text>
+          </Box>
+          <Divider sx={{ my: '24px' }} />
+          <Box sx={{ mt: 4, textAlign: 'center' }}>
+            <Image src={staticFilesRuntimeUrl(`/static/img/trailing-stop-loss-graph.svg`)} />
+          </Box>
+        </>
+      )}
+    </>
+  )
+}
