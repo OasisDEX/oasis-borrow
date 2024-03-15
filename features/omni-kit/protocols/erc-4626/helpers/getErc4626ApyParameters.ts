bb30300bd2b99d1d6e4db9ff062c67e8e8a76f7d
Jakub Swierczek <jakub.swierczek@outlook.com>
2024-03-15 13:58:32 +0100

    Update getErc4626ApyParameters function and server rewrites
    The getErc4626ApyParameters function is refactored to incorporate new interfaces and fetch real-time data over hardcoded values. This refactoring enriches the response data handling various reward tokens and market situations. Alongside, a rewrite rule is added to the server configuration, redirecting '/api/morpho' requests to the correct API URL.

--- features/omni-kit/protocols/erc-4626/helpers/getErc4626ApyParameters.ts
+++ features/omni-kit/protocols/erc-4626/helpers/getErc4626ApyParameters.ts
@@ -6,25 +6,84 @@ type GetErc4626ApyParametersResponse = ReturnType<
   Parameters<typeof views.common.getErc4626Position>[1]['getVaultApyParameters']
 >
 
-export async function getErc4626ApyParameters(): GetErc4626ApyParametersResponse {
-  return new Promise((resolve) =>
-    resolve({
-      vault: {
-        apy: '0.0175',
-        curator: 'Steakhouse',
-        fee: '0.05',
-      },
-      apyFromRewards: [
-        {
-          token: 'WSTETH',
-          value: '0.0125',
-        },
-        {
-          token: 'MORPHO',
-          value: '0.025',
-          per1kUsd: '250',
-        },
-      ],
-    }),
+interface RewardToken {
+  address: string
+  symbol: string
+  decimals: number
+  amountWei: string
+  humanReadable: string
+  apy: number
+}
+
+interface RewardsByMarket {
+  market: {
+    marketId: string
+    collateral: {
+      address: string
+      symbol: string
+      decimals: number
+      priceUsd: number | undefined
+    }
+    suppliedAssets: string
+    suppliedAssetsHumanReadable: string
+    allocation: number
+    liquidationLtv: number
+  }
+  tokens: RewardToken[]
+}
+
+interface GetRewardsResponse {
+  metaMorpho: {
+    address: string
+    weeklyApy: number
+    monthlyApy: number
+    dailyApy: number
+    apy: number
+    fee: number
+    totalAssets: string
+  }
+  rewardsByMarket: RewardsByMarket[]
+  rewardsByToken: RewardToken[]
+}
+
+export async function getErc4626ApyParameters(
+  vaultAddress: string,
+): GetErc4626ApyParametersResponse {
+  // TODO: Pass proper prices
+  const response = await fetch(
+    `/api/morpho/meta-morpho?address=${vaultAddress}&morhoPrice=1&wsEthPrice=4500`,
   )
+
+  if (response.status !== 200) {
+    console.warn('Failed to fetch vault details', response)
+    return {
+      vault: {
+        apy: '0',
+        curator: 'Steakhouse',
+        fee: '0',
+      },
+      apyFromRewards: [],
+      allocations: [],
+    }
+  }
+  const data = (await response.json()) as GetRewardsResponse
+  return {
+    vault: {
+      apy: data.metaMorpho.apy.toString(),
+      curator: 'Steakhouse',
+      fee: data.metaMorpho.fee.toString(),
+    },
+    apyFromRewards: data.rewardsByToken.map((token) => {
+      return {
+        token: token.symbol.toUpperCase(),
+        value: token.apy.toString(),
+        per1kUsd: token.humanReadable.toString(),
+      }
+    }),
+    allocations: data.rewardsByMarket.map((market) => ({
+      token: market.market.collateral.symbol.toUpperCase(),
+      supply: market.market.suppliedAssetsHumanReadable,
+      riskRatio: market.market.liquidationLtv.toString(),
+    })),
+  }
 }
