2a054bb8968c4da858aa422b2ca1fb8abe7c5ef9
Piotr Konowrocki <piotr@oazoapps.com>
2024-03-22 16:01:09 +0100

    fix infinity loading issue

--- features/omni-kit/protocols/erc-4626/hooks/useErc4626ApySimulation.ts
+++ features/omni-kit/protocols/erc-4626/hooks/useErc4626ApySimulation.ts
@@ -90,50 +90,48 @@ export function useErc4626ApySimulation({
 
       setCancelablePromise(promise)
 
-      void promise.then(({ vault, apyFromRewards }) => {
-        if (apyFromRewards) {
-          const rewardsApy = apyFromRewards.map(({ per1kUsd, token, value }) => ({
-            token,
-            value: new BigNumber(value),
-            ...(per1kUsd && { per1kUsd: new BigNumber(per1kUsd) }),
-          }))
-          const vaultApy = new BigNumber(vault.apy)
-          const totalApy = getErc4626Apy({
-            rewardsApy,
-            vaultApy,
-          })
+      void promise.then(({ vault, apyFromRewards = [] }) => {
+        const rewardsApy = apyFromRewards.map(({ per1kUsd, token, value }) => ({
+          token,
+          value: new BigNumber(value),
+          ...(per1kUsd && { per1kUsd: new BigNumber(per1kUsd) }),
+        }))
+        const vaultApy = new BigNumber(vault.apy)
+        const totalApy = getErc4626Apy({
+          rewardsApy,
+          vaultApy,
+        })
 
-          setApy({
-            rewardsApy: {
-              per1d: rewardsApy.map(({ per1kUsd, token, value }) => ({
-                token,
-                value: value.div(365),
-                ...(per1kUsd && { per1kUsd: per1kUsd.div(365) }),
-              })),
-              per30d: rewardsApy.map(({ per1kUsd, token, value }) => ({
-                token,
-                value: value.div(12),
-                ...(per1kUsd && { per1kUsd: per1kUsd.div(12) }),
-              })),
-              per365d: rewardsApy,
-            },
-            vaultApy: {
-              per1d: vaultApy.div(365),
-              per30d: vaultApy.div(12),
-              per365d: vaultApy,
-            },
-          })
-          setEarnings({
-            per1d: resolvedQuoteTokenAmount.times(totalApy.div(365)),
-            per30d: resolvedQuoteTokenAmount.times(totalApy.div(12)),
-            per365d: resolvedQuoteTokenAmount.times(totalApy),
-          })
-          setNetValue({
-            per1d: resolvedQuoteTokenAmount.plus(resolvedQuoteTokenAmount.times(totalApy.div(365))),
-            per30d: resolvedQuoteTokenAmount.plus(resolvedQuoteTokenAmount.times(totalApy.div(12))),
-            per365d: resolvedQuoteTokenAmount.plus(resolvedQuoteTokenAmount.times(totalApy)),
-          })
-        }
+        setApy({
+          rewardsApy: {
+            per1d: rewardsApy.map(({ per1kUsd, token, value }) => ({
+              token,
+              value: value.div(365),
+              ...(per1kUsd && { per1kUsd: per1kUsd.div(365) }),
+            })),
+            per30d: rewardsApy.map(({ per1kUsd, token, value }) => ({
+              token,
+              value: value.div(12),
+              ...(per1kUsd && { per1kUsd: per1kUsd.div(12) }),
+            })),
+            per365d: rewardsApy,
+          },
+          vaultApy: {
+            per1d: vaultApy.div(365),
+            per30d: vaultApy.div(12),
+            per365d: vaultApy,
+          },
+        })
+        setEarnings({
+          per1d: resolvedQuoteTokenAmount.times(totalApy.div(365)),
+          per30d: resolvedQuoteTokenAmount.times(totalApy.div(12)),
+          per365d: resolvedQuoteTokenAmount.times(totalApy),
+        })
+        setNetValue({
+          per1d: resolvedQuoteTokenAmount.plus(resolvedQuoteTokenAmount.times(totalApy.div(365))),
+          per30d: resolvedQuoteTokenAmount.plus(resolvedQuoteTokenAmount.times(totalApy.div(12))),
+          per365d: resolvedQuoteTokenAmount.plus(resolvedQuoteTokenAmount.times(totalApy)),
+        })
 
         setIsLoading(false)
       })
