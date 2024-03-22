d7cd6a3af8b7b9f47a5a902c314b527008c090e7
Piotr Konowrocki <piotr@oazoapps.com>
2024-03-22 12:56:29 +0100

    or instead of and in condition

--- features/omni-kit/protocols/erc-4626/hooks/useErc4626ApySimulation.ts
+++ features/omni-kit/protocols/erc-4626/hooks/useErc4626ApySimulation.ts
@@ -145,7 +145,7 @@ export function useErc4626ApySimulation({
   return {
     apy,
     earnings,
-    isLoading: !!(isLoading && isSimulationLoading),
+    isLoading: !!(isLoading || isSimulationLoading),
     netValue,
   }
 }
