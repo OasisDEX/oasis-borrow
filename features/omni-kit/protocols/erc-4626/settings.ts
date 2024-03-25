0c90fabd3b8e5a70f1d9d781f8fb69b520d5b54f
Piotr Konowrocki <piotr@oazoapps.com>
2024-03-25 16:40:36 +0100

    remove WBTC from config

--- features/omni-kit/protocols/erc-4626/settings.ts
+++ features/omni-kit/protocols/erc-4626/settings.ts
@@ -162,22 +162,6 @@ export const erc4626Vaults: Erc4626Config[] = [
       symbol: 'ETH',
     },
   },
-  {
-    address: '0xbeEf094333AEdD535c130958c204E84f681FD9FA',
-    curator: steakhouseCurator,
-    id: 'steakhouse-WBTC',
-    name: 'Steakhouse WBTC',
-    protocol: LendingProtocol.MorphoBlue,
-    networkId: NetworkIds.MAINNET,
-    pricePicker: morphoPricePicker,
-    rewards: [morphoRewards],
-    strategy: 'MetaMorpho Lending',
-    token: {
-      address: getNetworkContracts(NetworkIds.MAINNET).tokens.WBTC.address,
-      precision: getToken('WBTC').precision,
-      symbol: 'WBTC',
-    },
-  },
 ]
 
 export const erc4626VaultsById = keyBy(erc4626Vaults, 'id')
