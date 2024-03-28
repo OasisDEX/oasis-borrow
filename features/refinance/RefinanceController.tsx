6a38435393c177a402d2f54795745e59ba54af64
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-03-28 18:35:26 +0100

    Implemented pools in simulation

--- features/refinance/RefinanceController.tsx
+++ features/refinance/RefinanceController.tsx
@@ -2,37 +2,23 @@ import { VaultContainerSpinner, WithLoadingIndicator } from 'helpers/AppSpinner'
 import { WithErrorHandler } from 'helpers/errorHandlers/WithErrorHandler'
 import React from 'react'
 
-import { refinanceContext } from './RefinanceContext'
 import { useSdkSimulation } from './useSdkSimulation'
 
 export function RefinanceController() {
-  const context = React.useContext(refinanceContext)
-  if (context === undefined) {
-    throw new Error('RefinanceContextProvider is missing in the hierarchy')
-  }
-
-  // TODO: wallet not connected, add better handling
-  if (context.address === undefined) {
-    return null
-  }
-
-  const { error, user, chain, simulation, position, liquidationPrice } = useSdkSimulation(
-    context,
-    context.address,
-  )
+  const { error, user, chain, simulation, sourcePosition, liquidationPrice } = useSdkSimulation()
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     <WithErrorHandler error={[error!]}>
       <WithLoadingIndicator
-        value={[user, chain, position, simulation]}
+        value={[user, chain, sourcePosition, simulation, liquidationPrice]}
         customLoader={<VaultContainerSpinner />}
       >
-        {([_user, _chain, _position, _simulation]) => (
+        {([_user, _chain, _sourcePosition, _simulation, _liquidationPrice]) => (
           <div>
             {/* // TODO: Use Step Manager here */}
-            {_user?.wallet.address} : {_chain?.chainInfo.name} : {_position?.positionId} :{' '}
-            {_simulation?.targetPosition.positionId} : {liquidationPrice} :{' '}
+            {_user?.wallet.address} : {_chain?.chainInfo.name} : {_sourcePosition?.positionId} :{' '}
+            {_simulation?.targetPosition.positionId} : {_liquidationPrice} :{' '}
           </div>
         )}
       </WithLoadingIndicator>
--- features/refinance/useSdkSimulation.tsx
+++ features/refinance/useSdkSimulation.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react'
-import { type Chain, makeSDK, PositionUtils, type User } from 'summerfi-sdk-client'
+import { refinanceContext } from 'features/refinance/RefinanceContext'
+import React, { useEffect, useState } from 'react'
+import type { Chain, Protocol, User } from 'summerfi-sdk-client'
+import { makeSDK, PositionUtils } from 'summerfi-sdk-client'
 import type {
-  AddressValue,
+  IPoolId,
   IRefinanceParameters,
   Position,
   Simulation,
@@ -14,39 +16,49 @@ import {
   getChainInfoByChainId,
   Percentage,
   Price,
+  ProtocolName,
   RiskRatio,
   RiskRatioType,
   Wallet,
 } from 'summerfi-sdk-common'
 
-import { type RefinanceContext } from './RefinanceContext'
-
-export function useSdkSimulation(context: RefinanceContext, address: AddressValue) {
+export function useSdkSimulation() {
   const [error, setError] = useState<null | string>(null)
   const [user, setUser] = useState<null | User>(null)
   const [chain, setChain] = useState<null | Chain>(null)
-
-  const [position, setPosition] = useState<null | Position>(null)
+  const [liquidationPrice, setLiquidationPrice] = useState<null | string>(null)
+  const [sourcePosition, setSourcePosition] = useState<null | Position>(null)
   const [simulation, setSimulation] = useState<null | Simulation<SimulationType.Refinance>>(null)
 
-  const {
-    positionId,
-    slippage,
-    chainInfo,
-    liquidationPrice,
-    collateralTokenAmount,
-    debtTokenAmount,
-    collateralPrice,
-    debtPrice,
-  } = context
+  const context = React.useContext(refinanceContext)
 
   const sdk = makeSDK({ apiURL: '/api/sdk' })
-  const wallet = Wallet.createFrom({
-    address: Address.createFrom({ value: address, type: AddressType.Ethereum }),
-  })
 
   useEffect(() => {
     const fetchData = async () => {
+      if (context === undefined) {
+        throw new Error('RefinanceContextProvider is missing in the hierarchy')
+      }
+      const {
+        positionId,
+        slippage,
+        chainInfo,
+        liquidationPrice: _liquidationPrice,
+        collateralTokenAmount,
+        debtTokenAmount,
+        collateralPrice,
+        debtPrice,
+        address,
+      } = context
+
+      setLiquidationPrice(_liquidationPrice)
+
+      if (address === undefined) {
+        throw new Error('Wallet is not connected')
+      }
+      const wallet = Wallet.createFrom({
+        address: Address.createFrom({ value: address, type: AddressType.Ethereum }),
+      })
       const fetchedChain = await sdk.chains.getChain({
         chainInfo: getChainInfoByChainId(chainInfo.chainId),
       })
@@ -57,50 +69,70 @@ export function useSdkSimulation(context: RefinanceContext, address: AddressValu
         walletAddress: wallet.address,
       })
       setUser(fetchedUser)
-    }
 
-    void fetchData().catch((err) => {
-      setError(err.message)
-    })
-  }, [sdk, address, wallet, chainInfo.chainId])
+      const _chain: Chain | undefined = await sdk.chains.getChain({ chainInfo })
+      if (!_chain) {
+        throw new Error(`ChainId ${chainInfo.chainId} is not supported`)
+      }
+      const makerProtocol: Protocol | undefined = await _chain.protocols.getProtocol({
+        name: ProtocolName.Maker,
+      })
+      if (!makerProtocol) {
+        throw new Error(`Protocol ${ProtocolName.Maker} is not supported`)
+      }
+      const sourcePool = await makerProtocol.getPool({
+        poolId: {
+          protocol: {
+            name: ProtocolName.Maker,
+            chainInfo,
+          },
+        } as IPoolId,
+      })
 
-  useEffect(() => {
-    // TODO: grab from protocol.getPool
-    const positionPool = {} as any
-    const ltv = PositionUtils.getLTV({
-      collateralTokenAmount,
-      debtTokenAmount,
-      collateralPriceInUsd: Price.createFrom({
-        value: collateralPrice,
-        baseToken: collateralTokenAmount.token,
-        quoteToken: CurrencySymbol.USD,
-      }),
-      debtPriceInUsd: Price.createFrom({
-        value: debtPrice,
-        baseToken: debtTokenAmount.token,
-        quoteToken: CurrencySymbol.USD,
-      }),
-    })
-    const _position: Position = {
-      pool: positionPool,
-      collateralAmount: collateralTokenAmount,
-      debtAmount: debtTokenAmount,
-      positionId,
-      riskRatio: RiskRatio.createFrom({ ratio: ltv, type: RiskRatioType.LTV }),
-    }
+      const ltv = PositionUtils.getLTV({
+        collateralTokenAmount,
+        debtTokenAmount,
+        collateralPriceInUsd: Price.createFrom({
+          value: collateralPrice,
+          baseToken: collateralTokenAmount.token,
+          quoteToken: CurrencySymbol.USD,
+        }),
+        debtPriceInUsd: Price.createFrom({
+          value: debtPrice,
+          baseToken: debtTokenAmount.token,
+          quoteToken: CurrencySymbol.USD,
+        }),
+      })
+      const _sourcePosition: Position = {
+        pool: sourcePool,
+        collateralAmount: collateralTokenAmount,
+        debtAmount: debtTokenAmount,
+        positionId,
+        riskRatio: RiskRatio.createFrom({ ratio: ltv, type: RiskRatioType.LTV }),
+      }
+      setSourcePosition(_sourcePosition)
 
-    setPosition(_position)
+      const sparkProtocol: Protocol | undefined = await _chain.protocols.getProtocol({
+        name: ProtocolName.Spark,
+      })
+      if (!sparkProtocol) {
+        throw new Error(`Protocol ${ProtocolName.Spark} is not supported`)
+      }
+      const targetPool = await sparkProtocol.getPool({
+        poolId: {
+          protocol: {
+            name: ProtocolName.Spark,
+            chainInfo,
+          },
+        } as IPoolId,
+      })
 
-    // TODO: grab from protocol.getPool
-    const targetPool = {} as any
+      const refinanceParameters: IRefinanceParameters = {
+        position: _sourcePosition,
+        targetPool,
+        slippage: Percentage.createFrom({ value: slippage }),
+      }
 
-    const refinanceParameters: IRefinanceParameters = {
-      position: _position,
-      targetPool,
-      slippage: Percentage.createFrom({ value: slippage }),
-    }
-
-    const fetchData = async () => {
       const _simulation =
         await sdk.simulator.refinance.simulateRefinancePosition(refinanceParameters)
       setSimulation(_simulation)
@@ -108,16 +140,7 @@ export function useSdkSimulation(context: RefinanceContext, address: AddressValu
     void fetchData().catch((err) => {
       setError(err.message)
     })
-  }, [
-    collateralPrice,
-    collateralTokenAmount,
-    debtPrice,
-    debtTokenAmount,
-    liquidationPrice,
-    positionId,
-    sdk,
-    slippage,
-  ])
+  }, [sdk, context])
 
-  return { error, user, chain, position, simulation, liquidationPrice }
+  return { error, user, chain, sourcePosition, simulation, liquidationPrice }
 }
