eaf5515e93a20be09283b3cbcfd866f8105dec75
Marcin Ciarka <marcin@oazoapps.com>
2024-04-04 15:40:23 +0200

    Remove console.log statement in OmniProductController.tsx

--- features/omni-kit/controllers/OmniProductController.tsx
+++ features/omni-kit/controllers/OmniProductController.tsx
@@ -158,7 +158,6 @@ export const OmniProductController = <Auction, History, Position>({
   }, [dpmPositionData])
 
   const isYieldLoop = getOmniIsOmniYieldLoop({ collateralToken, pseudoProtocol, quoteToken })
-  console.log('[...errors, ...protocolDataErrors]', [...errors, ...protocolDataErrors])
   // Flag to determine whether full yield-loop UI experience is available for given protocol & pair
   const isYieldLoopWithData =
     !!settings.yieldLoopPairsWithData?.[networkId]?.includes(`${collateralToken}-${quoteToken}`) &&
