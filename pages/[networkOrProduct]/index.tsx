d0ef4aa217e883ae0a750789d848d0c4f082854e
Piotr Konowrocki <piotr@oazoapps.com>
2024-04-11 12:14:59 +0200

    simplify hub page and add redirect

--- pages/[networkOrProduct]/index.tsx
+++ pages/[networkOrProduct]/index.tsx
@@ -1,20 +1,47 @@
+import { AnimatedWrapper } from 'components/AnimatedWrapper'
+import { WithConnection } from 'components/connectWallet'
+import { AppLayout } from 'components/layouts/AppLayout'
 import { ProductHubProductType } from 'features/productHub/types'
-import ProductHubRouteHandler from 'features/productHub/views/ProductHubRouteHandler'
+import { ProductHubView } from 'features/productHub/views'
+import { useAppConfig } from 'helpers/config'
+import { useScrollToTop } from 'helpers/useScrollToTop'
 import type { GetStaticPaths, GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-export default ProductHubRouteHandler
+interface ProductHubPageProps {
+  product: ProductHubProductType
+}
+
+function ProductHubPage({ product }: ProductHubPageProps) {
+  const { AjnaSafetySwitch: ajnaSafetySwitchOn } = useAppConfig('features')
+
+  useScrollToTop(product)
+
+  return (
+    <AppLayout>
+      <WithConnection>
+        <AnimatedWrapper sx={{ mb: 5 }}>
+          <ProductHubView
+            product={product}
+            promoCardsCollection={ajnaSafetySwitchOn ? 'Home' : 'HomeWithAjna'}
+            url="/"
+          />
+        </AnimatedWrapper>
+      </WithConnection>
+    </AppLayout>
+  )
+}
+
+export default ProductHubPage
 
 export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
   const paths =
     locales?.flatMap((locale) =>
       Object.values(ProductHubProductType).flatMap((product) => ({
-        params: {
-          networkOrProduct: product,
-        },
+        params: { networkOrProduct: product },
         locale,
       })),
-    ) || []
+    ) ?? []
 
   return {
     paths,
@@ -23,13 +50,10 @@ export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
-  const product = params?.networkOrProduct as ProductHubProductType
-  const token = params?.token
   return {
     props: {
       ...(await serverSideTranslations(locale || 'en', ['common'])),
-      ...(product && { product }),
-      ...(token && { token }),
+      product: params?.networkOrProduct,
     },
   }
 }
