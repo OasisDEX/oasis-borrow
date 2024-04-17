e2da1f7cbd84a9f26d6fdf7ef5efafe6a52a1119
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-04-17 12:06:53 +0200

    fix lint

--- features/omni-kit/observables/getPositionFromUrlParams.ts
+++ features/omni-kit/observables/getPositionFromUrlParams.ts
@@ -88,7 +88,6 @@ export async function getPositionsFromUrlData({
   networkId,
   pairId,
   positionId,
-  protocol,
 }: GetPositionFromUrlDataParams): Promise<GetPositionFromUrlDataResponse> {
   const accounts = await getAccounts({ networkId, positionId })
 
