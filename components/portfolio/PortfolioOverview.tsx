39e7f89bc7289ff4e4d59f3100c11955d574e7cc
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-04-19 12:15:41 +0200

    Fix migration link in portfolio v2

--- components/portfolio/PortfolioOverview.tsx
+++ components/portfolio/PortfolioOverview.tsx
@@ -3,7 +3,6 @@ import { AppLink } from 'components/Links'
 import { PortfolioOverviewItem } from 'components/portfolio/PortfolioOverviewItem'
 import { Tag } from 'components/Tag'
 import { WithArrow } from 'components/WithArrow'
-import { getMigrationLink } from 'features/migrations/getMigrationLink'
 import type { PortfolioPosition } from 'handlers/portfolio/types'
 import { getLocalAppConfig } from 'helpers/config'
 import { formatCryptoBalance } from 'helpers/formatters/format'
@@ -100,15 +99,7 @@ export const PortfolioOverview = ({
           subValue={
             getLocalAppConfig('features').EnableMigrations && (
               <AppLink
-                href={
-                  biggestMigration
-                    ? getMigrationLink({
-                        protocol: biggestMigration.protocol,
-                        network: biggestMigration.network,
-                        address: biggestMigration.url,
-                      })
-                    : ''
-                }
+                href={biggestMigration ? biggestMigration.url : ''}
                 target="_self"
                 sx={{ mr: 3 }}
               >
