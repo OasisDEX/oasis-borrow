fe436c563198cf818ee75f19f8b7dab97d29425a
Marcin Ciarka <marcin@oazoapps.com>
2024-04-23 19:27:59 +0200

    light cache yields locally

--- helpers/lambda/yields/get-yields-request.ts
+++ helpers/lambda/yields/get-yields-request.ts
@@ -1,17 +1,28 @@
 import { getYieldsConfig } from './get-yields-config'
 import type { GetYieldsParams, GetYieldsResponse } from './get-yields-types'
 
+const cache: Record<string, GetYieldsResponse | null> = {}
+
 export const getYieldsRequest = async (
   params: GetYieldsParams,
   urlPrefix?: string,
 ): Promise<GetYieldsResponse | null> => {
   const { url } = getYieldsConfig(params)
+  // Use params.ltv as the cache key
+  const cacheKey = `${params.ltv.toFixed(4).toString()}-${params.collateralToken}-${params.quoteToken}-${params.networkId}`
 
+  // If the response is in the cache, return it
+  if (cacheKey in cache) {
+    return cache[cacheKey]
+  }
   try {
     const response = await fetch(`${urlPrefix ?? ''}${url}`, {
       method: 'GET',
     })
-    return (await response.json()) as GetYieldsResponse
+    const data = (await response.json()) as GetYieldsResponse
+    // Store the response in the cache
+    cache[cacheKey] = data
+    return data
   } catch (e) {
     console.error('Failed to read data about yields from server', e)
     return null
