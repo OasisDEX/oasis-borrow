2de5a6b49ba26129ec9c969cf0a0d4bbb1f886ea
FBrinkkemper <frankbrinkkemper@gmail.com>
2024-04-25 21:07:38 +0200

    adding 2 pairs

--- features/aave/strategies/base-aave-v3-strategies.ts
+++ features/aave/strategies/base-aave-v3-strategies.ts
@@ -123,10 +123,35 @@ const availableTokenPairs: TokenPairConfig[] = [
       },
     },
   },
+    {
+      collateral: 'WSTETH',
+      debt: 'ETH',
+      strategyType: StrategyType.Long,
+      productTypes: {
+        [ProductType.Borrow]: {
+          featureToggle: FeaturesEnum.BaseNetworkEnabled,
+          additionalManageActions: [
+            {
+              action: 'switch-to-earn',
+              featureToggle: FeaturesEnum.BaseNetworkEnabled,
+            },
+          ],
+        },
+        [ProductType.Earn]: {
+          featureToggle: FeaturesEnum.BaseNetworkEnabled,
+          additionalManageActions: [
+            {
+              action: 'switch-to-borrow',
+              featureToggle: FeaturesEnum.BaseNetworkEnabled,
+            },
+          ],
+        },
+      },
+  },
   {
     collateral: 'CBETH',
     debt: 'ETH',
-    strategyType: StrategyType.Short,
+    strategyType: StrategyType.Long,
     productTypes: {
       [ProductType.Earn]: {
         featureToggle: FeaturesEnum.BaseNetworkEnabled,
