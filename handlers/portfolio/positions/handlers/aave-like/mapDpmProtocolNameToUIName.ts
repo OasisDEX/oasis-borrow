23a15a8f6560a6c81e77c28a4cb72a3c593b053c
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-06-19 16:49:29 +0200

    fixed type

--- handlers/portfolio/positions/handlers/aave-like/mapDpmProtocolNameToUIName.ts
+++ handlers/portfolio/positions/handlers/aave-like/mapDpmProtocolNameToUIName.ts
@@ -1,7 +1,6 @@
 import { ProtocolName } from '@summer_fi/summerfi-sdk-common'
 import { aaveLikeProtocolNames } from 'handlers/portfolio/positions/handlers/aave-like/helpers'
-
-import type { DpmSubgraphData } from '/Users/piter/dev/oasis-borrow/handlers/portfolio/positions/helpers/getAllDpmsForWallet'
+import type { DpmSubgraphData } from 'handlers/portfolio/positions/helpers/getAllDpmsForWallet'
 
 // map SDK protocol name to the one hardcoded in the UI
 export const mapDpmProtocolNameToUIName = (dpm: DpmSubgraphData): DpmSubgraphData => {
