af53e9a8c0d78e116495bfbf3332047b798a99e9
Marcin Ciarka <marcin@oazoapps.com>
2024-07-02 14:25:10 +0200

    Add validation for Mixpanel event types

--- pages/api/t.tsx
+++ pages/api/t.tsx
@@ -1,6 +1,12 @@
 import { enableMixpanelDevelopmentMode } from 'analytics/analytics'
 import { config } from 'analytics/mixpanel'
-import type { MixpanelDevelopmentType } from 'analytics/types'
+import { TrackingProductType } from 'analytics/TrackingProductType'
+import {
+  MixpanelCommonAnalyticsSections,
+  type MixpanelDevelopmentType,
+  MixpanelEventTypes,
+  MixpanelPages,
+} from 'analytics/types'
 import { snakeCase } from 'lodash'
 import Mixpanel from 'mixpanel'
 import type { NextApiRequest, NextApiResponse } from 'next'
@@ -14,6 +20,21 @@ const handler = async function (
   { body: { distinctId, eventBody, eventName, ...rest } }: NextApiRequest,
   res: NextApiResponse<{ status: number }>,
 ) {
+  const hasProperEventName = Object.values(MixpanelEventTypes).includes(eventName)
+  const hasProperProduct = Object.values(TrackingProductType).includes(eventBody.product)
+  const hasProperSection = Object.values(MixpanelCommonAnalyticsSections).includes(
+    eventBody.section,
+  )
+  const hasProperPage = Object.values(MixpanelPages).includes(eventBody.page)
+  if (
+    !hasProperEventName ||
+    (eventBody.product && !hasProperProduct) ||
+    (eventBody.section && !hasProperSection) ||
+    (eventBody.page && !hasProperPage)
+  ) {
+    res.json({ status: 400 })
+    return
+  }
   try {
     !rest.currentUrl.endsWith('vault-info') && // disables tracking for this particular tab
       mixpanel.track(`${eventName}`, {
