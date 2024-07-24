604f8588a01d9b630ddf6e2726864cd3457f50d6
Marcin Ciarka <marcin@oazoapps.com>
2024-07-24 14:18:57 +0200

    feat: Add HomepageAnnouncement component and summer light brand gradient

---
+++ features/homepage/common/HomepageAnnouncement.tsx
@@ -0,0 +1,57 @@
+import { Icon } from 'components/Icon'
+import { AppLink } from 'components/Links'
+import { WithArrow } from 'components/WithArrow'
+import { summerLightBrandGradient } from 'helpers/getGradientColor'
+import React from 'react'
+import * as icons from 'theme/icons'
+import { Box, Text } from 'theme-ui'
+
+type HomepageAnnouncementProps = {
+  announcement: {
+    message: string
+    url: string
+    enabled: boolean
+    icon: string
+  }
+}
+
+export function HomepageAnnouncement({ announcement }: HomepageAnnouncementProps) {
+  return announcement.enabled ? (
+    <AppLink href={announcement.url}>
+      <Box
+        sx={{
+          background: 'neutral10',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          columnGap: 2,
+          borderRadius: 'round',
+          padding: 2,
+          paddingRight: 4,
+          width: 'fit-content',
+          margin: '0 auto',
+          marginTop: 3,
+        }}
+      >
+        {announcement.icon ? (
+          <Box
+            sx={{
+              background: summerLightBrandGradient,
+              padding: '0 5px',
+              width: '24px',
+              height: '24px',
+              minWidth: '24px',
+              minHeight: '24px',
+              borderRadius: 'round',
+              marginRight: 2,
+            }}
+          >
+            <Icon icon={icons[announcement.icon as keyof typeof icons]} size={24} />
+          </Box>
+        ) : null}
+        <Text variant="boldParagraph3">{announcement.message}</Text>
+        <WithArrow>&nbsp;</WithArrow>
+      </Box>
+    </AppLink>
+  ) : null
+}
--- helpers/getGradientColor.ts
+++ helpers/getGradientColor.ts
@@ -4,6 +4,7 @@ export const ajnaBrandGradient = 'linear-gradient(90deg, #f154db 0%, #974eea 100
 export const summerBrandGradient = 'linear-gradient(90deg, #007da3 0%, #e7a77f 50%, #e97047 100%)'
 export const summerFadedBrandGradient =
   'linear-gradient(90deg, #b2d8e3 0%, #f8e4d8 50%, #f8d4c7 100%)'
+export const summerLightBrandGradient = 'linear-gradient(92.03deg, #FFF3EF 0.78%, #F2FCFF 99.57%)'
 
 export function getGradientColor(gradient: string): ThemeUIStyleObject {
   return {
