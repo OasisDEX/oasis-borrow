76fe5ccbb6c98d123beda068a6cff3cc62a6488c
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-08-26 14:21:04 +0200

    map coll ratio to ltv

--- features/positionHistory/helpers/getMakerAutomationEvents.ts
+++ features/positionHistory/helpers/getMakerAutomationEvents.ts
@@ -1,7 +1,9 @@
+import BigNumber from 'bignumber.js'
 import type { NetworkIds } from 'blockchain/networks'
 import { getTokenSymbolBasedOnAddress } from 'blockchain/tokensMetadata'
 import type { SubgraphsResponses } from 'features/subgraphLoader/types'
 import { loadSubgraph } from 'features/subgraphLoader/useSubgraphLoader'
+import { one } from 'helpers/zero'
 
 /**
  * Returns mapped automation events from automation subgraph per given network id
@@ -39,8 +41,35 @@ export const getMakerAutomationEvents = async ({
     const closeToCollateral = item.trigger.kind === 'StopLossToCollateral'
     const resolvedCloseToCollateral = closeToCollateral ? 'ToCollateral' : 'ToDebt'
 
+    const decodedDataNames = item.trigger.decodedDataNames.map((item) => {
+      return (
+        {
+          collRatio: 'executionLtv',
+          execCollRatio: 'executionLtv',
+          targetCollRatio: 'targetLtv',
+        }[item] || item
+      )
+    })
+
+    const decodedData = item.trigger.decodedData.map((decodedItem, idx) => {
+      switch (decodedDataNames[idx]) {
+        case 'executionLtv':
+        case 'targetLtv':
+          if (item.trigger.simpleName === 'StopLoss') {
+            return one.div(new BigNumber(decodedItem).div(1000000)).toString()
+          }
+
+          return one.div(new BigNumber(decodedItem).div(100000000)).toString()
+        case 'maxBuyPrice':
+        case 'minSellPrice':
+          return new BigNumber(decodedItem).shiftedBy(-10)
+        default:
+          return decodedItem
+      }
+    })
+
     return {
-      trigger: item.trigger,
+      trigger: { ...item.trigger, decodedDataNames, decodedData },
       txHash: item.transaction,
       timestamp: Number(item.timestamp) * 1000,
       kind: `Automation${item.eventType}-${item.trigger.simpleName}${resolvedCloseToCollateral}`,
