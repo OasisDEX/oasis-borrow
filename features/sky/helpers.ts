fa08801f588139ad0d4801be1bf58948d470955d
Marcin Ciarka <marcin@oazoapps.com>
2024-09-23 13:12:07 +0200

    Refactor showAllowanceInfo function to handle NaN values and zero amounts

--- features/sky/helpers.ts
+++ features/sky/helpers.ts
@@ -1,4 +1,9 @@
 import type BigNumber from 'bignumber.js'
 
-export const showAllowanceInfo = (amount?: BigNumber, allowance?: BigNumber) =>
-  amount && allowance && !allowance.isNaN() && (allowance.isZero() || allowance.isLessThan(amount))
+export const showAllowanceInfo = (amount?: BigNumber, allowance?: BigNumber): boolean => {
+  if (!amount || !allowance || allowance.isNaN() || amount.isNaN() || amount.isZero()) {
+    return false
+  }
+
+  return allowance.isZero() || allowance.isLessThan(amount)
+}
