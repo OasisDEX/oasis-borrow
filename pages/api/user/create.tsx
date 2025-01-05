34d686d14666be115c598ebef10dddd135685b54
sebastianPiekarczyk <sebastian@oazoapps.com>
2025-01-05 19:24:14 +0100

    prettier

--- pages/api/user/create.tsx
+++ pages/api/user/create.tsx
@@ -39,7 +39,7 @@ const create = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!decoded) {
     return res.status(401).json({ authenticated: false })
   }
-  
+
   if (decoded.address.toLowerCase() !== params.address.toLowerCase()) {
     return res.status(401).json({ authenticated: false })
   }
