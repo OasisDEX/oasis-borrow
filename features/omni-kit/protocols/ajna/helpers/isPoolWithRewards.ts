2c8ad29692ac9d2c741afe7b171d2b8d6f9a7a1d
chrisbradbury88 <42005522+chrisbradbury88@users.noreply.github.com>
2025-08-07 10:59:52 +0100

    Update isPoolWithRewards.ts for Ajna Rewards (#4067)
    * Update isPoolWithRewards.ts for Ajna Rewards
    
    Remove all Ajna Rewards
    
    * Refactor isPoolWithRewards.ts to add reward check documentation
    
    ---------
    
    Co-authored-by: piekczyk <sebastian@oazoapps.com>

--- features/omni-kit/protocols/ajna/helpers/isPoolWithRewards.ts
+++ features/omni-kit/protocols/ajna/helpers/isPoolWithRewards.ts
@@ -1,4 +1,3 @@
-import BigNumber from 'bignumber.js'
 import { NetworkIds } from 'blockchain/networks'
 import type { AjnaWeeklyRewards } from 'features/omni-kit/protocols/ajna/types'
 import type { OmniSupportedNetworkIds } from 'features/omni-kit/types'
@@ -10,152 +9,18 @@ interface IsPoolWithRewardsParams {
 }
 
 export const ajnaWeeklyRewards: AjnaWeeklyRewards = {
-  [NetworkIds.MAINNET]: {
-    'RETH-DAI': {
-      amount: new BigNumber(45920),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'WBTC-DAI': {
-      amount: new BigNumber(45920),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'WSTETH-DAI': {
-      amount: new BigNumber(45920),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'ETH-USDC': {
-      amount: new BigNumber(16400),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'WBTC-USDC': {
-      amount: new BigNumber(45920),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'WSTETH-USDC': {
-      amount: new BigNumber(26240),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'USDC-ETH': {
-      amount: new BigNumber(13120),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'USDC-WBTC': {
-      amount: new BigNumber(13120),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'WSTETH-ETH': {
-      amount: new BigNumber(45920),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'RETH-ETH': {
-      amount: new BigNumber(32800),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'SDAI-USDC': {
-      amount: new BigNumber(39360),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'YFI-DAI': {
-      amount: new BigNumber(13120),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'SYTETH-DAI': {
-      amount: new BigNumber(13120),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'AJNA-DAI': {
-      amount: new BigNumber(22960),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'UNIV2-DAI': {
-      amount: new BigNumber(19680),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'MKR-DAI': {
-      amount: new BigNumber(19680),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'SUSDE-DAI': {
-      amount: new BigNumber(22960),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'MWSTETHWPUNKS20-WSTETH': {
-      amount: new BigNumber(3280),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'MWSTETHWPUNKS40-WSTETH': {
-      amount: new BigNumber(3280),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'SYRUPUSDC-USDC': {
-      amount: new BigNumber(50000),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-  },
-  [NetworkIds.BASEMAINNET]: {
-    'CBETH-ETH': {
-      amount: new BigNumber(32800),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'ETH-USDC': {
-      amount: new BigNumber(39360),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'WSTETH-ETH': {
-      amount: new BigNumber(13120),
-      borrowShare: new BigNumber(0.05),
-      earnShare: new BigNumber(0.95),
-    },
-    'DEGEN-USDC': {
-      amount: new BigNumber(24600),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'USDC-DEGEN': {
-      amount: new BigNumber(9840),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'SNX-USDC': {
-      amount: new BigNumber(24600),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'PRIME-USDC': {
-      amount: new BigNumber(13120),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-    'AERO-USDC': {
-      amount: new BigNumber(9840),
-      borrowShare: new BigNumber(0.4),
-      earnShare: new BigNumber(0.6),
-    },
-  },
+  [NetworkIds.MAINNET]: {},
+  [NetworkIds.BASEMAINNET]: {},
 }
 
+/**
+ * Checks if a pool has rewards available
+ * @param params - Pool parameters
+ * @param params.collateralToken - Collateral token symbol
+ * @param params.networkId - Network ID
+ * @param params.quoteToken - Quote token symbol
+ * @returns True if pool has rewards, false otherwise
+ */
 export function isPoolWithRewards({
   collateralToken,
   networkId,
